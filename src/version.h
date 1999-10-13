5c0a1633cf2189fa1cb79eec8ce0047ef5121d8d
Lars Gullik Bjønnes <larsbj@gullik.org>
1999-10-13 21:40:45 +0000

    prerelease pre2
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@199 a592a061-630c-0410-9148-cb99ea01b6c8

--- src/version.h
+++ src/version.h
@@ -1,3 +1,3 @@
 /* Version and release date definition */
-#define LYX_VERSION "1.1.1pre1"
-#define LYX_RELEASE "Sat, Oct 2, 1999"
+#define LYX_VERSION "1.1.1pre2"
+#define LYX_RELEASE "Wed, Oct 13, 1999"
