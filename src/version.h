2b41d9ca19a3b204c9ff3890c3949e4138fe6822
Lars Gullik Bjønnes <larsbj@gullik.org>
2000-02-03 19:32:45 +0000

    last updates
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@525 a592a061-630c-0410-9148-cb99ea01b6c8

--- src/version.h
+++ src/version.h
@@ -1,5 +1,5 @@
 /* Version and release date definition */
-#define LYX_VERSION "1.1.4cvs"
-#define LYX_RELEASE "Thu, Jan 20, 2000"
+#define LYX_VERSION "1.1.4"
+#define LYX_RELEASE "Thu, Feb 3, 2000"
 /* This version string is intended to be used in files created by LyX */
 #define LYX_DOCVERSION "LyX 1.1"
