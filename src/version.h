bdd722bab5f045f3db392b73cf54b9342b999e5f
Lars Gullik Bjønnes <larsbj@gullik.org>
2000-02-03 19:44:16 +0000

    upped the version number
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@527 a592a061-630c-0410-9148-cb99ea01b6c8

--- src/version.h
+++ src/version.h
@@ -1,5 +1,5 @@
 /* Version and release date definition */
-#define LYX_VERSION "1.1.4"
+#define LYX_VERSION "1.1.5cvs"
 #define LYX_RELEASE "Thu, Feb 3, 2000"
 /* This version string is intended to be used in files created by LyX */
 #define LYX_DOCVERSION "LyX 1.1"
