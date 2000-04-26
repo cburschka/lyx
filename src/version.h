4e96db7156f5219b98bcb3ccf592938d25677474
Lars Gullik Bjønnes <larsbj@gullik.org>
2000-04-26 17:02:01 +0000

    change version back to cvs
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@698 a592a061-630c-0410-9148-cb99ea01b6c8

--- src/version.h
+++ src/version.h
@@ -1,5 +1,5 @@
 /* Version and release date definition */
-#define LYX_VERSION "1.1.5pre1"
+#define LYX_VERSION "1.1.5cvs"
 #define LYX_RELEASE "Wed, Apr 26, 2000"
 /* This version string is intended to be used in files created by LyX */
 #define LYX_DOCVERSION "LyX 1.1"
