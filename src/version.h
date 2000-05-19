1b0c6092d798e3d4b2a942d984cf507e99636e74
Lars Gullik Bjønnes <larsbj@gullik.org>
2000-05-19 21:36:46 +0000

    the 1.1.5pre2 version
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@749 a592a061-630c-0410-9148-cb99ea01b6c8

--- src/version.h
+++ src/version.h
@@ -1,5 +1,5 @@
 /* Version and release date definition */
-#define LYX_VERSION "1.1.5cvs"
-#define LYX_RELEASE "Wed, Apr 26, 2000"
+#define LYX_VERSION "1.1.5pre2"
+#define LYX_RELEASE "Fri, May 19, 2000"
 /* This version string is intended to be used in files created by LyX */
 #define LYX_DOCVERSION "LyX 1.1"
