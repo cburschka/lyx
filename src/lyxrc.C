4ca058e2aea3c0ff9fe9d640b1af19e86b5b9c77
Jean-Marc Lasgouttes <lasgouttes@lyx.org>
2000-06-27 16:45:56 +0000

    Make sure primary|secondary keymaps given in lyxrc are correct
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/branches/lyx-1_1_5@843 a592a061-630c-0410-9148-cb99ea01b6c8

--- src/lyxrc.C
+++ src/lyxrc.C
@@ -471,15 +471,23 @@ int LyXRC::read(string const & filename)
 			break;
 			
 		case RC_KBMAP_PRIMARY:
-			if (lexrc.next())
-				primary_kbmap = lexrc.GetString();
+			if (lexrc.next()) 
+				if (!LibFileSearch("kbd", lexrc.GetString(), 
+						   "kmap").empty()) 
+					primary_kbmap = lexrc.GetString();
+				else 
+					lexrc.printError("LyX: Keymap `$$Token' not found");
 			break;
 			
 		case RC_KBMAP_SECONDARY:
-			if (lexrc.next())
-				secondary_kbmap = lexrc.GetString();
+			if (lexrc.next()) 
+				if (!LibFileSearch("kbd", lexrc.GetString(), 
+						   "kmap").empty()) 
+					secondary_kbmap = lexrc.GetString();
+				else 
+					lexrc.printError("LyX: Keymap `$$Token' not found");
 			break;
-			
+						
 		case RC_FONT_ENCODING:
 			if (lexrc.next())
 				fontenc = lexrc.GetString();
