0614d032c0a01ad287c6017a44b0c976a9ac61c2
Jean-Marc Lasgouttes <lasgouttes@lyx.org>
2000-07-04 14:20:58 +0000

    Update version number and fr.po
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/branches/lyx-1_1_5@855 a592a061-630c-0410-9148-cb99ea01b6c8

--- src/version.h
+++ src/version.h
@@ -1,5 +1,5 @@
 /* Version and release date definition */
-#define LYX_VERSION "1.1.5"
+#define LYX_VERSION "1.1.5fixcvs"
 #define LYX_RELEASE "Tue, Jun 6, 2000"
 /* This version string is intended to be used in files created by LyX */
 #define LYX_DOCVERSION "LyX 1.1"
