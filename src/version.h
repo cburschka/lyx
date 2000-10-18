577046cab9804d9272f8e9a1f29b1227591bed1f
Jean-Marc Lasgouttes <lasgouttes@lyx.org>
2000-10-18 09:21:18 +0000

    Set version to 1.1.5fixcvs
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/branches/lyx-1_1_5@1136 a592a061-630c-0410-9148-cb99ea01b6c8

--- src/version.h
+++ src/version.h
@@ -1,5 +1,5 @@
 /* Version and release date definition */
-#define LYX_VERSION "1.1.5fix2"
+#define LYX_VERSION "1.1.5fixcvs"
 #define LYX_RELEASE "Tue Oct 18, 2000"
 /* This version string is intended to be used in files created by LyX */
 #define LYX_DOCVERSION "LyX 1.1"
