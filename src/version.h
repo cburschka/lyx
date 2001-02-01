a4d1dd120dd2f21c7e4c5aae9f115dadc992c788
Jean-Marc Lasgouttes <lasgouttes@lyx.org>
2001-02-01 11:21:14 +0000

    Reset version to 1.1.6cvs
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/branches/BRANCH_1_1_6@1435 a592a061-630c-0410-9148-cb99ea01b6c8

--- src/version.h
+++ src/version.h
@@ -1,6 +1,6 @@
 /* Version and release date definition */
 ///
-#define LYX_VERSION "1.1.6fix1"
+#define LYX_VERSION "1.1.6cvs"
 ///
 #define LYX_RELEASE "Wed, Jan 31, 2001"
 /* This version string is intended to be used in files created by LyX */
