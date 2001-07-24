fc803468c72f35386bc3cb06b181f56155b8773d
Jean-Marc Lasgouttes <lasgouttes@lyx.org>
2001-07-24 14:25:07 +0000

    merge again po files :)
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/branches/BRANCH_1_1_6@2321 a592a061-630c-0410-9148-cb99ea01b6c8

--- src/version.h
+++ src/version.h
@@ -1,8 +1,8 @@
 /* Version and release date definition */
 ///
-#define LYX_VERSION "1.1.6cvs"
+#define LYX_VERSION "1.1.6fix3"
 ///
-#define LYX_RELEASE "Fri, May 18 2001"
+#define LYX_RELEASE "Fri, Jul 24 2001"
 /* This version string is intended to be used in files created by LyX */
 ///
 #define LYX_DOCVERSION "LyX 1.1"
