e62a70d8ba0a3e7af6701c670828d72d6ea363c5
Jean-Marc Lasgouttes <lasgouttes@lyx.org>
2001-07-25 10:46:03 +0000

    resume 1.1.6cvs development
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/branches/BRANCH_1_1_6@2337 a592a061-630c-0410-9148-cb99ea01b6c8

--- src/version.h
+++ src/version.h
@@ -1,6 +1,6 @@
 /* Version and release date definition */
 ///
-#define LYX_VERSION "1.1.6fix3"
+#define LYX_VERSION "1.1.6fixcvs"
 ///
 #define LYX_RELEASE "Fri, Jul 24 2001"
 /* This version string is intended to be used in files created by LyX */
