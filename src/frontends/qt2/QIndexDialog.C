eed0649017e15b303c9abb94ca162555f2ee30d4
John Levon <levon@movementarian.org>
2001-08-29 02:29:26 +0000

    whatsthis fix
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@2611 a592a061-630c-0410-9148-cb99ea01b6c8

--- src/frontends/qt2/QIndexDialog.C
+++ src/frontends/qt2/QIndexDialog.C
@@ -6,12 +6,16 @@
  * \author John Levon <moz@compsoc.man.ac.uk>
  */
 
+#include <config.h>
+
 #include "QIndexDialog.h"
 #include "Dialogs.h"
 #include "QIndex.h"
+#include "gettext.h"
 
-#include <qwidget.h>
 #include <qpushbutton.h>
+#include <qlineedit.h>
+#include <qwhatsthis.h>
 
 QIndexDialog::QIndexDialog(QIndex * form)
 	: QIndexDialogBase(0, 0, false, 0),
@@ -21,6 +25,21 @@ QIndexDialog::QIndexDialog(QIndex * form)
 		form, SLOT(slotOK()));
 	connect(closePB, SIGNAL(clicked()),
 		form, SLOT(slotClose()));
+
+	QWhatsThis::add(keywordED, _(
+"The format of the entry in the index.\n"
+"\n"
+"An entry can be specified as a sub-entry of\n"
+"another with \"!\" :\n"
+"\n"
+"cars!mileage\n"
+"\n"
+"You can cross-refer to another entry like so :\n"
+"\n"
+"cars!mileage|see{economy}\n"
+"\n"
+"For further details refer to the local LaTeX\n"
+"documentation.\n"));
 }
 
  
