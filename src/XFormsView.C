82d18e917b011ec7cf0cffdf8c06e917f95d9d25
Angus Leeming <leeming@lyx.org>
2001-09-17 09:16:09 +0000

    set minimum size to the main window.
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@2762 a592a061-630c-0410-9148-cb99ea01b6c8

--- src/XFormsView.C
+++ src/XFormsView.C
@@ -100,6 +100,7 @@ void XFormsView::setPosition(int x, int y)
 
 void XFormsView::show(int place, int border, string const & title)
 {
+	fl_set_form_minsize(form_, form_->w, form_->h);
 	fl_show_form(form_, place, border, title.c_str());
 	getLyXFunc()->initMiniBuffer();
 #if FL_REVISION < 89 || (FL_REVISION == 89 && FL_FIXLEVEL < 5)
