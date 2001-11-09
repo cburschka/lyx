6d95ea2651cb1d5a62a770dc250a6d7c3c353a9f
André Pönitz <poenitz@gmx.net>
2001-11-09 18:46:48 +0000

    read new column from lib/symbols
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@3002 a592a061-630c-0410-9148-cb99ea01b6c8

--- src/mathed/math_hash.C
+++ src/mathed/math_hash.C
@@ -166,6 +166,7 @@ void readSymbols(string const & filename)
 	lex.setFile(filename);
 	while (lex.isOK() && lex.next()) {
 		latexkeys tmp;
+		string xmlname;
 		tmp.name = lex.getString();
 		if (lex.next())
 			tmp.token = tokenEnum(lex.getString());
@@ -175,6 +176,8 @@ void readSymbols(string const & filename)
 			tmp.id = lex.getInteger();
 		if (lex.next())
 			tmp.type = lex.getString();
+		if (lex.next())
+			xmlname = lex.getString();
 		if (theWordList.find(tmp.name) != theWordList.end())
 			lyxerr << "readSymbols: token " << tmp.name
 			       << " already exists.\n";
