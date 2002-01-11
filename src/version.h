c70dd8a81ceb03f5edd75385b56602b882cd0d2e
Jean-Marc Lasgouttes <lasgouttes@lyx.org>
2002-01-11 16:28:26 +0000

    1.1.6fix4 is almost there...
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/branches/BRANCH_1_1_6@3338 a592a061-630c-0410-9148-cb99ea01b6c8

--- src/version.h
+++ src/version.h
@@ -1,8 +1,8 @@
 /* Version and release date definition */
 ///
-#define LYX_VERSION "1.1.6fixcvs"
+#define LYX_VERSION "1.1.6fix4"
 ///
-#define LYX_RELEASE "Fri, Jul 24 2001"
+#define LYX_RELEASE "Fri, Jan 11 2002"
 /* This version string is intended to be used in files created by LyX */
 ///
 #define LYX_DOCVERSION "LyX 1.1"
