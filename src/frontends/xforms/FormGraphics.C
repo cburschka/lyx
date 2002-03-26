70cec1dd6e6dc6886ecfcf40ac125596c72760ba
Angus Leeming <leeming@lyx.org>
2002-03-26 15:20:01 +0000

    Enable the insertion of non-existent graphics files once again.
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@3841 a592a061-630c-0410-9148-cb99ea01b6c8

--- src/frontends/xforms/FormGraphics.C
+++ src/frontends/xforms/FormGraphics.C
@@ -483,13 +483,13 @@ ButtonPolicy::SMInput FormGraphics::input(FL_OBJECT * ob, long)
 		if (out_name != in_name && !out_name.empty()) {
 			fl_set_input(file_->input_filename, out_name.c_str());
 		}
-		if (!controller().isFilenameValid(out_name))
-			return ButtonPolicy::SMI_INVALID;
+//  		if (!controller().isFilenameValid(out_name))
+//  			return ButtonPolicy::SMI_INVALID;
 
-	} else if (ob == file_->input_filename) {
-		string name = getStringFromInput(file_->input_filename);
-		if (!controller().isFilenameValid(name))
-			return ButtonPolicy::SMI_INVALID;
+//  	} else if (ob == file_->input_filename) {
+//  		string name = getStringFromInput(file_->input_filename);
+//  		if (!controller().isFilenameValid(name))
+//  			return ButtonPolicy::SMI_INVALID;
 
 	} else if (ob == file_->check_subcaption) {
 		setEnabled(file_->input_subcaption,
