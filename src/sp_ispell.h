f8e047c2bf0b0adcb51582066ed02890c7ddc1d4
André Pönitz <poenitz@gmx.net>
2002-05-29 12:20:06 +0000

    whitespace changes
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@4260 a592a061-630c-0410-9148-cb99ea01b6c8

--- src/sp_ispell.h
+++ src/sp_ispell.h
@@ -10,69 +10,59 @@ class BufferParams;
 
 class ISpell : public SpellBase
 {
-  public:
-
-    enum ActualSpellChecker {
+public:
+	///
+	enum ActualSpellChecker {
 		ASC_ISPELL,
 		ASC_ASPELL
-    };
+	};
+	///
+	ISpell();
+	///
+	ISpell(BufferParams const & params, string const & lang);
+	///
+	~ISpell();
 
-    ISpell();
+	/// initialize spell checker
+	void initialize(BufferParams const & params, string const & lang);
+	///
+	bool alive();
+	/// clean up after error
+	void cleanUp();
+	/// check word
+	enum spellStatus check(string const & word);
+	/// close spellchecker
+	void close();
+	///
+	void insert(string const & word);
+	///
+	void accept(string const & word);
+	/// store replacement
+	void store(string const & mis, string const & cor);
+	///
+	char const * nextMiss();
+	///
+	char const * error();
 
-    ISpell(BufferParams const & params, string const & lang);
-
-    ~ISpell();
-
-
-    /// initialize spell checker
-    void initialize(BufferParams const & params, string const & lang);
-
-    bool alive();
-
-    /// clean up after error
-    void cleanUp();
-
-    /// check word
-    enum spellStatus check(string const & word);
-
-    /// close spellchecker
-    void close();
-
-    void insert(string const & word);
-
-    void accept(string const & word);
-
-    /// store replacement
-    void store(string const & mis, string const & cor);
-
-    char const * nextMiss();
-
-    char const * error();
-
-  private:
-
-    ActualSpellChecker actual_spell_checker;
-
-    /// instream to communicate with ispell
-    FILE * in;
-
-    /// outstream to communicate with ispell
-    FILE * out;
-
-    /// spell error
-    char const * error_;
-
-    /// ?
-    int isp_fd;
-
-    ///
-    char * str;
-    ///
-    spellStatus flag;
-    ///
-    char * b;
-    ///
-    char * e;
+private:
+	///
+	ActualSpellChecker actual_spell_checker;
+	/// instream to communicate with ispell
+	FILE * in;
+	/// outstream to communicate with ispell
+	FILE * out;
+	/// spell error
+	char const * error_;
+	/// ?
+	int isp_fd;
+	///
+	char * str;
+	///
+	spellStatus flag;
+	///
+	char * b;
+	///
+	char * e;
 
 };
 
