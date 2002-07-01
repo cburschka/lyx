2f8ca779af88944938c83ef97593bcc8bd452db4
André Pönitz <poenitz@gmx.net>
2002-07-01 08:07:21 +0000

    Herbert's changes to the converter script
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@4504 a592a061-630c-0410-9148-cb99ea01b6c8

--- lib/scripts/lyxpreview2xpm.sh
+++ lib/scripts/lyxpreview2xpm.sh
@@ -6,9 +6,10 @@ dir=`dirname $2`
 base=${dir}/`basename $2 .xpm`
 
 cp $1 ${base}.tex 
-(cd ${dir} ; pdflatex --interaction batchmode ${base}.tex) 
-dvips -x 2000 -R -E -o ${base}.eps ${base}.dvi 
-convert ${base}.eps ${base}.xpm 
+#(cd ${dir} ; latex ${base}) 
+(cd ${dir} ; pdflatex --interaction batchmode ${base}) 
+dvips -x 1750 -R -o ${base}.ps ${base}.dvi 
+convert -crop 0x0 ${base}.ps ${base}.xpm 
 rm ${base}.tex ${base}.aux ${base}.dvi ${base}.log ${base}.eps || true
 
 exit 0
