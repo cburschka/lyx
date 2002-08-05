200b4fd4efba2c75034e2a93377f50efcf016047
André Pönitz <poenitz@gmx.net>
2002-08-05 08:44:08 +0000

    cosmetics: make the hand-drawn root more similr to what LaTeX does
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@4859 a592a061-630c-0410-9148-cb99ea01b6c8

--- src/mathed/math_rootinset.C
+++ src/mathed/math_rootinset.C
@@ -59,7 +59,8 @@ void MathRootInset::draw(MathPainterInfo & pi, int x, int y) const
 	xp[1] = x + w + 4;    yp[1] = y - a + 1;
 	xp[2] = x + w;        yp[2] = y + d;
 	xp[3] = x + w - 2;    yp[3] = y + (d - a)/2 + 2;
-	xp[4] = x;            yp[4] = y + (d - a)/2 + 2;
+	//xp[4] = x;            yp[4] = y + (d - a)/2 + 2;
+	xp[4] = x + w - 5;    yp[4] = y + (d - a)/2 + 4;
 	pi.pain.lines(xp, yp, 5, LColor::math);
 	drawMarkers(pi, x, y);
 }
