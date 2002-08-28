2421f91530ed593c368b096cb2bd686c87fe51da
Dekel Tsur <dekelts@tau.ac.il>
2002-08-28 12:02:55 +0000

    Don't use .extend() (doesn't work in python 1.5)
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@5133 a592a061-630c-0410-9148-cb99ea01b6c8

--- lib/lyx2lyx/lyxconvert_217.py
+++ lib/lyx2lyx/lyxconvert_217.py
@@ -41,10 +41,7 @@ def update_tabular(lines):
             break
 
         new_table = table_update(lines[i:j])
-        tail = lines[j:]
-        lines[i:] = []
-        lines.extend(new_table)
-        lines.extend(tail)
+	lines = lines[:i] + new_table + lines[j:]
         i = i + len(new_table)
 
 col_re = re.compile(r'<column alignment="(\d)" valignment="(\d)" leftline="(\d)" rightline="(\d)" width="(.*)" special="(.*)">')
