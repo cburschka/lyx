f4466f72e8935762e804ac5ad4a2b3d59da85672
John Levon <levon@movementarian.org>
2002-09-11 18:36:00 +0000

    compile fix.
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@5272 a592a061-630c-0410-9148-cb99ea01b6c8

--- src/mathed/dimension.C
+++ src/mathed/dimension.C
@@ -1,3 +1,16 @@
+/**
+ * \file dimension.C
+ * This file is part of LyX, the document processor.
+ * Licence details can be found in the file COPYING.
+ *
+ *  \author André Pönitz
+ *
+ * Full author contact details are available in file CREDITS
+ */
+
+#ifdef __GNUG__
+#pragma implementation
+#endif
 
 #include <config.h>
 #include "dimension.h"
