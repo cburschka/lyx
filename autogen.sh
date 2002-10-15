78c2dddb78e371c5ff7f192c9df8b0be986d5dc8
Lars Gullik Bjønnes <larsbj@gullik.org>
2002-10-15 08:10:57 +0000

    some small autogen improvements
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@5404 a592a061-630c-0410-9148-cb99ea01b6c8

--- autogen.sh
+++ autogen.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
-# Discover what version of autoconf we are using.
-autoversion=`autoconf --version | head -n 1`
+ACLOCAL=aclocal
+AUTOHEADER="autoheader"
+AUTOMAKE="automake -a -c --foreign"
+AUTOCONF="autoconf"
 
+# Discover what version of autoconf we are using.
+autoversion=`$AUTOCONF --version | head -n 1`
+
+echo "Using $autoversion"
 case $autoversion in
     *2.13)
 	cp config/acconfig.h .
@@ -19,35 +25,34 @@ case $autoversion in
 	cp config/relyx_configure.ac lib/reLyX/configure.ac
 	;;
     *)
-	echo "You are running autoconf $autoversion, that"
-	echo "version is not supported by LyX."
+	echo "This autoconf version is not supported by LyX."
 	echo "LyX only supports autoconf 2.13 and 2.53."
 	exit
 	;;
 esac
 
 
-ACLOCAL=aclocal
-AUTOHEADER="autoheader"
-AUTOMAKE="automake -a -c --foreign"
-AUTOCONF="autoconf"
-GNUM4=
-
 ACINCLUDE_FILES="lyxinclude.m4 libtool.m4 codeset.m4 gettext.m4 glibc21.m4 iconv.m4 isc-posix.m4 lcmessage.m4 progtest.m4 xforms.m4 qt.m4 gtk--.m4 gnome--.m4 gnome.m4 pspell.m4 pkg.m4"
 SIGCPP_ACINCLUDE_FILES="libtool.m4"
 
 echo -n "Locating GNU m4... "
-for prog in $M4 gm4 gnum4 m4 ; do
-  case `$prog --version 2>&1` in
-    *GNU*) ok=yes
-	   GNUM4=$prog
-	   echo "found: $GNUM4"
-	   break ;;
-    *) ;;
-  esac
+GNUM4=
+for prog in $M4 gm4 gnum4 m4; do
+	# continue if $prog generates error (e.g. does not exist)
+	( $prog --version ) < /dev/null > /dev/null 2>&1
+	if test $? -ne 0 ; then continue; fi
+
+	# /dev/null input prevents a hang of the script for some m4 compilers (e.g. on FreeBSD)
+	case `$prog --version < /dev/null 2>&1` in
+	*GNU*)	GNUM4=$prog
+		break ;;
+	esac
 done
-if test x$ok != xyes ; then
-    echo "not found."
+if test x$GNUM4 = x ; then
+	echo "not found."
+	exit
+else
+	echo `which $GNUM4`
 fi
 
 # Generate acinclude.m4
@@ -57,7 +62,7 @@ rm -f acinclude.m4
 echo "done."
 
 # Generate the Makefiles and configure files
-if ( $ACLOCAL --version ) </dev/null > /dev/null 2>&1; then
+if ( $ACLOCAL --version ) < /dev/null > /dev/null 2>&1; then
 	echo "Building macros..."
 	for dir in . lib/reLyX ; do
 	    echo "        $dir"
@@ -69,7 +74,7 @@ else
 	exit
 fi
 
-if ( $AUTOHEADER --version ) </dev/null > /dev/null 2>&1; then
+if ( $AUTOHEADER --version ) < /dev/null > /dev/null 2>&1; then
 	echo "Building config header template..."
 	for dir in . ; do
 	    echo "        $dir"
@@ -81,7 +86,7 @@ else
 	exit
 fi
 
-if ( $AUTOMAKE --version ) </dev/null > /dev/null 2>&1; then
+if ( $AUTOMAKE --version ) < /dev/null > /dev/null 2>&1; then
 	echo "Building Makefile templates..."
 	for dir in . lib/reLyX ; do
 	    echo "        $dir"
@@ -93,7 +98,7 @@ else
 	exit
 fi
 
-if ( $AUTOCONF --version ) </dev/null > /dev/null 2>&1; then
+if ( $AUTOCONF --version ) < /dev/null > /dev/null 2>&1; then
 	echo "Building configure..."
 	for dir in . lib/reLyX ; do
 	    echo "       $dir"
@@ -106,16 +111,11 @@ else
 fi
 
 # Autogenerate lib/configure.m4.
-if test x$GNUM4 != x ; then
-    echo -n "Building lib/configure ... "
-    rm -f lib/configure
-    $GNUM4 lib/configure.m4 > lib/configure
-    chmod a+x lib/configure
-    echo "done."
-else
-    echo "No GNU m4. Impossible to build lib/configure -- aborting"
-    exit
-fi
+echo -n "Building lib/configure ... "
+rm -f lib/configure
+$GNUM4 lib/configure.m4 > lib/configure
+chmod a+x lib/configure
+echo "done."
 
 echo
 echo 'run "./configure ; make"'
