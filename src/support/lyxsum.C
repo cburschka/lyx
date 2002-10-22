94158548581a8d94f3cb7cebb5314fc97b46b0eb
Jean-Marc Lasgouttes <lasgouttes@lyx.org>
2002-10-22 14:59:13 +0000

    ugly but working hack to fix link problems
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/branches/BRANCH-1_2_X@5472 a592a061-630c-0410-9148-cb99ea01b6c8

--- src/support/lyxsum.C
+++ src/support/lyxsum.C
@@ -18,6 +18,15 @@
 
 using std::endl;
 
+// OK, this is ugly, but it is the only workaround I found to compile
+// with gcc (any version) on a system which uses a non-GNU toolchain.
+// The problem is that gcc uses a weak symbol for a particular
+// instantiation and that the system linker usually does not
+// understand those weak symbols (seen on HP-UX, tru64, AIX and
+// others). Thus we force an explicit instanciation of this particular
+// template (JMarc)
+template class boost::detail::crc_table_t<32, 0x04C11DB7, true>;
+
 // Various implementations of lyx::sum(), depending on what methods
 // are available. Order is faster to slowest.
 #if defined(HAVE_MMAP) && defined(HAVE_MUNMAP)
