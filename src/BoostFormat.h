af4fca532fb249ae45460c7e6964a1f8d14f2273
Lars Gullik Bjønnes <larsbj@gullik.org>
2002-11-25 00:42:20 +0000

    add more explicit instantations
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@5706 a592a061-630c-0410-9148-cb99ea01b6c8

--- src/BoostFormat.h
+++ src/BoostFormat.h
@@ -13,8 +13,43 @@
 namespace boost
 {
 
-  extern
-  template basic_format<char>;
+extern
+template basic_format<char>;
+
+extern template
+std::ostream &
+operator<< <char, std::char_traits<char> >(std::ostream &,
+					   boost::basic_format<char> const &);
+
+namespace io
+{
+
+extern template
+std::string
+str<char, std::char_traits<char> >(boost::basic_format<char> const &);
+
+namespace detail
+{
+
+extern template
+bool parse_printf_directive<char, std::char_traits<char> >
+(std::string const &, std::string::size_type*,
+ format_item<char, std::char_traits<char> >*,
+ std::ios &, unsigned char);
+
+extern template
+void distribute<char, std::char_traits<char>, std::string const &>
+(boost::basic_format<char>&, std::string const &);
+
+extern template
+boost::basic_format<char> &
+feed<char, std::char_traits<char>,
+     std::string const &>(boost::basic_format<char> &,
+			  std::string const &);
+
+} // namespace detail
+
+} // namespace io
 
 } // namespace boost
 
--- src/boost-inst.C
+++ src/boost-inst.C
@@ -11,6 +11,41 @@ using boost::io::out_of_range_bit;
 
 template class basic_format<char>;
 
+template
+std::ostream &
+operator<< <char, std::char_traits<char> >(std::ostream &,
+					   boost::basic_format<char> const &);
+
+namespace io
+{
+
+template
+std::string
+str<char, std::char_traits<char> >(boost::basic_format<char> const &);
+
+namespace detail
+{
+
+template
+bool parse_printf_directive<char, std::char_traits<char> >
+(std::string const &, std::string::size_type*,
+ format_item<char, std::char_traits<char> >*,
+ std::ios &, unsigned char);
+
+template
+void distribute<char, std::char_traits<char>, std::string const &>
+(boost::basic_format<char> &, std::string const&);
+
+template
+boost::basic_format<char> &
+feed<char, std::char_traits<char>,
+     std::string const &>(boost::basic_format<char> &,
+			  std::string const &);
+
+} // namespace detail
+
+} // namespace io
+
 } // namespace boost
 
 #endif // USE_BOOST_FORMAT
