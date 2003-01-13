cc4e6a84995ff4384a0166dd6750e1b440db90e9
Angus Leeming <leeming@lyx.org>
2003-01-13 23:35:18 +0000

    Try and make convertDefault a little more robust.
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@5950 a592a061-630c-0410-9148-cb99ea01b6c8

--- lib/scripts/convertDefault.sh
+++ lib/scripts/convertDefault.sh
@@ -1,10 +1,23 @@
-#!/bin/sh
-# this is the default converter if no one other was
-# defined by the user in edit->preferences->converter
+#! /bin/sh
+# The default converter if no other has been defined by the user from the
+# Conversion->Converter tab of the Preferences dialog.
 #
-# the user can also redefine this default converter
-# with an own shell script in ~/.lyx/scripts
+# The user can also redefine this default converter, placing their
+# replacement in ~/.lyx/scripts
 #
-# converts an image from $1 to $2 format 
-convert -depth 8 $1 $2 
-exit 0
+# converts an image from $1 to $2 format
+convert -depth 8 $1 $2
+if [ $? -ne 0 ]; then
+    exit $?
+fi
+
+# It appears that convert succeeded, but we know better than to trust it ;-)
+# convert is passed strings in the form "FMT:FILENAME", so use the ':' to
+# delimit the two parts.
+FILE=`echo $2 | cut -d ':' -f 2`
+
+# FSTATUS == 0 is the file exists and == 1 if it does not.
+FSTATUS=0
+test -f $FILE || FSTATUS=1
+
+exit $FSTATUS
