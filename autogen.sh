5ca64b0d517d32b8be9bf247685ab5fd529e7cf9
Jean-Marc Lasgouttes <lasgouttes@lyx.org>
2003-02-11 13:07:37 +0000

    support autoconf 2.57
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@6092 a592a061-630c-0410-9148-cb99ea01b6c8

--- autogen.sh
+++ autogen.sh
@@ -19,7 +19,7 @@ case $autoversion in
 	rm -f lib/reLyX/configure.ac
 	EXTRA_ACINCLUDE_FILES="lyxinclude213.m4"
 	;;
-    *2.5[2346])
+    *2.5[23467])
 	rm -f acconfig.h
 	rm -f configure.in
 	cp config/configure.ac .
