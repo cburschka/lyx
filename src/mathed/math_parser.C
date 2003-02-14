79c6a1b810d958e4af920f689b778f69db31df71
Dekel Tsur <dekelts@tau.ac.il>
2003-02-14 16:20:44 +0000

    Do not remove braces for {<expr>}^
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@6157 a592a061-630c-0410-9148-cb99ea01b6c8

--- src/mathed/math_parser.C
+++ src/mathed/math_parser.C
@@ -746,9 +746,10 @@ void Parser::parse1(MathGridInset & grid, unsigned flags,
 			MathScriptInset * p = cell->back().nucleus()->asScriptInset();
 			// special handling of {}-bases
 			// is this always correct?
-			if (p->nuc().size() == 1 && p->nuc().back()->asNestInset() &&
-					p->nuc().back()->extraBraces())
-				p->nuc() = p->nuc().back()->asNestInset()->cell(0);
+			// It appears that this is wrong (Dekel)
+			//if (p->nuc().size() == 1 && p->nuc().back()->asNestInset() &&
+			//    p->nuc().back()->extraBraces())
+			//	p->nuc() = p->nuc().back()->asNestInset()->cell(0);
 			parse(p->cell(up), FLAG_ITEM, mode);
 			if (limits) {
 				p->limits(limits);
