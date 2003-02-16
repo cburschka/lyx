e27650b66b92894312e7409208a8acab5814e0f0
Jean-Marc Lasgouttes <lasgouttes@lyx.org>
2003-02-16 15:42:35 +0000

    buglet with encoding in prefs
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/branches/BRANCH_1_3_X@6178 a592a061-630c-0410-9148-cb99ea01b6c8

--- src/frontends/xforms/FormPreferences.C
+++ src/frontends/xforms/FormPreferences.C
@@ -2473,6 +2473,7 @@ void FormPreferences::ScreenFonts::apply(LyXRC & rc) const
 	if (rc.font_norm != str) {
 		changed = true;
 		rc.font_norm = str;
+		rc.set_font_norm_type();
 	}
 
 	bool button = fl_get_button(dialog_->check_scalable);
