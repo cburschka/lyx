ea315a33e6558a29037bb93f297df1efeb9ff527
Angus Leeming <leeming@lyx.org>
2003-02-27 15:53:23 +0000

    Make shortcuts consistent with the rest of LyX.
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@6296 a592a061-630c-0410-9148-cb99ea01b6c8

--- src/frontends/xforms/Alert_pimpl.C
+++ src/frontends/xforms/Alert_pimpl.C
@@ -31,29 +31,31 @@ void alert_pimpl(string const & s1, string const & s2, string const & s3)
 
 bool askQuestion_pimpl(string const & s1, string const & s2, string const & s3)
 {
-	fl_set_resource("flQuestion.yes.label", idex(_("Yes|Yy#y")).c_str());
-	fl_set_resource("flQuestion.no.label", idex(_("No|Nn#n")).c_str());
+	fl_set_resource("flQuestion.yes.label", idex(_("Yes|#y")).c_str());
+	fl_set_resource("flQuestion.no.label", idex(_("No|#n")).c_str());
 	return fl_show_question((s1 + "\n" + s2 + "\n" + s3).c_str(), 0);
 }
 
 
 int askConfirmation_pimpl(string const & s1, string const & s2, string const & s3)
 {
-	fl_set_choices_shortcut(scex(_("Yes|Yy#y")).c_str(),
-				scex(_("No|Nn#n")).c_str(),
-				scex(_("Cancel|^[")).c_str());
+	string const yes    = _("Yes|#Y");
+	string const no     = _("No|#N");
+	string const cancel = _("Cancel|^[");
+	
+	fl_set_choices_shortcut(scex(yes).c_str(), scex(no).c_str(),
+				scex(cancel).c_str());
 	return fl_show_choice(s1.c_str(), s2.c_str(), s3.c_str(),
-			      3, idex(_("Yes|Yy#y")).c_str(),
-			      idex(_("No|Nn#n")).c_str(),
-			      idex(_("Cancel|^[")).c_str(), 3);
+			      3, idex(yes).c_str(), idex(no).c_str(),
+			      idex(cancel).c_str(), 3);
 }
 
 
 pair<bool, string> const askForText_pimpl(string const & msg, string const & dflt)
 {
 	fl_set_resource("flInput.cancel.label", idex(_("Cancel|^[")).c_str());
-	fl_set_resource("flInput.ok.label", idex(_("OK|#O")).c_str());
-	fl_set_resource("flInput.clear.label", idex(_("Clear|#e")).c_str());
+	fl_set_resource("flInput.ok.label", idex(_("OK|^M")).c_str());
+	fl_set_resource("flInput.clear.label", idex(_("Clear|#C")).c_str());
 	char const * tmp = fl_show_input(msg.c_str(), dflt.c_str());
 	if (tmp != 0)
 		return make_pair<bool, string>(true, string(tmp));
