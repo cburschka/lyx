e3e2a6941dbfb3bd847fc6397f51f2a9bac6db83
Lars Gullik Bjønnes <larsbj@gullik.org>
2003-06-11 07:10:07 +0000

    the ios issue
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@7143 a592a061-630c-0410-9148-cb99ea01b6c8

--- src/main.C
+++ src/main.C
@@ -13,11 +13,15 @@
 #include "gettext.h"
 #include "support/os.h"
 
+#ifdef HAVE_IOS
 #include <ios>
+#endif
 
 int main(int argc, char * argv[])
 {
-	std::ios::sync_with_stdio(false);
+#ifdef HAVE_IOS
+	std::ios_base::sync_with_stdio(false);
+#endif
 
 	os::init(&argc, &argv);
 
