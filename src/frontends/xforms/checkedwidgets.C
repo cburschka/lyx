b3ac06d3a2b21060c786e4e8b11d20e14fc59ebf
Angus Leeming <leeming@lyx.org>
2003-12-04 16:56:49 +0000

    Keep doxygen happy.
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@8194 a592a061-630c-0410-9148-cb99ea01b6c8

--- src/frontends/xforms/checkedwidgets.C
+++ src/frontends/xforms/checkedwidgets.C
@@ -1,5 +1,5 @@
 /**
- * \file checkedwidgets.C
+ * \file xforms/checkedwidgets.C
  * This file is part of LyX, the document processor.
  * Licence details can be found in the file COPYING.
  *
--- src/frontends/xforms/checkedwidgets.h
+++ src/frontends/xforms/checkedwidgets.h
@@ -1,6 +1,6 @@
 // -*- C++ -*-
 /**
- * \file checkedwidgets.h
+ * \file xforms/checkedwidgets.h
  * This file is part of LyX, the document processor.
  * Licence details can be found in the file COPYING.
  *
