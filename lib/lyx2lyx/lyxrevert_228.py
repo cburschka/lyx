90176702765fe13a9c532a6cb17538502c64c93b
José Matox <jamatos@lyx.org>
2003-12-16 09:57:48 +0000

    Angus fix to lyxrevert_228.py
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@8257 a592a061-630c-0410-9148-cb99ea01b6c8

--- lib/lyx2lyx/lyxrevert_228.py
+++ lib/lyx2lyx/lyxrevert_228.py
@@ -15,6 +15,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
+import sys
 from parser_tools import find_tokens
 
 def convert_collapsable(lines):
