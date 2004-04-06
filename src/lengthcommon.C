5b992e53e5bf1559d15901cef322a89e666efc5a
Jean-Marc Lasgouttes <lasgouttes@lyx.org>
2004-04-06 13:48:13 +0000

    compilation fix
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@8613 a592a061-630c-0410-9148-cb99ea01b6c8

--- src/lengthcommon.C
+++ src/lengthcommon.C
@@ -18,22 +18,21 @@
 
 using std::string;
 
-
-int const num_units = LyXLength::UNIT_NONE;
-
 // I am not sure if "mu" should be possible to select (Lgb)
 
 // unit_name is for compatibility. Can be deleted when all works well.
 // means, when we have full language support for the lengths
 // in all gui's				(Herbert 2002-11-01)
-char const * const unit_name[num_units + 1] = {
+char const * const unit_name[] = {
 	"sp", "pt", "bp", "dd", "mm", "pc",
 	"cc", "cm", "in", "ex", "em", "mu",
 	"text%",  "col%", "page%", "line%",
 	"theight%", "pheight%", "" };
 
+int const num_units = int(sizeof(unit_name) / sizeof(unit_name[0]));
+
 // the latex units
-char const * const unit_name_ltx[num_units + 1] = {
+char const * const unit_name_ltx[] = {
 	"sp", "pt", "bp", "dd", "mm", "pc",
 	"cc", "cm", "in", "ex", "em", "mu",
 	// in 1.4 the following names should be used. then no
@@ -44,7 +43,7 @@ char const * const unit_name_ltx[num_units + 1] = {
 	"theight%", "pheight%", "" };
 
 // the LyX gui units
-char const * const unit_name_gui[num_units + 1] = {
+char const * const unit_name_gui[] = {
 	N_("sp"), N_("pt"), N_("bp"), N_("dd"), N_("mm"), N_("pc"),
 	N_("cc"), N_("cm"), N_("in"), N_("ex"), N_("em"), N_("mu"),
 	N_("text%"), N_("col%"), N_("page%"), N_("line%"),
