00b1adcbbe5727c5ea5c65e00209908ae87fb2dd
André Pönitz <poenitz@gmx.net>
2004-04-08 15:45:11 +0000

    \overset had the same problem as \dot
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@8637 a592a061-630c-0410-9148-cb99ea01b6c8

--- src/mathed/math_oversetinset.C
+++ src/mathed/math_oversetinset.C
@@ -30,21 +30,22 @@ void MathOversetInset::metrics(MetricsInfo & mi, Dimension & dim) const
 	cell(1).metrics(mi);
 	FracChanger dummy(mi.base);
 	cell(0).metrics(mi);
-	dim_.wid = max(cell(0).width(), cell(1).width()) + 4;
-	dim_.asc = cell(1).ascent() + cell(0).height() + 4;
-	dim_.des = cell(1).descent();
-	dim = dim_;
+	dim.wid = max(cell(0).width(), cell(1).width()) + 4;
+	dim.asc = cell(1).ascent() + cell(0).height() + 4;
+	dim.des = cell(1).descent();
+	metricsMarkers(dim);
+	dim_ = dim;
 }
 
 
 void MathOversetInset::draw(PainterInfo & pi, int x, int y) const
 {
-	int m  = x + pi.width / 2;
+	int m  = x + width() / 2;
 	int yo = y - cell(1).ascent() + cell(0).descent() - 1;
 	cell(1).draw(pi, m - cell(1).width() / 2, y);
 	FracChanger dummy(pi.base);
 	cell(0).draw(pi, m - cell(0).width() / 2, yo);
-	setPosCache(pi, x, y);
+	drawMarkers(pi, x, y);
 }
 
 
