bdbb7676a254f3925ee82ed42f67835adf80de5e
Angus Leeming <leeming@lyx.org>
2004-10-05 11:07:57 +0000

    Protect shell variables with quotes.
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/branches/BRANCH_1_3_X@9045 a592a061-630c-0410-9148-cb99ea01b6c8

--- lib/scripts/convertDefault.sh
+++ lib/scripts/convertDefault.sh
@@ -1,13 +1,21 @@
 #! /bin/sh
+
+# file convertDefault.sh
+# This file is part of LyX, the document processor.
+# Licence details can be found in the file COPYING.
+
+# author Herbert Voß
+
+# Full author contact details are available in file CREDITS.
+
 # The default converter if no other has been defined by the user from the
 # Conversion->Converter tab of the Preferences dialog.
-#
+
 # The user can also redefine this default converter, placing their
 # replacement in ~/.lyx/scripts
-#
-# converts an image from $1 to $2 format
 
-convert -depth 8 $1 $2 || {
+# converts an image from $1 to $2 format
+convert -depth 8 "$1" "$2" || {
 	echo "$0 ERROR"
 	echo "Execution of \"convert\" failed."
 	exit 1
@@ -18,9 +26,9 @@ convert -depth 8 $1 $2 || {
 # delimit the two parts.
 # Note that Win32 filenames have the form 'C:\my\file',
 # so use everything from the first ':' to the end of the line.
-FILE=`echo $2 | cut -d ':' -f 2-`
+FILE=`echo "$2" | cut -d ':' -f 2-`
 
-test -f $FILE || {
+test -f "$FILE" || {
 	echo "$0 ERROR"
 	echo "Unable to find file \"${FILE}\""
 	exit 1
