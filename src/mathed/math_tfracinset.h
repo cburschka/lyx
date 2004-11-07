e948588b098bbfe70db4b0085f8535c5d1fe977e
André Pönitz <poenitz@gmx.net>
2004-11-07 19:33:30 +0000

    forgot these
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@9200 a592a061-630c-0410-9148-cb99ea01b6c8

---
+++ src/mathed/math_tfracinset.h
@@ -0,0 +1,37 @@
+// -*- C++ -*-
+/**
+ * \file math_tfracinset.h
+ * This file is part of LyX, the document processor.
+ * Licence details can be found in the file COPYING.
+ *
+ * \author André Pönitz
+ *
+ * Full author contact details are available in file CREDITS.
+ */
+
+#ifndef MATH_TFRACINSET_H
+#define MATH_TFRACINSET_H
+
+#include "math_fracinset.h"
+
+
+/// \dfrac support
+class MathTfracInset : public MathFracInset {
+public:
+	///
+	MathTfracInset();
+	///
+	virtual std::auto_ptr<InsetBase> clone() const;
+	///
+	void metrics(MetricsInfo & mi, Dimension & dim) const;
+	///
+	void draw(PainterInfo &, int x, int y) const;
+	///
+	std::string name() const;
+	///
+	void mathmlize(MathMLStream &) const;
+	///
+	void validate(LaTeXFeatures & features) const;
+};
+
+#endif
