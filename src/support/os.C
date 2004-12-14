fb28ff9e2af747ec2d77223bb4f8184527b3d5a8
Angus Leeming <leeming@lyx.org>
2004-12-14 12:29:53 +0000

    * os.C: #include "os_win32.h" for _WIN32. * systemcall.C: The "/n" in "start /min/n" makes no sense under DOS.
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@9367 a592a061-630c-0410-9148-cb99ea01b6c8

--- src/support/os.C
+++ src/support/os.C
@@ -12,7 +12,7 @@
 
 #ifdef __EMX__
 #include "os_os2.C"
-#elif defined(__CYGWIN__) || defined(__CYGWIN32__)
+#elif defined(__CYGWIN__) || defined(__CYGWIN32__) || defined(_WIN32)
 #include "os_win32.C"
 #else
 #include "os_unix.C"
