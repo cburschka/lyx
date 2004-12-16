eef5f997f18822b0c6aa92c34543a74dc8ad0450
Angus Leeming <leeming@lyx.org>
2004-12-16 01:03:34 +0000

    An improved HAVE_MKDIR patch that compiles also with MinGW.
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@9385 a592a061-630c-0410-9148-cb99ea01b6c8

--- src/support/mkdir.C
+++ src/support/mkdir.C
@@ -20,5 +20,20 @@
 int lyx::support::mkdir(std::string const & pathname, unsigned long int mode)
 {
 	// FIXME: why don't we have mode_t in lyx::mkdir prototype ??
+#if HAVE_MKDIR
+# if MKDIR_TAKES_ONE_ARG
+	// MinGW32
+	return ::mkdir(pathname.c_str());
+# else
+	// POSIX
 	return ::mkdir(pathname.c_str(), mode_t(mode));
+# endif
+#else
+# if HAVE__MKDIR
+	// plain Windows 32
+	return ::_mkdir(pathname.c_str());
+# else
+#  error "Don't know how to create a directory on this system."
+# endif
+#endif
 }
