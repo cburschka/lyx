532408eba223a29d15ecae0111024b2dafeed52a
Angus Leeming <leeming@lyx.org>
2005-03-24 09:19:16 +0000

    Enable convertDefault.sh to work reliably under Windows.
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/branches/BRANCH_1_3_X@9740 a592a061-630c-0410-9148-cb99ea01b6c8

--- lib/scripts/convertDefault.sh
+++ lib/scripts/convertDefault.sh
@@ -21,17 +21,3 @@ convert -depth 8 "$1" "$2" || {
 	exit 1
 }
 
-# It appears that convert succeeded, but we know better than to trust it ;-)
-# convert is passed strings in the form "FMT:FILENAME", so use the ':' to
-# delimit the two parts.
-# Note that Win32 filenames have the form 'C:\my\file',
-# so use everything from the first ':' to the end of the line.
-FILE=`echo "$2" | cut -d ':' -f 2-`
-
-test -f "$FILE" || {
-	echo "$0 ERROR"
-	echo "Unable to find file \"${FILE}\""
-	exit 1
-}
-
-echo "$0 generated file \"${FILE}\" successfully."
