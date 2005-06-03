19ec3439cb7466e1b673cb9d5487382cfc56f7f5
Jürgen Spitzmüller <spitz@lyx.org>
2005-06-03 09:10:22 +0000

    limit qt-immodule fixes (bug 1830) to qt/x11
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/branches/BRANCH_1_3_X@9995 a592a061-630c-0410-9148-cb99ea01b6c8

--- src/frontends/qt2/QContentPane.C
+++ src/frontends/qt2/QContentPane.C
@@ -96,7 +96,7 @@ QContentPane::QContentPane(QWorkArea * parent)
 	setFocusPolicy(QWidget::WheelFocus);
 	setFocus();
 	setCursor(ibeamCursor);
-#if QT_VERSION >= 0x030200
+#if USE_INPUT_METHODS
 	// to make qt-immodule work
 	setInputMethodEnabled(true);
 #endif
@@ -108,7 +108,7 @@ QContentPane::QContentPane(QWorkArea * parent)
 }
 
 
-#if QT_VERSION >= 0x030200
+#if USE_INPUT_METHODS
 // to make qt-immodule work
 void QContentPane::imStartEvent(QIMEvent *e)
 {
