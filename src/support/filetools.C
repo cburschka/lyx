4f90d7f496c23126b8d0cf280eb0706aced95505
Angus Leeming <leeming@lyx.org>
2005-07-05 07:45:44 +0000

    Fix Georg's latex_path magic on Windows.
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/branches/BRANCH_1_3_X@10128 a592a061-630c-0410-9148-cb99ea01b6c8

--- src/support/filetools.C
+++ src/support/filetools.C
@@ -94,17 +94,19 @@ bool IsSGMLFilename(string const & filename)
 string const latex_path(string const & original_path, bool exclude_extension)
 {
 	string path = subst(original_path, "~", "\\string~");
-	if (path.find(' ') != string::npos)
+	if (path.find(' ') != string::npos) {
 		// We can't use '"' because " is sometimes active (e.g. if
 		// babel is loaded with the "german" option)
 		if (exclude_extension) {
 			string const base = ChangeExtension(path, string());
 			string const ext = GetExtension(path);
-			path = ChangeExtension(
-				"\\string\"" + base  + "\\string\"", ext);
-		} else
-
+			// ChangeExtension calls os::internal_path internally
+			// so don't use it to re-add the extension.
+			path = "\\string\"" + base  + "\\string\"." + ext;
+		} else {
 			path = "\\string\"" + path + "\\string\"";
+		}
+	}
 	return path;
 }
 
