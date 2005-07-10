14d7a4424e4648d293ffbf3ec18ea640270c852b
Georg Baum <Georg.Baum@post.rwth-aachen.de>
2005-07-10 09:30:07 +0000

    add forgotten comment about jumping cursor
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@10161 a592a061-630c-0410-9148-cb99ea01b6c8

--- src/mathed/math_gridinset.C
+++ src/mathed/math_gridinset.C
@@ -1040,6 +1040,11 @@ void MathGridInset::doDispatch(LCursor & cur, FuncRequest & cmd)
 
 	// insert file functions
 	case LFUN_DELETE_LINE_FORWARD:
+		// FIXME: We use recordUndoInset when a change reflects more
+		// than one cell, because recordUndo does not work for
+		// multiple cells. Unfortunately this puts the cursor in front
+		// of the inset after undo. This is (especilally for large
+		// grids) annoying.
 		recordUndoInset(cur);
 		//autocorrect_ = false;
 		//macroModeClose();
