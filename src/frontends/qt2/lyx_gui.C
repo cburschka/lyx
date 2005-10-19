52c945af744dc91be4710be8edca6df08f8da459
Angus Leeming <leeming@lyx.org>
2005-10-19 15:07:36 +0000

    Don't unlock QApplication's mutex as it leads to undefined behaviour (and crashes NetBSD).
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@10560 a592a061-630c-0410-9148-cb99ea01b6c8

--- src/frontends/qt2/lyx_gui.C
+++ src/frontends/qt2/lyx_gui.C
@@ -135,12 +135,7 @@ LQApplication::LQApplication(int & argc, char ** argv)
 
 
 LQApplication::~LQApplication()
-{
-#ifdef QT_THREAD_SUPPORT
-	if (locked())
-		unlock();
-#endif
-}
+{}
 
 
 #ifdef Q_WS_MACX
