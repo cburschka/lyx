da7b459900e0bc2d4b8d79fe6d10345612ba3ff7
Georg Baum <Georg.Baum@post.rwth-aachen.de>
2005-11-28 15:42:26 +0000

    sync lyx2lyx with 1.4
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/branches/BRANCH_1_3_X@10628 a592a061-630c-0410-9148-cb99ea01b6c8

--- lib/lyx2lyx/lyx_1_0_0.py
+++ lib/lyx2lyx/lyx_1_0_0.py
@@ -16,7 +16,63 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
-convert = [[215, []]]
+import re
+import string
+from parser_tools import find_token, find_re
+
+def obsolete_latex_title(file):
+    body = file.body
+    i = 0
+    while 1:
+        i = find_token(body, '\\layout', i)
+        if i == -1:
+            return
+
+        if string.find(string.lower(body[i]),'latex title') != -1:
+            body[i] = '\\layout Title'
+
+        i = i + 1
+
+
+# Update from tabular format 3 to 4 if necessary
+def update_tabular(file):
+    lines = file.body
+    lyxtable_re = re.compile(r".*\\LyXTable$")
+    i=0
+    while 1:
+        i = find_re(lines, lyxtable_re, i)
+        if i == -1:
+            break
+        i = i + 1
+        format = lines[i][8:]
+
+        if format != '3':
+            continue
+
+        lines[i]='multicol4'
+        i = i + 1
+        rows = int(string.split(lines[i])[0])
+        columns = int(string.split(lines[i])[1])
+
+        lines[i] = lines[i] + ' 0 0 -1 -1 -1 -1'
+        i = i + 1
+
+        for j in range(rows):
+            lines[i] = lines[i] + ' 0 0'
+            i = i + 1
+
+        for j in range(columns):
+            lines[i] = lines[i] + ' '
+            i = i + 1
+
+        while string.strip(lines[i]):
+            lines[i] = lines[i] + ' 0 0 0'
+            i = i + 1
+
+        lines[i] = string.strip(lines[i])
+
+
+convert = [[215, [obsolete_latex_title, update_tabular]]]
 revert  = []
 
 
