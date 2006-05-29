b75165353e49bd9fe84ef6a36cf6a0dfd308ac10
Jürgen Spitzmüller <spitz@lyx.org>
2006-05-29 17:28:02 +0000

    fix bug 2623:
    * lib/layouts/memoir.layout:
    	add ProvidesMakeindex
    
    * src/LaTeX.C (run):
    	rerun latex if idx file is empty (needed by memoir)
    	fix typo in comment
    * src/LaTeXFeatures.C (getPackages):
    	whitespace
    
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/branches/BRANCH_1_4_X@13963 a592a061-630c-0410-9148-cb99ea01b6c8

--- src/LaTeXFeatures.C
+++ src/LaTeXFeatures.C
@@ -293,7 +293,7 @@ string const LaTeXFeatures::getPackages() const
 
 	// makeidx.sty
 	if (isRequired("makeidx")) {
-		if (! tclass.provides(LyXTextClass::makeidx))
+		if (!tclass.provides(LyXTextClass::makeidx))
 			packages << "\\usepackage{makeidx}\n";
 		packages << "\\makeindex\n";
 	}
