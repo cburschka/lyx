a8bf36a4137faf17208f66689c6fe0401b3b0f5c
Abdelrazak Younes <younes@lyx.org>
2006-06-26 17:18:28 +0000

    GUI API Cleanup step 2: merge of the "younes" branch. What's missing
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@14234 a592a061-630c-0410-9148-cb99ea01b6c8

---
+++ src/frontends/qt4/GuiClipboard.C
@@ -0,0 +1,75 @@
+// -*- C++ -*-
+/**
+ * \file GuiClipboard.C
+ * This file is part of LyX, the document processor.
+ * Licence details can be found in the file COPYING.
+ *
+ * \author John Levon
+ * \author Abdelrazak Younes
+ *
+ * Full author contact details are available in file CREDITS.
+ */
+
+#include "GuiClipboard.h"
+#include "qt_helpers.h"
+
+#include "debug.h"
+
+#include <QApplication>
+#include <QClipboard>
+#include <QString>
+
+#include <string>
+
+#include "support/lstrings.h"
+using lyx::support::internalLineEnding;
+using lyx::support::externalLineEnding;
+
+using std::endl;
+using std::string;
+
+namespace lyx {
+namespace frontend {
+
+#ifdef Q_WS_X11
+QClipboard::Mode const CLIPBOARD_MODE = QClipboard::Selection;
+#else
+// FIXME external clipboard support is mostly broken for windows
+// because the following fixe would involves too much side effects WRT mouse selection.
+//QClipboard::Mode const CLIPBOARD_MODE = QClipboard::Clipboard;
+QClipboard::Mode const CLIPBOARD_MODE = QClipboard::Selection;
+#endif
+
+void GuiClipboard::haveSelection(bool own)
+{
+	if (!qApp->clipboard()->supportsSelection())
+		return;
+
+	if (own) {
+		qApp->clipboard()->setText(QString(), CLIPBOARD_MODE);
+	}
+	// We don't need to do anything if own = false, as this case is
+	// handled by QT.
+}
+
+
+string const GuiClipboard::get() const
+{
+	QString str = qApp->clipboard()->text(CLIPBOARD_MODE);
+	lyxerr[Debug::ACTION] << "getClipboard: " << (const char*) str << endl;
+	if (str.isNull())
+		return string();
+
+	return internalLineEnding(fromqstr(str));
+}
+
+
+void GuiClipboard::put(string const & str)
+{
+	lyxerr[Debug::ACTION] << "putClipboard: " << str << endl;
+
+	qApp->clipboard()->setText(toqstr(externalLineEnding(str)), CLIPBOARD_MODE);
+}
+
+} // namespace frontend
+} // namespace lyx
