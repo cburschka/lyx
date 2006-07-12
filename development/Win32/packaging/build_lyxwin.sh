9f009e5222e43c0d98033093622e7664bbdba67d
Joost Verburg <joostverburg@users.sourceforge.net>
2006-07-12 12:37:16 +0000

    build script for new Windows installer
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/branches/BRANCH_1_4_X@14424 a592a061-630c-0410-9148-cb99ea01b6c8

--- development/Win32/packaging/build_lyxwin.sh
+++ development/Win32/packaging/build_lyxwin.sh
@@ -1,38 +1,25 @@
 #! /bin/sh
 
 # This script aims to do everything necessary to automate the building
-# of a LyX/Win package.
+# of a LyX/Win.
 
 # Invocation:
-# sh build_lyxwin.sh "1.3.6-pre23"
+# sh build_lyxwin.sh "1.4.2-1"
 # The string will be shown in the "About LyX" dialog.
 
 # Notes:
 # It uses the MSYS environment and MinGW compiler.
-
 # It asks whether the Qt and LyX cvs trees are up to date.
 # It asks whether the Qt library has been compiled.
 # It checks that qt-mt3.dll and mingw10.dll exist.
-# It compiles the dv2dt and dt2dv utilites.
 # It compiles and installs LyX.
 
-# Once all this is done, you're ready to "package" LyX.
-# See the README for details.
-
-# The script compiles the .dll version of the Qt libraries. Linking of LyX
-# against this will, therefore, take "some time".
-
-# You may need to change these four variables.
-MINGW_DIR="/j/MinGW"
-QT_DIR="${HOME}"/Qt/3x-msys
+# You may need to change these two variables.
+MINGW_DIR=/mingw
+QT_DIR=/usr/local/qt-3
 
 # Everything from here on down should be OK "as is".
 LYX_DIR="../../.."
-PACKAGING_DIR="$LYX_DIR/development/Win32/packaging"
-DTL_DIR="$PACKAGING_DIR/dtl"
-
-ASPELL_INSTALL_DIR="c:/Aspell"
-LYX_ASPELL_DIR="/c/Aspell" # the Autotools don't like "C:/" syntax.
 LYX_RELATIVE_BUILDDIR=build
 LYX_INSTALL_DIR=installprefix
 
@@ -40,16 +27,13 @@ LYX_INSTALL_DIR=installprefix
 QT_DLL="${QT_DIR}/bin/qt-mt3.dll"
 MINGW_DLL="${MINGW_DIR}/bin/mingwm10.dll"
 
-DT2DV="${DTL_DIR}/dt2dv.exe"
-DV2DT="${DTL_DIR}/dv2dt.exe"
-
 # Change this to 'mv -f' when you are confident that
 # the various sed scripts are working correctly.
 MV='mv -f'
 
 check_dirs_exist()
 {
-    for dir in "$QT_DIR" "$LYX_DIR" "$DTL_DIR"
+    for dir in "$QT_DIR" "$LYX_DIR"
     do
       test -d "$dir" || {
 	  echo "$dir does not exist" >&2
@@ -75,31 +59,6 @@ check_files_exist()
     do
       test -r "${file}" || {
 	  echo "$file does not exist" >&2
-	  exit 1
-      }
-    done
-}
-
-
-build_dtl()
-{
-    # dt2dv and dv2dt
-    (
-	cd "$DTL_DIR" || {
-	    echo "Unable to cd $DTL_DIR" >&2
-	    exit 1
-	}
-
-	make || {
-	    echo "Failed to make $DTL_DIR" >&2
-	    exit 1
-	}
-    )
-
-    for file in "${DT2DV}" "${DV2DT}"
-    do
-      test -x "$file" || {
-	  echo "${file} does not exist or is not executable" >&2
 	  exit 1
       }
     done
@@ -168,7 +127,7 @@ build_lyx()
 		Error "Unable to create build dir, ${BUILDDIR}."
 	}
 
-	CONFIGURE="../configure --without-x --with-included-gettext --with-extra-prefix='${LYX_ASPELL_DIR}' --with-frontend=qt QTDIR='$QT_DIR' --disable-maintainer-mode --disable-debug --enable-optimization --disable-pch --disable-concept-checks --disable-stdlib-debug"
+	CONFIGURE="../configure --without-x --with-frontend=qt QTDIR='$QT_DIR' --disable-maintainer-mode --disable-debug --enable-optimization --disable-pch --disable-concept-checks --disable-stdlib-debug --with-version-suffix=1.4.x --with-extra-prefix=/usr/local"
 
 	echo "${CONFIGURE}"
 	cd "${BUILDDIR}"
@@ -204,7 +163,7 @@ install_lyx()
 	    exit 1
 	}
 
-	make install || {
+	make install-strip || {
 	    echo "Failed to install" >&2
 	    exit 1
 	}
@@ -217,7 +176,6 @@ test $# -ne 0 && LYX_VERSION_STR=$1
 check_dirs_exist || exit 1
 query_qt || exit 1
 check_files_exist || exit 1
-build_dtl || exit 1
 run_automake || exit 1
 build_lyx || exit 1
 install_lyx || exit 1
