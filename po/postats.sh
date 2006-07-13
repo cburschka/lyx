d06be6676365765ad237c9a15bca9e7f11737e1e
Jean-Marc Lasgouttes <lasgouttes@lyx.org>
2006-07-13 08:02:27 +0000

    Update again Slovak l10n; remerge.
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/branches/BRANCH_1_4_X@14438 a592a061-630c-0410-9148-cb99ea01b6c8

--- po/postats.sh
+++ po/postats.sh
@@ -18,7 +18,7 @@
 # modifiy this when you change version
 # Note that an empty lyx_branch variable (ie cvs HEAD)
 # will "do the right thing".
-lyx_version=1.4.2svn
+lyx_version=1.4.2
 lyx_branch=BRANCH_1_4_X
 
 
