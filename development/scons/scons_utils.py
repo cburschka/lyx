c8e38f5e05a05cc2f6937132906b8e3b09021a81
Bo Peng <bpeng@lyx.org>
2006-08-13 04:54:25 +0000

    Scons: stop globbing filenames, use explicit file list instead. 	* INSTALL.scons: stop lyx1.4.x/qt2 support 	* development/scons/scons_manifest.py: list of all filenames 	* development/scons/SConstruct: use explicit file list 	* development/scons/scons_utils.py: remove globFiles
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@14634 a592a061-630c-0410-9148-cb99ea01b6c8

--- development/scons/scons_utils.py
+++ development/scons/scons_utils.py
@@ -58,19 +58,6 @@ def env_subst(target, source, env):
     #st = os.stat(str(source[0]))
     #os.chmod(str(target[0]), stat.S_IMODE(st[stat.ST_MODE]) | stat.S_IWRITE)
 
-
-#
-# glob filenames
-#
-def globSource(dir, pattern, build_dir = None, exclude = [], include = []):
-    ''' glob files, in dir and use build_dir as returned path name '''
-    # exclude 'exclude+include' to avoid duplicate items in files
-    files = include + filter(lambda x: x not in exclude + include, glob.glob1(dir, pattern))
-    if build_dir is None:
-        return files
-    else:
-        return ['%s/%s' % (build_dir, x) for x in files]
-
 #
 # autoconf tests
 #
