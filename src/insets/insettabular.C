9917aca0b7aeddddb27badffc6db798f2d9e6b04
Jean-Marc Lasgouttes <lasgouttes@lyx.org>
2006-09-20 12:16:26 +0000

    * insettabular.C (cutSelection): reset cursor when it is invalid.
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@15088 a592a061-630c-0410-9148-cb99ea01b6c8

--- src/insets/insettabular.C
+++ src/insets/insettabular.C
@@ -1849,7 +1849,10 @@ void InsetTabular::cutSelection(LCursor & cur)
 	}
 
 	// cursor position might be invalid now
-	cur.pos() = cur.lastpos();
+	if (cur.pit() > cur.lastpit())
+		cur.pit() = cur.lastpit();
+	if (cur.pos() > cur.lastpos())
+		cur.pos() = cur.lastpos();
 	cur.clearSelection();
 }
 
