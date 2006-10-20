18ae289343da96e5394ca9cd21cc8abd1c4bfcd6
Lars Gullik Bjønnes <larsbj@gullik.org>
2006-10-20 09:41:11 +0000

    Abdels fix for pure virtual method called
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@15393 a592a061-630c-0410-9148-cb99ea01b6c8

--- src/lyx_main.C
+++ src/lyx_main.C
@@ -384,6 +384,9 @@ void LyX::prepareExit()
 			lyx::from_utf8(package().temp_dir()));
 		Alert::warning(_("Unable to remove temporary directory"), msg);
 	}
+
+        pimpl_->lyx_server_.reset();
+        pimpl_->lyx_socket_.reset();
 }
 
 
@@ -855,8 +858,11 @@ void LyX::emergencyCleanup() const
 	// a crash
 
 	pimpl_->buffer_list_.emergencyWriteAll();
-	if (lyx::use_gui)
+	if (lyx::use_gui) {
 		pimpl_->lyx_server_->emergencyCleanup();
+                pimpl_->lyx_server_.reset();
+                pimpl_->lyx_socket_.reset();
+        }
 }
 
 
