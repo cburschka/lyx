13ce016c7242516f92e53f9cfdacb12870c5d3d2
Bo Peng <bpeng@lyx.org>
2006-11-03 15:00:26 +0000

    update Bo Peng's credits. :-)
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@15711 a592a061-630c-0410-9148-cb99ea01b6c8

--- lib/generate_contributions.py
+++ lib/generate_contributions.py
@@ -892,7 +892,7 @@ contributers = [
                  "Re: Python version of configure script (preview version)",
                  "m=112681895510418",
                  "15 September 2005",
-                 u"Conversion of lib/configure to Python"),
+                 u"Conversion of all shell scripts to Python, session, view-source, auto-view features and scons build system."),
 
      contributer(u"Joacim Persson",
                  "sp2joap1 () ida ! his ! se",
