7b3e212744e532ce0fab58c7e6bc7e3fd180d79a
Jean-Marc Lasgouttes <lasgouttes@lyx.org>
2006-11-13 14:21:37 +0000

    update FSF address
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@15899 a592a061-630c-0410-9148-cb99ea01b6c8

--- src/frontends/controllers/ControlAboutlyx.C
+++ src/frontends/controllers/ControlAboutlyx.C
@@ -63,19 +63,19 @@ void ControlAboutlyx::getCredits(ostream & ss) const
 
 string const ControlAboutlyx::getCopyright() const
 {
-	return lyx::to_utf8(_("LyX is Copyright (C) 1995 by Matthias Ettrich,\n1995-2001 LyX Team"));
+	return lyx::to_utf8(_("LyX is Copyright (C) 1995 by Matthias Ettrich,\n1995-2006 LyX Team"));
 }
 
 
 string const ControlAboutlyx::getLicense() const
 {
-	return lyx::to_utf8(_("This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version."));
+	return lyx::to_utf8(_("LyX is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.\nSee the GNU General Public License for more details.\nYou should have received a copy of the GNU General Public License along with this program; if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA."));
 }
 
 
 string const ControlAboutlyx::getDisclaimer() const
 {
-	return lyx::to_utf8(_("LyX is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.\nSee the GNU General Public License for more details.\nYou should have received a copy of the GNU General Public License along with this program; if not, write to the Free Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA."));
+	return lyx::to_utf8(_("LyX is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.\nSee the GNU General Public License for more details.\nYou should have received a copy of the GNU General Public License along with this program; if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA."));
 }
 
 
