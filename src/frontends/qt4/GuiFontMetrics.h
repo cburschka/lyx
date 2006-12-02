88d16356f2926d57b261355857ccaac4c10b678e
Abdelrazak Younes <younes@lyx.org>
2006-12-02 21:55:28 +0000

    update comments.
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@16142 a592a061-630c-0410-9148-cb99ea01b6c8

--- src/frontends/qt4/GuiFontMetrics.h
+++ src/frontends/qt4/GuiFontMetrics.h
@@ -19,8 +19,6 @@
 #include <QFontMetrics>
 #include <QHash>
 
-// Starting with version 3.1.0, Qt/X11 does its own caching of
-// character width, so it is not necessary to provide ours.
 #if defined(Q_WS_MACX) || defined(Q_WS_WIN32)
 #define USE_LYX_FONTCACHE
 #endif
@@ -75,17 +73,13 @@ private:
 #ifdef USE_LYX_FONTCACHE
 
 	/// Cache of char widths
-	/** This cache adds 20Mo of memory to the LyX executable when
-	* loading UserGuide.lyx which contains a good number of fonts. If
-	* this turns out to be too much, we can switch to a \c QHash based
-	* solution.
-	**/
 	mutable QHash<char_type, int> width_cache_;
 
 	struct AscendDescend {
 		short int ascent;
 		short int descent;
 	};
+	/// Cache of char ascends and descends
 	mutable QHash<char_type, AscendDescend> metrics_cache_;
 	/// fill in \c metrics_cache_ at specified value.
 	void fillMetricsCache(char_type) const;
