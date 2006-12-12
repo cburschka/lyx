51839e89a7949360122aa15f8ea1a1a34e4c5cbb
Georg Baum <Georg.Baum@post.rwth-aachen.de>
2006-12-12 18:55:55 +0000

    Unify InsetLatexAccent output
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@16254 a592a061-630c-0410-9148-cb99ea01b6c8

--- src/tex2lyx/text.C
+++ src/tex2lyx/text.C
@@ -2088,7 +2088,7 @@ void parse_text(Parser & p, ostream & os, unsigned flags, bool outer,
 			 || t.cs() == "~" || t.cs() == "." || t.cs() == "=") {
 			// we need the trim as the LyX parser chokes on such spaces
 			context.check_layout(os);
-			os << "\n\\i \\" << t.cs() << "{"
+			os << "\\i \\" << t.cs() << "{"
 			   << trim(parse_text_snippet(p, FLAG_ITEM, outer, context), " ")
 			   << "}\n";
 		}
