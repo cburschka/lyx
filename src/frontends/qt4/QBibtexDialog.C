24e837d39031bb7d5ad91cc1d5dd3dd79185f821
Jürgen Spitzmüller <spitz@lyx.org>
2007-01-07 12:46:37 +0000

    * src/frontends/qt4/QBibtexDialog.C: sanitize button behaviour (bug 2895)
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@16570 a592a061-630c-0410-9148-cb99ea01b6c8

--- src/frontends/qt4/QBibtexDialog.C
+++ src/frontends/qt4/QBibtexDialog.C
@@ -74,7 +74,7 @@ QBibtexDialog::QBibtexDialog(QBibtex * form)
 	bcview->setOK(add_->addPB);
 	bcview->setCancel(add_->closePB);
 
-	add_->bibED->setValidator(new PathValidator(false, add_->bibED));
+	add_->bibED->setValidator(new PathValidator(true, add_->bibED));
 	addCheckedLineEdit(add_bc_.view(), add_->bibED, 0);
 
 	connect(add_->bibED, SIGNAL(textChanged(const QString&)),
@@ -87,7 +87,7 @@ QBibtexDialog::QBibtexDialog(QBibtex * form)
 		this, SLOT(addDatabase()));
 	connect(add_->bibLW, SIGNAL(itemActivated(QListWidgetItem *)),
 		add_, SLOT(accept()));
-	connect(add_->bibLW, SIGNAL(itemChanged(QListWidgetItem *)),
+	connect(add_->bibLW, SIGNAL(itemSelectionChanged()),
 		this, SLOT(availableChanged()));
 	connect(add_->browsePB, SIGNAL(clicked()),
 		this, SLOT(browseBibPressed()));
@@ -168,8 +168,8 @@ void QBibtexDialog::browseBibPressed()
 
 void QBibtexDialog::addPressed()
 {
-	add_->exec();
 	add_bc_.valid(false);
+	add_->exec();
 }
 
 
@@ -210,6 +210,7 @@ void QBibtexDialog::addDatabase()
 void QBibtexDialog::deletePressed()
 {
 	databaseLW->takeItem(databaseLW->currentRow());
+	form_->changed();
 }
 
 
@@ -222,7 +223,7 @@ void QBibtexDialog::databaseChanged()
 
 void QBibtexDialog::availableChanged()
 {
-	form_->changed();
+	add_bc_.valid(true);
 }
 
 
