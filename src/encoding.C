b619dbfedfdc8779ecfa586a14063bfe8ce2dbee
Georg Baum <Georg.Baum@post.rwth-aachen.de>
2007-02-16 07:50:15 +0000

    forgot this
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@17214 a592a061-630c-0410-9148-cb99ea01b6c8

--- src/encoding.C
+++ src/encoding.C
@@ -211,7 +211,7 @@ CharInfoMap unicodesymbols;
 Encoding::Encoding(string const & n, string const & l, string const & i)
 	: Name_(n), LatexName_(l), iconvName_(i)
 {
-	if (n == "utf8")
+	if (n == "utf8" || n == "utf8x")
 		// UTF8 can encode all 1<<20 + 1<<16 UCS4 code points
 		start_encodable_ = 0x110000;
 	else {
