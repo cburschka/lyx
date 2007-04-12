4760e2ad8658b279d69f3b4821131ab9d4b88988
Uwe Stöhr <uwestoehr@web.de>
2007-04-12 20:19:40 +0000

    fix for the math-matrix dialog image
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@17788 a592a061-630c-0410-9148-cb99ea01b6c8

--- development/scons/scons_manifest.py
+++ development/scons/scons_manifest.py
@@ -1522,6 +1522,7 @@ lib_images_files = Split('''
     dialog-show_character.xpm
     dialog-show_findreplace.xpm
     dialog-show_mathdelimiter.xpm
+    dialog-show_mathmatrix.xpm
     dialog-show_mathpanel.xpm
     dialog-show_print.xpm
     dialog-show_spellchecker.xpm
