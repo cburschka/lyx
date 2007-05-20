edd14e93f9b41239923bacf575a0e8d1e7ff5ad0
Uwe Stöhr <uwestoehr@web.de>
2007-05-20 14:06:34 +0000

    installer: better version for the launcher
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@18431 a592a061-630c-0410-9148-cb99ea01b6c8

--- development/Win32/packaging/AltInstaller/specials/Launcher/lyxLauncher.dpr
+++ development/Win32/packaging/AltInstaller/specials/Launcher/lyxLauncher.dpr
@@ -10,109 +10,28 @@ program lyxLauncher;
 {$APPTYPE CONSOLE}
 
 uses
-  Windows, SysUtils, ShellApi, Forms, Dialogs;
+  Windows, SysUtils, ShellApi, Dialogs;
 
-var Path : string;
+var Path,FileName : string;
+    hConsole : THandle;
+
+
+procedure StartLyX(hConsole: THandle; FileName,Path: string);
+// starts LyX
+
+var Params : PChar;
     hLyX : THandle;
-
-procedure HideWindow(ProgWin: string);
-// hides a given program window
-var
-  Handle : THandle;
 begin
 
- // find handle of the program window
- // Repeat until the handle is available
- // because Lyx needs some time to start
- Repeat
-  Handle := FindWindow(nil,Pchar(ProgWin));
- Until Handle <> 0;
- // minimize the window
- // SendMessage(Handle, WM_SYSCOMMAND, SC_MINIMIZE, 1);
- // hide the window from taskbar
- ShowWindow(Handle, SW_HIDE);
-
-end; //end procedure
-
-procedure ReadPath(FileName: string; LaunchName: string;
-                   ExecName: string; var PathR: string);
-// reads the path to the lyx.exe from a given text file
-var InString : string;
-    FileSize,Index,Last : Int64;
-    hFile : THandle;
-    PInString : PChar;
-
-begin
-
- try //try..except for procedure
-  // open the text file
-  hFile:= Windows.CreateFile(PChar(FileName),GENERIC_READ,0,nil,
-                            OPEN_EXISTING,
-                            FILE_FLAG_SEQUENTIAL_SCAN,0);
-  if hFile= INVALID_HANDLE_VALUE then
-  begin
-   MessageDlg('The file "' + FileName + '" could not be found!',
-               mtError,[mbOK],0);
-   exit;
-  end;
-  
-  try //try..finally for hFile
-   FileSize:= FileSeek(hFile,0,2); //get file size
-   if FileSize = -1 then
-    RaiseLastOSError;
-
-   //move file content to InString
-   FileSeek(hFile,0,0);
-   SetLength(InString,FileSize);
-   PInString:= PChar(InString);
-   FileRead(hFile,PInString^,FileSize);
-
-   //search the string backwards for the first appearance of ":"
-   Index:= FileSize;
-   Repeat
-    dec(Index);
-    if InString[Index] = ':' then
-     Break;
-   Until (InString[Index] = #10) or (Index = 1);
-
-   //if the last line of lyx.bat doesn't contain a ":" (a path)
-   if (InString[Index] = #10) or (Index = 1) then
-   begin
-    MessageDlg('The file lyx.bat is corrupted!',mtError,[mbOK],0);
-    exit;
-   end;
-
-   //jump before the ":" to the drive letter
-   dec(Index);
-   //search for the LaunchName = end of the path
-   Last:= Pos(LaunchName,InString);
-   //the InString contains between Index and Last the wanted path
-   PathR:= Copy(InString,Index,Last - Index);
-   //attach LyX's executable to the path
-   PathR:= Path + ExecName;
-
-  finally //close the text file
-   Windows.CloseHandle(hFile);
-  end; //end finally
-
- except //when an error occurred somewhere in the procedure
-  MessageDlg('The file "' + FileName + '" is corrupted!',mtError,[mbOK],0);
- end; //end except
-
-end; //end procedure
-
-
-begin //begin program
-
- //hide the window of this application
- ShowWindow(Application.Handle,SW_HIDE);
-
- // read path of the lyxLauncher.exe from the file lyx.bat 
- ReadPath('lyx.bat', 'lyxLauncher.exe', 'lyx.exe', Path);
+ // if a filename is given, convert it to a PChar; needed for the ShellExecute
+ if FileName <> '' then
+  Params:= PChar(FileName)
+ else
+  Params:= nil;
 
  // start LyX
- hLyX:= ShellExecute(Application.Handle,PChar('open'),
-                     PChar(Path),nil,nil,SW_SHOWNORMAL);
+ hLyX:= ShellExecute(hConsole,PChar('open'),
+                     PChar(Path),Params,nil,SW_SHOWNORMAL);
  if hLyX = ERROR_FILE_NOT_FOUND  then
  begin
   MessageDLG('The file'#13#10 + Path + #13#10
@@ -125,6 +44,55 @@ begin //begin program
              mtError,[mbOK],0);
   exit;
  end;
+
+end; // end procedure
+
+
+procedure HideWindow(ProgWin: string);
+// hides a given program window
+
+var Handle : THandle;
+begin
+
+ // find handle of the program window
+ // Repeat until the handle is available
+ // because Lyx needs some time to start
+ Repeat
+  Sleep(1000); // wait 1 second to give LyX time to open
+  Handle := FindWindow(nil,Pchar(ProgWin));
+ Until Handle <> 0;
+
+ // hide the window from taskbar
+ ShowWindow(Handle, SW_HIDE);
+
+end; //end procedure
+
+
+begin //begin program
+
+ //Read path to this application
+ Path:= ParamStr(0);
+
+ //get handle of this console window
+ // This application is called by the lyx.bat with the name "LyX"
+ hConsole := FindWindow(nil,Pchar('LyX'));
+ // hide the window of this console application
+ ShowWindow(hConsole,SW_HIDE);
+
+ // do the same for the real name of this console application
+ // because it depends on the computer speed if the "LyX" console window
+ // was closed before it could be processed
+ hConsole := FindWindow(nil,Pchar(Path));
+ ShowWindow(hConsole,SW_HIDE);
+
+ // replace in the path "lyxLauncher.exe" by "lyx.exe"
+ Path:= StringReplace(Path, 'lyxLauncher', 'lyx', [rfIgnoreCase]);
+
+ // read given filename of a LyX-document
+ FileName:= ParamStr(1);
+
+ // start LyX
+ StartLyX(hConsole,FileName,Path);
 
  // hide console window of lyx.exe
  HideWindow(Path);
