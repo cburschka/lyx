e0e66da204e03a7f354ec8020fb19e781b18b067
Jürgen Spitzmüller <spitz@lyx.org>
2007-06-07 17:31:08 +0000

    * src/Thesaurus.C: 	- check for aiksaurus errors (fix bug 2691)
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/branches/BRANCH_1_4_X@18700 a592a061-630c-0410-9148-cb99ea01b6c8

--- src/Thesaurus.C
+++ src/Thesaurus.C
@@ -12,12 +12,19 @@
 
 #include "Thesaurus.h"
 
+#include "gettext.h"
+
+#include "support/lstrings.h"
+
+#include "frontends/Alert.h"
+
 #include <algorithm>
 #include <string>
 
 using std::string;
 
 #ifdef HAVE_LIBAIKSAURUS
+using lyx::support::bformat;
 
 using std::sort;
 
@@ -37,6 +44,17 @@ Thesaurus::Meanings Thesaurus::lookup(string const & text)
 {
 	Meanings meanings;
 
+	string error = string(aik_->error());
+	if (!error.empty()) {
+		static bool sent_error = false;
+		if (!sent_error) {
+			Alert::error(_("Thesaurus failure"),
+				     bformat(_("Aiksaurus returned the following error:\n\n%1$s."),
+					     error));
+			sent_error = true;
+		}
+		return meanings;
+	}
 	if (!aik_->find(text.c_str()))
 		return meanings;
 
