cburschka/lyx

commit 8153ea8fa728e06a709e4147d4fcf977c5cdf513

Show corners of mathed with mouse hover, details see bug 3825
git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@18701 a592a061-630c-0410-9148-cb99ea01b6c8

src/insets/Inset.cpp
@@ -280,37 +280,6 @@ void Inset::metricsMarkers2(Dimension & dim, int framesize) const
 }
 
 
-void Inset::drawMarkers(PainterInfo & pi, int x, int y) const
-{
-	Color::color pen_color = editing(pi.base.bv)?
-		Color::mathframe : Color::mathcorners;
-
-	int const t = x + width() - 1;
-	int const d = y + descent();
-	pi.pain.line(x, d - 3, x, d, pen_color);
-	pi.pain.line(t, d - 3, t, d, pen_color);
-	pi.pain.line(x, d, x + 3, d, pen_color);
-	pi.pain.line(t - 3, d, t, d, pen_color);
-	setPosCache(pi, x, y);
-}
-
-
-void Inset::drawMarkers2(PainterInfo & pi, int x, int y) const
-{
-	Color::color pen_color = editing(pi.base.bv)?
-		Color::mathframe : Color::mathcorners;
-
-	drawMarkers(pi, x, y);
-	int const t = x + width() - 1;
-	int const a = y - ascent();
-	pi.pain.line(x, a + 3, x, a, pen_color);
-	pi.pain.line(t, a + 3, t, a, pen_color);
-	pi.pain.line(x, a, x + 3, a, pen_color);
-	pi.pain.line(t - 3, a, t, a, pen_color);
-	setPosCache(pi, x, y);
-}
-
-
 bool Inset::editing(BufferView * bv) const
 {
 	return bv->cursor().isInside(this);

src/insets/Inset.h
@@ -130,10 +130,6 @@ public:
 	/// draw inset decoration if necessary.
 	/// This can use \c drawMarkers() for example.
 	virtual void drawDecoration(PainterInfo &, int, int) const {}
-	/// draw four angular markers
-	void drawMarkers(PainterInfo & pi, int x, int y) const;
-	/// draw two angular markers
-	void drawMarkers2(PainterInfo & pi, int x, int y) const;
 	/// add space for markers
 	void metricsMarkers(Dimension & dim, int framesize = 1) const;
 	/// add space for markers

src/mathed/InsetMathNest.cpp
@@ -74,12 +74,13 @@ using std::istringstream;
 
 
 InsetMathNest::InsetMathNest(idx_type nargs)
-	: cells_(nargs), lock_(false)
+	: cells_(nargs), lock_(false), mouse_hover_(false)
 {}
 
 
 InsetMathNest::InsetMathNest(InsetMathNest const & inset)
-	: InsetMath(inset), cells_(inset.cells_), lock_(inset.lock_)
+	: InsetMath(inset), cells_(inset.cells_), lock_(inset.lock_),
+	  mouse_hover_(false)
 {}
 
 
@@ -87,6 +88,7 @@ InsetMathNest & InsetMathNest::operator=(InsetMathNest const & inset)
 {
 	cells_ = inset.cells_;
 	lock_ = inset.lock_;
+	mouse_hover_ = false;
 	InsetMath::operator=(inset);
 	return *this;
 }
@@ -355,6 +357,44 @@ int InsetMathNest::latex(Buffer const &, odocstream & os,
 	WriteStream wi(os, runparams.moving_arg, true);
 	write(wi);
 	return wi.line();
+}
+
+
+void InsetMathNest::drawMarkers(PainterInfo & pi, int x, int y) const
+{
+	Color::color pen_color = mouse_hover_ || editing(pi.base.bv) ?
+		Color::mathframe : Color::mathcorners;
+
+	int const t = x + width() - 1;
+	int const d = y + descent();
+	pi.pain.line(x, d - 3, x, d, pen_color);
+	pi.pain.line(t, d - 3, t, d, pen_color);
+	pi.pain.line(x, d, x + 3, d, pen_color);
+	pi.pain.line(t - 3, d, t, d, pen_color);
+	setPosCache(pi, x, y);
+}
+
+
+void InsetMathNest::drawMarkers2(PainterInfo & pi, int x, int y) const
+{
+	Color::color pen_color = mouse_hover_ || editing(pi.base.bv)?
+		Color::mathframe : Color::mathcorners;
+
+	drawMarkers(pi, x, y);
+	int const t = x + width() - 1;
+	int const a = y - ascent();
+	pi.pain.line(x, a + 3, x, a, pen_color);
+	pi.pain.line(t, a + 3, t, a, pen_color);
+	pi.pain.line(x, a, x + 3, a, pen_color);
+	pi.pain.line(t - 3, a, t, a, pen_color);
+	setPosCache(pi, x, y);
+}
+
+
+bool InsetMathNest::setMouseHover(bool mouse_hover)
+{
+	mouse_hover_ = mouse_hover;
+	return true;
 }
 
 

src/mathed/InsetMathNest.h
@@ -108,6 +108,13 @@ public:
 
 	/// This signal is emitted when the inset is destroyed.
 	boost::signal<void()> * destroyedSignal() { return &destroyed; }
+	
+	/// draw four angular markers
+	void drawMarkers(PainterInfo & pi, int x, int y) const;
+	/// draw two angular markers
+	void drawMarkers2(PainterInfo & pi, int x, int y) const;
+	///
+	bool setMouseHover(bool mouse_hover);
 
 protected:
 	///
@@ -156,6 +163,8 @@ protected:
 	cells_type cells_;
 	/// if the inset is locked, it can't be entered with the cursor
 	bool lock_;
+	///
+	bool mouse_hover_;
 
 private:
 	/// This signal is emitted when the inset is destroyed.

commit 358e7136f2af787e64cf7c2253d002351db580ad

Fix compiling errors caused by r18701 (moving drawMarkers and drawMarkers2 back to Inset)
git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@18708 a592a061-630c-0410-9148-cb99ea01b6c8

src/insets/Inset.cpp
@@ -280,6 +280,37 @@ void Inset::metricsMarkers2(Dimension & dim, int framesize) const
 }
 
 
+void Inset::drawMarkers(PainterInfo & pi, int x, int y) const
+{
+	Color::color pen_color = mouseHovered() || editing(pi.base.bv)?
+		Color::mathframe : Color::mathcorners;
+
+	int const t = x + width() - 1;
+	int const d = y + descent();
+	pi.pain.line(x, d - 3, x, d, pen_color);
+	pi.pain.line(t, d - 3, t, d, pen_color);
+	pi.pain.line(x, d, x + 3, d, pen_color);
+	pi.pain.line(t - 3, d, t, d, pen_color);
+	setPosCache(pi, x, y);
+}
+
+
+void Inset::drawMarkers2(PainterInfo & pi, int x, int y) const
+{
+	Color::color pen_color = mouseHovered() || editing(pi.base.bv)?
+		Color::mathframe : Color::mathcorners;
+
+	drawMarkers(pi, x, y);
+	int const t = x + width() - 1;
+	int const a = y - ascent();
+	pi.pain.line(x, a + 3, x, a, pen_color);
+	pi.pain.line(t, a + 3, t, a, pen_color);
+	pi.pain.line(x, a, x + 3, a, pen_color);
+	pi.pain.line(t - 3, a, t, a, pen_color);
+	setPosCache(pi, x, y);
+}
+
+
 bool Inset::editing(BufferView * bv) const
 {
 	return bv->cursor().isInside(this);

src/insets/Inset.h
@@ -130,6 +130,10 @@ public:
 	/// draw inset decoration if necessary.
 	/// This can use \c drawMarkers() for example.
 	virtual void drawDecoration(PainterInfo &, int, int) const {}
+	/// draw four angular markers
+	void drawMarkers(PainterInfo & pi, int x, int y) const;
+	/// draw two angular markers
+	void drawMarkers2(PainterInfo & pi, int x, int y) const;
 	/// add space for markers
 	void metricsMarkers(Dimension & dim, int framesize = 1) const;
 	/// add space for markers
@@ -195,6 +199,12 @@ public:
 	/// is called when the mouse enter or leave this inset
 	/// return true if this inset needs repaint
 	virtual bool setMouseHover(bool) { return false; }
+	/// return true if this inset is hovered (under mouse)
+	/// This is by now only used by mathed to draw corners 
+	/// (Inset::drawMarkers() and Inset::drawMarkers2()).
+	/// Other insets do not have to redefine this function to 
+	/// return the correct status of mouseHovered.
+	virtual bool mouseHovered() const { return false; }
 
 	/// request "external features"
 	virtual void validate(LaTeXFeatures &) const {}

src/mathed/InsetMathNest.cpp
@@ -360,37 +360,6 @@ int InsetMathNest::latex(Buffer const &, odocstream & os,
 }
 
 
-void InsetMathNest::drawMarkers(PainterInfo & pi, int x, int y) const
-{
-	Color::color pen_color = mouse_hover_ || editing(pi.base.bv) ?
-		Color::mathframe : Color::mathcorners;
-
-	int const t = x + width() - 1;
-	int const d = y + descent();
-	pi.pain.line(x, d - 3, x, d, pen_color);
-	pi.pain.line(t, d - 3, t, d, pen_color);
-	pi.pain.line(x, d, x + 3, d, pen_color);
-	pi.pain.line(t - 3, d, t, d, pen_color);
-	setPosCache(pi, x, y);
-}
-
-
-void InsetMathNest::drawMarkers2(PainterInfo & pi, int x, int y) const
-{
-	Color::color pen_color = mouse_hover_ || editing(pi.base.bv)?
-		Color::mathframe : Color::mathcorners;
-
-	drawMarkers(pi, x, y);
-	int const t = x + width() - 1;
-	int const a = y - ascent();
-	pi.pain.line(x, a + 3, x, a, pen_color);
-	pi.pain.line(t, a + 3, t, a, pen_color);
-	pi.pain.line(x, a, x + 3, a, pen_color);
-	pi.pain.line(t - 3, a, t, a, pen_color);
-	setPosCache(pi, x, y);
-}
-
-
 bool InsetMathNest::setMouseHover(bool mouse_hover)
 {
 	mouse_hover_ = mouse_hover;

src/mathed/InsetMathNest.h
@@ -109,12 +109,10 @@ public:
 	/// This signal is emitted when the inset is destroyed.
 	boost::signal<void()> * destroyedSignal() { return &destroyed; }
 	
-	/// draw four angular markers
-	void drawMarkers(PainterInfo & pi, int x, int y) const;
-	/// draw two angular markers
-	void drawMarkers2(PainterInfo & pi, int x, int y) const;
 	///
 	bool setMouseHover(bool mouse_hover);
+	///
+	bool mouseHovered() const { return mouse_hover_; }
 
 protected:
 	///