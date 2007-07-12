0234b98b10b762192f4e2687f282974fc166e965
Jean-Marc Lasgouttes <lasgouttes@lyx.org>
2007-07-12 14:25:24 +0000

    2007-07-12  André Pönitz  <poenitz@lyx.org>
    	* InsetMathOverset.cpp (draw): fix drawing when the first argument
    	has a non-zero descent.
    
    
    
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@19060 a592a061-630c-0410-9148-cb99ea01b6c8

--- src/mathed/InsetMathOverset.cpp
+++ src/mathed/InsetMathOverset.cpp
@@ -49,7 +49,7 @@ bool InsetMathOverset::metrics(MetricsInfo & mi, Dimension & dim) const
 void InsetMathOverset::draw(PainterInfo & pi, int x, int y) const
 {
 	int m  = x + width() / 2;
-	int yo = y - cell(1).ascent() + cell(0).descent() - 1;
+	int yo = y - cell(1).ascent() - cell(0).descent() - 1;
 	cell(1).draw(pi, m - cell(1).width() / 2, y);
 	FracChanger dummy(pi.base);
 	cell(0).draw(pi, m - cell(0).width() / 2, yo);
