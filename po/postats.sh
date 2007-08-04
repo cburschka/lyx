64a52bf76b206b2df8dea3443a3cda01bf282d4f
Jean-Marc Lasgouttes <lasgouttes@lyx.org>
2007-08-04 12:48:41 +0000

    LyX 1.5.1 is coming
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/branches/BRANCH_1_5_X@19304 a592a061-630c-0410-9148-cb99ea01b6c8

--- po/postats.sh
+++ po/postats.sh
@@ -18,7 +18,7 @@
 # modifiy this when you change version
 # Note that an empty lyx_branch variable (ie cvs HEAD)
 # will "do the right thing".
-lyx_version=1.5.0svn
+lyx_version=1.5.1
 lyx_branch=
 
 
