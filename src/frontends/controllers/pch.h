12b09d52517972bdc91ba23917e6687c100e6b42
André Pönitz <poenitz@gmx.net>
2007-08-11 09:36:14 +0000

    merge controllers/Makefile.am and controllers/tests/Makefile.am
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@19423 a592a061-630c-0410-9148-cb99ea01b6c8

--- src/frontends/controllers/pch.h
+++ src/frontends/controllers/pch.h
@@ -1,16 +1,37 @@
 #include <config.h>
 
+#include <boost/assert.hpp>
+#include <boost/crc.hpp>
 #include <boost/cregex.hpp>
+// #include <boost/format.hpp> // mult def symbols problem (_1,_2 etc)
 #include <boost/regex.hpp>
+#include <boost/scoped_array.hpp>
 #include <boost/scoped_ptr.hpp>
 #include <boost/shared_ptr.hpp>
+#include <boost/signal.hpp>
+#include <boost/signals/trackable.hpp>
+#include <boost/version.hpp>
+#include <boost/tokenizer.hpp>
 #include <boost/utility.hpp>
 
 #include <algorithm>
+#include <cerrno>
+#include <csignal>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <ctime>
+#include <deque>
 #include <fstream>
+#include <functional>
 #include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
 #include <list>
 #include <map>
+#include <ostream>
+#include <queue>
 #include <sstream>
 #include <string>
 #include <utility>
