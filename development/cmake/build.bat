83d0919c21e6294db4de41fbf1d11b8e4e03ac1d
Peter Kümmel <syntheticpp@gmx.net>
2007-08-18 12:35:02 +0000

    add batch file to completely rebuild a release version
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@19629 a592a061-630c-0410-9148-cb99ea01b6c8

---
+++ development/cmake/build.bat
@@ -0,0 +1,11 @@
+:: be sure there is a solution
+cmake ..\trunk\development\cmake
+
+:: clean
+devenv lyx-qt4.sln /clean Release
+
+:: rebuild all generated files
+cmake ..\trunk\development\cmake -Dmerge_rebuild=1
+
+:: build release version
+devenv lyx-qt4.sln /build Release
