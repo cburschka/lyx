6de44e53f579283dd44ea32e268a5b27167afd61
Abdelrazak Younes <younes@lyx.org>
2007-08-27 09:31:38 +0000

    Fix crash when inserting float.
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/branches/BRANCH_1_5_X@19828 a592a061-630c-0410-9148-cb99ea01b6c8

--- src/Text3.cpp
+++ src/Text3.cpp
@@ -1277,7 +1277,11 @@ void Text::dispatch(Cursor & cur, FuncRequest & cmd)
 		// reposition the cursor to the caption
 		cur.pit() = cap_pit;
 		cur.pos() = 0;
-		cur.dispatch(FuncRequest(LFUN_CAPTION_INSERT));
+		// FIXME: This Text/Cursor dispatch handling is a mess!
+		// We cannot use Cursor::dispatch here it needs access to up to
+		// date metrics.
+		cur.text()->dispatch(cur, FuncRequest(LFUN_CAPTION_INSERT));
+		cur.updateFlags(Update::Force);
 		// FIXME: When leaving the Float (or Wrap) inset we should
 		// delete any empty paragraph left above or below the
 		// caption.
