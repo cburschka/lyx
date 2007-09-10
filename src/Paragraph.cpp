e93b4dc5293dbe90a62ce3a90cdf77029345a2d0
Jürgen Spitzmüller <spitz@lyx.org>
2007-09-10 13:44:49 +0000

    * src/Paragraph.cpp (getLabelWidthString): 	- don't judge possibility of configurable label string by the existence of such, 	  but rather by the margintype (bug 4127)
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/branches/BRANCH_1_5_X@20187 a592a061-630c-0410-9148-cb99ea01b6c8

--- src/Paragraph.cpp
+++ src/Paragraph.cpp
@@ -1626,7 +1626,7 @@ docstring const & Paragraph::getLabelstring() const
 // the next two functions are for the manual labels
 docstring const Paragraph::getLabelWidthString() const
 {
-	if (!params().labelWidthString().empty())
+	if (layout()->margintype == MARGIN_MANUAL)
 		return params().labelWidthString();
 	else
 		return _("Senseless with this layout!");
