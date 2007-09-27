cburschka/lyx

commit 212386be8a1573fb52b5da718961835816a3c8e2

merge ButtonController and its view (Qt2BC in this case)
git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@20018 a592a061-630c-0410-9148-cb99ea01b6c8

src/frontends/qt4/ButtonController.cpp
@@ -1,19 +1,16 @@
 /**
- * \file Qt2BC.cpp
+ * \file ButtonController.cpp
  * This file is part of LyX, the document processor.
  * Licence details can be found in the file COPYING.
  *
  * \author Allan Rae
- * \author Angus Leeming
  *
  * Full author contact details are available in file CREDITS.
  */
 
 #include <config.h>
 
-#include "Qt2BC.h"
-#include "BCView.h"
-#include "ButtonPolicy.h"
+#include "ButtonController.h"
 #include "debug.h"
 #include "qt_helpers.h"
 
@@ -22,17 +19,79 @@
 #include <QLabel>
 #include <QValidator>
 
-
 namespace lyx {
 namespace frontend {
 
-
-Qt2BC::Qt2BC(ButtonController & parent)
-	: BCView(parent), okay_(0), apply_(0), cancel_(0), restore_(0)
+ButtonController::ButtonController()
+	: okay_(0), apply_(0), cancel_(0), restore_(0),
+		policy_(ButtonPolicy::IgnorantPolicy)
 {}
 
 
-void Qt2BC::refresh() const
+void ButtonController::setPolicy(ButtonPolicy::Policy policy)
+{
+	policy_ = ButtonPolicy(policy);
+}
+
+
+void ButtonController::ok()
+{
+	input(ButtonPolicy::SMI_OKAY);
+}
+
+
+void ButtonController::input(ButtonPolicy::SMInput in)
+{
+	if (ButtonPolicy::SMI_NOOP == in)
+		return;
+	policy_.input(in);
+	refresh();
+}
+
+
+void ButtonController::apply()
+{
+	input(ButtonPolicy::SMI_APPLY);
+}
+
+
+void ButtonController::cancel()
+{
+	input(ButtonPolicy::SMI_CANCEL);
+}
+
+
+void ButtonController::restore()
+{
+	input(ButtonPolicy::SMI_RESTORE);
+}
+
+
+void ButtonController::hide()
+{
+	input(ButtonPolicy::SMI_HIDE);
+}
+
+
+void ButtonController::setValid(bool v)
+{
+	input(v ? ButtonPolicy::SMI_VALID : ButtonPolicy::SMI_INVALID);
+}
+
+
+bool ButtonController::setReadOnly(bool ro)
+{
+	LYXERR(Debug::GUI) << "Setting controller ro: " << ro << std::endl;
+
+	policy_.input(ro ?
+		ButtonPolicy::SMI_READ_ONLY : ButtonPolicy::SMI_READ_WRITE);
+	refreshReadOnly();
+	refresh();
+	return ro;
+}
+
+
+void ButtonController::refresh() const
 {
 	LYXERR(Debug::GUI) << "Calling BC refresh()" << std::endl;
 
@@ -40,21 +99,21 @@ void Qt2BC::refresh() const
 
 	if (okay_) {
 		bool const enabled =
-			all_valid && bp().buttonStatus(ButtonPolicy::OKAY);
+			all_valid && policy().buttonStatus(ButtonPolicy::OKAY);
 		okay_->setEnabled(enabled);
 	}
 	if (apply_) {
 		bool const enabled =
-			all_valid && bp().buttonStatus(ButtonPolicy::APPLY);
+			all_valid && policy().buttonStatus(ButtonPolicy::APPLY);
 		apply_->setEnabled(enabled);
 	}
 	if (restore_) {
 		bool const enabled =
-			all_valid && bp().buttonStatus(ButtonPolicy::RESTORE);
+			all_valid && policy().buttonStatus(ButtonPolicy::RESTORE);
 		restore_->setEnabled(enabled);
 	}
 	if (cancel_) {
-		bool const enabled = bp().buttonStatus(ButtonPolicy::CANCEL);
+		bool const enabled = policy().buttonStatus(ButtonPolicy::CANCEL);
 		if (enabled)
 			cancel_->setText(toqstr(_("Cancel")));
 		else
@@ -63,12 +122,12 @@ void Qt2BC::refresh() const
 }
 
 
-void Qt2BC::refreshReadOnly() const
+void ButtonController::refreshReadOnly() const
 {
 	if (read_only_.empty())
 		return;
 
-	bool const enable = !bp().isReadOnly();
+	bool const enable = !policy().isReadOnly();
 
 	Widgets::const_iterator end = read_only_.end();
 	Widgets::const_iterator iter = read_only_.begin();
@@ -77,7 +136,7 @@ void Qt2BC::refreshReadOnly() const
 }
 
 
-void Qt2BC::setWidgetEnabled(QWidget * obj, bool enabled) const
+void ButtonController::setWidgetEnabled(QWidget * obj, bool enabled) const
 {
 	if (QLineEdit * le = qobject_cast<QLineEdit*>(obj))
 		le->setReadOnly(!enabled);
@@ -88,13 +147,13 @@ void Qt2BC::setWidgetEnabled(QWidget * obj, bool enabled) const
 }
 
 
-void Qt2BC::addCheckedLineEdit(QLineEdit * input, QWidget * label)
+void ButtonController::addCheckedLineEdit(QLineEdit * input, QWidget * label)
 {
 	checked_widgets.push_back(CheckedLineEdit(input, label));
 }
 
 
-bool Qt2BC::checkWidgets() const
+bool ButtonController::checkWidgets() const
 {
 	bool valid = true;
 
@@ -115,20 +174,12 @@ bool Qt2BC::checkWidgets() const
 //
 //////////////////////////////////////////////////////////////
 
-void addCheckedLineEdit(BCView & bcview, QLineEdit * input, QWidget * label)
-{
-	Qt2BC * bc = static_cast<Qt2BC *>(&bcview);
-	bc->addCheckedLineEdit(input, label);
-}
-
-
 static void setWarningColor(QWidget * widget)
 {
 	QPalette pal = widget->palette();
 	pal.setColor(QPalette::Active, QPalette::Foreground, QColor(255, 0, 0));
 	widget->setPalette(pal);
 }
-
 
 
 CheckedLineEdit::CheckedLineEdit(QLineEdit * input, QWidget * label)
@@ -152,7 +203,7 @@ bool CheckedLineEdit::check() const
 	else
 		setWarningColor(input_);
 
-	if (label_) {
+	if (!label_) {
 		if (valid)
 			label_->setPalette(QPalette());
 		else

src/frontends/controllers/ButtonController.cpp
@@ -1,114 +1,0 @@
-/**
- * \file ButtonController.cpp
- * This file is part of LyX, the document processor.
- * Licence details can be found in the file COPYING.
- *
- * \author Allan Rae
- *
- * Full author contact details are available in file CREDITS.
- */
-
-#include <config.h>
-
-#include "ButtonController.h"
-#include "BCView.h"
-#include "debug.h"
-
-namespace lyx {
-namespace frontend {
-
-BCView & ButtonController::view() const
-{
-	BOOST_ASSERT(view_.get());
-	return *view_.get();
-}
-
-void ButtonController::view(BCView * view)
-{
-	view_.reset(view);
-}
-
-
-void ButtonController::setPolicy(ButtonPolicy::Policy policy)
-{
-	policy_ = ButtonPolicy(policy);
-}
-
-
-void ButtonController::refresh() const
-{
-	view().refresh();
-}
-
-
-void ButtonController::refreshReadOnly() const
-{
-	view().refreshReadOnly();
-}
-
-
-void ButtonController::ok()
-{
-	input(ButtonPolicy::SMI_OKAY);
-}
-
-
-void ButtonController::input(ButtonPolicy::SMInput in)
-{
-	if (ButtonPolicy::SMI_NOOP == in)
-		return;
-	policy_.input(in);
-	view().refresh();
-}
-
-
-void ButtonController::apply()
-{
-	input(ButtonPolicy::SMI_APPLY);
-}
-
-
-void ButtonController::cancel()
-{
-	input(ButtonPolicy::SMI_CANCEL);
-}
-
-
-void ButtonController::restore()
-{
-	input(ButtonPolicy::SMI_RESTORE);
-}
-
-
-void ButtonController::hide()
-{
-	input(ButtonPolicy::SMI_HIDE);
-}
-
-
-void ButtonController::valid(bool v)
-{
-	if (v) {
-		input(ButtonPolicy::SMI_VALID);
-	} else {
-		input(ButtonPolicy::SMI_INVALID);
-	}
-}
-
-
-bool ButtonController::readOnly(bool ro)
-{
-	LYXERR(Debug::GUI) << "Setting controller ro: " << ro << std::endl;
-
-	if (ro) {
-		policy_.input(ButtonPolicy::SMI_READ_ONLY);
-	} else {
-		policy_.input(ButtonPolicy::SMI_READ_WRITE);
-	}
-	view().refreshReadOnly();
-	view().refresh();
-	return ro;
-}
-
-} // namespace frontend
-} // namespace lyx

commit cf41e919eb35086b1506d71458f435364f2aad1c

ButtonController.cpp: fix a bug introduced in r20018: don't enable all widgets in the dialog, see http://www.mail-archive.com/lyx-devel@lists.lyx.org/msg128222.html
git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@20548 a592a061-630c-0410-9148-cb99ea01b6c8

src/frontends/qt4/ButtonController.cpp
@@ -85,7 +85,10 @@ bool ButtonController::setReadOnly(bool ro)
 
 	policy_.input(ro ?
 		ButtonPolicy::SMI_READ_ONLY : ButtonPolicy::SMI_READ_WRITE);
-	refreshReadOnly();
+	// refreshReadOnly(); This will enable all widgets in dialogs, no matter if
+	//                    they allowed to be enabled, so when you plan to
+	//                    reenable this call, read this before:
+    // http://www.mail-archive.com/lyx-devel@lists.lyx.org/msg128222.html
 	refresh();
 	return ro;
 }