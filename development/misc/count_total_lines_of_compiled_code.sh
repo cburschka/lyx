dba88f4ae6c6cdceeed4f9ff573b2df08ca2e7d6
André Pönitz <poenitz@gmx.net>
2007-09-30 18:45:11 +0000

    add a script to count compiled lines of code
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@20613 a592a061-630c-0410-9148-cb99ea01b6c8

---
+++ development/misc/count_total_lines_of_compiled_code.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+qt=/usr/include/qt4
+build=../../../build
+
+inc="-I$qt -I$qt/QtCore -I$qt/QtGui"
+inc="$inc -I$build/src"
+inc="$inc -I$build/src/frontends/qt4"
+inc="$inc -I../../boost"
+inc="$inc -I../../src"
+inc="$inc -I../../src/frontends"
+inc="$inc -I../../src/frontends/controllers"
+inc="$inc -I../../src/frontends/qt4"
+
+s=0
+for i in `find ../.. -name *.cpp` ; do
+	#echo $i
+	#echo "g++ $inc -DQT_NO_STL -E $i"
+	l=`g++ $inc -DQT_NO_STL -E $i | wc -l`
+	s=$[s + l]
+	printf "%10d  %-40s\n" $l $i
+done
+echo "Total: $s"
+
