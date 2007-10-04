f95b09aa14669332a2b749c85923e01fb8982e54
Jürgen Spitzmüller <spitz@lyx.org>
2007-10-04 09:28:26 +0000

    * src/BufferView.cpp: 	- revert the singlepar optimization (r20567) 	  for the time being
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/branches/BRANCH_1_5_X@20728 a592a061-630c-0410-9148-cb99ea01b6c8

--- src/BufferView.cpp
+++ src/BufferView.cpp
@@ -1482,19 +1482,13 @@ void BufferView::updateMetrics(bool singlepar)
 
 	// If the paragraph metrics has changed, we can not
 	// use the singlepar optimisation.
-	if (singlepar) {
-		pit_type const bottom_pit = cursor_.bottom().pit();
-		int old_height = tm.parMetrics(bottom_pit).height();
+	if (singlepar
 		// In Single Paragraph mode, rebreak only
 		// the (main text, not inset!) paragraph containing the cursor.
 		// (if this paragraph contains insets etc., rebreaking will
 		// recursively descend)
-		tm.redoParagraph(bottom_pit);
-		// Paragraph height has changed so we cannot proceed to
-		// the singlePar optimisation.
-		if (tm.parMetrics(bottom_pit).height() != old_height)
-			singlepar = false;
-	}
+		&& tm.redoParagraph(cursor_.bottom().pit()))
+		singlepar = false;
 
 	pit_type const pit = anchor_ref_;
 	int pit1 = pit;
