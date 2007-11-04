f26f32155a5c6227af62c81ea3185d05cb3755a6
André Pönitz <poenitz@gmx.net>
2007-11-04 09:38:51 +0000

    numbers...
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@21416 a592a061-630c-0410-9148-cb99ea01b6c8

--- development/tools/count_lines_of_included_code.sh
+++ development/tools/count_lines_of_included_code.sh
@@ -1,37 +1,5 @@
-
-for i in \
-	boost/bind.hpp \
-	boost/array.hpp \
-	boost/assert.hpp \
-	boost/crc.hpp \
-	boost/cregex.hpp \
-	boost/current_function.hpp \
-	boost/function.hpp \
-	boost/iterator/indirect_iterator.hpp \
-	boost/scoped_array.hpp \
-	boost/scoped_ptr.hpp \
-	boost/shared_ptr.hpp \
-	boost/signal.hpp \
-	boost/signals/connection.hpp \
-	boost/signals/trackable.hpp \
-	boost/tokenizer.hpp \
-	boost/tuple/tuple.hpp \
-	boost/utility.hpp \
-	boost/version.hpp \
-	boost/signals/trackable.hpp \
-	string \
-	vector \
-	map \
-	list \
-	deque \
-	QObject \
-	QString \
-	QList \
-	QVector \
-	QMap \
-	QHash \
-	QRegExp
-do
+#!/bin/bash
+for i in "$@" ; do
 	echo "#include <$i>"  > 1.cpp
 	inc='-I/suse/usr/src/lyx/trunk/boost -I/usr/include/qt4/QtCore -I/usr/include/qt4'
 	l=`g++ $inc -DQT_NO_STL -E 1.cpp | wc -l`
