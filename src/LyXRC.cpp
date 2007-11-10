cburschka/lyx

commit 87bd0e79ad5fdbac1ee254601d28eb7cc5511a2b

Fix bug 2739: Option for alphabetical sorting of environments in the toolbar
git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@21536 a592a061-630c-0410-9148-cb99ea01b6c8

src/LyXRC.cpp
@@ -107,6 +107,7 @@ keyword_item lyxrcTags[] = {
 	{ "\\language_global_options", LyXRC::RC_LANGUAGE_GLOBAL_OPTIONS },
 	{ "\\language_package", LyXRC::RC_LANGUAGE_PACKAGE },
 	{ "\\language_use_babel", LyXRC::RC_LANGUAGE_USE_BABEL },
+	{ "\\sort_layouts", LyXRC::RC_SORT_LAYOUTS },
 	{ "\\load_session", LyXRC::RC_LOADSESSION },
 	{ "\\make_backup", LyXRC::RC_MAKE_BACKUP },
 	{ "\\mark_foreign_language", LyXRC::RC_MARK_FOREIGN_LANGUAGE },
@@ -269,6 +270,7 @@ void LyXRC::setDefaults() {
 	language_package = "\\usepackage{babel}";
 	language_command_begin = "\\selectlanguage{$$lang}";
 	language_command_local = "\\foreignlanguage{$$lang}{";
+	sort_layouts = false;
 	default_language = "english";
 	show_banner = true;
 	windows_style_tex_paths = false;
@@ -1178,6 +1180,11 @@ int LyXRC::read(Lexer & lexrc)
 					convert<unsigned int>(lexrc.getString());
 			break;
 
+		case RC_SORT_LAYOUTS:
+			if (lexrc.next())
+				sort_layouts = lexrc.getBool();
+			break;
+
 		case RC_LAST: break; // this is just a dummy
 		}
 	}
@@ -1340,7 +1347,14 @@ void LyXRC::write(ostream & os, bool ignore_system_lyxrc, string const & name) c
 		}
 		if (tag != RC_LAST)
 			break;
-
+	case RC_SORT_LAYOUTS:
+		if (ignore_system_lyxrc ||
+		    sort_layouts != system_lyxrc.sort_layouts) {
+			os << "# Sort layouts alphabetically.\n"
+			   << "\\sort_layouts " << convert<string>(sort_layouts) << '\n';
+		}
+		if (tag != RC_LAST)
+			break;
 	case RC_VIEWDVI_PAPEROPTION:
 		if (ignore_system_lyxrc ||
 		    view_dvi_paper_option

src/LyXFunc.cpp
@@ -2013,6 +2013,9 @@ void LyXFunc::dispatch(FuncRequest const & cmd)
 
 			actOnUpdatedPrefs(lyxrc_orig, lyxrc);
 
+			if (lyx_view_ && lyx_view_->buffer())
+				lyx_view_->updateLayoutChoice(true);
+
 			/// We force the redraw in any case because there might be
 			/// some screen font changes.
 			/// FIXME: only the current view will be updated. the Gui
@@ -2098,7 +2101,7 @@ void LyXFunc::dispatch(FuncRequest const & cmd)
 			theSelection().haveSelection(view()->cursor().selection());
 
 			if (view()->cursor().inTexted()) {
-				lyx_view_->updateLayoutChoice();
+				lyx_view_->updateLayoutChoice(false);
 			}
 		}
 	}
@@ -2587,6 +2590,7 @@ void actOnUpdatedPrefs(LyXRC const & lyxrc_orig, LyXRC const & lyxrc_new)
 	case LyXRC::RC_USE_PIXMAP_CACHE:
 	case LyXRC::RC_USE_SPELL_LIB:
 	case LyXRC::RC_VIEWDVI_PAPEROPTION:
+	case LyXRC::RC_SORT_LAYOUTS:
 	case LyXRC::RC_VIEWER:
 	case LyXRC::RC_LAST:
 		break;

src/LyXRC.h
@@ -79,6 +79,7 @@ public:
 		RC_LANGUAGE_GLOBAL_OPTIONS,
 		RC_LANGUAGE_PACKAGE,
 		RC_LANGUAGE_USE_BABEL,
+		RC_SORT_LAYOUTS,
 		RC_USELASTFILEPOS,
 		RC_LOADSESSION,
 		RC_MAKE_BACKUP,
@@ -375,6 +376,8 @@ public:
 	bool use_converter_cache;
 	/// The maximum age of cache files in seconds
 	unsigned int converter_cache_maxage;
+	/// Sort layouts alphabetically
+	bool sort_layouts;
 };
 
 

src/frontends/LyXView.h
@@ -141,7 +141,7 @@ public:
 	void setBuffer(Buffer * b); ///< \c Buffer to set.
 
 	/// updates the possible layouts selectable
-	virtual void updateLayoutChoice() = 0;
+	virtual void updateLayoutChoice(bool force) = 0;
 
 	/// update the toolbar
 	virtual void updateToolbars() = 0;

src/frontends/WorkArea.cpp
@@ -206,7 +206,7 @@ void WorkArea::dispatch(FuncRequest const & cmd0, KeyModifier mod)
 
 	// Skip these when selecting
 	if (cmd.action != LFUN_MOUSE_MOTION) {
-		lyx_view_->updateLayoutChoice();
+		lyx_view_->updateLayoutChoice(false);
 		lyx_view_->updateToolbars();
 	}
 
@@ -230,7 +230,7 @@ void WorkArea::resizeBufferView()
 	// We are already inside a paint event.
 	lyx_view_->setBusy(true);
 	buffer_view_->resize(width(), height());
-	lyx_view_->updateLayoutChoice();
+	lyx_view_->updateLayoutChoice(false);
 	lyx_view_->setBusy(false);
 }
 

src/frontends/qt4/GuiPrefs.cpp
@@ -1619,6 +1619,8 @@ PrefUserInterface::PrefUserInterface(GuiPreferences * form, QWidget * parent)
 		this, SIGNAL(changed()));
 	connect(cursorFollowsCB, SIGNAL(clicked()),
 		this, SIGNAL(changed()));
+	connect(sortEnvironmentsCB, SIGNAL(clicked()),
+		this, SIGNAL(changed()));
 	connect(autoSaveSB, SIGNAL(valueChanged(int)),
 		this, SIGNAL(changed()));
 	connect(autoSaveCB, SIGNAL(clicked()),
@@ -1645,6 +1647,7 @@ void PrefUserInterface::apply(LyXRC & rc) const
 	}
 	rc.geometry_xysaved = loadWindowLocationCB->isChecked();
 	rc.cursor_follows_scrollbar = cursorFollowsCB->isChecked();
+	rc.sort_layouts = sortEnvironmentsCB->isChecked();
 	rc.autosave = autoSaveSB->value() * 60;
 	rc.make_backup = autoSaveCB->isChecked();
 	rc.num_lastfiles = lastfilesSB->value();
@@ -1665,6 +1668,7 @@ void PrefUserInterface::update(LyXRC const & rc)
 	}
 	loadWindowLocationCB->setChecked(rc.geometry_xysaved);
 	cursorFollowsCB->setChecked(rc.cursor_follows_scrollbar);
+	sortEnvironmentsCB->setChecked(rc.sort_layouts);
 	// convert to minutes
 	int mins(rc.autosave / 60);
 	if (rc.autosave && !mins)

src/frontends/qt4/GuiToolbar.cpp
@@ -33,6 +33,7 @@
 #include "Action.h"
 #include "qt_helpers.h"
 #include "InsertTableWidget.h"
+#include "LyXRC.h"
 
 #include "support/filetools.h"
 #include "support/lstrings.h"
@@ -274,6 +275,29 @@ void GuiLayoutBox::set(docstring const & layout)
 }
 
 
+void GuiLayoutBox::addItemSort(QString const & item, bool sorted)
+{
+	int const end = count();
+	if (!sorted || end < 2 || item[0].category() != QChar::Letter_Uppercase) {
+		addItem(item);
+		return;
+	}
+
+	// Let the default one be at the beginning
+	int i = 1;
+	for (setCurrentIndex(i); currentText() < item;) {
+		// e.g. --Separator--
+		if (currentText()[0].category() != QChar::Letter_Uppercase)
+			break;
+		if (++i == end)
+			break;
+		setCurrentIndex(i);
+	}
+
+	insertItem(i, item);
+}
+
+
 void GuiLayoutBox::updateContents()
 {
 	TextClass const & tc = textClass(owner_);
@@ -285,8 +309,10 @@ void GuiLayoutBox::updateContents()
 	TextClass::const_iterator const end = tc.end();
 	for (; it != end; ++it) {
 		// ignore obsolete entries
-		addItem(toqstr(translateIfPossible((*it)->name())));
+		addItemSort(toqstr(translateIfPossible((*it)->name())), lyxrc.sort_layouts);
 	}
+
+	setCurrentIndex(0);
 
 	// needed to recalculate size hint
 	hide();

src/frontends/qt4/GuiToolbar.h
@@ -43,6 +43,8 @@ public:
 	void set(docstring const & layout);
 	/// Populate the layout combobox.
 	void updateContents();
+	/// Add Item to Layout box according to sorting settings from preferences
+	void addItemSort(QString const & item, bool sorted);
 
 private Q_SLOTS:
 	void selected(const QString & str);

commit 873ee236972111daf401762ac60a4b86cdada67a

* src/LyXRC.cpp (lyxrcTags[]): 	- fix ordering (broken in r21536).
git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@21543 a592a061-630c-0410-9148-cb99ea01b6c8

src/LyXRC.cpp
@@ -107,7 +107,6 @@ keyword_item lyxrcTags[] = {
 	{ "\\language_global_options", LyXRC::RC_LANGUAGE_GLOBAL_OPTIONS },
 	{ "\\language_package", LyXRC::RC_LANGUAGE_PACKAGE },
 	{ "\\language_use_babel", LyXRC::RC_LANGUAGE_USE_BABEL },
-	{ "\\sort_layouts", LyXRC::RC_SORT_LAYOUTS },
 	{ "\\load_session", LyXRC::RC_LOADSESSION },
 	{ "\\make_backup", LyXRC::RC_MAKE_BACKUP },
 	{ "\\mark_foreign_language", LyXRC::RC_MARK_FOREIGN_LANGUAGE },
@@ -154,6 +153,7 @@ keyword_item lyxrcTags[] = {
 	{ "\\serverpipe", LyXRC::RC_SERVERPIPE },
 	{ "\\set_color", LyXRC::RC_SET_COLOR },
 	{ "\\show_banner", LyXRC::RC_SHOW_BANNER },
+	{ "\\sort_layouts", LyXRC::RC_SORT_LAYOUTS },
 	{ "\\spell_command", LyXRC::RC_SPELL_COMMAND },
 	{ "\\tempdir_path", LyXRC::RC_TEMPDIRPATH },
 	{ "\\template_path", LyXRC::RC_TEMPLATEPATH },
@@ -176,7 +176,7 @@ keyword_item lyxrcTags[] = {
 	{ "\\user_name", LyXRC::RC_USER_NAME },
 	{ "\\view_dvi_paper_option", LyXRC::RC_VIEWDVI_PAPEROPTION },
 	// compatibility with versions older than 1.4.0 only
-	{ "\\viewer" ,LyXRC::RC_VIEWER}
+	{ "\\viewer", LyXRC::RC_VIEWER}
 };
 
 const int lyxrcCount = sizeof(lyxrcTags) / sizeof(keyword_item);