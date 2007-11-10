cburschka/lyx

commit c0a625149f7c01debc7c595e10c9ee8efe25ec69

* src/Text.cpp (getPossibleLabel): 	- remove line breaks from label proposals (fixes bug 4334)
git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/branches/BRANCH_1_5_X@21488 a592a061-630c-0410-9148-cb99ea01b6c8

src/Text.cpp
@@ -90,6 +90,7 @@ using support::bformat;
 using support::contains;
 using support::lowercase;
 using support::split;
+using support::rsplit;
 using support::uppercase;
 
 using cap::cutSelection;
@@ -1896,6 +1897,10 @@ docstring Text::getPossibleLabel(Cursor & cur) const
 
 	docstring text;
 	docstring par_text = pars_[pit].asString(cur.buffer(), false);
+	string piece;
+	// the return string of math matrices might contain linebreaks
+	// FIXME: unicode
+	par_text = from_utf8(rsplit(to_utf8(par_text), piece, '\n'));
 	for (int i = 0; i < lyxrc.label_init_length; ++i) {
 		if (par_text.empty())
 			break;

status.15x
@@ -94,6 +94,8 @@ What's new
 - Fix a crash when deleting a row or column of a math matrix while its
   content is selected (bug 4323).
 
+- Fix a crash when inserting a label after a math matrix (bug 4334).
+
 - Fix a crash when overwriting a selection that spans multiple paragraphs
   (bug 4317).
 

commit 77c1dbe3ba2b2ae5e628a228b95ec94b29c70b8f

* src/Text.cpp: 	- safer (but slightly less nice) fix for bug 4334. Fixes bug 4347 that was introduced in r21488.
git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/branches/BRANCH_1_5_X@21540 a592a061-630c-0410-9148-cb99ea01b6c8

src/Text.cpp
@@ -90,7 +90,7 @@ using support::bformat;
 using support::contains;
 using support::lowercase;
 using support::split;
-using support::rsplit;
+using support::subst;
 using support::uppercase;
 
 using cap::cutSelection;
@@ -1899,8 +1899,7 @@ docstring Text::getPossibleLabel(Cursor & cur) const
 	docstring par_text = pars_[pit].asString(cur.buffer(), false);
 	string piece;
 	// the return string of math matrices might contain linebreaks
-	// FIXME: unicode
-	par_text = from_utf8(rsplit(to_utf8(par_text), piece, '\n'));
+	par_text = subst(par_text, '\n', '-');
 	for (int i = 0; i < lyxrc.label_init_length; ++i) {
 		if (par_text.empty())
 			break;