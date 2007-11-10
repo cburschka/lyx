cburschka/lyx

commit cd4c58fb92374c116e4d5d700a500aca8bd34771

* src/Text.cpp (getPossibleLabel): 	- remove line breaks from label proposals (fixes bug 4334)
git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@21489 a592a061-630c-0410-9148-cb99ea01b6c8

src/Text.cpp
@@ -85,6 +85,7 @@ namespace lyx {
 
 using support::bformat;
 using support::contains;
+using support::rsplit;
 using support::split;
 
 using cap::cutSelection;
@@ -1274,6 +1275,10 @@ docstring Text::getPossibleLabel(Cursor & cur) const
 
 	docstring text;
 	docstring par_text = pars_[pit].asString(cur.buffer(), false);
+	string piece;
+	// the return string of math matrices might contain linebreaks
+	// FIXME: unicode
+	par_text = from_utf8(rsplit(to_utf8(par_text), piece, '\n'));
 	for (int i = 0; i < lyxrc.label_init_length; ++i) {
 		if (par_text.empty())
 			break;

commit 208feabc07e8191deb93c13ad118f59f4dc8987b

* src/Text.cpp: 	- safer (but slightly less nice) fix for bug 4334. Fixes bug 4347 that was introduced in r21489.
git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@21541 a592a061-630c-0410-9148-cb99ea01b6c8

src/Text.cpp
@@ -86,8 +86,8 @@ namespace lyx {
 
 using support::bformat;
 using support::contains;
-using support::rsplit;
 using support::split;
+using support::subst;
 
 using cap::cutSelection;
 using cap::pasteParagraphList;
@@ -1278,8 +1278,7 @@ docstring Text::getPossibleLabel(Cursor & cur) const
 	docstring par_text = pars_[pit].asString(cur.buffer(), false);
 	string piece;
 	// the return string of math matrices might contain linebreaks
-	// FIXME: unicode
-	par_text = from_utf8(rsplit(to_utf8(par_text), piece, '\n'));
+	par_text = subst(par_text, '\n', '-');
 	for (int i = 0; i < lyxrc.label_init_length; ++i) {
 		if (par_text.empty())
 			break;