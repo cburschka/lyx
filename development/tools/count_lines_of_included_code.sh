4dfc6f46c90ffb25d906719f2d96d949b1b76233
André Pönitz <poenitz@gmx.net>
2007-11-27 19:48:49 +0000

    add -I.
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@21822 a592a061-630c-0410-9148-cb99ea01b6c8

--- development/tools/count_lines_of_included_code.sh
+++ development/tools/count_lines_of_included_code.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 for i in "$@" ; do
 	echo "#include <$i>"  > 1.cpp
-	inc='-I/suse/usr/src/lyx/trunk/boost -I/usr/include/qt4/QtCore -I/usr/include/qt4'
+	inc='-I. -I/suse/usr/src/lyx/trunk/boost -I/usr/include/qt4/QtCore -I/usr/include/qt4'
 	l=`g++ $inc -DQT_NO_STL -E 1.cpp | wc -l`
 	printf "%-40s: %d\n" $i $l
 done
