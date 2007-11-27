8fc0bf6030db8a522139e933d506507f367e9832
Peter Kümmel <syntheticpp@gmx.net>
2007-11-27 20:01:23 +0000

    use the same mechanism to trigger moc as in frontends/qt4
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@21826 a592a061-630c-0410-9148-cb99ea01b6c8

---
+++ src/support/SignalSlotPrivate.cpp
@@ -0,0 +1,15 @@
+// -*- C++ -*-
+/**
+ * \file SignalSlotPrivate.cpp
+ * This file is part of LyX, the document processor.
+ * Licence details can be found in the file COPYING.
+ *
+ * \author André Pönitz
+ *
+ * Full author contact details are available in file CREDITS.
+ */
+
+
+// trigger moc 
+#include "SignalSlotPrivate_moc.cpp"
+
