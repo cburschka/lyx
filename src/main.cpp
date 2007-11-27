f7ab948b9f5a892003604b12ea0c165baa2b9d50
Abdelrazak Younes <younes@lyx.org>
2007-11-27 08:07:49 +0000

    The obligatory checking has been entirely removed since boost 1.34
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@21814 a592a061-630c-0410-9148-cb99ea01b6c8

--- src/main.cpp
+++ src/main.cpp
@@ -17,21 +17,15 @@
 
 #include "support/os.h"
 
-#include <boost/filesystem/path.hpp>
-
 #ifdef HAVE_IOS
 #include <ios>
 #endif
-
-
-namespace fs = boost::filesystem;
 
 int main(int argc, char * argv[])
 {
 #ifdef HAVE_IOS
 	std::ios_base::sync_with_stdio(false);
 #endif
-	fs::path::default_name_check(fs::no_check);
 
 	// To avoid ordering of global object problems with some
 	// stdlibs we do the initialization here, but still as
