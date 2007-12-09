dc9dcc9c1fb8361bd25848b4dca6c1300d81a7c4
Uwe Stöhr <uwestoehr@web.de>
2007-12-09 17:47:41 +0000

    set encoding from utf8 back to latin1
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@22047 a592a061-630c-0410-9148-cb99ea01b6c8

--- src/tex2lyx/preamble.cpp
+++ src/tex2lyx/preamble.cpp
@@ -3,8 +3,8 @@
  * This file is part of LyX, the document processor.
  * Licence details can be found in the file COPYING.
  *
- * \author AndrÃ© PÃ¶nitz
- * \author Uwe StÃ¶hr
+ * \author André Pönitz
+ * \author Uwe Stöhr
  *
  * Full author contact details are available in file CREDITS.
  */
