cburschka/lyx

commit 78caccb1c595e5c0b5f3bb2626ef9410b02fe1e1

cmake/merge: fix compile, this split conflicts with the others
git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@22137 a592a061-630c-0410-9148-cb99ea01b6c8

src/factory.cpp
@@ -363,7 +363,7 @@ Inset * createInset(Buffer & buf, FuncRequest const & cmd)
 		if (message.type_ == ErrorException) {
 			Alert::error(message.title_, message.details_);
 			LyX::cref().emergencyCleanup();
-			abort();
+			support::abort();
 		} else if (message.type_ == WarningException) {
 			Alert::warning(message.title_, message.details_);
 			return 0;

src/FuncRequest.cpp
@@ -70,7 +70,7 @@ mouse_button::state FuncRequest::button() const
 }
 
 
-void split(vector<string> & args, string const & str)
+void splitArg(vector<string> & args, string const & str)
 {
 	istringstream is(str);
 	while (is) {
@@ -93,7 +93,7 @@ void split(vector<string> & args, string const & str)
 string FuncRequest::getArg(unsigned int i) const
 {
 	vector<string> args;
-	split(args, to_utf8(argument_));
+	splitArg(args, to_utf8(argument_));
 	return i < args.size() ? args[i] : string();
 }
 

commit 397784e8535b358fd903f0d9f2d443bd631f88b8

factory.cpp: fix wrong call of abort() introduced in r22137 (I got:  factory.cpp(366) : error C2039: 'abort': Is no element of 'lyx::support')
git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@22140 a592a061-630c-0410-9148-cb99ea01b6c8

src/factory.cpp
@@ -363,7 +363,7 @@ Inset * createInset(Buffer & buf, FuncRequest const & cmd)
 		if (message.type_ == ErrorException) {
 			Alert::error(message.title_, message.details_);
 			LyX::cref().emergencyCleanup();
-			support::abort();
+			abort();
 		} else if (message.type_ == WarningException) {
 			Alert::warning(message.title_, message.details_);
 			return 0;