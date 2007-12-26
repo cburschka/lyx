6799fbc1c632ba569474da45528fd3ceb24e1e0c
Jürgen Spitzmüller <spitz@lyx.org>
2007-12-26 10:27:32 +0000

    * src/insets/InsetSpace.cpp: 	- further improve appearance of protected space.
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/branches/BRANCH_1_5_X@22306 a592a061-630c-0410-9148-cb99ea01b6c8

--- src/insets/InsetSpace.cpp
+++ src/insets/InsetSpace.cpp
@@ -90,7 +90,7 @@ void InsetSpace::draw(PainterInfo & pi, int x, int y) const
 
 	xp[0] = x;
 	yp[0] = y - max(h / 4, 1);
-	if (kind_ == NORMAL) {
+	if (kind_ == NORMAL || kind_ == PROTECTED) {
 		xp[1] = x;     yp[1] = y;
 		xp[2] = x + w; yp[2] = y;
 	} else {
