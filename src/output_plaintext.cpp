31496b4a165de131e646f3f5731dacf842ead383
Jürgen Spitzmüller <spitz@lyx.org>
2007-12-29 10:22:01 +0000

    * src/output_plaintext.cpp: 	- fix plaintext output. Abdel, you still should have a look at this.
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@22330 a592a061-630c-0410-9148-cb99ea01b6c8

--- src/output_plaintext.cpp
+++ src/output_plaintext.cpp
@@ -34,7 +34,8 @@ namespace lyx {
 void writePlaintextFile(Buffer const & buf, FileName const & fname,
 	OutputParams const & runparams)
 {
-	odocfstream ofs("UTF-8");
+	odocfstream ofs;
+	ofs << setEncoding("UTF-8");
 	if (!openFileWrite(ofs, fname))
 		return;
 	writePlaintextFile(buf, ofs, runparams);
