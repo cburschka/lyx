0a77e567250781e4d2a5c8574af936c1be847d16
Jürgen Spitzmüller <spitz@lyx.org>
2008-03-05 23:31:10 +0000

    * lib/configure.py: 	- Use verbose htlatex command instead of oolatex, if available, 	  since the latter caused problems on some systems (bug 4604).
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/branches/BRANCH_1_5_X@23495 a592a061-630c-0410-9148-cb99ea01b6c8

--- lib/configure.py
+++ lib/configure.py
@@ -380,9 +380,10 @@ def checkConverterEntries():
         rc_entry = [ r'\converter odt        latex      "%%"	""' ])
     # On SuSE the scripts have a .sh suffix, and on debian they are in /usr/share/tex4ht/
     # Both SuSE and debian have oolatex
-    checkProg('a LaTeX -> Open Document converter', ['oolatex $$i', 'oolatex.sh $$i', \
-        '/usr/share/tex4ht/oolatex $$i', \
-        'htlatex $$i \'xhtml,ooffice\' \'ooffice/! -cmozhtf\' \'-coo\' \'-cvalidate\''],
+    checkProg('a LaTeX -> Open Document converter', [
+        'htlatex $$i \'xhtml,ooffice\' \'ooffice/! -cmozhtf\' \'-coo\' \'-cvalidate\'', \
+	'oolatex $$i', 'oolatex.sh $$i', \
+        '/usr/share/tex4ht/oolatex $$i'],
         rc_entry = [ r'\converter latex      odt        "%%"	"needaux"' ])
     # On windows it is called latex2rt.exe
     checkProg('a LaTeX -> RTF converter', ['latex2rtf -p -S -o $$o $$i', 'latex2rt -p -S -o $$o $$i'],
