8705679ac8dcadc669469c01f9eabeb32196472c
André Pönitz <poenitz@gmx.net>
2008-04-12 15:11:59 +0000

    forgot this.
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@24249 a592a061-630c-0410-9148-cb99ea01b6c8

--- src/support/assert.h
+++ src/support/assert.h
@@ -1,1 +1,26 @@
-#define LASSERT(a,b)
+// -*- C++ -*-
+/**
+ * \file support/assert.h
+ *
+ * This file is part of LyX, the document processor.
+ * Licence details can be found in the file COPYING.
+ *
+ * \author André Pönitz
+ *
+ * Full author contact details are available in file CREDITS.
+ */
+
+#ifndef LASSERT_H
+#define LASSERT_H
+
+namespace lyx {
+
+void doAssert(char const * expr, char const * file, long line);
+
+} // namespace lyx
+
+#define LASSERT(expr, escape) \
+	if (expr) {} else { lyx::doAssert(#expr, __FILE__, __LINE__); escape; }
+
+
+#endif // LASSERT
