a805a50fd9428917c12f906846567abfa19a830a
Abdelrazak Younes <younes@lyx.org>
2008-05-19 08:00:15 +0000

    Fix crash in bug 4857. Sorry Jürgen, I don't have the time to track this issue. The patch is very short so I take the liberty to commit it directly. This is similar to what we do now in trunk and I don't know why this was still not done in branch (my fault). This (now fixed) bug reveals another problem though, just look at the table of contents; I think this is related to the bibitem inside the caption.
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/branches/BRANCH_1_5_X@24830 a592a061-630c-0410-9148-cb99ea01b6c8

--- src/insets/InsetCaption.cpp
+++ src/insets/InsetCaption.cpp
@@ -29,6 +29,7 @@
 #include "output_latex.h"
 #include "OutputParams.h"
 #include "Paragraph.h"
+#include "ParIterator.h"
 #include "paragraph_funcs.h"
 #include "TocBackend.h"
 
@@ -115,12 +116,14 @@ void InsetCaption::setCustomLabel(docstring const & label)
 }
 
 
-void InsetCaption::addToToc(TocList & toclist, Buffer const & buf, ParConstIterator const &) const
+void InsetCaption::addToToc(TocList & toclist, Buffer const & buf,
+	ParConstIterator const & cpit) const
 {
 	if (type_.empty())
 		return;
 
-	ParConstIterator pit = par_const_iterator_begin(*this);
+	ParConstIterator pit = cpit;
+	pit.push_back(CursorSlice(const_cast<InsetCaption &>(*this)));
 
 	Toc & toc = toclist[type_];
 	docstring const str = convert<docstring>(counter_)
