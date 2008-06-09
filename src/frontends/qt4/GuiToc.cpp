5e6a1a3da8086b6df19972c4711d5d92b5ea2b93
Abdelrazak Younes <younes@lyx.org>
2008-06-09 12:54:14 +0000

    GuiToc: set the object name for proper state restoration.
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@25206 a592a061-630c-0410-9148-cb99ea01b6c8

--- src/frontends/qt4/GuiToc.cpp
+++ src/frontends/qt4/GuiToc.cpp
@@ -35,6 +35,7 @@ namespace frontend {
 GuiToc::GuiToc(GuiView & parent, Qt::DockWidgetArea area, Qt::WindowFlags flags)
 	: DockView(parent, "toc", qt_("Outline"), area, flags)
 {
+	setObjectName("toc");
 	widget_ = new TocWidget(parent, this);
 	setWidget(widget_);
 }
