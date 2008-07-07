391c3234ff2766924e9bd3b22f65be3065624775
Pavel Sanda <sanda@lyx.org>
2008-07-07 19:16:13 +0000

    Removal of LFUN_SERVER_CHAR_AFTER. This lfun do *nothing*.
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@25493 a592a061-630c-0410-9148-cb99ea01b6c8

--- src/LyXAction.cpp
+++ src/LyXAction.cpp
@@ -2312,7 +2312,6 @@ void LyXAction::init()
 		{ LFUN_KEYMAP_SECONDARY, "keymap-secondary", ReadOnly, Edit },
 		{ LFUN_KEYMAP_TOGGLE, "keymap-toggle", ReadOnly, Edit },
 
-		{ LFUN_SERVER_CHAR_AFTER, "server-char-after", ReadOnly, System },
 		{ LFUN_SERVER_GET_LAYOUT, "server-get-layout", ReadOnly, System },
 /*!
  * \var lyx::FuncCode lyx::LFUN_SERVER_GET_FILENAME
