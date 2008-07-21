e8bb46e5340267e7210e28f4f9049636c30709de
Pavel Sanda <sanda@lyx.org>
2008-07-21 16:22:35 +0000

    message
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@25771 a592a061-630c-0410-9148-cb99ea01b6c8

--- src/VCBackend.cpp
+++ src/VCBackend.cpp
@@ -44,7 +44,7 @@ int VCS::doVCCommand(string const & cmd, FileName const & path)
 	int const ret = one.startscript(Systemcall::Wait, cmd);
 	if (ret)
 		frontend::Alert::error(_("Revision control error."),
-			bformat(_("Please check you have installed the program\n"
+			bformat(_("Please check you have installed the program called in\n"
 				  "'%1$s'."),
 			from_ascii(cmd)));
 	return ret;
