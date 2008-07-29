26b3fdcc48c1765e451256545c88a97938c1a005
Pavel Sanda <sanda@lyx.org>
2008-07-29 10:00:11 +0000

    not needed now
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@25962 a592a061-630c-0410-9148-cb99ea01b6c8

--- src/frontends/qt4/GuiSelection.cpp
+++ src/frontends/qt4/GuiSelection.cpp
@@ -55,7 +55,7 @@ void GuiSelection::haveSelection(bool own)
 	// an application actually requests it.
 	// This way calling Selection::have() is cheap and we can do it as
 	// often as we want.
-	LYXERR(Debug::SELECTION, "GuiSelection: setting dummy selection");
+	//LYXERR(Debug::SELECTION, "GuiSelection: setting dummy selection");
 	if (own)
 		qApp->clipboard()->setText(QString(), QClipboard::Selection);
 	// We don't need to do anything if own = false, as this case is
