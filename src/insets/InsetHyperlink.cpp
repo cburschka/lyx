cburschka/lyx

commit a258a932d9d5f64dbc53f0577f704df2f61fd472

InsetHyperlink.cpp: add "http://" when the hyperlink type is web and no protocol is specified (as requested on the lyx-devel list)
git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@26218 a592a061-630c-0410-9148-cb99ea01b6c8

src/insets/InsetHyperlink.cpp
@@ -84,6 +84,13 @@ int InsetHyperlink::latex(odocstream & os, OutputParams const & runparams) const
 				url.replace(pos, 1, backslash + chars_url[k]);
 			}
 		}
+
+		// add "http://" when the type is web (type = empty)
+		// and no "://" is given
+		docstring type = getParam("type");
+		if (url.find(from_ascii("://")) == string::npos	&& type == "")
+			url = from_ascii("http://") + url;
+
 	} // end if (!url.empty())
 
 	docstring name = getParam("name");

commit ac3f391b46d25784f3129e24658b73c4bc15febf

InsetHyperlink.cpp: fix a bug I introduced in r26218
git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@26262 a592a061-630c-0410-9148-cb99ea01b6c8

src/insets/InsetHyperlink.cpp
@@ -86,9 +86,11 @@ int InsetHyperlink::latex(odocstream & os, OutputParams const & runparams) const
 		}
 
 		// add "http://" when the type is web (type = empty)
-		// and no "://" is given
+		// and no "://" or "run:" is given
 		docstring type = getParam("type");
-		if (url.find(from_ascii("://")) == string::npos	&& type == "")
+		if (url.find(from_ascii("://")) == string::npos
+			&& url.find(from_ascii("run:")) == string::npos
+			&& type == "")
 			url = from_ascii("http://") + url;
 
 	} // end if (!url.empty())