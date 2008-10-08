7d4b69d2dcd608e4dba95c5e391bdccfa6187d49
José Matox <jamatos@lyx.org>
2008-10-08 18:24:25 +0000

    autoconf 2.63 works too (it seems to be a bug fix release)
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@26816 a592a061-630c-0410-9148-cb99ea01b6c8

--- autogen.sh
+++ autogen.sh
@@ -38,11 +38,11 @@ test "$autoversion" != "" && {
 
 
 case $autoversion in
-    *' '2.59[cd]|*' '2.60[ab]|*' '2.6[0-2])
+    *' '2.59[cd]|*' '2.60[ab]|*' '2.6[0-3])
 	;;
     *)
 	echo "This autoconf version is not supported by LyX."
-	echo "LyX only supports autoconf 2.59c-2.62."
+	echo "LyX only supports autoconf 2.59c-2.63."
 	exit 1
 	;;
 esac
