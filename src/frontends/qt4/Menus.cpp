02e4744ce31da7982b768ff001d4f1c0870022a8
Richard Heck <rgheck@comcast.net>
2008-10-12 04:04:55 +0000

    Fix bug 5346.
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@26854 a592a061-630c-0410-9148-cb99ea01b6c8

--- src/frontends/qt4/Menus.cpp
+++ src/frontends/qt4/Menus.cpp
@@ -850,13 +850,20 @@ void MenuDefinition::expandFlexInsert(Buffer const * buf, string s)
 		buf->params().documentClass().insetLayouts();
 	TextClass::InsetLayouts::const_iterator cit = insetLayouts.begin();
 	TextClass::InsetLayouts::const_iterator end = insetLayouts.end();
+	bool addedOne = false;
 	for (; cit != end; ++cit) {
 		docstring const label = cit->first;
-		if (cit->second.lyxtype() == s)
+		if (cit->second.lyxtype() == s) {
 			addWithStatusCheck(MenuItem(MenuItem::Command, 
 				toqstr(translateIfPossible(label)),
 				FuncRequest(LFUN_FLEX_INSERT, label)));
+			addedOne = true;
+		}
 	}
+	if (!addedOne)
+		add(MenuItem(MenuItem::Command,
+				    qt_("No custom insets defined!"),
+				    FuncRequest(LFUN_NOACTION)));
 }
 
 
