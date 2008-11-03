043c362dab9303d9ebd3023484fa10bedc8b97a2
Jürgen Spitzmüller <spitz@lyx.org>
2008-11-03 13:43:33 +0000

    * output_latex.cpp: 	- slightly change FIXME comment. This only applies to the newly added condition. 	  The other conditions should be left untouched, even if we find a way to access the TeXRow.
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@27237 a592a061-630c-0410-9148-cb99ea01b6c8

--- src/output_latex.cpp
+++ src/output_latex.cpp
@@ -724,16 +724,17 @@ TeXOnePar(Buffer const & buf,
 	// Note from JMarc: we will re-add a \n explicitly in
 	// TeXEnvironment, because it is needed in this case
 	if (nextpit != paragraphs.end()) {
-		// FIXME What we really want to do here is output a newline only if
-		// we have not just output a newline. But in the present state of play,
-		// we don't have access to that information. For some ideas about how
-		// to fix this, see this thread:
-		// http://www.mail-archive.com/lyx-devel@lists.lyx.org/msg145787.html
 		Layout const & next_layout = nextpit->layout();
-		if (style == next_layout 
-	      // no blank lines before environments!
-		    || !next_layout.isEnvironment() 
+		if (style == next_layout
+		    // no blank lines before environments!
+		    || !next_layout.isEnvironment()
 		    // unless there's a depth change
+		    // FIXME What we really want to do here is put every \begin and \end
+		    // tag on a new line (which was not the case with nested environments).
+		    // But in the present state of play, we don't have access to the
+		    // information whether the current TeX row is empty or not.
+		    // For some ideas about how to fix this, see this thread:
+		    // http://www.mail-archive.com/lyx-devel@lists.lyx.org/msg145787.html
 		    || nextpit->params().depth() != pit->params().depth()) {
 			os << '\n';
 			texrow.newline();
