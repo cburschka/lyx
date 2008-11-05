b65ef30b34258f75c5e40a423dc6c99f8300f4a0
Jürgen Spitzmüller <spitz@lyx.org>
2008-11-05 18:37:47 +0000

    * GuiDocument.cpp (formatStrVec): 	- module names can have non-ASCII chars.
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@27274 a592a061-630c-0410-9148-cb99ea01b6c8

--- src/frontends/qt4/GuiDocument.cpp
+++ src/frontends/qt4/GuiDocument.cpp
@@ -1364,22 +1364,22 @@ namespace {
 		if (v.size() == 0)
 			return docstring();
 		if (v.size() == 1) 
-			return from_ascii(v[0]);
+			return from_utf8(v[0]);
 		if (v.size() == 2) {
 			docstring retval = _("%1$s and %2$s");
 			retval = subst(retval, _("and"), s);
-			return bformat(retval, from_ascii(v[0]), from_ascii(v[1]));
+			return bformat(retval, from_utf8(v[0]), from_utf8(v[1]));
 		}
 		// The idea here is to format all but the last two items...
 		int const vSize = v.size();
 		docstring t2 = _("%1$s, %2$s");
-		docstring retval = from_ascii(v[0]);
+		docstring retval = from_utf8(v[0]);
 		for (int i = 1; i < vSize - 2; ++i)
-			retval = bformat(t2, retval, from_ascii(v[i])); 
+			retval = bformat(t2, retval, from_utf8(v[i])); 
 		//...and then to  plug them, and the last two, into this schema
 		docstring t = _("%1$s, %2$s, and %3$s");
 		t = subst(t, _("and"), s);
-		return bformat(t, retval, from_ascii(v[vSize - 2]), from_ascii(v[vSize - 1]));
+		return bformat(t, retval, from_utf8(v[vSize - 2]), from_utf8(v[vSize - 1]));
 	}
 	
 	vector<string> idsToNames(vector<string> const & idList)
