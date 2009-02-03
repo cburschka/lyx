1a00ebbd907ca2ee4f7ed59b85c464fba694c543
Jürgen Spitzmüller <spitz@lyx.org>
2009-02-03 16:29:27 +0000

    * LyXFileDialog.cpp: 	- limit workaround for Qt bug that was fixed in Qt 4.3.4 to previous Qt versions 	  (related to bug 4513).
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/branches/BRANCH_1_6_X@28337 a592a061-630c-0410-9148-cb99ea01b6c8

--- src/frontends/qt4/LyXFileDialog.cpp
+++ src/frontends/qt4/LyXFileDialog.cpp
@@ -50,12 +50,14 @@ LyXFileDialog::LyXFileDialog(QString const & title,
 	: QFileDialog(qApp->focusWidget(), title, path)
 {
 	setFilters(filters);
-	QDir dir(path);
+#if QT_VERSION < 0x040304
 	// FIXME: workaround for a bug in qt which makes LyX crash
-	// with hidden paths (bug 4513). Recheck with recent Qt versions.
+	// with hidden paths (bug 4513). Fixed as of Qt 4.3.4
+	QDir dir(path);
 	if (path.contains("/."))
 		dir.setFilter(QDir::Hidden);
 	setDirectory(dir);
+#endif
 	setWindowTitle(title);
 
 	QList<QHBoxLayout *> layout = findChildren<QHBoxLayout *>();
