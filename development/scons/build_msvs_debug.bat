f800571674573904b9254c170f419e53f72a21a5
Uwe Stöhr <uwestoehr@web.de>
2009-02-05 15:49:09 +0000

    SCons: update version suffix for LyX 2.0svn
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@28362 a592a061-630c-0410-9148-cb99ea01b6c8

--- development/scons/build_msvs_debug.bat
+++ development/scons/build_msvs_debug.bat
@@ -1,2 +1,2 @@
-call scons install prefix=..\..\build-msvc use_vc=yes frontend=qt4 mode=debug version_suffix=16 gettext=system nls=yes extra_inc_path=..\..\lyx-windows-deps-msvc2008\include extra_lib_path=..\..\lyx-windows-deps-msvc2008\lib extra_bin_path=..\..\lyx-windows-deps-msvc2008\bin qt_dir=..\..\lyx-windows-deps-msvc2008\qt-4
+call scons install prefix=..\..\build-msvc use_vc=yes frontend=qt4 mode=debug version_suffix=20 gettext=system nls=yes extra_inc_path=..\..\lyx-windows-deps-msvc2008\include extra_lib_path=..\..\lyx-windows-deps-msvc2008\lib extra_bin_path=..\..\lyx-windows-deps-msvc2008\bin qt_dir=..\..\lyx-windows-deps-msvc2008\qt-4
 call scons msvs_projects
