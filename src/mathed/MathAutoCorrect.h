78bc661ff8a1811051b8f58388f08f6abcc50dc2
Vincent van Ravesteijn <vfr@lyx.org>
2009-02-08 01:21:54 +0000

    * MathAutoCorrect: kill compiler warning.
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@28390 a592a061-630c-0410-9148-cb99ea01b6c8

--- src/mathed/MathAutoCorrect.h
+++ src/mathed/MathAutoCorrect.h
@@ -12,12 +12,14 @@
 #ifndef MATHAUTOCORRECT_H
 #define MATHAUTOCORRECT_H
 
+#include "support/strfwd.h"
+
 namespace lyx {
 
 class MathAtom;
 
 // make "corrections" according to file lib/autocorrect
-bool math_autocorrect(MathAtom & at, char c);
+bool math_autocorrect(MathAtom & at, char_type c);
 
 } // namespace lyx
 
