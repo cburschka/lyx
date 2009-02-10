da5c1995b10d65bd2c50dc931d0665c182361a07
Jürgen Spitzmüller <spitz@lyx.org>
2009-02-10 08:24:55 +0000

    * TocWidget.cpp: 	- improve performance with Outliner open with Qt >= 4.3 by using native methods instead of home-brewn code. 	  This should especially pay off on the Mac, if the Outliner shows as a drawer (bug 4443).
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/branches/BRANCH_1_6_X@28431 a592a061-630c-0410-9148-cb99ea01b6c8

--- src/frontends/qt4/TocWidget.cpp
+++ src/frontends/qt4/TocWidget.cpp
@@ -142,8 +142,15 @@ void TocWidget::setTreeDepth(int depth)
 	if (!tocTV->model())
 		return;
 
+#if QT_VERSION >= 0x040300
+	// this should be faster than our own code below
+	if (depth == 0)
+		tocTV->collapseAll();
+	else
+		tocTV->expandToDepth(depth - 1);
+#else
 	// expanding and then collapsing is probably better,
-	// but my qt 4.1.2 doesn't have expandAll()..
+	// but my qt 4.1.2 doesn't have expandAll().
 	//tocTV->expandAll();
 	QModelIndexList indices = tocTV->model()->match(
 		tocTV->model()->index(0, 0),
@@ -155,6 +162,7 @@ void TocWidget::setTreeDepth(int depth)
 		QModelIndex index = indices[i];
 		tocTV->setExpanded(index, indexDepth(index) < depth_);
 	}
+#endif
 }
 
 
