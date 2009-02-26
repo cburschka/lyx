36aa11863b8087ee09b50eb1d2b8fe7802eb6ede
Vincent van Ravesteijn <vfr@lyx.org>
2009-02-26 00:24:12 +0000

    Fix bug 5806: http://bugzilla.lyx.org/show_bug.cgi?id=5806. Branch names with spaces impossible
    Read the whole line to catch the whole branch name.
    
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@28620 a592a061-630c-0410-9148-cb99ea01b6c8

--- src/insets/InsetBranch.cpp
+++ src/insets/InsetBranch.cpp
@@ -298,7 +298,8 @@ void InsetBranchParams::write(ostream & os) const
 
 void InsetBranchParams::read(Lexer & lex)
 {
-	lex >> branch;
+	lex.eatLine();
+	branch = lex.getDocString();
 }
 
 } // namespace lyx
