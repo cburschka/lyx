e8e363338cacbbfe31c04e2dfcdaa971f1d64fe8
Pavel Sanda <sanda@lyx.org>
2009-03-18 13:21:06 +0000

    Context-branch menu uselessly shows both open/close items.
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@28853 a592a061-630c-0410-9148-cb99ea01b6c8

--- src/insets/InsetBranch.cpp
+++ src/insets/InsetBranch.cpp
@@ -175,8 +175,11 @@ bool InsetBranch::getStatus(Cursor & cur, FuncRequest const & cmd,
 		break;
 
 	case LFUN_INSET_TOGGLE:
-		if (cmd.argument() == "open" || cmd.argument() == "close" ||
-		    cmd.argument() == "toggle")
+		if (cmd.argument() == "open")
+			flag.setEnabled(status_ != Open);
+		else if (cmd.argument() == "close")
+			flag.setEnabled(status_ == Open);
+		else if (cmd.argument() == "toggle")
 			flag.setEnabled(true);
 		else if (cmd.argument() == "assign" || cmd.argument().empty()) {
 			if (isBranchSelected())
