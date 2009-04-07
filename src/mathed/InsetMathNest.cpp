48236d215c21a0ad61b46fa2bf5ee7da167af3f2
Uwe Stöhr <uwestoehr@web.de>
2009-04-07 00:23:38 +0000

    InsetMathNest.cpp: forgot this in last commit
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@29132 a592a061-630c-0410-9148-cb99ea01b6c8

--- src/mathed/InsetMathNest.cpp
+++ src/mathed/InsetMathNest.cpp
@@ -1938,6 +1938,8 @@ MathCompletionList::MathCompletionList(Cursor const & cur)
 	globals.push_back(from_ascii("\\normalcolor"));
 	globals.push_back(from_ascii("\\textcolor"));
 	globals.push_back(from_ascii("\\cfrac"));
+	globals.push_back(from_ascii("\\cfracleft"));
+	globals.push_back(from_ascii("\\cfracright"));
 	globals.push_back(from_ascii("\\dfrac"));
 	globals.push_back(from_ascii("\\tfrac"));
 	globals.push_back(from_ascii("\\dbinom"));
