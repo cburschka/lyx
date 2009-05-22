9d02de79d82294b36a38609403215535d3efc317
Jürgen Spitzmüller <spitz@lyx.org>
2009-05-22 17:17:45 +0000

    forgot to add these files in r29779.
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@29784 a592a061-630c-0410-9148-cb99ea01b6c8

---
+++ src/frontends/qt4/GuiPrintNomencl.cpp
@@ -0,0 +1,158 @@
+/**
+ * \file GuiPrintNomencl.cpp
+ * This file is part of LyX, the document processor.
+ * Licence details can be found in the file COPYING.
+ *
+ * \author Jürgen Spitzmüller
+ * \author Uwe Stöhr
+ *
+ * Full author contact details are available in file CREDITS.
+ */
+
+#include <config.h>
+
+#include "GuiPrintNomencl.h"
+
+#include "LengthCombo.h"
+#include "qt_helpers.h"
+#include "Validator.h"
+
+#include "Spacing.h"
+#include "FuncRequest.h"
+
+#include "insets/InsetNomencl.h"
+
+#include "support/debug.h"
+#include "support/gettext.h"
+#include "support/lstrings.h"
+
+#include <QLineEdit>
+#include <QPushButton>
+#include <QValidator>
+
+using namespace std;
+
+namespace lyx {
+namespace frontend {
+
+GuiPrintNomencl::GuiPrintNomencl(GuiView & lv)
+	: GuiDialog(lv, "nomencl_print", qt_("Nomenclature settings")),
+	  params_(insetCode("nomencl_print"))
+{
+	setupUi(this);
+
+	connect(okPB, SIGNAL(clicked()), this, SLOT(slotOK()));
+	connect(applyPB, SIGNAL(clicked()), this, SLOT(slotApply()));
+	connect(closePB, SIGNAL(clicked()), this, SLOT(slotClose()));
+
+	connect(valueLE, SIGNAL(textChanged(QString)),
+		this, SLOT(change_adaptor()));
+	connect(unitLC, SIGNAL(selectionChanged(lyx::Length::UNIT)),
+		this, SLOT(change_adaptor()));
+
+	valueLE->setValidator(unsignedLengthValidator(valueLE));
+
+	// Manage the ok, apply, restore and cancel/close buttons
+	bc().setPolicy(ButtonPolicy::OkApplyCancelReadOnlyPolicy);
+	bc().setOK(okPB);
+	bc().setApply(applyPB);
+	bc().setCancel(closePB);
+
+	// disable for read-only documents
+	bc().addReadOnly(valueLE);
+	bc().addReadOnly(unitLC);
+
+	// initialize the length validator
+	bc().addCheckedLineEdit(valueLE, valueLA);
+
+	setWidthCO->addItem(qt_("Predefined"),
+		QVariant(toqstr("none")));
+	setWidthCO->addItem(qt_("Auto-determined"),
+		QVariant(toqstr("auto")));
+	setWidthCO->addItem(qt_("Custom"),
+		QVariant(toqstr("custom")));
+}
+
+
+void GuiPrintNomencl::change_adaptor()
+{
+	changed();
+}
+
+
+void GuiPrintNomencl::on_setWidthCO_activated(int i)
+{
+	bool const custom =
+		(setWidthCO->itemData(i).toString()
+		 == "custom");
+	valueLE->setEnabled(custom);
+	unitLC->setEnabled(custom);
+	valueLA->setEnabled(custom);
+	changed();
+}
+
+
+void GuiPrintNomencl::paramsToDialog(InsetCommandParams const & /*icp*/)
+{
+	setWidthCO->setCurrentIndex(
+		setWidthCO->findData(toqstr(params_["set_width"])));
+	
+	lengthToWidgets(valueLE,
+			unitLC,
+			params_["width"],
+			Length::defaultUnit());
+
+	bc().setValid(isValid());
+}
+
+
+void GuiPrintNomencl::updateContents()
+{
+	bool const custom = (setWidthCO->itemData(
+				  setWidthCO->currentIndex()).toString()
+			     == "custom");
+	valueLE->setEnabled(custom);
+	unitLC->setEnabled(custom);
+	valueLA->setEnabled(custom);
+}
+
+
+void GuiPrintNomencl::applyView()
+{
+	docstring const set_width = qstring_to_ucs4(setWidthCO->itemData(
+		setWidthCO->currentIndex()).toString());
+	params_["set_width"] = set_width;
+	docstring width;
+	if (set_width == from_ascii("custom"))
+		width = from_utf8(widgetsToLength(valueLE, unitLC));
+	params_["width"] = width;
+}
+
+
+bool GuiPrintNomencl::initialiseParams(std::string const & data)
+{
+	InsetCommand::string2params("nomencl_print", data, params_);
+	paramsToDialog(params_);
+	return true;
+}
+
+
+void GuiPrintNomencl::dispatchParams()
+{
+	std::string const lfun = InsetCommand::params2string("nomencl_print", params_);
+	dispatch(FuncRequest(getLfun(), lfun));
+}
+
+
+
+Dialog * createGuiPrintNomencl(GuiView & lv)
+{
+	return new GuiPrintNomencl(lv);
+}
+
+
+} // namespace frontend
+} // namespace lyx
+
+
+#include "moc_GuiPrintNomencl.cpp"
---
+++ src/frontends/qt4/GuiPrintNomencl.h
@@ -0,0 +1,59 @@
+// -*- C++ -*-
+/**
+ * \file GuiPrintNomencl.h
+ * This file is part of LyX, the document processor.
+ * Licence details can be found in the file COPYING.
+ *
+ * \author Jürgen Spitzmüller
+ * \author Uwe Stöhr
+ *
+ * Full author contact details are available in file CREDITS.
+ */
+
+#ifndef GUIPRINTNOMENCL_H
+#define GUIPRINTNOMENCL_H
+
+#include "GuiDialog.h"
+#include "ui_PrintNomenclUi.h"
+
+#include "insets/InsetCommandParams.h"
+
+
+namespace lyx {
+namespace frontend {
+
+class GuiPrintNomencl : public GuiDialog, public Ui::PrintNomenclUi
+{
+	Q_OBJECT
+
+public:
+	GuiPrintNomencl(GuiView & lv);
+
+private Q_SLOTS:
+	void change_adaptor();
+	void on_setWidthCO_activated(int);
+
+private:
+	/// Apply changes
+	void applyView();
+	/// Update dialog before showing it
+	void updateContents();
+	///
+	bool initialiseParams(std::string const & data);
+	///
+	void paramsToDialog(InsetCommandParams const & icp);
+	///
+	void clearParams() { params_.clear(); }
+	///
+	void dispatchParams();
+	///
+	bool isBufferDependent() const { return true; }
+
+	///
+	InsetCommandParams params_;
+};
+
+} // namespace frontend
+} // namespace lyx
+
+#endif // GUIPRINTNOMENCL_H
