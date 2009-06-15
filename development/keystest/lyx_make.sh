752099a7c55282635a686e3203a45405381c387d
Pavel Sanda <sanda@lyx.org>
2009-06-15 20:20:24 +0000

    Make keystest part of our build system. Update sources only on reguest.
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@30124 a592a061-630c-0410-9148-cb99ea01b6c8

--- development/keystest/lyx_make.sh
+++ development/keystest/lyx_make.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-#This script updates LyX, runs LyX, starts spamming it with hundreds of 
+#development/keystest/lyx_make.sh [--update]
+#This script runs LyX (and possibly updates), starts spamming it with hundreds of 
 #keypresses, and logs all output, including backtraces to development/keystest/out/GDB .
 #Use report.sh to generated the more useful bug reports in development/keystest/out/{or}*
 
@@ -12,8 +13,17 @@ fi
 mkdir -p $LT/out
 if which wmctrl xvkbd bash xterm python
 then 
- svn up
- ./autogen.sh && ./configure --enable-debug -- && nice -18 make && (bash $LT/autolyx & sleep 9 ; xterm -e python $LT/test.py)
+
+ if [ a"$1" == a--update ]; then
+  svn up
+  ./autogen.sh && ./configure --enable-debug -- && nice -18 make && (bash $LT/autolyx & sleep 9 ; xterm -e python $LT/test.py)
+ else
+  DBG=`src/lyx --version 2>&1 | grep C++.Compiler.flags|grep -- -g`;
+  if [ -z "$DBG" ]; then echo Wrong build of LyX binary. ; exit; fi
+
+  bash $LT/autolyx & sleep 9 ; xterm -e python $LT/test.py
+ fi
+
 else
  echo NEEDS the following packages: 
  echo wmctrl xvkbd bash xterm python
