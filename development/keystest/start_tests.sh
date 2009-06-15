9c6e9083ed2aff5dacf39c019a866113efc058b4
Pavel Sanda <sanda@lyx.org>
2009-06-15 20:20:22 +0000

    Cosmetic leftovers
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@30123 a592a061-630c-0410-9148-cb99ea01b6c8

--- development/keystest/start_tests.sh
+++ development/keystest/start_tests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#This script  runs LyX, starts spamming it with hundreds of 
-#keypresses, and logs all output, including backtraces to LT/out/GDB
-#Use report.sh to generated the more useful bug reports in LT/out/{or}*
+#This script runs LyX, starts spamming it with hundreds of 
+#keypresses, and logs all output, including backtraces to $LT/out/GDB
+#Use report.sh to generated the more useful bug reports in $LT/out/{or}*
 
 LT=development/keystest
 
