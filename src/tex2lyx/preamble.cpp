c8554cfdeba7b3b9d28d2783f1b6d50a4d11adda
Uwe Stöhr <uwestoehr@web.de>
2009-06-19 00:03:31 +0000

    branch: tex2lyx/preamble.cpp: - remove unneeded comment (sorry for all these small bits)
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/branches/BRANCH_1_6_X@30170 a592a061-630c-0410-9148-cb99ea01b6c8

--- src/tex2lyx/preamble.cpp
+++ src/tex2lyx/preamble.cpp
@@ -563,7 +563,6 @@ void parse_preamble(Parser & p, ostream & os,
 			h_preamble << t.asInput();
 
 		else if (t.cat() == catComment) {
-			// regex to parse comments
 			string const comment = t.asInput();
 			// magically switch encoding default if it looks like XeLaTeX
 			static string const magicXeLaTeX =
