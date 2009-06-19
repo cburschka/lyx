e06029fda53ef05987ca4b7c1ba8529a9ff9333b
Uwe Stöhr <uwestoehr@web.de>
2009-06-19 00:10:18 +0000

    tex2lyx/preamble.cpp: support for fileformat 262 (nothing to do for tex2lyx)
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@30171 a592a061-630c-0410-9148-cb99ea01b6c8

--- src/tex2lyx/preamble.cpp
+++ src/tex2lyx/preamble.cpp
@@ -471,7 +471,7 @@ void handle_package(Parser &p, string const & name, string const & opts)
 void end_preamble(ostream & os, TextClass const & /*textclass*/)
 {
 	os << "#LyX file created by tex2lyx " << PACKAGE_VERSION << "\n"
-	   << "\\lyxformat 258\n"
+	   << "\\lyxformat 262\n"
 	   << "\\begin_document\n"
 	   << "\\begin_header\n"
 	   << "\\textclass " << h_textclass << "\n";
