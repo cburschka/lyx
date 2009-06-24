2aff2e7ad3db80bfa1e2d5bc77d5c802fdb43bf8
Pavel Sanda <sanda@lyx.org>
2009-06-24 19:12:07 +0000

    Backport r30251.
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/branches/BRANCH_1_6_X@30252 a592a061-630c-0410-9148-cb99ea01b6c8

--- lib/configure.py
+++ lib/configure.py
@@ -389,9 +389,9 @@ def checkConverterEntries():
     checkProg('an MS Word -> LaTeX converter', ['wvCleanLatex $$i $$o'],
         rc_entry = [ r'\converter word       latex      "%%"	""' ])
     #
-    path, elyxer = checkProg('a LyX -> HTML converter', ['elyxer.py --directory $$r $$i $$o'],
+    path, elyxer = checkProg('a LyX -> HTML converter', ['elyxer.py --directory $$r $$i $$o','elyxer --directory $$r $$i $$o'],
       rc_entry = [ r'\converter lyx      html       "%%"	""' ])
-    if elyxer.find('elyxer.py') >= 0:
+    if elyxer.find('elyxer') >= 0:
       addToRC(r'''\copier    html       "python -tt $$s/scripts/ext_copy.py -e html,png,jpg,jpeg,css $$i $$o"''')
     else:
       # On SuSE the scripts have a .sh suffix, and on debian they are in /usr/share/tex4ht/
