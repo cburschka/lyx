7d2331fc17eb1419eb400df18210f16388b5b8f0
Pavel Sanda <sanda@lyx.org>
2009-06-26 12:30:22 +0000

    Shouldn't be needed
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@30267 a592a061-630c-0410-9148-cb99ea01b6c8

--- src/LyXFunc.cpp
+++ src/LyXFunc.cpp
@@ -473,7 +473,6 @@ FuncStatus LyXFunc::getStatus(FuncRequest const & cmd) const
 		break;
 	case LFUN_VC_LOCKING_TOGGLE:
 		enable = !buf->isReadonly() && buf->lyxvc().lockingToggleEnabled();
-		flag.setEnabled(enable);
 		if (enable)
 			flag.setOnOff(!buf->lyxvc().locker().empty());
 		break;
