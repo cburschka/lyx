9d35e6690c5d5e6987f954dfb850dfe8597b54f1
Jürgen Spitzmüller <spitz@lyx.org>
2009-07-10 06:58:54 +0000

    revert unintended commit.
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@30443 a592a061-630c-0410-9148-cb99ea01b6c8

--- src/LaTeXFeatures.cpp
+++ src/LaTeXFeatures.cpp
@@ -195,11 +195,8 @@ static docstring const changetracking_none_def = from_ascii(
 	"\\newcommand{\\lyxdeleted}[3]{}\n");
 
 static docstring const textgreek_def = from_ascii(
-	"\\providecommand*{\\perispomeni}{\\char126}\n"
-	"\\AtBeginDocument{\\DeclareRobustCommand{\\greektext}{%\n"
-	"  \\fontencoding{LGR}\\selectfont\\def\\encodingdefault{LGR}\n"
-	"  \\renewcommand{\\~}{\\perispomeni}\n"
-	"}}\n"
+	"\\DeclareRobustCommand{\\greektext}{%\n"
+	"  \\fontencoding{LGR}\\selectfont\\def\\encodingdefault{LGR}}\n"
 	"\\DeclareRobustCommand{\\textgreek}[1]{\\leavevmode{\\greektext #1}}\n"
 	"\\DeclareFontEncoding{LGR}{}{}\n");
 
