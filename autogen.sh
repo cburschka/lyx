c1fa134b90d2cea8b5f99719a5c202e2467decce
Pavel Sanda <sanda@lyx.org>
2009-08-04 11:49:24 +0000

    Upgrade to autoconf 2.64
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@30859 a592a061-630c-0410-9148-cb99ea01b6c8

--- autogen.sh
+++ autogen.sh
@@ -38,11 +38,11 @@ test "$autoversion" != "" && {
 
 
 case $autoversion in
-    *' '2.59[cd]|*' '2.60[ab]|*' '2.6[0-3])
+    *' '2.59[cd]|*' '2.60[ab]|*' '2.6[0-4])
 	;;
     *)
 	echo "This autoconf version is not supported by LyX."
-	echo "LyX only supports autoconf 2.59c-2.63."
+	echo "LyX only supports autoconf 2.59c-2.64."
 	exit 1
 	;;
 esac
