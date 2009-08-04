cburschka/lyx

commit 13bb684eb0f18c690b04ef63b7f7ab9a8c993087

Improve LaTeX output of arrays in math. Always output a '\n' except in the case that the line is completely empty.
see also:
http://www.mail-archive.com/lyx-devel@lists.lyx.org/msg153349.html

git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@30822 a592a061-630c-0410-9148-cb99ea01b6c8

src/mathed/InsetMathGrid.cpp
@@ -1009,8 +1009,7 @@ void InsetMathGrid::write(WriteStream & os,
 		eol = eolString(row, os.fragile());
 		os << eol;
 		// append newline only if line wasn't completely empty
-		// and this was not the last line in the grid
-		if (!emptyline && row + 1 < end_row)
+		if (!(emptyline && eol.empty())) 
 			os << "\n";
 	}
 	// @TODO use end_row instead of nrows() ?

commit 11c3f2d1bc8337421d640d645bfaa7e83d32c89f

Fix the math output mess I caused in r30822.
git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@30860 a592a061-630c-0410-9148-cb99ea01b6c8

src/mathed/InsetMathGrid.cpp
@@ -1009,7 +1009,9 @@ void InsetMathGrid::write(WriteStream & os,
 		eol = eolString(row, os.fragile());
 		os << eol;
 		// append newline only if line wasn't completely empty
-		if (!(emptyline && eol.empty())) 
+		// and the formula is not written on a single line
+		bool const empty = emptyline && eol.empty();
+		if (!empty && nrows() > 1)
 			os << "\n";
 	}
 	// @TODO use end_row instead of nrows() ?