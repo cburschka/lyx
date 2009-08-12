4c338ac22d24a13ea37534409877482a5e30feb3
Pavel Sanda <sanda@lyx.org>
2009-08-12 22:09:50 +0000

    Fix bug #2045. http://www.mail-archive.com/lyx-devel@lists.lyx.org/msg153796.html
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@30997 a592a061-630c-0410-9148-cb99ea01b6c8

--- src/mathed/InsetMathNest.cpp
+++ src/mathed/InsetMathNest.cpp
@@ -953,6 +953,10 @@ void InsetMathNest::doDispatch(Cursor & cur, FuncRequest & cmd)
 		handleFont(cur, cmd.argument(), "textnormal");
 		break;
 
+	case LFUN_FONT_UNDERLINE:
+		cur.recordUndo();
+		cur.handleNest(createInsetMath("underline"));
+		break;
 	case LFUN_MATH_MODE: {
 #if 1
 		// ignore math-mode on when already in math mode
@@ -1288,6 +1292,7 @@ bool InsetMathNest::getStatus(Cursor & cur, FuncRequest const & cmd,
 		flag.setEnabled(true);
 		break;
 
+	case LFUN_FONT_UNDERLINE:
 	case LFUN_FONT_FRAK:
 		flag.setEnabled(currentMode() != TEXT_MODE);
 		break;
