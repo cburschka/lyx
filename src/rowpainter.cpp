c41a38d7d3a9412e13a377d679c195f4a15381cc
Vincent van Ravesteijn <vfr@lyx.org>
2009-08-22 17:51:41 +0000

    Revert the rtl-part of r30962.
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@31205 a592a061-630c-0410-9148-cb99ea01b6c8

--- src/rowpainter.cpp
+++ src/rowpainter.cpp
@@ -680,8 +680,7 @@ void RowPainter::paintLast()
 		FontInfo const font = labelFont();
 		FontMetrics const & fm = theFontMetrics(font);
 		docstring const & str = par_.layout().endlabelstring();
-		double const x = is_rtl ? 
-			text_metrics_.width() - row_.width() - fm.width(str) : x_;
+		double const x = is_rtl ? x_ - fm.width(str) : x_;
 		pi_.pain.text(int(x), yo_, str, font);
 		break;
 	}
