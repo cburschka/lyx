da55181c3312def80366c246dc26159746435ee1
Pavel Sanda <sanda@lyx.org>
2009-10-15 21:37:09 +0000

    More info into emergency dialog. This is especially useful in case more master/child emergency files were detected.
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@31642 a592a061-630c-0410-9148-cb99ea01b6c8

--- src/Buffer.cpp
+++ src/Buffer.cpp
@@ -3302,7 +3302,8 @@ bool Buffer::readFileHelper(FileName const & s)
 				str = _("Document was successfully recovered.");
 			else
 				str = _("Document was NOT successfully recovered.");
-			str += "\n\n" + _("Remove emergency file now?");
+			str += "\n\n" + _("Remove emergency file now?") +
+				"\n(" + from_utf8(e.absFilename())+")";
 
 			if (!Alert::prompt(_("Delete emergency file?"), str, 1, 1,
 					_("&Remove"), _("&Keep it"))) {
