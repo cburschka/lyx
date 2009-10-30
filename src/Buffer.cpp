cburschka/lyx

commit 037951834060b2231ea6d8f40d7a313172110a9e

Issue a warning if a document has two parents. We should do something smart then, but what?
- Do we want to ask the user which of the parents he would like to be the document's parent ?
- Do we want to disable any functionality related to parents ? Like references, bibliography, document outline etc.

git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@31809 a592a061-630c-0410-9148-cb99ea01b6c8

src/Buffer.cpp
@@ -253,7 +253,11 @@ public:
 		return parent_buffer; 
 	}
 	///
-	void setParent(Buffer const * pb) { parent_buffer = pb; }
+	void setParent(Buffer const * pb) {
+		if (parent_buffer != pb)
+			LYXERR0("Warning: a buffer should not have two parents!");
+		parent_buffer = pb;
+	}
 private:
 	/// So we can force access via the accessors.
 	mutable Buffer const * parent_buffer;

commit 25e5dfe33024c49b5c5d6fea954f08c70ca43fb5

Do not issue the warning introduced in r31809 if we are setting the parent for the first time, or we are removing the parent.
git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@31810 a592a061-630c-0410-9148-cb99ea01b6c8

src/Buffer.cpp
@@ -254,7 +254,7 @@ public:
 	}
 	///
 	void setParent(Buffer const * pb) {
-		if (parent_buffer != pb)
+		if (parent_buffer && pb && parent_buffer != pb)
 			LYXERR0("Warning: a buffer should not have two parents!");
 		parent_buffer = pb;
 	}