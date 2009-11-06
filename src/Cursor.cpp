f38483d441f465443253cd4ea51d35208e676b09
Vincent van Ravesteijn <vfr@lyx.org>
2009-11-06 18:08:06 +0000

    branch: Fix bug #6237: The boundary has to be set correctly when moving up with a selection. If not, the cursor will remain on the same row when there are two e.g. displayed equations after each other. This will cause an infinite loop when SCREEN_UP is executed.
    (I added a small extra safety measure to not change the logic resulting from r31864. I'm not sure it can harm.)
    
    see r31863, r31864.
    
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/branches/BRANCH_1_6_X@31880 a592a061-630c-0410-9148-cb99ea01b6c8

--- src/Cursor.cpp
+++ src/Cursor.cpp
@@ -1859,31 +1859,43 @@ bool Cursor::upDownInText(bool up, bool & updateNeeded)
 			operator=(dummy);
 		}
 	} else {
-		// if there is a selection, we stay out of any inset, and just jump to the right position:
+		// if there is a selection, we stay out of any inset,
+		// and just jump to the right position:
 		Cursor old = *this;
+		int next_row = row;
+		bool update = false;
 		if (up) {
 			if (row > 0) {
-				top().pos() = min(tm.x2pos(pit(), row - 1, xo), top().lastpos());
+				--next_row;
+				update = true;
 			} else if (pit() > 0) {
 				--pit();
 				TextMetrics & tm = bv_->textMetrics(text());
 				if (!tm.contains(pit()))
 					tm.newParMetricsUp();
 				ParagraphMetrics const & pmcur = tm.parMetrics(pit());
-				top().pos() = min(tm.x2pos(pit(), pmcur.rows().size() - 1, xo), top().lastpos());
+				next_row = pmcur.rows().size() - 1;
+				update = true;
 			}
 		} else {
 			if (row + 1 < int(pm.rows().size())) {
-				top().pos() = min(tm.x2pos(pit(), row + 1, xo), top().lastpos());
+				++next_row;
+				update = true;
 			} else if (pit() + 1 < int(text()->paragraphs().size())) {
 				++pit();
 				TextMetrics & tm = bv_->textMetrics(text());
 				if (!tm.contains(pit()))
 					tm.newParMetricsDown();
-				top().pos() = min(tm.x2pos(pit(), 0, xo), top().lastpos());
+				next_row = 0;
+				update = true;
 			}
 		}
+		if (update) {
+			top().pos() = min(tm.x2pos(pit(), next_row, xo), top().lastpos());
 
+			boundary(tm.x2pos(pit(), next_row, xo)
+				== tm.x2pos(pit(), next_row, tm.width()));
+		}
 		updateNeeded |= bv().checkDepm(*this, old);
 	}
 
