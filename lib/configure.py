4147409b37d360691ab1d2c6360655505749166c
Uwe Stöhr <uwestoehr@web.de>
2009-11-12 23:24:55 +0000

    configure.py: revert back to r31950, except of the comment that was removed in this revision
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@31959 a592a061-630c-0410-9148-cb99ea01b6c8

--- lib/configure.py
+++ lib/configure.py
@@ -621,11 +621,11 @@ def checkConverterEntries():
     if elyxerfound:
       addToRC(r'''\converter lyx      html       "python -m elyxer --directory $$r $$i $$o"	""''')
     else:
-      path, elyxer = checkProg('a LyX -> HTML converter', ['elyxer.py', 'elyxer'],
-        rc_entry = '')
+      path, elyxer = checkProg('a LyX -> HTML converter',
+        ['elyxer.py --directory $$r $$i $$o', 'elyxer --directory $$r $$i $$o'],
+        rc_entry = [ r'\converter lyx      html       "%%"	""' ])
       if elyxer.find('elyxer') >= 0:
         elyxerfound = True
-        addToRC(r'''\converter lyx      html       "python -tt elyxer.py --directory $$r $$i $$o"	""''')
 
     if elyxerfound:
       addToRC(r'''\copier    html       "python -tt $$s/scripts/ext_copy.py -e html,png,jpg,jpeg,css $$i $$o"''')
