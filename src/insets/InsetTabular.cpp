cburschka/lyx

commit f19b6178a9505e4dfe17f1836096f4ce1040a200

Fix bug #4360: Instant preview does not render formulas which contain math-macro from input file.
Patch from Stefan.

git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@32157 a592a061-630c-0410-9148-cb99ea01b6c8

src/insets/InsetTabular.cpp
@@ -5289,13 +5289,19 @@ bool InsetTabular::insertPlaintextString(BufferView & bv, docstring const & buf,
 }
 
 
-void InsetTabular::addPreview(PreviewLoader & loader) const
+void InsetTabular::addPreview(DocIterator const & inset_pos,
+	PreviewLoader & loader) const
 {
 	row_type const rows = tabular.row_info.size();
 	col_type const columns = tabular.column_info.size();
+	DocIterator cell_pos = inset_pos;
+
+	cell_pos.push_back(CursorSlice(*const_cast<InsetTabular *>(this)));
 	for (row_type i = 0; i < rows; ++i) {
-		for (col_type j = 0; j < columns; ++j)
-			tabular.cellInset(i, j)->addPreview(loader);
+		for (col_type j = 0; j < columns; ++j) {
+			cell_pos.top().idx() = index(i, j);
+			tabular.cellInset(i, j)->addPreview(cell_pos, loader);
+		}
 	}
 }
 

src/graphics/PreviewLoader.cpp
@@ -701,15 +701,6 @@ void PreviewLoader::Impl::dumpPreamble(odocstream & os) const
 	   << "\\def\\lyxlock{}\n"
 	   << "\n";
 
-	// Loop over the insets in the buffer and dump all the math-macros.
-	Inset & inset = buffer_.inset();
-	InsetIterator it = inset_iterator_begin(inset);
-	InsetIterator const end = inset_iterator_end(inset);
-
-	for (; it != end; ++it)
-		if (it->lyxCode() == MATHMACRO_CODE)
-			it->latex(os, runparams);
-
 	// All equation labels appear as "(#)" + preview.sty's rendering of
 	// the label name
 	if (lyxrc.preview_hashed_labels)

src/graphics/Previews.cpp
@@ -72,7 +72,7 @@ void Previews::generateBufferPreviews(Buffer const & buffer) const
 	InsetIterator const end = inset_iterator_end(inset);
 
 	for (; it != end; ++it)
-		it->addPreview(ploader);
+		it->addPreview(it, ploader);
 
 	ploader.startLoading();
 }

src/insets/Inset.h
@@ -449,7 +449,8 @@ public:
 	 *  Most insets have no interest in this capability, so the method
 	 *  defaults to empty.
 	 */
-	virtual void addPreview(graphics::PreviewLoader &) const {}
+	virtual void addPreview(DocIterator const & insetPos,
+		graphics::PreviewLoader &) const {}
 
 	/** Classifies the unicode characters appearing in a math inset
 	 *  depending on whether they are to be translated as latex

src/insets/InsetExternal.cpp
@@ -757,7 +757,8 @@ void InsetExternal::validate(LaTeXFeatures & features) const
 }
 
 
-void InsetExternal::addPreview(graphics::PreviewLoader & ploader) const
+void InsetExternal::addPreview(DocIterator const & /*inset_pos*/,
+	graphics::PreviewLoader & ploader) const	
 {
 	RenderMonitoredPreview * const ptr = renderer_->asMonitoredPreview();
 	if (!ptr)

src/insets/InsetExternal.h
@@ -139,7 +139,7 @@ private:
 	/// Update needed features for this inset.
 	void validate(LaTeXFeatures & features) const;
 	///
-	void addPreview(graphics::PreviewLoader &) const;
+	void addPreview(DocIterator const &, graphics::PreviewLoader &) const;
 	///
 	bool showInsetDialog(BufferView * bv) const;
 	///

src/insets/InsetInclude.cpp
@@ -949,7 +949,8 @@ void add_preview(RenderMonitoredPreview & renderer, InsetInclude const & inset,
 } // namespace anon
 
 
-void InsetInclude::addPreview(graphics::PreviewLoader & ploader) const
+void InsetInclude::addPreview(DocIterator const & /*inset_pos*/,
+	graphics::PreviewLoader & ploader) const
 {
 	Buffer const & buffer = ploader.buffer();
 	if (!preview_wanted(params(), buffer))

src/insets/InsetInclude.h
@@ -84,7 +84,7 @@ public:
 	///
 	void validate(LaTeXFeatures &) const;
 	///
-	void addPreview(graphics::PreviewLoader &) const;
+	void addPreview(DocIterator const &, graphics::PreviewLoader &) const;
 	///
 	void addToToc(DocIterator const &);
 	///

commit 2c32b53923d5a37627db54d229e7d5f8b8c74d5a

Access the correct cell function.
Fixes a bug introduced in r32157 while fixing bug #4360.

git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@32200 a592a061-630c-0410-9148-cb99ea01b6c8

src/insets/InsetTabular.cpp
@@ -5299,7 +5299,7 @@ void InsetTabular::addPreview(DocIterator const & inset_pos,
 	cell_pos.push_back(CursorSlice(*const_cast<InsetTabular *>(this)));
 	for (row_type i = 0; i < rows; ++i) {
 		for (col_type j = 0; j < columns; ++j) {
-			cell_pos.top().idx() = index(i, j);
+			cell_pos.top().idx() = tabular.cellIndex(i, j);
 			tabular.cellInset(i, j)->addPreview(cell_pos, loader);
 		}
 	}