4935e6127a928c05c81693299644b5e2a9f3bad7
Jürgen Spitzmüller <spitz@lyx.org>
2009-11-30 13:38:41 +0000

    * new French translation of the powerdot example, by Jean-Pierre.
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@32235 a592a061-630c-0410-9148-cb99ea01b6c8

--- development/scons/scons_manifest.py
+++ development/scons/scons_manifest.py
@@ -2515,6 +2515,7 @@ lib_examples_fr_files = Split('''
     Braille.lyx
     exemple_brut.lyx
     exemple_lyxifie.lyx
+    exemple_powerdot.lyx
     Foils.lyx
     ListesPuces.lyx
     multicol.lyx
