cburschka/lyx

commit f19b6178a9505e4dfe17f1836096f4ce1040a200

Fix bug #4360: Instant preview does not render formulas which contain math-macro from input file.
Patch from Stefan.

git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@32157 a592a061-630c-0410-9148-cb99ea01b6c8

src/mathed/InsetMathHull.cpp
@@ -28,6 +28,7 @@
 #include "FuncStatus.h"
 #include "LaTeXFeatures.h"
 #include "LyXRC.h"
+#include "MacroTable.h"
 #include "sgml.h"
 #include "TextPainter.h"
 #include "TocBackend.h"
@@ -456,22 +457,44 @@ void InsetMathHull::initUnicodeMath() const
 }
 
 
-void InsetMathHull::addPreview(graphics::PreviewLoader & ploader) const
+void InsetMathHull::addPreview(DocIterator const & insetPos,
+	graphics::PreviewLoader & /*ploader*/) const
 {
 	if (RenderPreview::status() == LyXRC::PREVIEW_ON) {
-		docstring const snippet = latexString(*this);
-		preview_->addPreview(snippet, ploader);
+		reloadPreview(insetPos);
 	}
 }
 
 
-bool InsetMathHull::notifyCursorLeaves(Cursor const & /*old*/, Cursor & cur)
+void InsetMathHull::reloadPreview(DocIterator const & pos) const  
+{  
+	Buffer const * buffer = pos.buffer();  
+
+	// collect macros at this position  
+	MacroNameSet macros;  
+	buffer->listMacroNames(macros);  
+	MacroNameSet::iterator it = macros.begin();  
+	MacroNameSet::iterator end = macros.end();  
+	odocstringstream macro_preamble;  
+	for (; it != end; ++it) {  
+		MacroData const * data = buffer->getMacro(*it, pos, true);  
+		if (data) {  
+			data->write(macro_preamble, true);  
+			macro_preamble << endl;  
+		}
+	}  
+
+	// start preview  
+	docstring const snippet = macro_preamble.str() + latexString(*this);  
+	LYXERR(Debug::MACROS, "Preview snippet: " << snippet);  
+	preview_->addPreview(snippet, *buffer);  
+	preview_->startLoading(*buffer);  
+}  
+
+bool InsetMathHull::notifyCursorLeaves(Cursor const & old, Cursor & cur)
 {
 	if (RenderPreview::status() == LyXRC::PREVIEW_ON) {
-		Buffer const * buffer = cur.buffer();
-		docstring const snippet = latexString(*this);
-		preview_->addPreview(snippet, *buffer);
-		preview_->startLoading(*buffer);
+		reloadPreview(old);
 		cur.updateFlags(Update::Force);
 	}
 	return false;

src/mathed/InsetMathHull.h
@@ -132,7 +132,10 @@ public:
 	///
 	//bool insetAllowed(InsetCode code) const;
 	///
-	void addPreview(graphics::PreviewLoader &) const;
+	void addPreview(DocIterator const & insetPos,
+		graphics::PreviewLoader &) const;
+	/// Recreate the preview if preview is enabled.
+	void reloadPreview(DocIterator const & pos) const;
 	///
 	void initUnicodeMath() const;
 

src/graphics/PreviewLoader.cpp
@@ -701,15 +701,6 @@ void PreviewLoader::Impl::dumpPreamble(odocstream & os) const
 	   << "\\def\\lyxlock{}\n"
 	   << "\n";
 
-	// Loop over the insets in the buffer and dump all the math-macros.
-	Inset & inset = buffer_.inset();
-	InsetIterator it = inset_iterator_begin(inset);
-	InsetIterator const end = inset_iterator_end(inset);
-
-	for (; it != end; ++it)
-		if (it->lyxCode() == MATHMACRO_CODE)
-			it->latex(os, runparams);
-
 	// All equation labels appear as "(#)" + preview.sty's rendering of
 	// the label name
 	if (lyxrc.preview_hashed_labels)

src/graphics/Previews.cpp
@@ -72,7 +72,7 @@ void Previews::generateBufferPreviews(Buffer const & buffer) const
 	InsetIterator const end = inset_iterator_end(inset);
 
 	for (; it != end; ++it)
-		it->addPreview(ploader);
+		it->addPreview(it, ploader);
 
 	ploader.startLoading();
 }

src/insets/Inset.h
@@ -449,7 +449,8 @@ public:
 	 *  Most insets have no interest in this capability, so the method
 	 *  defaults to empty.
 	 */
-	virtual void addPreview(graphics::PreviewLoader &) const {}
+	virtual void addPreview(DocIterator const & insetPos,
+		graphics::PreviewLoader &) const {}
 
 	/** Classifies the unicode characters appearing in a math inset
 	 *  depending on whether they are to be translated as latex

src/insets/InsetExternal.cpp
@@ -757,7 +757,8 @@ void InsetExternal::validate(LaTeXFeatures & features) const
 }
 
 
-void InsetExternal::addPreview(graphics::PreviewLoader & ploader) const
+void InsetExternal::addPreview(DocIterator const & /*inset_pos*/,
+	graphics::PreviewLoader & ploader) const	
 {
 	RenderMonitoredPreview * const ptr = renderer_->asMonitoredPreview();
 	if (!ptr)

src/insets/InsetExternal.h
@@ -139,7 +139,7 @@ private:
 	/// Update needed features for this inset.
 	void validate(LaTeXFeatures & features) const;
 	///
-	void addPreview(graphics::PreviewLoader &) const;
+	void addPreview(DocIterator const &, graphics::PreviewLoader &) const;
 	///
 	bool showInsetDialog(BufferView * bv) const;
 	///

src/insets/InsetInclude.cpp
@@ -949,7 +949,8 @@ void add_preview(RenderMonitoredPreview & renderer, InsetInclude const & inset,
 } // namespace anon
 
 
-void InsetInclude::addPreview(graphics::PreviewLoader & ploader) const
+void InsetInclude::addPreview(DocIterator const & /*inset_pos*/,
+	graphics::PreviewLoader & ploader) const
 {
 	Buffer const & buffer = ploader.buffer();
 	if (!preview_wanted(params(), buffer))

commit b56e4c5697305776476fae2e39540abd2a9bad98

Do not start loading a preview each time one is added. This caused LyX to generate LaTeX code of the document over and over again for each math inset. As a consequence, the loading of the User's Guide took ages.
Fixes a bug introduced in r32157 while fixing bug #4360.

git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@32352 a592a061-630c-0410-9148-cb99ea01b6c8

src/mathed/InsetMathHull.cpp
@@ -461,13 +461,13 @@ void InsetMathHull::addPreview(DocIterator const & inset_pos,
 	graphics::PreviewLoader & /*ploader*/) const
 {
 	if (RenderPreview::status() == LyXRC::PREVIEW_ON) {
-		reloadPreview(inset_pos);
+		preparePreview(inset_pos);
 	}
 }
 
 
-void InsetMathHull::reloadPreview(DocIterator const & pos) const  
-{  
+void InsetMathHull::preparePreview(DocIterator const & pos) const  
+{
 	Buffer const * buffer = pos.buffer();  
 
 	// collect macros at this position  
@@ -484,17 +484,16 @@ void InsetMathHull::reloadPreview(DocIterator const & pos) const
 		}
 	}  
 
-	// start preview  
 	docstring const snippet = macro_preamble.str() + latexString(*this);  
 	LYXERR(Debug::MACROS, "Preview snippet: " << snippet);  
 	preview_->addPreview(snippet, *buffer);  
-	preview_->startLoading(*buffer);  
 }  
 
 bool InsetMathHull::notifyCursorLeaves(Cursor const & old, Cursor & cur)
 {
 	if (RenderPreview::status() == LyXRC::PREVIEW_ON) {
-		reloadPreview(old);
+		preparePreview(old);
+		preview_->startLoading(buffer());
 		cur.updateFlags(Update::Force);
 	}
 	return false;

src/mathed/InsetMathHull.h
@@ -134,8 +134,9 @@ public:
 	///
 	void addPreview(DocIterator const & inset_pos,
 		graphics::PreviewLoader &) const;
-	/// Recreate the preview if preview is enabled.
-	void reloadPreview(DocIterator const & pos) const;
+	/// Prepare the preview if preview is enabled. A subsequent
+	/// call to reloadPreview will recreate the preview.
+	void preparePreview(DocIterator const & pos) const;
 	///
 	void initUnicodeMath() const;
 