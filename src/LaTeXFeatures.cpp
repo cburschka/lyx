28ce123a18e4f37d8ff8a81fba462b8d0b16ac51
Jürgen Spitzmüller <spitz@lyx.org>
2009-12-10 08:26:22 +0000

    Simplify the definition of multi-accented polytonic Greek, patch by Günter Milde.
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@32450 a592a061-630c-0410-9148-cb99ea01b6c8

--- src/LaTeXFeatures.cpp
+++ src/LaTeXFeatures.cpp
@@ -195,10 +195,9 @@ static docstring const changetracking_none_def = from_ascii(
 	"\\newcommand{\\lyxdeleted}[3]{}\n");
 
 static docstring const textgreek_def = from_ascii(
-	"\\providecommand*{\\perispomeni}{\\char126}\n"
 	"\\AtBeginDocument{\\DeclareRobustCommand{\\greektext}{%\n"
 	"  \\fontencoding{LGR}\\selectfont\\def\\encodingdefault{LGR}%\n"
-	"  \\renewcommand{\\~}{\\perispomeni}\n"
+	"  \\renewcommand{\\~}{\\char126}%\n"
 	"}}\n"
 	"\\DeclareRobustCommand{\\textgreek}[1]{\\leavevmode{\\greektext #1}}\n"
 	"\\DeclareFontEncoding{LGR}{}{}\n");
