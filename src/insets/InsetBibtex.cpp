e71268a7051e0f0f82fce8f1d45484bda967d629
Richard Heck <rgheck@comcast.net>
2009-12-14 16:04:33 +0000

    Add comment.
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@32527 a592a061-630c-0410-9148-cb99ea01b6c8

--- src/insets/InsetBibtex.cpp
+++ src/insets/InsetBibtex.cpp
@@ -898,6 +898,9 @@ void InsetBibtex::validate(LaTeXFeatures & features) const
 {
 	if (features.bufferParams().use_bibtopic)
 		features.require("bibtopic");
+	// FIXME XHTML
+	// It'd be better to be able to get this from an InsetLayout, but at present
+	// InsetLayouts do not seem really to work for things that aren't InsetTexts.
 	if (features.runparams().flavor == OutputParams::HTML)
 		features.addPreambleSnippet("<style type=\"text/css\">\n"
 			"div.bibtexentry { margin-left: 2em; text-indent: -2em; }\n"
