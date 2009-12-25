bae47150c634ca48b7a0a5561c6332720e0f1e61
Richard Heck <rgheck@comcast.net>
2009-12-25 23:18:38 +0000

    Must wrap everything...
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@32635 a592a061-630c-0410-9148-cb99ea01b6c8

--- src/mathed/InsetMathDots.cpp
+++ src/mathed/InsetMathDots.cpp
@@ -105,7 +105,7 @@ void InsetMathDots::mathmlize(MathStream & os) const
 		ent = "&vellip;";
 	else
 		LASSERT(false, ent = "&hellip;");
-	os << from_ascii(ent);
+	os << MTag("mi") << from_ascii(ent) << ETag("mi");
 }
 
 } // namespace lyx
