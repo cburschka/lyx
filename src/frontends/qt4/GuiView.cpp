cburschka/lyx

commit c04850645e6874e1cbc59730b228c7b4a2def979

When reloading a child document, reset the parent of the child to the parent of the child and not to the master.
git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@32738 a592a061-630c-0410-9148-cb99ea01b6c8

src/frontends/qt4/GuiView.cpp
@@ -697,7 +697,7 @@ void GuiView::dropEvent(QDropEvent * event)
 			cmd = FuncRequest(LFUN_FILE_OPEN, file);
 		}
 
-		// Asynchronously post the event. DropEvent usually come
+		// Asynchronously post the event. DropEvent usually comes
 		// from the BufferView. But reloading a file might close
 		// the BufferView from within its own event handler.
 		guiApp->dispatchDelayed(cmd);
@@ -2413,8 +2413,8 @@ void GuiView::reloadBuffer()
 void GuiView::reloadBuffer(Buffer * buf)
 {
 	FileName filename = buf->fileName();
-	Buffer const * master = buf->masterBuffer();
-	bool const is_child = master != buf;
+	Buffer const * parent = buf->parent();
+	bool const is_child = parent != buf;
 	// The user has already confirmed that the changes, if any, should
 	// be discarded. So we just release the Buffer and don't call closeBuffer();
 	theBufferList().release(buf);
@@ -2423,9 +2423,9 @@ void GuiView::reloadBuffer(Buffer * buf)
 	docstring str;
 	if (buf) {
 		// re-allocate master if necessary
-		if (is_child && theBufferList().isLoaded(master)
-		    && buf->masterBuffer() != master)
-			buf->setParent(master);
+		if (is_child && theBufferList().isLoaded(parent)
+		      && buf->parent() != parent)
+			buf->setParent(parent);
 		buf->updateLabels();
 		setBuffer(buf);
 		buf->errors("Parse");

commit 785418bd2bb5d4ccfc51393b3cfe868dfbf08ebe

Correct ugly/wrong code introduced in r32738. parent will never be equal to buf.
git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@32831 a592a061-630c-0410-9148-cb99ea01b6c8

src/frontends/qt4/GuiView.cpp
@@ -21,10 +21,10 @@
 #include "GuiApplication.h"
 #include "GuiCommandBuffer.h"
 #include "GuiCompleter.h"
-#include "GuiWorkArea.h"
 #include "GuiKeySymbol.h"
 #include "GuiToc.h"
 #include "GuiToolbar.h"
+#include "GuiWorkArea.h"
 #include "LayoutBox.h"
 #include "Menus.h"
 #include "TocModel.h"
@@ -2421,7 +2421,6 @@ void GuiView::reloadBuffer(Buffer * buf)
 	// e.g., read-only status could have changed due to version control
 	filename.refresh();
 	Buffer const * parent = buf->parent();
-	bool const is_child = parent != buf;
 	// The user has already confirmed that the changes, if any, should
 	// be discarded. So we just release the Buffer and don't call closeBuffer();
 	theBufferList().release(buf);
@@ -2430,7 +2429,7 @@ void GuiView::reloadBuffer(Buffer * buf)
 	docstring str;
 	if (buf) {
 		// re-allocate master if necessary
-		if (is_child && theBufferList().isLoaded(parent)
+		if (parent && theBufferList().isLoaded(parent)
 		      && buf->parent() != parent)
 			buf->setParent(parent);
 		buf->updateLabels();