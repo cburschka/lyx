c615a2eeeaecdfb5cfe06c8d4f4fee9e46263fda
Richard Heck <rgheck@comcast.net>
2010-01-08 14:38:15 +0000

    Do not reload on check-in if check-in is cancelled.
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@32881 a592a061-630c-0410-9148-cb99ea01b6c8

--- src/frontends/qt4/GuiView.cpp
+++ src/frontends/qt4/GuiView.cpp
@@ -2458,7 +2458,8 @@ void GuiView::dispatchVC(FuncRequest const & cmd)
 			break;
 		if (buffer->lyxvc().inUse() && !buffer->isReadonly()) {
 			msg = buffer->lyxvc().checkIn();
-			reloadBuffer();
+			if (!msg.empty())
+				reloadBuffer();
 		}
 		break;
 
