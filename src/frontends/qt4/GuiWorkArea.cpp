cburschka/lyx

commit 6a9f44eaa2801632b4aebbb66ed6528cc1f30827

Refactorization: Buffer::changed() can update the metrics as well, so we don't have to do it in scrollToCursor if we call Buffer::changed() anyway.
git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@33909 a592a061-630c-0410-9148-cb99ea01b6c8

src/frontends/qt4/GuiWorkArea.cpp
@@ -533,7 +533,8 @@ void GuiWorkArea::resizeBufferView()
 	buffer_view_->resize(viewport()->width(), viewport()->height());
 	if (cursor_in_view)
 		buffer_view_->scrollToCursor();
-	updateScreen();
+	else
+		updateScreen();
 
 	// Update scrollbars which might have changed due different
 	// BufferView dimension. This is especially important when the

src/BufferView.cpp
@@ -816,7 +816,7 @@ void BufferView::showCursor()
 void BufferView::showCursor(DocIterator const & dit, bool recenter)
 {
 	if (scrollToCursor(dit, recenter)) {
-		buffer_.changed(false);
+		buffer_.changed(true);
 		updateHoveredInset();
 	}
 }
@@ -824,8 +824,10 @@ void BufferView::showCursor(DocIterator const & dit, bool recenter)
 
 void BufferView::scrollToCursor()
 {
-	if (scrollToCursor(d->cursor_, false))
+	if (scrollToCursor(d->cursor_, false)) {
+		buffer_.changed(true);
 		updateHoveredInset();
+	}
 }
 
 
@@ -873,11 +875,7 @@ bool BufferView::scrollToCursor(DocIterator const & dit, bool recenter)
 			scrolled = scrollDown(ypos - height_ + defaultRowHeight() ); 
 
 		// else, nothing to do, the cursor is already visible so we just return.
-		if (scrolled != 0) {
-			updateMetrics();
-			return true;
-		}
-		return false;
+		return scrolled != 0;
 	}
 
 	// fix inline completion position
@@ -904,7 +902,6 @@ bool BufferView::scrollToCursor(DocIterator const & dit, bool recenter)
 	else
 		d->anchor_ypos_ = defaultRowHeight() * 2;
 
-	updateMetrics();
 	return true;
 }
 

commit e4b3e9b7bc6eab3f5fe72c507ca120df6c5e1a61

Fix the painting I broke in r33909.
git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@33912 a592a061-630c-0410-9148-cb99ea01b6c8

src/frontends/qt4/GuiWorkArea.cpp
@@ -533,8 +533,7 @@ void GuiWorkArea::resizeBufferView()
 	buffer_view_->resize(viewport()->width(), viewport()->height());
 	if (cursor_in_view)
 		buffer_view_->scrollToCursor();
-	else
-		updateScreen();
+	updateScreen();
 
 	// Update scrollbars which might have changed due different
 	// BufferView dimension. This is especially important when the