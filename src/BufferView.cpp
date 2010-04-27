cburschka/lyx

commit 24cd681863d42a2696848728c7b5e3cf6a106823

Fix bug 5573. http://bugzilla.lyx.org/show_bug.cgi?id=5573.
git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@28397 a592a061-630c-0410-9148-cb99ea01b6c8

src/BufferView.cpp
@@ -819,7 +819,8 @@ void BufferView::showCursor(DocIterator const & dit)
 		if (ypos - row_dim.ascent() < 0)
 			scrolled = scrollUp(- ypos + row_dim.ascent());
 		else if (ypos + row_dim.descent() > height_)
-			scrolled = scrollDown(ypos - height_ + row_dim.descent());
+			scrolled = scrollDown(ypos - defaultRowHeight());
+
 		// else, nothing to do, the cursor is already visible so we just return.
 		if (scrolled != 0) {
 			updateMetrics();
@@ -845,8 +846,10 @@ void BufferView::showCursor(DocIterator const & dit)
 		d->anchor_ypos_ = offset + pm.ascent();
 	else if (d->anchor_pit_ == max_pit)
 		d->anchor_ypos_ = height_ - offset - row_dim.descent();
+	else if (offset > height_)
+		d->anchor_ypos_ = height_ - offset - defaultRowHeight();
 	else
-		d->anchor_ypos_ = defaultRowHeight() * 2 - offset - row_dim.descent();
+		d->anchor_ypos_ = defaultRowHeight() * 2;
 
 	updateMetrics();
 	buffer_.changed();

commit abd26a734c6bf37de1fcf33498597edbf94b9ab6

branch: Fix bug #6141: Scrolling error with insets at top of file.
Fix the regression introduced in r28397 while fixing bug #5573.

see r34296, r28397 and #5573.

git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/branches/BRANCH_1_6_X@34307 a592a061-630c-0410-9148-cb99ea01b6c8

src/BufferView.cpp
@@ -839,10 +839,23 @@ bool BufferView::scrollToCursor(DocIterator const & dit, bool recenter)
 		int scrolled = 0;
 		if (recenter)
 			scrolled = scroll(ypos - height_/2);
+
+		// If the top part of the row falls of the screen, we scroll
+		// up to align the top of the row with the top of the screen.
 		else if (ypos - row_dim.ascent() < 0)
-			scrolled = scrollUp(- ypos + row_dim.ascent());
-		else if (ypos + row_dim.descent() > height_)
-			scrolled = scrollDown(ypos - height_ + defaultRowHeight());
+			scrolled = scrollUp(-ypos + row_dim.ascent());
+
+		// If the bottom of the row falls of the screen, we scroll down.
+		// However, we have to be careful not to scroll that much that
+		// the top falls of the screen.
+		else if (ypos + row_dim.descent() > height_) {
+			int ynew = height_ - row_dim.descent();
+			if (ynew < row_dim.ascent())
+				ynew = row_dim.ascent();
+			int const scroll = ypos - ynew;
+			scrolled = scrollDown(scroll);
+		}
+
 		// else, nothing to do, the cursor is already visible so we just return.
 		if (scrolled != 0) {
 			updateMetrics();

status.16x
@@ -313,6 +313,9 @@ What's new
 
 - Allow to change a shortcut back to a previous value.
 
+- Fix the painting of large insets at the top of the document
+  (bug 6141).
+
 
 * DOCUMENTATION AND LOCALIZATION
 