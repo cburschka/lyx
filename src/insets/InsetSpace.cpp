b3870eac5002a420f52894098d9da0fcb5823ef1
Pavel Sanda <sanda@lyx.org>
2010-04-28 21:50:06 +0000

    typo
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@34338 a592a061-630c-0410-9148-cb99ea01b6c8

--- src/insets/InsetSpace.cpp
+++ src/insets/InsetSpace.cpp
@@ -83,7 +83,7 @@ docstring InsetSpace::toolTip(BufferView const &, int, int) const
 		message = _("Quad Space");
 		break;
 	case InsetSpaceParams::QQUAD:
-		message = _(" Double Quad Space");
+		message = _("Double Quad Space");
 		break;
 	case InsetSpaceParams::ENSPACE:
 		message = _("Enspace");
