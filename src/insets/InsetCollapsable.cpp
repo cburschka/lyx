cburschka/lyx

commit 708b73cab792f7398e7b2f977dd6d2f98f6281d9

Fix bug #3900: Mathed corners displayed without mouse hover.
git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@34348 a592a061-630c-0410-9148-cb99ea01b6c8

src/insets/InsetCollapsable.cpp
@@ -63,6 +63,16 @@ InsetCollapsable::InsetCollapsable(InsetCollapsable const & rhs)
 }
 
 
+InsetCollapsable::~InsetCollapsable()
+{
+	map<BufferView const *, bool>::iterator it = mouse_hover_.begin();
+	map<BufferView const *, bool>::iterator end = mouse_hover_.end();
+	for (; it != end; ++it)
+		if (it->second)
+			it->first->clearLastInset(this);
+}
+
+
 InsetCollapsable::CollapseStatus InsetCollapsable::status(BufferView const & bv) const
 {
 	if (decoration() == InsetLayout::CONGLOMERATE)

src/insets/InsetCommand.cpp
@@ -63,6 +63,11 @@ InsetCommand::~InsetCommand()
 {
 	if (!mailer_name_.empty())
 		hideDialogs(mailer_name_, this);
+	map<BufferView const *, bool>::iterator it = mouse_hover_.begin();
+	map<BufferView const *, bool>::iterator end = mouse_hover_.end();
+	for (; it != end; ++it)
+		if (it->second)
+			it->first->clearLastInset(this);
 }
 
 

src/BufferView.cpp
@@ -403,11 +403,6 @@ bool BufferView::fitCursor()
 
 void BufferView::processUpdateFlags(Update::flags flags)
 {
-	// last_inset_ points to the last visited inset. This pointer may become  
-	// invalid because of keyboard editing. Since all such operations  
-	// causes screen update(), I reset last_inset_ to avoid such a problem.  
-	d->last_inset_ = 0;  
-
 	// This is close to a hot-path.
 	LYXERR(Debug::DEBUG, "BufferView::processUpdateFlags()"
 		<< "[fitcursor = " << (flags & Update::FitCursor)
@@ -1923,6 +1918,16 @@ void BufferView::updateHoveredInset() const
 		// This should be changed if it is further utilized.
 		buffer_.changed(false);
 	}
+}
+
+
+void BufferView::clearLastInset(Inset * inset) const
+{
+	if (d->last_inset_ != inset) {
+		LYXERR0("Wrong last_inset!");
+		LASSERT(false, /**/);
+	}
+	d->last_inset_ = 0;
 }
 
 

src/BufferView.h
@@ -308,6 +308,8 @@ public:
 	Inset * editedInset(std::string const & name) const;
 	/// Associate an inset associated with given dialog name.
 	void editInset(std::string const & name, Inset * inset);
+	///
+	void clearLastInset(Inset * inset) const;
 
 private:
 	/// noncopyable

src/insets/InsetCollapsable.h
@@ -37,6 +37,8 @@ public:
 	///
 	InsetCollapsable(InsetCollapsable const & rhs);
 	///
+	~InsetCollapsable();
+	///
 	InsetCollapsable * asInsetCollapsable() { return this; }
 	///
 	InsetCollapsable const * asInsetCollapsable() const { return this; }

src/mathed/InsetMathNest.cpp
@@ -91,6 +91,16 @@ InsetMathNest::InsetMathNest(InsetMathNest const & inset)
 {}
 
 
+InsetMathNest::~InsetMathNest()
+{
+	map<BufferView const *, bool>::iterator it = mouse_hover_.begin();
+	map<BufferView const *, bool>::iterator end = mouse_hover_.end();
+	for (; it != end; ++it)
+		if (it->second)
+			it->first->clearLastInset(this);
+}
+
+
 InsetMathNest & InsetMathNest::operator=(InsetMathNest const & inset)
 {
 	cells_ = inset.cells_;

src/mathed/InsetMathNest.h
@@ -31,7 +31,7 @@ public:
 	/// nestinsets have a fixed size to start with
 	InsetMathNest(Buffer * buf, idx_type ncells);
 	///
-	virtual ~InsetMathNest() {}
+	virtual ~InsetMathNest();
 	///
 	void setBuffer(Buffer &);
 

commit 9c8e7429fabd3b3c39a3374667ae624c3dbdcd01

Fix the assertion assert introduced in r34348 which got fired when eg. deselecting an InsetCommand:
- we have to have a copy constructor in InsetCommand to clear the mouse_hover_ map and not to copy it,
- there is no need to have mouse_hover_() in the initialisation list,
- there is no need to copy the auto_open_ map.


git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@34353 a592a061-630c-0410-9148-cb99ea01b6c8

src/insets/InsetCollapsable.cpp
@@ -50,17 +50,15 @@ InsetCollapsable::InsetCollapsable(Buffer * buf, InsetText::UsePlain ltype)
 }
 
 
+// The sole purpose of this copy constructor is to make sure
+// that the mouse_hover_ map is not copied and remains empty.
 InsetCollapsable::InsetCollapsable(InsetCollapsable const & rhs)
 	: InsetText(rhs),
 	  status_(rhs.status_),
 	  labelstring_(rhs.labelstring_),
 	  button_dim(rhs.button_dim),
-	  openinlined_(rhs.openinlined_),
-	  auto_open_(rhs.auto_open_),
-	  // the sole purpose of this copy constructor
-	  mouse_hover_()
-{
-}
+	  openinlined_(rhs.openinlined_)
+{}
 
 
 InsetCollapsable::~InsetCollapsable()
@@ -182,7 +180,7 @@ Dimension InsetCollapsable::dimensionCollapsed(BufferView const & bv) const
 
 void InsetCollapsable::metrics(MetricsInfo & mi, Dimension & dim) const
 {
-	auto_open_[mi.base.bv] =  mi.base.bv->cursor().isInside(this);
+	auto_open_[mi.base.bv] = mi.base.bv->cursor().isInside(this);
 
 	FontInfo tmpfont = mi.base.font;
 	mi.base.font = getLayout().font();
@@ -250,7 +248,7 @@ void InsetCollapsable::draw(PainterInfo & pi, int x, int y) const
 {
 	BufferView const & bv = *pi.base.bv;
 
-	auto_open_[&bv] =  bv.cursor().isInside(this);
+	auto_open_[&bv] = bv.cursor().isInside(this);
 
 	FontInfo tmpfont = pi.base.font;
 	pi.base.font = getLayout().font();

src/insets/InsetCommand.cpp
@@ -59,6 +59,13 @@ InsetCommand::InsetCommand(Buffer * buf, InsetCommandParams const & p,
 {}
 
 
+// The sole purpose of this copy constructor is to make sure
+// that the mouse_hover_ map is not copied and remains empty.
+InsetCommand::InsetCommand(InsetCommand const & rhs)
+	: Inset(rhs), p_(rhs.p_), mailer_name_(rhs.mailer_name_)
+{}
+
+
 InsetCommand::~InsetCommand()
 {
 	if (!mailer_name_.empty())

src/insets/InsetCommand.h
@@ -38,6 +38,8 @@ public:
 	InsetCommand(Buffer *, InsetCommandParams const &,
 		std::string const & mailer_name);
 	///
+	InsetCommand(InsetCommand const & rhs);
+	///
 	~InsetCommand();
 
 	/// returns true if params are successfully read