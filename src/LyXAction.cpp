555df7e2a19dcffae135fb58ad67cd1f09511b4e
Pavel Sanda <sanda@lyx.org>
2010-05-24 13:38:15 +0000

    *doxy
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@34487 a592a061-630c-0410-9148-cb99ea01b6c8

--- src/LyXAction.cpp
+++ src/LyXAction.cpp
@@ -2574,7 +2574,8 @@ void LyXAction::init()
  * \li Notion: Skipping "auto" when allowauto is false.
  * \li Syntax: toolbar-toggle <NAME> [allowauto]
  * \li Params: <NAME>: standard|extra|table|math|mathmacrotemplate|
-                       minibuffer|review|view/update|math_panels|vcs
+                       minibuffer|review|view/update|math_panels|vcs|
+                       view-others|update-others
  * \li Origin: Edwin, 21 May 2007
  * \endvar
  */
