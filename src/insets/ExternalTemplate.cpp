53743d59315bcd08820047275f4a9f74e875a661
Vincent van Ravesteijn <vfr@lyx.org>
2010-06-01 07:50:22 +0000

    branch: Fix bug #6677: GUI button disappears in external template inset.
    Initialize the preview_mode variable.
    
    see r34346.
    
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/branches/BRANCH_1_6_X@34569 a592a061-630c-0410-9148-cb99ea01b6c8

--- src/insets/ExternalTemplate.cpp
+++ src/insets/ExternalTemplate.cpp
@@ -50,7 +50,7 @@ static TransformIDTranslator const & transformIDTranslator()
 
 // We have to have dummy default commands for security reasons!
 Template::Template()
-	: inputFormat("*")
+	: inputFormat("*"), preview_mode(PREVIEW_OFF), automaticProduction(false)
 {}
 
 
