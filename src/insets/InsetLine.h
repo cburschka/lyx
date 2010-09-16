3b512d591511c5cbb2f76bbbc53a825ddf0cad67
Abdelrazak Younes <younes@lyx.org>
2010-09-16 10:14:46 +0000

    header cleanup and doxy.
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@35412 a592a061-630c-0410-9148-cb99ea01b6c8

--- src/insets/InsetLine.h
+++ src/insets/InsetLine.h
@@ -21,48 +21,46 @@ namespace lyx {
 
 class LaTeXFeatures;
 
-class InsetLine : public InsetCommand {
+class InsetLine : public InsetCommand
+{
 public:
-	///
 	InsetLine(Buffer * buf, InsetCommandParams const &);
-	///
+
+	/// InsetCommand inherited methods.
+	//@{
+	docstring screenLabel() const;
+	static ParamInfo const & findInfo(std::string const &);
+	static std::string defaultCommand() { return "rule"; };
+	static bool isCompatibleCommand(std::string const & s)
+	{ return s == "rule"; }
+	//@}
+
+private:
+
+	/// Inset inherited methods.
+	//@{
+	InsetCode lyxCode() const { return LINE_CODE; }
+	Dimension const dimension(BufferView const &) const;
 	int docbook(odocstream &, OutputParams const &) const;
 	/// Does nothing at the moment.
 	docstring xhtml(XHTMLStream &, OutputParams const &) const;
-	///
-	InsetCode lyxCode() const { return LINE_CODE; }
-	///
 	bool hasSettings() const { return true; }
-	///
-	docstring screenLabel() const;
-	///
-	static ParamInfo const & findInfo(std::string const &);
-	///
-	static std::string defaultCommand() { return "rule"; };
-	///
-	static bool isCompatibleCommand(std::string const & s)
-		{ return s == "rule"; }
-	///
-	Dimension const dimension(BufferView const &) const;
-
-private:
-	///
 	void metrics(MetricsInfo &, Dimension &) const;
-	///
 	void draw(PainterInfo & pi, int x, int y) const;
-	///
 	int latex(odocstream &, OutputParams const &) const;
-	///
 	int plaintext(odocstream &, OutputParams const &) const;
-	///
 	void doDispatch(Cursor & cur, FuncRequest & cmd);
-	///
 	bool getStatus(Cursor & cur, FuncRequest const & cmd, FuncStatus &) const;
-	///
 	Inset * clone() const { return new InsetLine(*this); }
-	///
+	//@}
+
+	/// cached line height and offset.
+	/// These value are independent of the BufferView size and thus
+	/// can be shared between views.
+	//@{
 	mutable int height_;
 	mutable int offset_;
+	//@}
 };
 
 
