3a400812e16102fab7f67862e979b863fc80a1f3
Uwe Stöhr <uwestoehr@web.de>
2010-09-17 23:12:19 +0000

    InsetLine.h revert changes made in r35417
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@35428 a592a061-630c-0410-9148-cb99ea01b6c8

--- src/insets/InsetLine.h
+++ src/insets/InsetLine.h
@@ -30,11 +30,8 @@ public:
 	/// InsetCommand inherited methods.
 	//@{
 	docstring screenLabel() const;
-	///
 	static ParamInfo const & findInfo(std::string const &);
-	///
 	static std::string defaultCommand() { return "rule"; };
-	///
 	static bool isCompatibleCommand(std::string const & s)
 	{ return s == "rule"; }
 	//@}
@@ -44,27 +41,17 @@ private:
 	/// Inset inherited methods.
 	//@{
 	InsetCode lyxCode() const { return LINE_CODE; }
-	///
 	Dimension const dimension(BufferView const &) const;
-	///
 	int docbook(odocstream &, OutputParams const &) const;
 	/// Does nothing at the moment.
 	docstring xhtml(XHTMLStream &, OutputParams const &) const;
-	///
 	bool hasSettings() const { return true; }
-	///
 	void metrics(MetricsInfo &, Dimension &) const;
-	///
 	void draw(PainterInfo & pi, int x, int y) const;
-	///
 	int latex(odocstream &, OutputParams const &) const;
-	///
 	int plaintext(odocstream &, OutputParams const &) const;
-	///
 	void doDispatch(Cursor & cur, FuncRequest & cmd);
-	///
 	bool getStatus(Cursor & cur, FuncRequest const & cmd, FuncStatus &) const;
-	///
 	Inset * clone() const { return new InsetLine(*this); }
 	//@}
 
