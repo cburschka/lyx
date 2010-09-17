6faa1b37135ef461b03059c860de8e062b560d98
Uwe Stöhr <uwestoehr@web.de>
2010-09-17 01:32:23 +0000

    GuiLine: honor Abdel as author
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@35420 a592a061-630c-0410-9148-cb99ea01b6c8

--- src/frontends/qt4/GuiLine.cpp
+++ src/frontends/qt4/GuiLine.cpp
@@ -3,6 +3,7 @@
  * This file is part of LyX, the document processor.
  * Licence details can be found in the file COPYING.
  *
+ * \author Abdelrazak Younes
  * \author Uwe Stöhr
  *
  * Full author contact details are available in file CREDITS.
