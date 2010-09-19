e6b883ebd360a230152daf3f65563cbe23cd4a76
Pavel Sanda <sanda@lyx.org>
2010-09-19 21:17:22 +0000

    Backport autoconf patch.
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/branches/BRANCH_1_6_X@35451 a592a061-630c-0410-9148-cb99ea01b6c8

--- autogen.sh
+++ autogen.sh
@@ -38,11 +38,11 @@ test "$autoversion" != "" && {
 
 
 case $autoversion in
-    *' '2.59[cd]|*' '2.60[ab]|*' '2.6[0-5])
+    *' '2.59[cd]|*' '2.60[ab]|*' '2.6[0-57])
 	;;
     *)
 	echo "This autoconf version is not supported by LyX."
-	echo "LyX only supports autoconf 2.59c-2.65."
+	echo "LyX only supports autoconf 2.59c-2.65, 2.67."
 	exit 1
 	;;
 esac
