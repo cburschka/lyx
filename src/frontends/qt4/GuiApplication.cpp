e703e02db59f9e6e1836c70bc94deff8ba5eb01c
Vincent van Ravesteijn <vfr@lyx.org>
2010-10-29 19:07:41 +0000

    Compile fix in case DEVEL_VERSION is defined.
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@35919 a592a061-630c-0410-9148-cb99ea01b6c8

--- src/frontends/qt4/GuiApplication.cpp
+++ src/frontends/qt4/GuiApplication.cpp
@@ -1352,9 +1352,12 @@ void GuiApplication::dispatch(FuncRequest const & cmd, DispatchResult & dr)
 		current_view_->message(bformat(_("Opening help file %1$s..."),
 					       makeDisplayPath(fname.absFileName())));
 		Buffer * buf = current_view_->loadDocument(fname, false);
+
 #ifndef DEVEL_VERSION
 		if (buf)
 			buf->setReadonly(true);
+#else
+		buf;
 #endif
 		break;
 	}
