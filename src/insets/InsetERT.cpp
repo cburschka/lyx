2afa7eabd0daabe45281f3d47c63cecd687051b0
Jürgen Spitzmüller <spitz@lyx.org>
2010-11-12 09:31:55 +0000

    * revert r36189, since there is no consensus for this change.
    Happy Friday, everybody!
    
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@36257 a592a061-630c-0410-9148-cb99ea01b6c8

--- src/insets/InsetERT.cpp
+++ src/insets/InsetERT.cpp
@@ -140,9 +140,9 @@ bool InsetERT::getStatus(Cursor & cur, FuncRequest const & cmd,
 docstring const InsetERT::buttonLabel(BufferView const & bv) const
 {
 	if (decoration() == InsetLayout::CLASSIC)
-		return isOpen(bv) ? _("TeX") : getNewLabel(_("TeX"));
+		return isOpen(bv) ? _("ERT") : getNewLabel(_("ERT"));
 	else
-		return getNewLabel(_("TeX"));
+		return getNewLabel(_("ERT"));
 }
 
 
