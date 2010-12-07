571663dcf8556195db4e0841282a87a9ff4e7553
Pavel Sanda <sanda@lyx.org>
2010-12-07 00:13:18 +0000

    Replay r36747
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@36756 a592a061-630c-0410-9148-cb99ea01b6c8

--- src/frontends/qt4/GuiView.h
+++ src/frontends/qt4/GuiView.h
@@ -232,9 +232,9 @@ private Q_SLOTS:
 
 	/// For completion of autosave or export threads.
 	void processingThreadStarted();
-  void processingThreadFinished(bool show_errors);
+	void processingThreadFinished(bool show_errors);
 	void processingThreadFinished();
-  void autoSaveThreadFinished();
+	void autoSaveThreadFinished();
 	void indicateProcessing();
 
 	/// must be called in GUI thread
