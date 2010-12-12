cburschka/lyx

commit de7964e501f65c1893f909ee20d3825fe49085af

Fix bug #4889: Painting problem with some insets. RowPainter::paintOnlyInsets is only meant to paint nested Insets, so I guess only the InsetText based insets.
git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@36630 a592a061-630c-0410-9148-cb99ea01b6c8

src/rowpainter.cpp
@@ -742,7 +742,7 @@ void RowPainter::paintOnlyInsets()
 	for (pos_type pos = row_.pos(); pos != end; ++pos) {
 		// If outer row has changed, nested insets are repaint completely.
 		Inset const * inset = par_.getInset(pos);
-		if (!inset)
+		if (!inset || !inset->asInsetText())
 			continue;
 		if (x_ > pi_.base.bv->workWidth()
 		    || !cache.getInsets().has(inset))

commit 1f76d67c662872789b461b1d3b9fac9f1a4dc3c7

Fix bug #7164: Text in tables not painted immediately.
Fixes the regression introduces in r36630. I already fixed this, but forgot to commit apparently.

git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@36848 a592a061-630c-0410-9148-cb99ea01b6c8

src/rowpainter.cpp
@@ -742,7 +742,9 @@ void RowPainter::paintOnlyInsets()
 	for (pos_type pos = row_.pos(); pos != end; ++pos) {
 		// If outer row has changed, nested insets are repaint completely.
 		Inset const * inset = par_.getInset(pos);
-		if (!inset || !inset->asInsetText())
+		bool const nested_inset = inset &&
+			(inset->asInsetText() || inset->asInsetTabular());
+		if (!nested_inset)
 			continue;
 		if (x_ > pi_.base.bv->workWidth()
 		    || !cache.getInsets().has(inset))