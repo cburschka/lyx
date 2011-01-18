bc176dc904dbce62615949e70cbb562ea79c6a58
Richard Heck <rgheck@comcast.net>
2011-01-18 16:50:43 +0000

    Add conversion of \language_use_babel.
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@37254 a592a061-630c-0410-9148-cb99ea01b6c8

--- lib/scripts/prefs2prefs_prefs.py
+++ lib/scripts/prefs2prefs_prefs.py
@@ -24,20 +24,32 @@
 no_match = (False, [])
 
 def remove_obsolete(line):
-  tags = ("\\use_tempdir", "\\spell_command", "\\personal_dictionary",
-        "\\plaintext_roff_command", "\\use_alt_language", 
-        "\\use_escape_chars", "\\use_input_encoding",
-        "\\use_personal_dictionary", "\\use_pspell",
-        "\\use_spell_lib")
-  line = line.lstrip()
-  for tag in tags:
-    if line.startswith(tag):
-      return (True, "")
-  return no_match
+	tags = ("\\use_tempdir", "\\spell_command", "\\personal_dictionary",
+				"\\plaintext_roff_command", "\\use_alt_language", 
+				"\\use_escape_chars", "\\use_input_encoding",
+				"\\use_personal_dictionary", "\\use_pspell",
+				"\\use_spell_lib")
+	line = line.lstrip()
+	for tag in tags:
+		if line.startswith(tag):
+			return (True, "")
+	return no_match
 
+def language_use_babel(line):
+	if not line.startswith("\language_use_babel"):
+		return no_match
+	re_lub = re.compile(r'^\\language_use_babel\s+(true|false)')
+	m = re_lub.match(line)
+	val = m.group(1)
+	newval = '0'
+	if val == 'false':
+		newval = '3'
+	newline = "\\language_package_selection " + newval
+	return (True, newline)
 
 conversions = [
 	[ # this will be a long list of conversions for format 0
-    remove_obsolete
+		remove_obsolete,
+		language_use_babel
 	] # end conversions for format 0
 ]
