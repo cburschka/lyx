cburschka/lyx

commit af3fe8ab883d687d9800eb81f4452a63e6b0239e

Fix bug #6070: Width unit cannot be set in tabular dialog.
git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/branches/BRANCH_1_6_X@30650 a592a061-630c-0410-9148-cb99ea01b6c8

src/frontends/qt4/GuiTabular.cpp
@@ -53,6 +53,8 @@ GuiTabular::GuiTabular(GuiView & lv)
 	bottomspaceED->setValidator(new LengthValidator(bottomspaceED));
 	interlinespaceED->setValidator(new LengthValidator(interlinespaceED));
 
+	widthUnit->setCurrentItem(useMetricUnits() ? Length::CM : Length::IN);
+
 	connect(topspaceED, SIGNAL(returnPressed()),
 		this, SLOT(topspace_changed()));
 	connect(topspaceUnit, SIGNAL(selectionChanged(lyx::Length::UNIT)),
@@ -745,10 +747,10 @@ void GuiTabular::updateContents()
 	interlinespaceCO->setEnabled(!isReadonly);
 
 	string colwidth;
-	if (!pwidth.zero())
+	if (!pwidth.zero()) {
 		colwidth = pwidth.asString();
-	lengthToWidgets(widthED, widthUnit,
-		colwidth, default_unit);
+		lengthToWidgets(widthED, widthUnit, colwidth, default_unit);
+	}
 
 	widthED->setEnabled(!isReadonly);
 	widthUnit->setEnabled(!isReadonly);

status.16x
@@ -72,6 +72,8 @@ What's new
 
 - Reverse searching added to the merge changes dialog (bug 2213).
 
+- Copy as Reference is now implemented for equation labels too (bug 5948).
+
 
 * DOCUMENTATION AND LOCALIZATION
 
@@ -228,8 +230,6 @@ What's new
   cursor could end up on a row that is only partly visible. This causes
   the cursor to end up somewhere else when scrolling back (bug 5944).
 
-- Copy as Reference is now implemented for equation labels too (bug 5948).
-
 - Fix selection of math insets on some archs.
 
 - Fix of painter glitches caused by unitialized variables (bug 6077).
@@ -238,6 +238,9 @@ What's new
 
 - Fix the enabling of the page height and width fields in the Document
   Settings dialog.
+  
+- The width unit of table cells can be set before entering the
+  value (bug 6070).
 
 
 * DOCUMENTATION AND LOCALIZATION

commit 11ea521aa26f347b544ac4bb2693aced7126d350

* GuiTabular.cpp: Fix regression introduced in r30650 (LyX 1.6.4, not in trunk):   	The column width field was not cleared for non-fixed columns, leading to unwanted width applications.
I wonder why this bug was never reported.

git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/branches/BRANCH_1_6_X@37371 a592a061-630c-0410-9148-cb99ea01b6c8

src/frontends/qt4/GuiTabular.cpp
@@ -737,7 +737,8 @@ void GuiTabular::updateContents()
 	if (!pwidth.zero()) {
 		colwidth = pwidth.asString();
 		lengthToWidgets(widthED, widthUnit, colwidth, default_unit);
-	}
+	} else
+		widthED->clear();
 
 	// if there is a LaTeX argument, the width and alignment will be overwritten
 	// therefore disable them in this case

status.16x
@@ -130,6 +130,9 @@ What's new
 
 - Do not allow to put the cursor in an InsetInfo. Instead, a context
   menu item is added to facilitate copying of the contents (bug 5446).
+  
+- Clear column width field in the tabular dialog when moving in a column
+  without a fixed width.
 
 - Fix the display of the horizontal alignment of table cells (bug 7007).
 