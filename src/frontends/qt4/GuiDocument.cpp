5a2121f77eddd8c0762bae6849d2cc0d7a96c09e
Jürgen Spitzmüller <spitz@lyx.org>
2011-02-09 08:23:11 +0000

    Check for fontspec an configure the GUI accordingly. If fontspec is not found, the "Use non-TeX fonts" checkbox is disabled, and the tooltip hints at installing the package. Also add fontspec to LaTeXConfig.lyx, documenting that v2 of the package is needed for LuaTeX support.
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@37567 a592a061-630c-0410-9148-cb99ea01b6c8

--- src/frontends/qt4/GuiDocument.cpp
+++ src/frontends/qt4/GuiDocument.cpp
@@ -798,6 +798,11 @@ GuiDocument::GuiDocument(GuiView & lv)
 		fontModule->fontsDefaultCO->addItem(
 			qt_(GuiDocument::fontfamilies_gui[n]));
 
+	if (!LaTeXFeatures::isAvailable("fontspec"))
+		fontModule->osFontsCB->setToolTip(
+			qt_("Use OpenType- and TrueType fonts directly (requires XeTeX or LuaTeX)\n"
+			    "You need to install the package \"fontspec\" to use this feature"));
+
 
 	// page layout
 	pageLayoutModule = new UiWidget<Ui::PageLayoutUi>;
@@ -2874,9 +2879,12 @@ void GuiDocument::paramsToDialog()
 	if (index == -1)
 		index = 0;
 	outputModule->defaultFormatCO->setCurrentIndex(index);
-	fontModule->osFontsCB->setEnabled(bp_.baseClass()->outputType() == lyx::LATEX);
+	bool const os_fonts_available =
+		bp_.baseClass()->outputType() == lyx::LATEX
+		&& LaTeXFeatures::isAvailable("fontspec");
+	fontModule->osFontsCB->setEnabled(os_fonts_available);
 	fontModule->osFontsCB->setChecked(
-		bp_.baseClass()->outputType() == lyx::LATEX && bp_.useNonTeXFonts);
+		os_fonts_available && bp_.useNonTeXFonts);
 
 	outputModule->outputsyncCB->setChecked(bp_.output_sync);
 	outputModule->synccustomCB->setEditText(toqstr(bp_.output_sync_macro));
@@ -2889,7 +2897,7 @@ void GuiDocument::paramsToDialog()
 	updateFontsize(documentClass().opt_fontsize(),
 			bp_.fontsize);
 
-	if (bp_.useNonTeXFonts) {
+	if (bp_.useNonTeXFonts && os_fonts_available) {
 		fontModule->fontencLA->setEnabled(false);
 		fontModule->fontencCO->setEnabled(false);
 		fontModule->fontencLE->setEnabled(false);
