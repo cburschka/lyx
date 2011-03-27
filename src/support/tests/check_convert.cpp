b1c7450f7360648fb0be0dbe87f375cebe83d508
Enrico Forestieri <forenr@lyx.org>
2011-03-27 23:47:58 +0000

    Fix make check
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@38088 a592a061-630c-0410-9148-cb99ea01b6c8

--- src/support/tests/check_convert.cpp
+++ src/support/tests/check_convert.cpp
@@ -11,6 +11,12 @@ using namespace std;
 
 
 namespace lyx {
+	// Dummy LyXRC support
+	struct LyXRC { string icon_set; } lyxrc;
+
+	// Keep the linker happy on Windows
+	void lyx_exit(int) {}
+
 	docstring const _(string const & s) { return from_ascii(s); }
 }
 
