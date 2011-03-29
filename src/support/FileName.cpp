458cf0c513588f186dd90bc34ad78d0c12737104
Pavel Sanda <sanda@lyx.org>
2011-03-29 11:57:44 +0000

    Fix #5891 (the simpler version).
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/branches/BRANCH_1_6_X@38126 a592a061-630c-0410-9148-cb99ea01b6c8

--- src/support/FileName.cpp
+++ src/support/FileName.cpp
@@ -557,7 +557,10 @@ unsigned long FileName::checksum() const
 		return result;
 
 	struct stat info;
-	fstat(fd, &info);
+	if (fstat(fd, &info)) {
+		close(fd);
+		return result;
+	}
 
 	void * mm = mmap(0, info.st_size, PROT_READ,
 			 MAP_PRIVATE, fd, 0);
