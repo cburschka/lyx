0d40d710ca6d016c21590f634505790bdec4f882
Richard Heck <rgheck@comcast.net>
2011-05-09 15:27:42 +0000

    Add updatestats.sh to branch, too.
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/branches/BRANCH_2_0_X@38660 a592a061-630c-0410-9148-cb99ea01b6c8

---
+++ development/tools/updatestats.sh
@@ -0,0 +1,57 @@
+#!/bin/sh
+# A script to update the LyX i18n.inc statistics
+
+FARM="/cvs/lyx-www/farm/cookbook/LyX";
+
+# Sanity check
+if [ ! -f "$FARM/i18n.php" ]; then 
+  echo "$FARM does not look like LyX's www tree!";
+  exit 1;
+fi
+
+# Get us to the root of the tree we are in.
+MYDIR=${0%updatestats.sh};
+if [ -n "$MYDIR" ]; then
+  cd $MYDIR;
+fi
+LYXROOT=$(pwd);
+cd ../../;
+
+# Are we in trunk or branch?
+TRUNK="TRUE";
+if ls status.* 2>/dev/null | grep -q status; then 
+  TRUNK="";
+fi
+
+# Sanity check
+if ! cd po/; then 
+  echo "Cannot cd to po/ directory!";
+  pwd
+  exit 1;
+fi
+
+echo Running make i18n.inc...
+make i18n.inc;
+if [ -n "$TRUNK" ]; then
+  mv i18n.inc i18n_trunk.inc
+  I18NFILE=i18n_trunk.inc;
+else
+  I18NFILE=i18n.inc;
+fi
+
+if ! cd $FARM; then
+  echo "Unable to cd to $FARM!";
+  exit 1;
+fi
+
+echo
+echo Updating the www-user tree...
+svn up
+
+echo Copying $I18NFILE...;
+cp $LYXROOT/po/$I18NFILE .;
+
+echo Committing...;
+svn commit -m "* $I18NFILE: update stats" $I18NFILE;
+
+echo DONE!
