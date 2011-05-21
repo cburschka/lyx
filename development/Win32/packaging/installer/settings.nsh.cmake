1f25841d5983e0ee96876fd6b3e41ebdc14798ed
Peter Kümmel <syntheticpp@gmx.net>
2011-05-21 08:57:52 +0000

    cmake: prepage ready-to-use nsis files generation
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@38795 a592a061-630c-0410-9148-cb99ea01b6c8

---
+++ development/Win32/packaging/installer/settings.nsh.cmake
@@ -0,0 +1,48 @@
+/*
+
+Settings for LyX installer
+
+These typically need to be modified for each LyX release
+
+*/
+
+#--------------------------------
+# Version number
+
+!define APP_VERSION_MAJOR 2
+!define APP_VERSION_MINOR 0
+!define APP_VERSION_REVISION 0
+!define APP_VERSION_BUILD 0 # Start with 1 for the installer releases of each version
+
+!define /date APP_VERSION "${APP_VERSION_MAJOR}.${APP_VERSION_MINOR}svn %Y%m%d" # Version to display
+
+#--------------------------------
+# Installer file name
+
+# Typical names for the release are "LyX-2.0.0-1-Installer.exe" etc.
+
+!ifndef ExeFile
+  !define /date ExeFile "LyX-${APP_VERSION_MAJOR}${APP_VERSION_MINOR}svn-%Y%m%d-Installer.exe"
+!endif
+!ifndef BundleExeFile
+  !define /date BundleExeFile "LyX-${APP_VERSION_MAJOR}${APP_VERSION_MINOR}svn-%Y%m%d-Installer-Bundle.exe"
+!endif
+!ifndef MinimalExeFile
+  !define /date MinimalExeFile "LyX-${APP_VERSION_MAJOR}${APP_VERSION_MINOR}svn-%Y%m%d-Installer-Minimal.exe"
+!endif
+
+#--------------------------------
+# File locations
+
+!define FILES_LYX "C:\LyX\lyx-20-install"
+!define FILES_DEPS "C:\LyX\lyx-20-build\msvc2010-deps\deps20"
+!define FILES_BUNDLE "C:\LyX\depsbundle"
+!define FILES_QT "C:\Qt\qt-everywhere-opensource-src-4.7.1"
+
+#--------------------------------
+# MiKTeX
+# Sizes in KB
+
+!define SETUPFILE_LATEX "basic-miktex-2.9.4146.exe"
+!define SIZE_DOWNLOAD_LATEX 160106
+!define SIZE_LATEX 380000
