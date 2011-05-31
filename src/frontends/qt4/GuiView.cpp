8bbe99f10032a1d3de27f60a76881248a879ce20
Pavel Sanda <sanda@lyx.org>
2011-05-31 20:23:06 +0000

    Another glitch ;)
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@38906 a592a061-630c-0410-9148-cb99ea01b6c8

--- src/frontends/qt4/GuiView.cpp
+++ src/frontends/qt4/GuiView.cpp
@@ -3140,7 +3140,7 @@ void GuiView::dispatch(FuncRequest const & cmd, DispatchResult & dr)
 #if QT_VERSION < 0x040400
 			if (!doc_buffer->doExport(argument, false)) {
 				dr.setError(true);
-				dr.setMessage(bformat(_("Error exporting to format: %1$s."),
+				dr.setMessage(bformat(_("Error exporting to format: %1$s"),
 					cmd.argument()));
 			}
 #else
