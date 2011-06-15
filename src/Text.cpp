b0056c65545ae5156900277879cc103a0f11c3ed
Richard Heck <rgheck@comcast.net>
2011-06-15 11:45:18 +0000

    Fix bug #7629. Problem was I only reverted part of a previous fix that broke the string freeze.
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/branches/BRANCH_2_0_X@39059 a592a061-630c-0410-9148-cb99ea01b6c8

--- src/Text.cpp
+++ src/Text.cpp
@@ -356,7 +356,7 @@ void Text::readParToken(Paragraph & par, Lexer & lex,
 		// of this document. For example, when you apply class article to a beamer document,
 		// all unknown layouts such as frame will be added to document class article so that
 		// these layouts can keep their original names.
-
+		tclass.addLayoutIfNeeded(layoutname);
 		par.setLayout(bp.documentClass()[layoutname]);
 
 		// Test whether the layout is obsolete.
