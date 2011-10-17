668ef9c36581c747515a237b5cf9297c16f1e96c
Peter Kümmel <syntheticpp@gmx.net>
2011-10-17 20:42:02 +0000

    cmake: build and install lyx by calling one batch script
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@39879 a592a061-630c-0410-9148-cb99ea01b6c8

--- development/cmake/build.bat
+++ development/cmake/build.bat
@@ -1,20 +1,26 @@
 echo off
 
 echo -------------------------------------------------------------------------------------
+echo -
 echo Usage build.bat devel/install/deploy STUDIO(optional)
 echo     devel   - Builds Visual Studio project files for development on LyX
 echo     install - Builds Visual Studio project files with all enabled for installation
 echo     deploy  - Builds Makefiles and installs LyX
 echo     STUDIO  - Used Visual Studio version, default is "Visual Studio 9 2008"
 echo               use "Visual Studio 10" for Visual Studio 10
+echo
 echo -------------------------------------------------------------------------------------
-echo Be sure you've set qmake in PATH and set the variables:
-echo     GNUWIN32_DIR
+echo -
+echo Call this script from a build directory or set variables (or edit this file)
 echo     LYX_SOURCE
 echo     LXY_BUILD
-echo Or edit this file.
+echo
+echo Be sure you've set qmake in PATH.
+echo -
+echo GNUWin32 will be downloaded if you don't set the variable GNUWIN32_DIR
 echo -------------------------------------------------------------------------------------
-
+echo -
+echo -
 
 if [%1]==[] (
 	echo ERROR: no options.
@@ -23,36 +29,33 @@ if [%1]==[] (
 )
 
 REM Add path to qmake here or set PATH correctly on your system.
-set PATH=D:\Qt\bin;%PATH%
+::set PATH=D:\Qt\bin;%PATH%
 
 REM Edit pathes here or set the environment variables on you system.
-set GNUWIN32_DIR=D:\LyXSVN\lyx-devel\lyx-windows-deps-msvc2008
-set LYX_SOURCE=D:\LyXSVN\lyx-devel
-set LYX_BUILD=D:\LyXSVN\lyx-devel\compile-result
-
-if [%LYX_BUILD%]==[] (
-	echo ERROR:  LYX_BUILD not set.
-	echo Exiting now.
-	goto :eof
-)
+::set GNUWIN32_DIR=D:\LyXSVN\lyx-devel\lyx-windows-deps-msvc2008
+::set LYX_SOURCE=D:\LyXSVN\lyx-devel
+::set LYX_BUILD=D:\LyXSVN\lyx-devel\compile-result
 
 if [%LYX_SOURCE%]==[] (
-	echo ERROR:  LYX_SOURCE not set.
-	echo Exiting now.
-	goto :eof
+	set LYX_SOURCE=%~DP0\..\..
 )
+echo LyX source: "%LYX_SOURCE%"
+
+if [%LYX_BUILD%]==[] (
+	set LYX_BUILD=%CD%
+)
+echo LyX build : "%LYX_BUILD%"
+
+
 
 if [%GNUWIN32_DIR%]==[] (
-	echo ERROR:  GNUWIN32_DIR not set.
-	echo Exiting now.
-	goto :eof
+	echo GNUWIN32_DIR not set.
+	echo Downloading win32 deps.
+	set DEPENDENCIES_DOWNLOAD="-DLYX_DEPENDENCIES_DOWNLOAD=1"
+) else (
+	echo LyX deps  : "%GNUWIN32_DIR%"
+	set PATH="%GNUWIN32_DIR%\bin";"%PATH%"
 )
-
-echo LyX source: "%LYX_SOURCE%"
-echo LyX build : "%LYX_BUILD%"
-echo LyX deps  : "%GNUWIN32_DIR%"
-
-set PATH=%GNUWIN32_DIR%\bin;%PATH%
 
 
 if not exist %LYX_BUILD% (
@@ -66,12 +69,11 @@ if not exist %LYX_BUILD% (
 cd "%LYX_BUILD%"
 
 
-REM Delete all files indirectory
-del /s/q *
-del CMakeCache.txt
+REM start with a new cmake run
+::del CMakeCache.txt
 
 if [%2]==[] (
-	set USED_STUDIO="Visual Studio 9 2008"
+	set USED_STUDIO="Visual Studio 10"
 ) else (
 	set USED_STUDIO=%2%
 )
@@ -79,29 +81,17 @@ if [%2]==[] (
 
 if "%1%" == "devel" (
 	REM Build solution to develop LyX
-	cmake %LYX_SOURCE%\development\cmake -G%USED_STUDIO% -DLYX_MERGE_FILES=0 -DLYX_NLS=1 -DLYX_RELEAE=0
-	REM needed when running lyx from the debugger
-	set LYX_DIR_20x=%LYX_SOURCE%\lib
-	start lyx.sln /build Debug
+	cmake %LYX_SOURCE% -G%USED_STUDIO% -DLYX_MERGE_FILES=0 -DLYX_NLS=1 %DEPENDENCIES_DOWNLOAD%
+	msbuild lyx.sln /p:Configuration=Debug /t:LyX /t:text2lyx
 )
 
 if "%1%" == "install" (
 	REM Build solution to develop LyX
-	cmake %LYX_SOURCE%\development\cmake -G%USED_STUDIO% -DLYX_MERGE_FILES=1 -DLYX_INSTALL=1 -DLYX_RELEASE=1
-	REM needed when running lyx from the debugger
-	set LYX_DIR_20x=
-	start lyx.sln /build Debug
+	cmake %LYX_SOURCE% -G%USED_STUDIO% -DLYX_MERGE_FILES=1 -DLYX_INSTALL=1 %DEPENDENCIES_DOWNLOAD%
+	msbuild lyx.sln         /p:Configuration=Release /t:ALL_BUILD
+	msbuild INSTALL.vcxproj /p:Configuration=Release 
 )
 
-if "%1%" == "deploy" (
-	REM Build complete installed LyX
-	cmake "%LYX_SOURCE%"\development\cmake -G"NMake Makefiles" -DLYX_MERGE_FILES=1 -DLYX_INSTALL=1 -DLYX_RELEASE=1
-	nmake
-	nmake install
-)
-
-:: return to the cmake folder where this script was started from
-cd %LYX_SOURCE%\development\cmake
 
 
 :eof
