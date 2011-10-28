cburschka/lyx

commit 1122af1e84ee35834e73be4947d88dbb93cd9869

backporting tex2lyx: support for language packages
git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/branches/BRANCH_2_0_X@39988 a592a061-630c-0410-9148-cb99ea01b6c8

src/tex2lyx/preamble.cpp
@@ -127,6 +127,10 @@ const char * const known_polish_quotes_languages[] = {"afrikaans", "croatian",
 const char * const known_swedish_quotes_languages[] = {"finnish",
 "swedish", 0};
 
+/// known language packages from the times before babel
+const char * const known_old_language_packages[] = {"french", "frenchle",
+"frenchpro", "german", "ngerman", "pmfrench", 0};
+
 char const * const known_fontsizes[] = { "10pt", "11pt", "12pt", 0 };
 
 const char * const known_roman_fonts[] = { "ae", "beraserif", "bookman",
@@ -171,7 +175,7 @@ string h_textclass               = "article";
 string h_use_default_options     = "false";
 string h_options;
 string h_language                = "english";
-string h_language_package        = "default";
+string h_language_package        = "none";
 string h_fontencoding            = "default";
 string h_font_roman              = "default";
 string h_font_sans               = "default";
@@ -523,18 +527,23 @@ void handle_package(Parser &p, string const & name, string const & opts,
 	else if (name == "mathdots")
 		h_use_mathdots = "2";
 
-	else if (name == "babel" && !opts.empty()) {
-		// check if more than one option was used - used later for inputenc
-		// in case inputenc is parsed before babel, set the encoding to auto
-		if (options.begin() != options.end() - 1) {
-			one_language = false;
-			h_inputencoding = "auto";
+	else if (name == "babel") {
+		h_language_package = "default";
+		// we have to do nothing if babel is loaded without any options, otherwise
+		// we would pollute the preamble with this call in every roundtrip
+		if (!opts.empty()) {
+			// check if more than one option was used - used later for inputenc
+			// in case inputenc is parsed before babel, set the encoding to auto
+			if (options.begin() != options.end() - 1) {
+				one_language = false;
+				h_inputencoding = "auto";
+			}
+			// babel takes the last language of the option of its \usepackage
+			// call as document language. If there is no such language option, the
+			// last language in the documentclass options is used.
+			handle_opt(options, known_languages, h_language);
+			delete_opt(options, known_languages);
 		}
-		// babel takes the last language of the option of its \usepackage
-		// call as document language. If there is no such language option, the
-		// last language in the documentclass options is used.
-		handle_opt(options, known_languages, h_language);
-		delete_opt(options, known_languages);
 	}
 
 	else if (name == "fontenc") {
@@ -559,6 +568,13 @@ void handle_package(Parser &p, string const & name, string const & opts,
 		if (!options.empty())
 			p.setEncoding(options.back());
 		options.clear();
+	}
+
+	else if (is_known(name, known_old_language_packages)) {
+		// known language packages from the times before babel
+		// if they are found and not also babel, they will be used as
+		// cutom language package
+		h_language_package = "\\usepackage{" + name + "}";
 	}
 
 	else if (name == "makeidx")

commit 62486d95e316182ea6f7e3deb94ce70171712c44

tex2lyx/preamble.cpp: prevent bug #7858 (that I accidentally introduced in r39988) and this way fix bug #7861 as side-effect
git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/branches/BRANCH_2_0_X@40051 a592a061-630c-0410-9148-cb99ea01b6c8

src/tex2lyx/preamble.cpp
@@ -544,8 +544,12 @@ void handle_package(Parser &p, string const & name, string const & opts,
 
 	else if (name == "babel") {
 		h_language_package = "default";
-		// we have to do nothing if babel is loaded without any options, otherwise
-		// we would pollute the preamble with this call in every roundtrip
+		// One might think we would have to do nothing if babel is loaded
+		// without any options to prevent pollution of the preamble with this
+		// babel call in every roundtrip.
+		// But the user could have defined babel-specific things afterwards. So
+		// we need to keep it in the preamble to prevent cases like bug #7861.
+		h_preamble << "\\usepackage{babel}\n";
 		if (!opts.empty()) {
 			// check if more than one option was used - used later for inputenc
 			// in case inputenc is parsed before babel, set the encoding to auto

status.20x
@@ -133,6 +133,8 @@ What's new
 
 - Fix tex2lyx handling of recognized Roman fonts (bug 7856).
 
+- Fix tex2lyx handling babel-specific preamble commands (bug 7861).
+
 - Store the autosave files of unnamed buffers in the correct directory
   and make sure they are not left behind after saving (bug 7793).
 