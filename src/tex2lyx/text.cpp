874e1b52998b62047b354e9cd0edc2725ad7bd10
Uwe Stöhr <uwestoehr@web.de>
2012-03-04 13:48:49 +0000

    tex2lyx/text.cpp: some whitespace fixes
    git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/branches/BRANCH_2_0_X@40851 a592a061-630c-0410-9148-cb99ea01b6c8

--- src/tex2lyx/text.cpp
+++ src/tex2lyx/text.cpp
@@ -561,7 +561,7 @@ void skip_spaces_braces(Parser & p, bool keepws = false)
 	   should be handled by this function:
 	   - abc \j{} xyz
 	   - abc \j {} xyz
-	   - abc \j 
+	   - abc \j
 	     {} xyz
 	   - abc \j %comment
 	     {} xyz
@@ -605,7 +605,7 @@ void output_command_layout(ostream & os, Parser & p, bool outer,
 	while (optargs < context.layout->optargs) {
 		eat_whitespace(p, os, context, false);
 		if (p.next_token().cat() == catEscape ||
-		    p.next_token().character() != '[') 
+		    p.next_token().character() != '[')
 			break;
 		p.get_token(); // eat '['
 		begin_inset(os, "Argument\n");
@@ -922,7 +922,7 @@ void parse_box(Parser & p, ostream & os, unsigned outer_flags,
 			// the inner env
 			if (!inner_type.empty() && (inner_flags & FLAG_END))
 				active_environments.pop_back();
-	
+
 			// Ensure that the end of the outer box is parsed correctly:
 			// The opening brace has been eaten by parse_outer_box()
 			if (!outer_type.empty() && (outer_flags & FLAG_ITEM)) {
@@ -1469,7 +1469,7 @@ void parse_environment(Parser & p, ostream & os, bool outer,
 			while (optargs < context.layout->optargs) {
 				eat_whitespace(p, os, context, false);
 				if (p.next_token().cat() == catEscape ||
-				    p.next_token().character() != '[') 
+				    p.next_token().character() != '[')
 					break;
 				p.get_token(); // eat '['
 				if (need_layout) {
@@ -2951,7 +2951,7 @@ void parse_text(Parser & p, ostream & os, unsigned flags, bool outer,
 			end_inset(os);
 			skip_spaces_braces(p);
 		}
-		
+
 		else if (t.cs() == "lyxline") {
 			// swallow size argument (it is not used anyway)
 			p.getArg('{', '}');
@@ -3176,7 +3176,7 @@ void parse_text(Parser & p, ostream & os, unsigned flags, bool outer,
 			end_inset(os);
 			preamble.registerAutomaticallyLoadedPackage("nomencl");
 		}
-		
+
 		else if (t.cs() == "label") {
 			context.check_layout(os);
 			begin_command_inset(os, "label", "label");
@@ -3323,7 +3323,7 @@ void parse_text(Parser & p, ostream & os, unsigned flags, bool outer,
 		else if (t.cs() == "selectlanguage") {
 			context.check_layout(os);
 			// save the language for the case that a
-			// \foreignlanguage is used 
+			// \foreignlanguage is used
 
 			context.font.language = babel2lyx(p.verbatim_item());
 			os << "\n\\lang " << context.font.language << "\n";
@@ -3430,18 +3430,18 @@ void parse_text(Parser & p, ostream & os, unsigned flags, bool outer,
 			handle_ert(os, t.asInput(), context);
 
 		// accents (see Table 6 in Comprehensive LaTeX Symbol List)
-		else if (t.cs().size() == 1 
+		else if (t.cs().size() == 1
 			 && contains("\"'.=^`bcdHkrtuv~", t.cs())) {
 			context.check_layout(os);
 			// try to see whether the string is in unicodesymbols
 			docstring rem;
-			string command = t.asInput() + "{" 
+			string command = t.asInput() + "{"
 				+ trim(p.verbatim_item())
 				+ "}";
 			docstring s = encodings.fromLaTeXCommand(from_utf8(command), rem);
 			if (!s.empty()) {
 				if (!rem.empty())
-					cerr << "When parsing " << command 
+					cerr << "When parsing " << command
 					     << ", result is " << to_utf8(s)
 					     << "+" << to_utf8(rem) << endl;
 				os << to_utf8(s);
