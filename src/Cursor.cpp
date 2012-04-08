cburschka/lyx

commit 6d75800f5d23f5b051302cb5ff49940748fed4c1

Fix bug #6936.
If \hline is entered, do not create an unknown inset, but increase the number
of hlines of the current row if that is allowed. The same idea is applied to
copy-paste (not part of the bug report).
This is also a test for committing via git.

src/mathed/InsetMathGrid.cpp
@@ -13,6 +13,7 @@
 
 #include "InsetMathGrid.h"
 
+#include "InsetMathUnknown.h"
 #include "MathData.h"
 #include "MathParser.h"
 #include "MathStream.h"
@@ -173,6 +174,22 @@ void InsetMathGrid::setDefaults()
 		colinfo_[col].skip_  = defaultColSpace(col);
 		colinfo_[col].special_.clear();
 	}
+}
+
+
+bool InsetMathGrid::interpretString(Cursor & cur, docstring const & str)
+{
+	if (str == "\\hline") {
+		FuncRequest fr = FuncRequest(LFUN_INSET_MODIFY, "tabular add-hline-above");
+		FuncStatus status;
+		if (getStatus(cur, fr, status)) {
+			if (status.enabled()) {
+				rowinfo_[cur.row()].lines_++;
+				return true;
+			}
+		}
+	}
+	return InsetMathNest::interpretString(cur, str);
 }
 
 
@@ -1332,11 +1349,25 @@ void InsetMathGrid::doDispatch(Cursor & cur, FuncRequest & cmd)
 				mathed_parse_normal(grid, topaste, parseflg | Parse::VERBATIM);
 			}
 
+		bool hline_enabled = false;
+		FuncRequest fr = FuncRequest(LFUN_INSET_MODIFY, "tabular add-hline-above");
+		FuncStatus status;
+		if (getStatus(cur, fr, status))
+			hline_enabled = status.enabled();
 		if (grid.nargs() == 1) {
 			// single cell/part of cell
 			cur.recordUndo();
 			cur.cell().insert(cur.pos(), grid.cell(0));
 			cur.pos() += grid.cell(0).size();
+			if (hline_enabled)
+				rowinfo_[cur.row()].lines_ += grid.rowinfo_[0].lines_;
+			else {
+				for (unsigned int l = 0; l < grid.rowinfo_[0].lines_; ++l) {
+					 cur.cell().insert(0,
+						MathAtom(new InsetMathUnknown(from_ascii("\\hline"))));
+					 cur.pos()++;
+				}
+			}
 		} else {
 			// multiple cells
 			cur.recordUndoInset();
@@ -1349,6 +1380,15 @@ void InsetMathGrid::doDispatch(Cursor & cur, FuncRequest & cmd)
 					idx_type i = index(r + cur.row(), c + col(cur.idx()));
 					cell(i).insert(0, grid.cell(grid.index(r, c)));
 				}
+				if (hline_enabled)
+					rowinfo_[r].lines_ += grid.rowinfo_[r].lines_;
+				else {
+					for (unsigned int l = 0; l < grid.rowinfo_[r].lines_; ++l) {
+						idx_type i = index(r + cur.row(), 0);
+						cell(i).insert(0,
+							MathAtom(new InsetMathUnknown(from_ascii("\\hline"))));
+					}
+				}
 				// append the left over horizontal cells to the last column
 				idx_type i = index(r + cur.row(), ncols() - 1);
 				for (InsetMath::col_type c = numcols; c < grid.ncols(); ++c)
@@ -1356,9 +1396,18 @@ void InsetMathGrid::doDispatch(Cursor & cur, FuncRequest & cmd)
 			}
 			// append the left over vertical cells to the last _cell_
 			idx_type i = nargs() - 1;
-			for (row_type r = numrows; r < grid.nrows(); ++r)
+			for (row_type r = numrows; r < grid.nrows(); ++r) {
 				for (col_type c = 0; c < grid.ncols(); ++c)
 					cell(i).append(grid.cell(grid.index(r, c)));
+				if (hline_enabled)
+					rowinfo_[r].lines_ += grid.rowinfo_[r].lines_;
+				else {
+					for (unsigned int l = 0; l < grid.rowinfo_[r].lines_; ++l) {
+						cell(i).insert(0,
+							MathAtom(new InsetMathUnknown(from_ascii("\\hline"))));
+					}
+				}
+			}
 		}
 		cur.clearSelection(); // bug 393
 		// FIXME audit setBuffer calls

src/mathed/InsetMathNest.cpp
@@ -872,7 +872,7 @@ void InsetMathNest::doDispatch(Cursor & cur, FuncRequest & cmd)
 		// if relevant. Think typing "\frac<space>".
 		if (cmd.argument()[0] == ' '
 		    && cur.inMacroMode() && cur.macroName() != "\\"
-		    && cur.macroModeClose()) {
+		    && cur.macroModeClose() && cur.pos() > 0) {
 			MathAtom const atom = cur.prevAtom();
 			if (atom->asNestInset() && atom->isActive()) {
 				cur.posBackward();

src/mathed/InsetMathGrid.h
@@ -194,6 +194,8 @@ public:
 	virtual char defaultColAlign(col_type) { return 'c'; }
 	///
 	void setDefaults();
+	///
+	virtual bool interpretString(Cursor & cur, docstring const & str);
 
 	///
 	virtual int colsep() const;

src/mathed/InsetMathNest.h
@@ -172,7 +172,7 @@ public:
 	/// interpret \p str and insert the result at the current position of
 	/// \p cur if it is something known. Return whether \p cur was
 	/// inserted.
-	bool interpretString(Cursor & cur, docstring const & str);
+	virtual bool interpretString(Cursor & cur, docstring const & str);
 
 private:
 	/// lfun handler

src/tex2lyx/table.cpp
@@ -1272,7 +1272,7 @@ void handle_tabular(Parser & p, ostream & os, string const & name,
 	os << "\n<lyxtabular version=\"3\" rows=\"" << rowinfo.size()
 	   << "\" columns=\"" << colinfo.size() << "\">\n";
 	os << "<features"
-	   << write_attribute("rotate", "0")
+	   << write_attribute("rotate", false)
 	   << write_attribute("booktabs", booktabs)
 	   << write_attribute("islongtable", is_long_tabular);
 	if (is_long_tabular) {

commit 1fa39e1f953434700f1af4fb2eb9b193dd1709df

Fix undo of entering \hline
It was broken with 6d75800f5d23 as noticed by Enrico. The fix is to use undo
groups (thanks Jean-Marc).

src/Cursor.cpp
@@ -1599,8 +1599,11 @@ bool Cursor::macroModeClose()
 
 	docstring const name = s.substr(1);
 	InsetMathNest * const in = inset().asInsetMath()->asNestInset();
-	if (in && in->interpretString(*this, s))
+	if (in && in->interpretString(*this, s)) {
+		// end undo group that was opened before in was created
+		endUndoGroup();
 		return true;
+	}
 	MathAtom atom = buffer()->getMacro(name, *this, false) ?
 		MathAtom(new MathMacro(buffer(), name)) : createInsetMath(name, buffer());
 
@@ -1633,7 +1636,9 @@ bool Cursor::macroModeClose()
 		else
 			insert(selection);
 	}
-	
+
+	// end undo group that was opened before in was created
+	endUndoGroup();
 	return true;
 }
 

src/mathed/InsetMathGrid.cpp
@@ -184,6 +184,7 @@ bool InsetMathGrid::interpretString(Cursor & cur, docstring const & str)
 		FuncStatus status;
 		if (getStatus(cur, fr, status)) {
 			if (status.enabled()) {
+				cur.recordUndoInset();
 				rowinfo_[cur.row()].lines_++;
 				return true;
 			}

src/mathed/InsetMathNest.cpp
@@ -848,10 +848,14 @@ void InsetMathNest::doDispatch(Cursor & cur, FuncRequest & cmd)
 
 	case LFUN_SELF_INSERT:
 		if (cmd.argument().size() != 1) {
+			// use a group because interpretString() might
+			// record an extra undo step
+			cur.beginUndoGroup();
 			cur.recordUndoSelection();
 			docstring const arg = cmd.argument();
 			if (!interpretString(cur, arg))
 				cur.insert(arg);
+			cur.endUndoGroup();
 			break;
 		}
 		// Don't record undo steps if we are in macro mode and thus
@@ -1663,6 +1667,8 @@ bool InsetMathNest::interpretChar(Cursor & cur, char_type const c)
 				cur.cell().erase(cur.pos());
 				cur.plainInsert(MathAtom(
 					new InsetMathBig(name.substr(1), delim)));
+				// end undo group that was opened before p was created
+				cur.endUndoGroup();
 				return true;
 			}
 		}
@@ -1706,9 +1712,12 @@ bool InsetMathNest::interpretChar(Cursor & cur, char_type const c)
 		bool reduced = cap::reduceSelectionToOneCell(cur);
 		if (reduced || !cur.selection()) {
 			docstring const safe = cap::grabAndEraseSelection(cur);
-			if (!cur.inRegexped())
+			if (!cur.inRegexped()) {
+				// open a group because interpretString() might
+				// record an extra undo step when finalizing
+				cur.beginUndoGroup();
 				cur.insert(MathAtom(new InsetMathUnknown(from_ascii("\\"), safe, false)));
-			else
+			} else
 				cur.niceInsert(createInsetMath("backslash", buf));
 		}
 		return true;

src/mathed/MathData.cpp
@@ -28,8 +28,6 @@
 #include "CoordCache.h"
 #include "Cursor.h"
 
-#include "mathed/InsetMathUnknown.h"
-
 #include "support/debug.h"
 #include "support/docstream.h"
 