cburschka/lyx

commit c93c44bf64475f67f26634a4d90e18d425a0c80b

Fix bug #7844: \date{} in main text is recognized
git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@40091 a592a061-630c-0410-9148-cb99ea01b6c8

src/tex2lyx/Preamble.cpp
@@ -294,6 +294,15 @@ string Preamble::addModules(string const & lyxpreamble, string const & modules)
 }
 
 
+void Preamble::suppressDate(bool suppress)
+{
+	if (suppress)
+		h_suppress_date = "true";
+	else
+		h_suppress_date = "false";
+}
+
+
 void Preamble::add_package(string const & name, vector<string> & options)
 {
 	// every package inherits the global options
@@ -750,7 +759,7 @@ void Preamble::handle_if(Parser & p, bool in_lyx_preamble)
 }
 
 
-void Preamble::end_preamble(ostream & os, TeX2LyXDocClass const & /*tc*/)
+void Preamble::writeLyXHeader(ostream & os)
 {
 	// translate from babel to LyX names
 	h_language = babel2lyx(h_language);
@@ -881,8 +890,8 @@ void Preamble::end_preamble(ostream & os, TeX2LyXDocClass const & /*tc*/)
 }
 
 
-void Preamble::parse(Parser & p, ostream & os,
-	string const & forceclass, TeX2LyXDocClass & tc)
+void Preamble::parse(Parser & p, string const & forceclass,
+                     TeX2LyXDocClass & tc)
 {
 	// initialize fixed types
 	special_columns['D'] = 3;
@@ -1358,7 +1367,6 @@ void Preamble::parse(Parser & p, ostream & os,
 		ss << tc.sides();
 		h_papersides = ss.str();
 	}
-	end_preamble(os, tc);
 }
 
 

src/tex2lyx/Preamble.h
@@ -42,10 +42,15 @@ public:
 	///
 	std::string addModules(std::string const & lyxpreamble,
 	                       std::string const & modules);
-
 	///
-	void parse(Parser & p, std::ostream & os,
-	           std::string const & forceclass, TeX2LyXDocClass & tc);
+	void suppressDate(bool suppress);
+
+
+	/// Parses the LaTeX preamble into internal data
+	void parse(Parser & p, std::string const & forceclass,
+	           TeX2LyXDocClass & tc);
+	/// Writes the LyX file header from internal data
+	void writeLyXHeader(std::ostream & os);
 
 private:
 	///
@@ -141,8 +146,6 @@ private:
 	                    std::string const & opts, bool in_lyx_preamble);
 	///
 	void handle_if(Parser & p, bool in_lyx_preamble);
-	///
-	void end_preamble(std::ostream & os, TeX2LyXDocClass const & tc);
 };
 
 

src/tex2lyx/text.cpp
@@ -2124,6 +2124,26 @@ void parse_text(Parser & p, ostream & os, unsigned flags, bool outer,
 			eat_whitespace(p, os, context, true);
 		}
 
+		// Must catch empty dates before findLayout is called below
+		else if (t.cs() == "date") {
+			string const date = p.verbatim_item();
+			if (date.empty())
+				preamble.suppressDate(true);
+			else {
+				preamble.suppressDate(false);
+				if (context.new_layout_allowed &&
+				    (newlayout = findLayout(context.textclass,
+				                            t.cs(), true))) {
+					// write the layout
+					output_command_layout(os, p, outer,
+							context, newlayout);
+					p.skip_spaces();
+				} else
+					handle_ert(os, "\\date{" + date + '}',
+							context);
+			}
+		}
+
 		// Starred section headings
 		// Must attempt to parse "Section*" before "Section".
 		else if ((p.next_token().asInput() == "*") &&

src/tex2lyx/tex2lyx.cpp
@@ -657,8 +657,7 @@ void tex2lyx(idocstream & is, ostream & os, string encoding)
 	p.setEncoding(encoding);
 	//p.dump();
 
-	ostringstream ps;
-	preamble.parse(p, ps, documentclass, textclass);
+	preamble.parse(p, documentclass, textclass);
 
 	active_environments.push_back("document");
 	Context context(true, textclass);
@@ -681,6 +680,8 @@ void tex2lyx(idocstream & is, ostream & os, string encoding)
 			ms << *it << '\n';
 		ms << "\\end_modules\n";
 	}
+	ostringstream ps;
+	preamble.writeLyXHeader(ps);
 	os << preamble.addModules(ps.str(), ms.str());
 
 	ss.seekg(0);

commit c1965123ed2a5390a1d719f4ba3f92c27376adff

Fix bug #8104 (\date argument was eaten)
I introduced this bug in r40091 where support for empty dates was added.
I also noticed that the title_layout_found flag was not properly passed down
to all variants of parse_text*. Since this is rather a global variable I
placed it into the preamble class.

The test case of bug #8104 shows other problems as well, but those are no
regressions and will be fixed separately.

OK for branch?

src/tex2lyx/Preamble.cpp
@@ -395,7 +395,7 @@ string remove_braces(string const & value)
 } // anonymous namespace
 
 
-Preamble::Preamble() : one_language(true)
+Preamble::Preamble() : one_language(true), title_layout_found(false)
 {
 	//h_backgroundcolor;
 	//h_boxbgcolor;

src/tex2lyx/Preamble.h
@@ -55,6 +55,10 @@ public:
 	void addModule(std::string const & module);
 	///
 	void suppressDate(bool suppress);
+	///
+	bool titleLayoutFound() const { return title_layout_found; }
+	///
+	void titleLayoutFound(bool found) { title_layout_found = found; }
 	/// Register an author named \p name in the author list
 	void registerAuthor(std::string const & name);
 	/// Get author named \p name (must be registered first)
@@ -77,6 +81,9 @@ private:
 
 	/// needed to handle encodings with babel
 	bool one_language;
+
+	/// was at least one title layout found?
+	bool title_layout_found;
 
 	std::ostringstream h_preamble;
 	std::string h_backgroundcolor;

src/tex2lyx/text.cpp
@@ -1186,8 +1186,7 @@ void parse_unknown_environment(Parser & p, string const & name, ostream & os,
 
 
 void parse_environment(Parser & p, ostream & os, bool outer,
-                       string & last_env, bool & title_layout_found,
-                       Context & parent_context)
+                       string & last_env, Context & parent_context)
 {
 	Layout const * newlayout;
 	InsetLayout const * newinsetlayout = 0;
@@ -1554,8 +1553,8 @@ void parse_environment(Parser & p, ostream & os, bool outer,
 		context.check_end_deeper(os);
 		parent_context.new_paragraph(os);
 		p.skip_spaces();
-		if (!title_layout_found)
-			title_layout_found = newlayout->intitle;
+		if (!preamble.titleLayoutFound())
+			preamble.titleLayoutFound(newlayout->intitle);
 		set<string> const & req = newlayout->requires();
 		for (set<string>::const_iterator it = req.begin(); it != req.end(); it++)
 			preamble.registerAutomaticallyLoadedPackage(*it);
@@ -1971,7 +1970,6 @@ void parse_text(Parser & p, ostream & os, unsigned flags, bool outer,
 	bool const use_natbib = preamble.isPackageUsed("natbib");
 	bool const use_jurabib = preamble.isPackageUsed("jurabib");
 	string last_env;
-	bool title_layout_found = false;
 	while (p.good()) {
 		Token const & t = p.get_token();
 
@@ -2298,7 +2296,7 @@ void parse_text(Parser & p, ostream & os, unsigned flags, bool outer,
 
 		else if (t.cs() == "begin")
 			parse_environment(p, os, outer, last_env,
-			                  title_layout_found, context);
+			                  context);
 
 		else if (t.cs() == "end") {
 			if (flags & FLAG_END) {
@@ -2469,10 +2467,14 @@ void parse_text(Parser & p, ostream & os, unsigned flags, bool outer,
 
 		// Must catch empty dates before findLayout is called below
 		else if (t.cs() == "date") {
+			eat_whitespace(p, os, context, false);
+			p.pushPosition();
 			string const date = p.verbatim_item();
-			if (date.empty())
+			p.popPosition();
+			if (date.empty()) {
 				preamble.suppressDate(true);
-			else {
+				p.verbatim_item();
+			} else {
 				preamble.suppressDate(false);
 				if (context.new_layout_allowed &&
 				    (newlayout = findLayout(context.textclass,
@@ -2480,16 +2482,17 @@ void parse_text(Parser & p, ostream & os, unsigned flags, bool outer,
 					// write the layout
 					output_command_layout(os, p, outer,
 							context, newlayout);
-					p.skip_spaces();
-					if (!title_layout_found)
-						title_layout_found = newlayout->intitle;
+					parse_text_snippet(p, os, FLAG_ITEM, outer, context);
+					if (!preamble.titleLayoutFound())
+						preamble.titleLayoutFound(newlayout->intitle);
 					set<string> const & req = newlayout->requires();
 					for (set<string>::const_iterator it = req.begin();
 					     it != req.end(); it++)
 						preamble.registerAutomaticallyLoadedPackage(*it);
 				} else
-					handle_ert(os, "\\date{" + date + '}',
-							context);
+					handle_ert(os,
+						"\\date{" + p.verbatim_item() + '}',
+						context);
 			}
 		}
 
@@ -2502,8 +2505,8 @@ void parse_text(Parser & p, ostream & os, unsigned flags, bool outer,
 			p.get_token();
 			output_command_layout(os, p, outer, context, newlayout);
 			p.skip_spaces();
-			if (!title_layout_found)
-				title_layout_found = newlayout->intitle;
+			if (!preamble.titleLayoutFound())
+				preamble.titleLayoutFound(newlayout->intitle);
 			set<string> const & req = newlayout->requires();
 			for (set<string>::const_iterator it = req.begin(); it != req.end(); it++)
 				preamble.registerAutomaticallyLoadedPackage(*it);
@@ -2515,8 +2518,8 @@ void parse_text(Parser & p, ostream & os, unsigned flags, bool outer,
 			// write the layout
 			output_command_layout(os, p, outer, context, newlayout);
 			p.skip_spaces();
-			if (!title_layout_found)
-				title_layout_found = newlayout->intitle;
+			if (!preamble.titleLayoutFound())
+				preamble.titleLayoutFound(newlayout->intitle);
 			set<string> const & req = newlayout->requires();
 			for (set<string>::const_iterator it = req.begin(); it != req.end(); it++)
 				preamble.registerAutomaticallyLoadedPackage(*it);
@@ -2808,7 +2811,7 @@ void parse_text(Parser & p, ostream & os, unsigned flags, bool outer,
 		}
 
 		else if (t.cs() == "makeindex" || t.cs() == "maketitle") {
-			if (title_layout_found) {
+			if (preamble.titleLayoutFound()) {
 				// swallow this
 				skip_spaces_braces(p);
 			} else

src/tex2lyx/test/test-insets.tex
@@ -42,6 +42,13 @@
 
 \begin{document}
 
+\title{Title}
+
+\date %stupid stuff
+{two days ago}
+
+\maketitle
+
 \tableofcontents
 \lstlistoflistings
 