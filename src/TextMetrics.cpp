cburschka/lyx

commit 0a137e315ab0534aaca8da72bdd4a80ca3e58f7f

* TextMetrics.cpp: fix metrics calculation with preceeding blank in free spacing context (bug #6746)
git-svn-id: svn://svn.lyx.org/lyx/lyx-devel/trunk@34705 a592a061-630c-0410-9148-cb99ea01b6c8

src/TextMetrics.cpp
@@ -1219,10 +1219,18 @@ pos_type TextMetrics::getColumnNearX(pit_type const pit,
 		return 0;
 	}
 
-	// if the first character is a separator, we are in RTL
-	// text. This character will not be painted on screen
+	// This (rtl_support test) is not needed, but gives
+	// some speedup if rtl_support == false
+	bool const lastrow = lyxrc.rtl_support && row.endpos() == par.size();
+
+	// If lastrow is false, we don't need to compute
+	// the value of rtl.
+	bool const rtl = lastrow ? text_->isRTL(par) : false;
+
+	// if the first character is a separator, and we are in RTL
+	// text, this character will not be painted on screen
 	// and thus we should not count it and skip to the next.
-	if (par.isSeparator(bidi.vis2log(vc)))
+	if (rtl && par.isSeparator(bidi.vis2log(vc)))
 		++vc;
 
 	while (vc < end && tmpx <= x) {
@@ -1250,13 +1258,7 @@ pos_type TextMetrics::getColumnNearX(pit_type const pit,
 	LASSERT(vc <= end, /**/);  // This shouldn't happen.
 
 	boundary = false;
-	// This (rtl_support test) is not needed, but gives
-	// some speedup if rtl_support == false
-	bool const lastrow = lyxrc.rtl_support && row.endpos() == par.size();
 
-	// If lastrow is false, we don't need to compute
-	// the value of rtl.
-	bool const rtl = lastrow ? text_->isRTL(par) : false;
 	if (lastrow &&
 	    ((rtl  &&  left_side && vc == row.pos() && x < tmpx - 5) ||
 	     (!rtl && !left_side && vc == end  && x > tmpx + 5))) {

commit d7f68078cf3bbf3ea7f5d2b3ac5c7e73555c4681

Fix bug #7518: Assert when selecting in RTL text
If we are in rtl text, we *need* to check whether the first character is a
space. We can't use the rtl variable for this because this is only
computed on the last row.

This bug was introduced in [0a137e31/lyxgit] to fix the computation for
freespacing paragraphs. We better can just check whether the paragraph is
freespacing or not.

src/TextMetrics.cpp
@@ -1235,8 +1235,9 @@ pos_type TextMetrics::getColumnNearX(pit_type const pit,
 
 	// if the first character is a separator, and we are in RTL
 	// text, this character will not be painted on screen
-	// and thus we should not count it and skip to the next.
-	if (rtl && par.isSeparator(bidi.vis2log(vc)))
+	// and thus we should not count it and skip to the next. Only
+	// in freespacing paragraphs, this first character is painted.
+	if (!par.isFreeSpacing() && par.isSeparator(bidi.vis2log(vc)))
 		++vc;
 
 	while (vc < end && tmpx <= x) {