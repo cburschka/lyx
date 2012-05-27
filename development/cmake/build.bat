3b6bf1ee065751fce131e4bd9c15acee6d4a39b6
Uwe Stöhr <uwestoehr@lyx.org>
2012-05-27 02:15:18 +0200

    cmake/build.bat: correct a path proposal

--- development/cmake/build.bat
+++ development/cmake/build.bat
@@ -36,7 +36,7 @@ REM Add path to qmake here or set PATH correctly on your system.
 set PATH="D:\Qt\bin";%PATH%
 
 REM Edit pathes here or set the environment variables on you system.
-set GNUWIN32_DIR=D:\LyXGit\Master\lyx-devel\lyx-windows-deps-msvc2010
+set GNUWIN32_DIR=D:\LyXGit\Master\lyx-windows-deps-msvc2010
 set LYX_SOURCE=D:\LyXGit\Master\
 set LYX_BUILD=D:\LyXGit\Master\compile-result
 
