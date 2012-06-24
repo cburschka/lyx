5b374e0c29ac5e89ffe248792028612799424505
Juergen Spitzmueller <spitz@lyx.org>
2012-06-24 10:56:08 +0200

    do not output empty language switch commands (bug #8216)

--- src/output_latex.cpp
+++ src/output_latex.cpp
@@ -581,7 +581,8 @@ void TeXOnePar(Buffer const & buf,
 					os << "\\L{";
 			}
 			// With CJK, the CJK tag has to be closed first (see below)
-			if (runparams.encoding->package() != Encoding::CJK) {
+			if (runparams.encoding->package() != Encoding::CJK
+			    && !par_lang.empty()) {
 				os << from_ascii(subst(
 					lang_begin_command,
 					"$$lang",
@@ -642,7 +643,8 @@ void TeXOnePar(Buffer const & buf,
 					os << "%\n";
 				}
 				// With CJK, the CJK tag had to be closed first (see above)
-				if (runparams.encoding->package() == Encoding::CJK) {
+				if (runparams.encoding->package() == Encoding::CJK
+				    && !par_lang.empty()) {
 					os << from_ascii(subst(
 						lang_begin_command,
 						"$$lang",
