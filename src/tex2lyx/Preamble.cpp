6e10237b1d2ca14e3c093bbeba8101fd2787ff99
Uwe Stöhr <uwestoehr@lyx.org>
2012-06-30 15:42:33 +0200

    tex2lyx: remove LyX 2.1-only languages
    Preamble.cpp: newzealand and australian are only supported by LyX 2.1

--- src/tex2lyx/Preamble.cpp
+++ src/tex2lyx/Preamble.cpp
@@ -44,16 +44,12 @@ Preamble preamble;
 
 namespace {
 
-//add this to known_languages when updating to lyxformat 266:
-// "armenian" (needs special handling since not supported by standard babel)
-//add these to known_languages when updating to lyxformat 268:
-//"chinese-simplified", "chinese-traditional", "japanese", "korean"
-// Both changes require first that support for non-babel languages (CJK,
-// armtex) is added.
+// "chinese-simplified", "chinese-traditional", "japanese-cjk", "korean"
+// cannot be supported because it is impossible to determine the correct document
+// language if CJK is used.
 /**
  * known babel language names (including synonyms)
  * not in standard babel: arabic, arabtex, armenian, belarusian, serbian-latin, thai
- * not yet supported by LyX: kurmanji
  * please keep this in sync with known_coded_languages line by line!
  */
 const char * const known_languages[] = {"acadian", "afrikaans", "albanian",
@@ -106,7 +102,7 @@ const char * const polyglossia_languages[] = {
 "lsorbian", "serbian", "vietnamese", "bulgarian", "galician", "magyar", "slovak",
 "welsh", "catalan", "german", "malayalam", "slovenian", "coptic", "greek",
 "marathi", "spanish",
-"american", "ancient", "australian", "british", "monotonic", "newzealand",
+"american", "ancient", "british", "monotonic",
 "polytonic", 0};
 
 /**
@@ -124,7 +120,7 @@ const char * const coded_polyglossia_languages[] = {
 "lowersorbian", "serbian", "vietnamese", "bulgarian", "galician", "magyar", "slovak",
 "welsh", "catalan", "ngerman", "malayalam", "slovene", "coptic", "greek",
 "marathi", "spanish",
-"american", "ancientgreek", "australian", "british", "greek", "newzealand",
+"american", "ancientgreek", "british", "greek",
 "polutonikogreek", 0};
 
 /// languages with english quotes (.lyx names)
