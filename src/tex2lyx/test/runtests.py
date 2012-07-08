1c0bd0132c605d18229085c42cd2fba1b30151a8
Georg Baum <baum@lyx.org>
2012-07-08 19:28:19 +0200

    Add very simple tex2lyx regression test suite.
    It is invoked by 'make check' (automake only, it would be nice if someone
    could add it to cmake as well), or by calling
    
    python src/tex2lyx/test/runtests.py <path to tex2lyx binary>
    
    by hand. Currently, it does not compare the output (this comes later).
    The added .lyx files are from tex2lyx around mid of april, so that you
    can see the regressions of the current version if you run the test
    yourself (simply run git diff afterwards).
    
    The home made test runner is quite stupid, but better than nothing.
    Feel free to improve it or replace it with something better, as long
    as running it stays as simple as now.

---
+++ src/tex2lyx/test/runtests.py
@@ -0,0 +1,48 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# file src/tex2lyx/test/runtests.py
+# This file is part of LyX, the document processor.
+# Licence details can be found in the file COPYING.
+
+# author Georg Baum
+
+# Full author contact details are available in file CREDITS
+
+# This script reads a unicode symbol file and completes it in the given range
+
+import os, string, sys
+
+
+def usage(prog_name):
+    return "Usage: %s [<tex2lyx binary>]" % prog_name
+
+
+def main(argv):
+    # Parse and manipulate the command line arguments.
+    sys.path.append(os.path.join(os.path.dirname(sys.argv[0]), '../../../lib/scripts'))
+    from lyxpreview_tools import error
+
+    if len(argv) == 1:
+        tex2lyx = './tex2lyx'
+    elif len(argv) == 2:
+        tex2lyx = argv[1]
+    else:
+        error(usage(argv[0]))
+
+    basedir = os.path.dirname(argv[0])
+
+    files = ['test.ltx', 'test-structure.tex', 'test-insets.tex', \
+             'box-color-size-space-align.tex', 'CJK.tex', \
+             'XeTeX-polyglossia.tex']
+
+    for f in files:
+        texfile = os.path.join(os.path.dirname(argv[0]), f)
+        cmd = '%s -roundtrip -f %s' % (tex2lyx, texfile)
+        if os.system(cmd) != 0:
+            error('Running `%s´ failed.' % cmd)
+
+
+if __name__ == "__main__":
+    main(sys.argv)
+
