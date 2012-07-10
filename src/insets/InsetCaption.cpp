f623ded10945d1bc81efcb9ac4b63c25de5c21a1
Richard Heck <rgheck@lyx.org>
2012-07-10 17:44:21 -0400

    Fix bug #7948 by not hardcoding XHTML info for captions but instead using InsetLayout.

--- src/insets/InsetCaption.cpp
+++ src/insets/InsetCaption.cpp
@@ -305,13 +305,18 @@ docstring InsetCaption::xhtml(XHTMLStream & xs, OutputParams const & rp) const
 {
 	if (rp.html_disable_captions)
 		return docstring();
-	string attr = "class='float-caption";
-	if (!floattype_.empty())
-		attr += " float-caption-" + floattype_;
-	attr += "'";
-	xs << html::StartTag("div", attr);
+	InsetLayout const & il = getLayout();
+	string const tag = il.htmltag();
+	string attr = il.htmlattr();
+	if (!type_.empty()) {
+		string const our_class = "float-caption-" + type_;
+		size_t const loc = attr.find("class='");
+		if (loc != string::npos)
+			attr.insert(loc + 1, our_class);
+	}
+	xs << html::StartTag(tag, attr);
 	docstring def = getCaptionAsHTML(xs, rp);
-	xs << html::EndTag("div");
+	xs << html::EndTag(tag);
 	return def;
 }
 
