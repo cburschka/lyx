13dfbd1927ea9a4e8954a6ad0c58e0ef90c71d29
Pavel Sanda <sanda@lyx.org>
2012-09-20 13:35:53 +0200

    Simplify translation string.

--- src/Buffer.cpp
+++ src/Buffer.cpp
@@ -3373,7 +3373,7 @@ void Buffer::getSourceCode(odocstream & os, string const format,
 			writeLyXHTMLSource(os, runparams, output);
 		} else if (runparams.flavor == OutputParams::TEXT) {
 			if (output == OnlyPreamble) {
-				os << _("% Plaintext does not have a preamble.");
+				os << "% "<< _("Plain text does not have a preamble.");
 			} else
 				writePlaintextFile(*this, os, runparams);
 		} else if (params().isDocBook()) {
