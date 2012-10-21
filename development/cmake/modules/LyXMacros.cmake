e94e9e415f4a6ea074ea8ee34deee57b2b5d4d17
Kornel Benko <kornel@lyx.org>
2012-10-21 19:16:37 +0200

    Cmake build: Recreate Resources.qrc at build-time if needed. (E.g. after `make clean')

--- development/cmake/modules/LyXMacros.cmake
+++ development/cmake/modules/LyXMacros.cmake
@@ -229,6 +229,11 @@ macro(lyx_qt_resources_file _qrc_name _to_dir _list)
 		file(APPEND  ${_qrc_name} "</qresource>\n")
 		file(APPEND  ${_qrc_name} "</RCC>\n")
 	endif()
+	add_custom_command(
+	  OUTPUT ${_qrc_name}
+	  COMMAND ${CMAKE_COMMAND} --build ${CMAKE_BINARY_DIR} --target rebuild_cache
+	  )
+
 endmacro(lyx_qt_resources_file)
 
 
