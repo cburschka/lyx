cburschka/lyx

commit bc51243587e198e8d64b2bf686516d197bdccf0d

insets/InsetLine.cpp: use std::abs
Use std::abs in stead of std::max(v, -v).

src/insets/InsetLine.cpp
@@ -118,7 +118,7 @@ void InsetLine::metrics(MetricsInfo & mi, Dimension & dim) const
 
 	// set a minimal width
 	int const minw = (dim.wid < 0) ? 24 : 4;
-	dim.wid = max(minw, max(dim.wid, -dim.wid));
+	dim.wid = max(minw, abs(dim.wid));
 
 	Length height = Length(to_ascii(getParam("height")));
 	height_ = height.inPixels(max_width, fm.width(char_type('M')));

commit 76a5f726a7175d652b23f5926156724e2d5b010d

InsetLine.cpp: add <cstdlib>
This fixes a problem introduced in bc51243587e198e8d64b2bf686516d197bdccf0d

src/insets/InsetLine.cpp
@@ -35,6 +35,8 @@
 #include "support/gettext.h"
 #include "support/lstrings.h"
 
+#include <cstdlib>
+
 using namespace std;
 
 namespace lyx {