a834d161d201be873a5bd9777a38378c47082274
Juergen Spitzmueller <spitz@lyx.org>
2012-11-20 19:00:06 +0100

    Allow arguments in PassThru environments (bug #7646) (cherry picked from commit 34ef91bb70937c68462fd82fdc9373b0235e0492)

--- src/Text3.cpp
+++ src/Text3.cpp
@@ -2257,6 +2257,7 @@ bool Text::getStatus(Cursor & cur, FuncRequest const & cmd,
 
 	FontInfo const & fontinfo = cur.real_current_font.fontInfo();
 	bool enable = true;
+	bool allow_in_passthru = false;
 	InsetCode code = NO_CODE;
 
 	switch (cmd.action()) {
@@ -2447,6 +2448,7 @@ bool Text::getStatus(Cursor & cur, FuncRequest const & cmd,
 		break;
 	case LFUN_ARGUMENT_INSERT: {
 		code = ARG_CODE;
+		allow_in_passthru = true;
 		Layout const & lay = cur.paragraph().layout();
 		int const numargs = lay.reqargs + lay.optargs;
 		enable = cur.paragraph().insetList().count(ARG_CODE) < numargs;
@@ -2811,7 +2813,7 @@ bool Text::getStatus(Cursor & cur, FuncRequest const & cmd,
 	if (code != NO_CODE
 	    && (cur.empty() 
 		|| !cur.inset().insetAllowed(code)
-		|| cur.paragraph().layout().pass_thru))
+		|| (cur.paragraph().layout().pass_thru && !allow_in_passthru)))
 		enable = false;
 
 	flag.setEnabled(enable);
