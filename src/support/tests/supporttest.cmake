892e237fe1c5d7574b08d017070262239465f70b
Kornel Benko <kornel@lyx.org>
2012-12-15 00:15:59 +0100

    Cmake build: Testing Make this all more the cmake way

---
+++ src/support/tests/supporttest.cmake
@@ -0,0 +1,30 @@
+# This file is part of LyX, the document processor.
+# Licence details can be found in the file COPYING.
+#
+# Copyright (c) 2012 Kornel Benko kornel@lyx.org
+#
+#
+# Script should be called like:
+# cmake -DCommand=xxx \
+#       -DInput=xxx \
+#       -DOutput=xxx \
+#       -P xxxx/supporttest.cmake
+#
+# 
+# Command: The executable, whose output is to be examined
+# Input: The file name of expected data
+# Output: The file name for data catched from output of the executable
+
+execute_process(COMMAND "${Command}"
+  OUTPUT_FILE "${Output}"
+  RESULT_VARIABLE _testres)
+
+if(_testres)
+  message(FATAL_ERROR "${Command} not working")
+endif()
+
+execute_process(COMMAND ${CMAKE_COMMAND} -E compare_files "${Input}" "${Output}"
+                RESULT_VARIABLE _testres)
+if(_testres)
+  message(FATAL_ERROR "Files are not eqal")
+endif()
