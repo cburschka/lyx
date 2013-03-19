73c424323d4fb4218d0f49ab187a5cebc463cf37
Georg Baum <baum@lyx.org>
2013-03-19 21:17:15 +0100

    Update lyxrc.defaults to current format
    Actually, configure.py was already producing the current format, since it
    did not output default_language.

--- lib/configure.py
+++ lib/configure.py
@@ -1387,7 +1387,7 @@ if __name__ == '__main__':
     lyx_check_config = True
     lyx_kpsewhich = True
     outfile = 'lyxrc.defaults'
-    lyxrc_fileformat = 7
+    lyxrc_fileformat = 10
     rc_entries = ''
     lyx_keep_temps = False
     version_suffix = ''
