ae2d0e0d43f2832cfccb938f1df2d42b7b9dd2af
Vincent van Ravesteijn <vfr@lyx.org>
2013-06-04 19:37:40 +0200

    Update format of lyxrc.default
    No changes are necessary.

--- lib/configure.py
+++ lib/configure.py
@@ -1407,7 +1407,7 @@ if __name__ == '__main__':
     lyx_check_config = True
     lyx_kpsewhich = True
     outfile = 'lyxrc.defaults'
-    lyxrc_fileformat = 11
+    lyxrc_fileformat = 14
     rc_entries = ''
     lyx_keep_temps = False
     version_suffix = ''
