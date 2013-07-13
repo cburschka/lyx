cburschka/lyx

commit c14b9e67bcf6ac16534a6d37df766c4583722045

Implement paste from LaTeX and HTML (bug #3096)
As discussed on the list. No automatic contents detection is done, the user
needs to use the special paste menu instead. I used the new TempFile class
for safe temporary file handling.
The documentation would go into section 2.2 of UserGuide.lyx, but I am not
allowed to edit that document.

src/CutAndPaste.cpp
@@ -1010,16 +1010,21 @@ void pasteFromStack(Cursor & cur, ErrorList & errorList, size_t sel_index)
 }
 
 
-void pasteClipboardText(Cursor & cur, ErrorList & errorList, bool asParagraphs)
+void pasteClipboardText(Cursor & cur, ErrorList & errorList, bool asParagraphs,
+                        Clipboard::TextType type)
 {
 	// Use internal clipboard if it is the most recent one
+	// This overrides asParagraphs and type on purpose!
 	if (theClipboard().isInternal()) {
 		pasteFromStack(cur, errorList, 0);
 		return;
 	}
 
 	// First try LyX format
-	if (theClipboard().hasLyXContents()) {
+	if ((type == Clipboard::LyXTextType ||
+	     type == Clipboard::LyXOrPlainTextType ||
+	     type == Clipboard::AnyTextType) &&
+	    theClipboard().hasTextContents(Clipboard::LyXTextType)) {
 		string lyx = theClipboard().getAsLyX();
 		if (!lyx.empty()) {
 			// For some strange reason gcc 3.2 and 3.3 do not accept
@@ -1035,8 +1040,42 @@ void pasteClipboardText(Cursor & cur, ErrorList & errorList, bool asParagraphs)
 		}
 	}
 
+	// Then try TeX and HTML
+	Clipboard::TextType types[2] = {Clipboard::HtmlTextType, Clipboard::LaTeXTextType};
+	string names[2] = {"html", "latex"};
+	for (int i = 0; i < 2; ++i) {
+		if (type != types[i] && type != Clipboard::AnyTextType)
+			continue;
+		bool available = theClipboard().hasTextContents(types[i]);
+
+		// If a specific type was explicitly requested, try to
+		// interpret plain text: The user told us that the clipboard
+		// contents is in the desired format
+		if (!available && type == types[i]) {
+			types[i] = Clipboard::PlainTextType;
+			available = theClipboard().hasTextContents(types[i]);
+		}
+
+		if (available) {
+			docstring text = theClipboard().getAsText(types[i]);
+			available = !text.empty();
+			if (available) {
+				// For some strange reason gcc 3.2 and 3.3 do not accept
+				// Buffer buffer(string(), false);
+				Buffer buffer("", false);
+				buffer.setUnnamed(true);
+				if (buffer.importString(names[i], text, errorList)) {
+					cur.recordUndo();
+					pasteParagraphList(cur, buffer.paragraphs(),
+						buffer.params().documentClassPtr(), errorList);
+					return;
+				}
+			}
+		}
+	}
+
 	// Then try plain text
-	docstring const text = theClipboard().getAsText();
+	docstring const text = theClipboard().getAsText(Clipboard::PlainTextType);
 	if (text.empty())
 		return;
 	cur.recordUndo();
@@ -1065,7 +1104,7 @@ void pasteSimpleText(Cursor & cur, bool asParagraphs)
 		asParagraphs = false;
 	} else {
 		// Then try plain text
-		text = theClipboard().getAsText();
+		text = theClipboard().getAsText(Clipboard::PlainTextType);
 	}
 
 	if (text.empty())

src/CutAndPaste.h
@@ -87,8 +87,9 @@ void pasteSelection(Cursor & cur, ErrorList &);
 /// Replace the current selection with the clipboard contents as text
 /// (internal or external: which is newer).
 /// Does handle undo. Does only work in text, not mathed.
-void pasteClipboardText(Cursor & cur, ErrorList & errorList,
-	bool asParagraphs = true);
+/// \p asParagraphs is only considered if plain text is pasted.
+void pasteClipboardText(Cursor & cur, ErrorList & errorList, bool asParagraphs,
+	Clipboard::TextType preferedType = Clipboard::LyXOrPlainTextType);
 /// Replace the current selection with the clipboard contents as graphic.
 /// Does handle undo. Does only work in text, not mathed.
 void pasteClipboardGraphics(Cursor & cur, ErrorList & errorList,

src/Text3.cpp
@@ -1248,11 +1248,15 @@ void Text::dispatch(Cursor & cur, FuncRequest & cmd)
 				     && !theClipboard().hasTextContents())
 				pasteClipboardGraphics(cur, bv->buffer().errorList("Paste"));
 			else
-				pasteClipboardText(cur, bv->buffer().errorList("Paste"));
+				pasteClipboardText(cur, bv->buffer().errorList("Paste"), true);
 		} else if (isStrUnsignedInt(arg)) {
 			// we have a numerical argument
 			pasteFromStack(cur, bv->buffer().errorList("Paste"),
 				       convert<unsigned int>(arg));
+		} else if (arg == "html" || arg == "latex") {
+			Clipboard::TextType type = (arg == "html") ?
+				Clipboard::HtmlTextType : Clipboard::LaTeXTextType;
+			pasteClipboardText(cur, bv->buffer().errorList("Paste"), true, type);
 		} else {
 			Clipboard::GraphicsType type = Clipboard::AnyGraphicsType;
 			if (arg == "pdf")
@@ -1267,7 +1271,6 @@ void Text::dispatch(Cursor & cur, FuncRequest & cmd)
 				type = Clipboard::EmfGraphicsType;
 			else if (arg == "wmf")
 				type = Clipboard::WmfGraphicsType;
-
 			else
 				LASSERT(false, /**/);
 
@@ -2769,6 +2772,20 @@ bool Text::getStatus(Cursor & cur, FuncRequest const & cmd,
 			// it's a number and therefore means the internal stack
 			unsigned int n = convert<unsigned int>(arg);
 			enable = cap::numberOfSelections() > n;
+			break;
+		}
+
+		// explicit text type?
+		if (arg == "html") {
+			// Do not enable for PlainTextType, since some tidying in the
+			// frontend is needed for HTML, which is too unsafe for plain text.
+			enable = theClipboard().hasTextContents(Clipboard::HtmlTextType);
+			break;
+		} else if (arg == "latex") {
+			// LaTeX is usually not available on the clipboard with
+			// the correct MIME type, but in plain text.
+			enable = theClipboard().hasTextContents(Clipboard::PlainTextType) ||
+			         theClipboard().hasTextContents(Clipboard::LaTeXTextType);
 			break;
 		}
 

lib/ui/stdmenus.inc
@@ -156,6 +156,8 @@ Menuset
 	Menu "edit_paste"
 		Item "Plain Text|T" "clipboard-paste"
 		Item "Plain Text, Join Lines|J" "clipboard-paste paragraph"
+		Item "HTML Text|H" "paste html"
+		Item "LaTeX Text|L" "paste latex"
 		Separator
 		Item "Selection|S" "primary-selection-paste"
 		Item "Selection, Join Lines|i" "primary-selection-paste paragraph"

src/Buffer.cpp
@@ -102,6 +102,7 @@
 #include "support/Package.h"
 #include "support/PathChanger.h"
 #include "support/Systemcall.h"
+#include "support/TempFile.h"
 #include "support/textutils.h"
 #include "support/types.h"
 
@@ -978,6 +979,49 @@ bool Buffer::readDocument(Lexer & lex)
 }
 
 
+bool Buffer::importString(string const & format, docstring const & contents, ErrorList & errorList)
+{
+	Format const * fmt = formats.getFormat(format);
+	if (!fmt)
+		return false;
+	// It is important to use the correct extension here, since some
+	// converters create a wrong output file otherwise (e.g. html2latex)
+	TempFile const tempfile("Buffer_importStringXXXXXX." + fmt->extension());
+	FileName const name(tempfile.name());
+	ofdocstream os(name.toFilesystemEncoding().c_str());
+	bool const success = (os << contents);
+	os.close();
+
+	bool converted = false;
+	if (success) {
+		params().compressed = false;
+
+		// remove dummy empty par
+		paragraphs().clear();
+
+		converted = importFile(format, name, errorList);
+	}
+
+	if (name.exists())
+		name.removeFile();
+	return converted;
+}
+
+
+bool Buffer::importFile(string const & format, FileName const & name, ErrorList & errorList)
+{
+	if (!theConverters().isReachable(format, "lyx"))
+		return false;
+
+	TempFile const tempfile("Buffer_importFileXXXXXX.lyx");
+	FileName const lyx(tempfile.name());
+	if (theConverters().convert(0, name, lyx, name, format, "lyx", errorList))
+		return readFile(lyx) == ReadSuccess;
+
+	return false;
+}
+
+
 bool Buffer::readString(string const & s)
 {
 	params().compressed = false;
@@ -1125,7 +1169,7 @@ Buffer::ReadStatus Buffer::parseLyXFormat(Lexer & lex,
 Buffer::ReadStatus Buffer::convertLyXFormat(FileName const & fn,
 	FileName & tmpfile, int from_format)
 {
-	tmpfile = FileName::tempName("Buffer_convertLyXFormat");
+	tmpfile = FileName::tempName("Buffer_convertLyXFormatXXXXXX.lyx");
 	if(tmpfile.empty()) {
 		Alert::error(_("Conversion failed"),
 			bformat(_("%1$s is from a different"
@@ -2765,11 +2809,12 @@ string Buffer::absFileName() const
 
 string Buffer::filePath() const
 {
-	int last = d->filename.onlyPath().absFileName().length() - 1;
+	string const abs = d->filename.onlyPath().absFileName();
+	if (abs.empty())
+		return abs;
+	int last = abs.length() - 1;
 
-	return d->filename.onlyPath().absFileName()[last] == '/'
-		? d->filename.onlyPath().absFileName()
-		: d->filename.onlyPath().absFileName() + "/";
+	return abs[last] == '/' ? abs : abs + '/';
 }
 
 

src/Buffer.h
@@ -222,6 +222,10 @@ public:
 	/// emergency or autosave files, one should use \c loadLyXFile.
 	/// /sa loadLyXFile
 	ReadStatus loadThisLyXFile(support::FileName const & fn);
+	/// import a new document from a string
+	bool importString(std::string const &, docstring const &, ErrorList &);
+	/// import a new file
+	bool importFile(std::string const &, support::FileName const &, ErrorList &);
 	/// read a new document from a string
 	bool readString(std::string const &);
 	/// Reloads the LyX file

src/LyXAction.cpp
@@ -1215,7 +1215,7 @@ void LyXAction::init()
  * \var lyx::FuncCode lyx::LFUN_PASTE
  * \li Action: Pastes material (text or picture) from the active clipboard.
  * \li Syntax: paste [<TYPE>|<NUM>]
- * \li Params: <TYPE>: emf|pdf|png|jpeg|linkback|wmf \n
+ * \li Params: <TYPE>: emf|pdf|png|jpeg|linkback|wmf|latex|html \n
                <NUM>: number of the selection in the internal clipboard stack to be pasted.
  * \endvar
  */

src/frontends/Clipboard.h
@@ -42,6 +42,15 @@ public:
 		AnyGraphicsType
 	};
 
+	enum TextType {
+		AnyTextType,
+		LyXOrPlainTextType,
+		PlainTextType,
+		HtmlTextType,
+		LaTeXTextType,
+		LyXTextType,
+	};
+
 	/**
 	 * Get the system clipboard contents. The format is as written in
 	 * .lyx files (may even be an older version than ours if it comes
@@ -51,8 +60,8 @@ public:
 	 * clipboard.
 	 */
 	virtual std::string const getAsLyX() const = 0;
-	/// Get the contents of the window system clipboard in plain text format.
-	virtual docstring const getAsText() const = 0;
+	/// Get the contents of the window system clipboard in any text format except LyxTextType.
+	virtual docstring const getAsText(TextType type) const = 0;
 	/// Get the contents of the window system clipboard as graphics file.
 	virtual FileName getAsGraphics(Cursor const & cur, GraphicsType type) const = 0;
 
@@ -67,10 +76,8 @@ public:
 	 */
 	virtual void put(std::string const & lyx, docstring const & html, docstring const & text) = 0;
 
-	/// Does the clipboard contain LyX contents?
-	virtual bool hasLyXContents() const = 0;
 	/// Does the clipboard contain text contents?
-	virtual bool hasTextContents() const = 0;
+	virtual bool hasTextContents(TextType type = AnyTextType) const = 0;
 	/// Does the clipboard contain graphics contents of a certain type?
 	virtual bool hasGraphicsContents(GraphicsType type = AnyGraphicsType) const = 0;
 	/// state of clipboard.

commit c0ea37f33791515a1991151581021092b2022595

Fix image pasting regression
I introduced a regression in c14b9e67 for pasting images:
If an image is on the clipboard both as PNG and HTML with just an url,
but no plain text, pasting would fail. The reason for this was that
text contents was detected (the HTML code), nd preferred, but actually
pasting it resulted in an empty string, since the HTML import could not
handle the url This error was not checked.
The solution is first to try text paste if both text and image content
is present, and then  try image paste if the text failed.

src/CutAndPaste.cpp
@@ -1053,27 +1053,26 @@ void pasteParagraphList(Cursor & cur, ParagraphList const & parlist,
 }
 
 
-void pasteFromStack(Cursor & cur, ErrorList & errorList, size_t sel_index)
+bool pasteFromStack(Cursor & cur, ErrorList & errorList, size_t sel_index)
 {
 	// this does not make sense, if there is nothing to paste
 	if (!checkPastePossible(sel_index))
-		return;
+		return false;
 
 	cur.recordUndo();
 	pasteParagraphList(cur, theCuts[sel_index].first,
 			   theCuts[sel_index].second, errorList);
+	return true;
 }
 
 
-void pasteClipboardText(Cursor & cur, ErrorList & errorList, bool asParagraphs,
+bool pasteClipboardText(Cursor & cur, ErrorList & errorList, bool asParagraphs,
                         Clipboard::TextType type)
 {
 	// Use internal clipboard if it is the most recent one
 	// This overrides asParagraphs and type on purpose!
-	if (theClipboard().isInternal()) {
-		pasteFromStack(cur, errorList, 0);
-		return;
-	}
+	if (theClipboard().isInternal())
+		return pasteFromStack(cur, errorList, 0);
 
 	// First try LyX format
 	if ((type == Clipboard::LyXTextType ||
@@ -1090,7 +1089,7 @@ void pasteClipboardText(Cursor & cur, ErrorList & errorList, bool asParagraphs,
 				cur.recordUndo();
 				pasteParagraphList(cur, buffer.paragraphs(),
 					buffer.params().documentClassPtr(), errorList);
-				return;
+				return true;
 			}
 		}
 	}
@@ -1119,11 +1118,14 @@ void pasteClipboardText(Cursor & cur, ErrorList & errorList, bool asParagraphs,
 				// Buffer buffer(string(), false);
 				Buffer buffer("", false);
 				buffer.setUnnamed(true);
-				if (buffer.importString(names[i], text, errorList)) {
+				available = buffer.importString(names[i], text, errorList);
+				if (available)
+					available = !buffer.paragraphs().empty();
+				if (available && !buffer.paragraphs()[0].empty()) {
 					cur.recordUndo();
 					pasteParagraphList(cur, buffer.paragraphs(),
 						buffer.params().documentClassPtr(), errorList);
-					return;
+					return true;
 				}
 			}
 		}
@@ -1132,12 +1134,13 @@ void pasteClipboardText(Cursor & cur, ErrorList & errorList, bool asParagraphs,
 	// Then try plain text
 	docstring const text = theClipboard().getAsText(Clipboard::PlainTextType);
 	if (text.empty())
-		return;
+		return false;
 	cur.recordUndo();
 	if (asParagraphs)
 		cur.text()->insertStringAsParagraphs(cur, text, cur.current_font);
 	else
 		cur.text()->insertStringAsLines(cur, text, cur.current_font);
+	return true;
 }
 
 

src/CutAndPaste.h
@@ -88,7 +88,7 @@ void pasteSelection(Cursor & cur, ErrorList &);
 /// (internal or external: which is newer).
 /// Does handle undo. Does only work in text, not mathed.
 /// \p asParagraphs is only considered if plain text is pasted.
-void pasteClipboardText(Cursor & cur, ErrorList & errorList, bool asParagraphs,
+bool pasteClipboardText(Cursor & cur, ErrorList & errorList, bool asParagraphs,
 	Clipboard::TextType preferedType = Clipboard::LyXOrPlainTextType);
 /// Replace the current selection with the clipboard contents as graphic.
 /// Does handle undo. Does only work in text, not mathed.
@@ -96,7 +96,7 @@ void pasteClipboardGraphics(Cursor & cur, ErrorList & errorList,
 	Clipboard::GraphicsType preferedType = Clipboard::AnyGraphicsType);
 /// Replace the current selection with cut buffer \c sel_index
 /// Does handle undo. Does only work in text, not mathed.
-void pasteFromStack(Cursor & cur, ErrorList & errorList, size_t sel_index);
+bool pasteFromStack(Cursor & cur, ErrorList & errorList, size_t sel_index);
 /// Paste the clipboard as simple text, removing any formatting
 void pasteSimpleText(Cursor & cur, bool asParagraphs);
 

src/Text3.cpp
@@ -1226,13 +1226,16 @@ void Text::dispatch(Cursor & cur, FuncRequest & cmd)
 		// without argument?
 		string const arg = to_utf8(cmd.argument());
 		if (arg.empty()) {
+			bool tryGraphics = true;
 			if (theClipboard().isInternal())
 				pasteFromStack(cur, bv->buffer().errorList("Paste"), 0);
-			else if (theClipboard().hasGraphicsContents() 
-				     && !theClipboard().hasTextContents())
+			else if (theClipboard().hasTextContents()) {
+				if (pasteClipboardText(cur, bv->buffer().errorList("Paste"),
+				                       true, Clipboard::AnyTextType))
+					tryGraphics = false;
+			}
+			if (tryGraphics && theClipboard().hasGraphicsContents())
 				pasteClipboardGraphics(cur, bv->buffer().errorList("Paste"));
-			else
-				pasteClipboardText(cur, bv->buffer().errorList("Paste"), true);
 		} else if (isStrUnsignedInt(arg)) {
 			// we have a numerical argument
 			pasteFromStack(cur, bv->buffer().errorList("Paste"),