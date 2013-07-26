16cdf70d0c4bd18eb066fee21ee7e4f35257d2f5
Stephan Witt <switt@lyx.org>
2013-07-26 13:04:32 +0200

    enable automake version 1.14

--- autogen.sh
+++ autogen.sh
@@ -16,12 +16,12 @@ test "$automake_version" != "" && {
 }
 
 case $automake_version in
-    *' '1.[8-9]*|*' '1.1[0123]*)
+    *' '1.[8-9]*|*' '1.1[01234]*)
 	;;
     *)
 
 	echo "This automake version is not supported by LyX."
-	echo "LyX only supports automake 1.8 to 1.13."
+	echo "LyX only supports automake 1.8 to 1.14."
 	exit 1
 	;;
 esac
