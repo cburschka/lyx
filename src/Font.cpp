cburschka/lyx

commit 47f7d44747002726307e615ddf0ad35dc8b59dbf

fix bug #8215
Font.cpp: CJK parts must not have a closing brace because they are within an environment

src/Font.cpp
@@ -540,7 +540,8 @@ int Font::latexWriteEndChanges(otexstream & os, BufferParams const & bparams,
 	}
 
 	if (closeLanguage &&
-			language() != base.language() && language() != next.language()) {
+			language() != base.language() && language() != next.language()
+			&& language()->encoding()->package() != Encoding::CJK) {
 		os << '}';
 		++count;
 	}

commit 969f49c2c5b55b428852f0b2c1c6e78c770608f3

Fix unbalanced bracket problem with CJK
The bug was introduced with commit [47f7d447/lyxgit], where the unnecessary trailing bracket in CJK environments was suppresed, but not the preceding bracket (which is only output if CJK is a secondary language).

src/Font.cpp
@@ -286,7 +286,7 @@ int Font::latexWriteStartChanges(odocstream & os, BufferParams const & bparams,
 			tmp += "{";
 			os << from_ascii(tmp);
 			count += tmp.length();
-		} else {
+		} else if (language()->encoding()->package() != Encoding::CJK) {
 			os << '{';
 			count += 1;
 		}
@@ -321,7 +321,7 @@ int Font::latexWriteStartChanges(odocstream & os, BufferParams const & bparams,
 				      "$$lang", language()->babel());
 			os << from_ascii(tmp);
 			count += tmp.length();
-		} else {
+		} else if (language()->encoding()->package() != Encoding::CJK) {
 			os << '{';
 			count += 1;
 		}