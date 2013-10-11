cburschka/lyx

commit 2af09e2e0e9246f702c9845815113dfc5ef9b704

backport fix for bug #8215
Font.cpp: CJK parts must not have a closing brace because they are within an environment

src/Font.cpp
@@ -539,8 +539,9 @@ int Font::latexWriteEndChanges(otexstream & os, BufferParams const & bparams,
 		open_encoding_ = false;
 	}
 
-	if (closeLanguage &&
-			language() != base.language() && language() != next.language()) {
+	if (closeLanguage
+	    && language() != base.language() && language() != next.language()
+	    && language()->encoding()->package() != Encoding::CJK) {
 		os << '}';
 		++count;
 	}

status.20x
@@ -63,6 +63,9 @@ What's new
 
 - Do not ignore polyglossia commands in partial source preview (bug 8209).
 
+- Export correct language change commands if document coontains different
+  CJK languages (bug 8215).
+
 
 * USER INTERFACE
 

commit b9976f59291e431a6dc9c5291172d9bb61d8c552

Fix unbalanced bracket problem with CJK
The bug was introduced with commit [2af09e2e/lyxgit], where the unnecessary trailing bracket in CJK environments was suppresed, but not the preceding bracket (which is only output if CJK is a secondary language).

src/Font.cpp
@@ -284,7 +284,7 @@ int Font::latexWriteStartChanges(odocstream & os, BufferParams const & bparams,
 			tmp += "{";
 			os << from_ascii(tmp);
 			count += tmp.length();
-		} else {
+		} else if (language()->encoding()->package() != Encoding::CJK) {
 			os << '{';
 			count += 1;
 		}
@@ -319,7 +319,7 @@ int Font::latexWriteStartChanges(odocstream & os, BufferParams const & bparams,
 				      "$$lang", language()->babel());
 			os << from_ascii(tmp);
 			count += tmp.length();
-		} else {
+		} else if (language()->encoding()->package() != Encoding::CJK) {
 			os << '{';
 			count += 1;
 		}

status.20x
@@ -59,6 +59,9 @@ What's new
 
 - Fix problem with unbalanced braces in XeTeX output (bug 8765).
 
+- Fix problems with unbalanced braces with a secondary CJK language
+  (bug 8215).
+
 - Fix state of certain language packages (polyglossia, japanese) in
   child documents (bug 8770).
 