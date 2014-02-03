eb219e1130f443d0bf4803d701416670723cc949
Vincent van Ravesteijn <vfr@lyx.org>
2014-02-03 21:10:48 +0100

    CMake: Regenerate deleted gmo files

--- development/cmake/po/update-gmo.py
+++ development/cmake/po/update-gmo.py
@@ -3,5 +3,5 @@
 ###############
 import sys, os, shutil
 
-if os.stat(sys.argv[1]).st_size > 0:
+if os.stat(sys.argv[1]).st_size > 0 or not os.path.isfile(sys.argv[3]):
     shutil.copyfile(sys.argv[2], sys.argv[3])
