b8661bcfb33bb7f8c0598c0f0c45229b77526bbb
Juergen Spitzmueller <spitz@lyx.org>
2014-03-22 12:22:41 +0100

    style

--- src/BufferView.cpp
+++ src/BufferView.cpp
@@ -2499,7 +2499,7 @@ void BufferView::putSelectionAt(DocIterator const & cur,
 bool BufferView::selectIfEmpty(DocIterator & cur)
 {
 	if ((cur.inTexted() && !cur.paragraph().empty())
-	    || cur.inMathed() && !cur.cell().empty())
+	    || (cur.inMathed() && !cur.cell().empty()))
 		return false;
 
 	pit_type const beg_pit = cur.pit();
