cburschka/lyx

commit c5ef7cfcc4cb9deb7faa587264490ff77f233e04

Do not insert trailing blaks to translatable string while generating .pot files.

po/lyx_pot.py
@@ -80,32 +80,32 @@ def layouts_l10n(input_files, output, base, layouttranslations):
     '''Generate pot file from lib/layouts/*.{layout,inc,module}'''
     ClassDescription = re.compile(r'^\s*#\s*\\Declare(LaTeX|DocBook)Class.*\{(.*)\}$', re.IGNORECASE)
     ClassCategory = re.compile(r'^\s*#\s*\\DeclareCategory\{(.*)\}$', re.IGNORECASE)
-    Style = re.compile(r'^\s*Style\s+(.*)\s*$', re.IGNORECASE)
+    Style = re.compile(r'^\s*Style\s+(.*\S)\s*$', re.IGNORECASE)
     # match LabelString, EndLabelString, LabelStringAppendix and maybe others but no comments
-    LabelString = re.compile(r'^[^#]*LabelString\S*\s+(.*)\s*$', re.IGNORECASE)
-    MenuString = re.compile(r'^[^#]*MenuString\S*\s+(.*)\s*$', re.IGNORECASE)
-    Tooltip = re.compile(r'^[^#]*Tooltip\S*\s+(.*)\s*$', re.IGNORECASE)
-    GuiName = re.compile(r'^\s*GuiName\s+(.*)\s*$', re.IGNORECASE)
-    ListName = re.compile(r'^\s*ListName\s+(.*)\s*$', re.IGNORECASE)
-    CategoryName = re.compile(r'^\s*Category\s+(.*)\s*$', re.IGNORECASE)
+    LabelString = re.compile(r'^[^#]*LabelString\S*\s+(.*\S)\s*$', re.IGNORECASE)
+    MenuString = re.compile(r'^[^#]*MenuString\S*\s+(.*\S)\s*$', re.IGNORECASE)
+    Tooltip = re.compile(r'^[^#]*Tooltip\S*\s+(.*\S)\s*$', re.IGNORECASE)
+    GuiName = re.compile(r'^\s*GuiName\s+(.*\S)\s*$', re.IGNORECASE)
+    ListName = re.compile(r'^\s*ListName\s+(.*\S)\s*$', re.IGNORECASE)
+    CategoryName = re.compile(r'^\s*Category\s+(.*\S)\s*$', re.IGNORECASE)
     NameRE = re.compile(r'^\s*#\s*\\DeclareLyXModule.*{(.*)}$', re.IGNORECASE)
     InsetLayout = re.compile(r'^InsetLayout\s+\"?(.*)\"?\s*$', re.IGNORECASE)
     FlexCheck = re.compile(r'^Flex:(.*)', re.IGNORECASE)
     DescBegin = re.compile(r'^\s*#DescriptionBegin\s*$', re.IGNORECASE)
     DescEnd = re.compile(r'^\s*#\s*DescriptionEnd\s*$', re.IGNORECASE)
-    Category = re.compile(r'^\s*#\s*Category:\s+(.*)\s*$', re.IGNORECASE)
+    Category = re.compile(r'^\s*#\s*Category:\s+(.*\S)\s*$', re.IGNORECASE)
     I18nPreamble = re.compile(r'^\s*((Lang)|(Babel))Preamble\s*$', re.IGNORECASE)
     EndI18nPreamble = re.compile(r'^\s*End((Lang)|(Babel))Preamble\s*$', re.IGNORECASE)
     I18nString = re.compile(r'_\(([^\)]+)\)')
     CounterFormat = re.compile(r'^\s*PrettyFormat\s+"?(.*)"?\s*$', re.IGNORECASE)
     CiteFormat = re.compile(r'^\s*CiteFormat', re.IGNORECASE)
-    KeyVal = re.compile(r'^\s*_\w+\s+(.*)\s*$')
+    KeyVal = re.compile(r'^\s*_\w+\s+(.*\S)\s*$')
     Float = re.compile(r'^\s*Float\s*$', re.IGNORECASE)
-    UsesFloatPkg = re.compile(r'^\s*UsesFloatPkg\s+(.*)\s*$', re.IGNORECASE)
-    IsPredefined = re.compile(r'^\s*IsPredefined\s+(.*)\s*$', re.IGNORECASE)
+    UsesFloatPkg = re.compile(r'^\s*UsesFloatPkg\s+(.*\S)\s*$', re.IGNORECASE)
+    IsPredefined = re.compile(r'^\s*IsPredefined\s+(.*\S)\s*$', re.IGNORECASE)
     End = re.compile(r'^\s*End', re.IGNORECASE)
     Comment = re.compile(r'^(.*)#')
-    Translation = re.compile(r'^\s*Translation\s+(.*)\s*$', re.IGNORECASE)
+    Translation = re.compile(r'^\s*Translation\s+(.*\S)\s*$', re.IGNORECASE)
     KeyValPair = re.compile(r'\s*"(.*)"\s+"(.*)"')
 
     oldlanguages = []
@@ -489,7 +489,7 @@ def external_l10n(input_files, output, base):
     Template = re.compile(r'^Template\s+(.*)', re.IGNORECASE)
     GuiName = re.compile(r'\s*GuiName\s+(.*)', re.IGNORECASE)
     HelpTextStart = re.compile(r'\s*HelpText\s', re.IGNORECASE)
-    HelpTextSection = re.compile(r'\s*(\S.*)\s*$')
+    HelpTextSection = re.compile(r'\s*(\S.*\S)\s*$')
     HelpTextEnd = re.compile(r'\s*HelpTextEnd\s', re.IGNORECASE)
     i = -1
     for src in input_files:

commit 5e530e075734cee46f39d52a9f6fd7cda40545f5

lyx_pot.py: Do not strip blanks in HelpText.
This fixes a thinko introduced in [c5ef7cfc/lyxgit]

Fixes: #9054.

po/lyx_pot.py
@@ -500,7 +500,7 @@ def external_l10n(input_files, output, base):
     Template = re.compile(r'^Template\s+(.*)', re.IGNORECASE)
     GuiName = re.compile(r'\s*GuiName\s+(.*)', re.IGNORECASE)
     HelpTextStart = re.compile(r'\s*HelpText\s', re.IGNORECASE)
-    HelpTextSection = re.compile(r'\s*(\S.*\S)\s*$')
+    HelpTextSection = re.compile(r'\s*(\S.*)\s*$')
     HelpTextEnd = re.compile(r'\s*HelpTextEnd\s', re.IGNORECASE)
     i = -1
     for src in input_files: