76417c7326a9ad6822e0351f565000ecc950d636
Georg Baum <baum@lyx.org>
2014-05-29 13:15:07 +0200

    Do not corrupt documents without newline at EOF
    trim_eol() assumes that a line always ends either with \n, \r, or \r\n.
    This assumption is always valid except for the last line of a document, since it
    may miss the trailing newline. LyX does not create such documents, bu they may
    result from automatic creation tools, and LyX can read them, so lyx2lyx should
    be able to read them as well.
    (cherry picked from commit c75c6e446a8bc0dbaabaf156c0abfd622c050129)
    
    Fix the fix
    
    MAC-style (pre-OS X) line ends were not recognized anymore
    (cherry picked from commit 55af9cb006563032e35fcc8bc5c8c1284c0344da)

--- lib/lyx2lyx/LyX.py
+++ lib/lyx2lyx/LyX.py
@@ -145,6 +145,9 @@ def get_backend(textclass):
 
 def trim_eol(line):
     " Remove end of line char(s)."
+    if line[-1] != '\n' and line[-1] != '\r':
+        # May happen for the last line of a document
+        return line
     if line[-2:-1] == '\r':
         return line[:-2]
     else:
