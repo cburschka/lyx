8b31f838c5ceab2cc9aae03e31cdcc673c2a4381
Georg Baum <baum@lyx.org>
2014-11-14 21:28:40 +0100

    Make the fix for bug 8999 actually work
    This backports 1cdbb94f which was spotted by cppcheck.

--- src/mathed/MathMacro.cpp
+++ src/mathed/MathMacro.cpp
@@ -329,10 +329,11 @@ private:
 void MathMacro::updateRepresentation(Cursor * cur, MacroContext const & mc,
 		UpdateType utype)
 {
+	// block recursive calls (bug 8999)
 	if (isUpdating_)
 		return;
 
-	UpdateLocker(*this);
+	UpdateLocker locker(*this);
 
 	// known macro?
 	if (macro_ == 0)
