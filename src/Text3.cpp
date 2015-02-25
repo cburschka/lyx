cburschka/lyx

commit b627b8701b6598f5402cbab55a83026c9e3b1e86

Some performance stuff reported by cppcheck

src/Text3.cpp
@@ -476,7 +476,7 @@ void Text::dispatch(Cursor & cur, FuncRequest & cmd)
 	cur.noScreenUpdate();
 
 	LBUFERR(this == cur.text());
-	CursorSlice const oldTopSlice = cur.top();
+	CursorSlice const & oldTopSlice = cur.top();
 	bool const oldBoundary = cur.boundary();
 	bool const oldSelection = cur.selection();
 	// Signals that, even if needsUpdate == false, an update of the

src/Buffer.cpp
@@ -1851,7 +1851,7 @@ void Buffer::writeDocBookSource(odocstream & os, string const & fname,
 	d->texrow.reset();
 
 	DocumentClass const & tclass = params().documentClass();
-	string const top_element = tclass.latexname();
+	string const & top_element = tclass.latexname();
 
 	bool const output_preamble =
 		output == FullSource || output == OnlyPreamble;

src/LyX.cpp
@@ -763,7 +763,7 @@ void cleanDuplicateEnvVars()
 	}
 
 	// Loop over the list of duplicated variables
-	for (std::set<std::string>::iterator dupe = dupes.begin(); dupe != dupes.end(); dupe++) {
+	for (std::set<std::string>::iterator dupe = dupes.begin(); dupe != dupes.end(); ++dupe) {
 		const char *name = (*dupe).c_str();
 		char *val = getenv(name);
 		if (val != NULL) {

src/Paragraph.cpp
@@ -620,7 +620,7 @@ bool Paragraph::isMergedOnEndOfParDeletion(bool trackChanges) const
 	if (!trackChanges)
 		return true;
 
-	Change const change = d->changes_.lookup(size());
+	Change const & change = d->changes_.lookup(size());
 	return change.inserted() && change.currentAuthor();
 }
 
@@ -1640,7 +1640,7 @@ void Paragraph::write(ostream & os, BufferParams const & bparams,
 	int column = 0;
 	for (pos_type i = 0; i <= size(); ++i) {
 
-		Change const change = lookupChange(i);
+		Change const & change = lookupChange(i);
 		if (change != running_change)
 			flushString(os, write_buffer);
 		Changes::lyxMarkChange(os, bparams, column, running_change, change);

src/Text.cpp
@@ -113,8 +113,8 @@ static bool moveItem(Paragraph & fromPar, pos_type fromPos,
 	// Therefore, it should only be used for breaking and merging paragraphs
 
 	// We need a copy here because the character at fromPos is going to be erased.
-	Font const tmpFont = fromPar.getFontSettings(params, fromPos);
-	Change const tmpChange = fromPar.lookupChange(fromPos);
+	Font const & tmpFont = fromPar.getFontSettings(params, fromPos);
+	Change const & tmpChange = fromPar.lookupChange(fromPos);
 
 	if (Inset * tmpInset = fromPar.getInset(fromPos)) {
 		fromPar.releaseInset(fromPos);
@@ -2057,11 +2057,11 @@ void Text::charsTranspose(Cursor & cur)
 
 	// Store the characters to be transposed (including font information).
 	char_type const char1 = par.getChar(pos1);
-	Font const font1 =
+	Font const & font1 =
 		par.getFontSettings(cur.buffer()->params(), pos1);
 
 	char_type const char2 = par.getChar(pos2);
-	Font const font2 =
+	Font const & font2 =
 		par.getFontSettings(cur.buffer()->params(), pos2);
 
 	// And finally, we are ready to perform the transposition.

src/Text2.cpp
@@ -408,7 +408,7 @@ void Text::toggleFree(Cursor & cur, Font const & font, bool toggleall)
 	// Try implicit word selection
 	// If there is a change in the language the implicit word selection
 	// is disabled.
-	CursorSlice const resetCursor = cur.top();
+	CursorSlice const & resetCursor = cur.top();
 	bool const implicitSelection =
 		font.language() == ignore_language
 		&& font.fontInfo().number() == FONT_IGNORE

src/TextMetrics.cpp
@@ -446,7 +446,7 @@ bool TextMetrics::redoParagraph(pit_type const pit)
 		MacroContext mc(&buffer, parPos);
 		MetricsInfo mi(bv_, font.fontInfo(), w, mc);
 		ii->inset->metrics(mi, dim);
-		Dimension const old_dim = pm.insetDimension(ii->inset);
+		Dimension const & old_dim = pm.insetDimension(ii->inset);
 		if (old_dim != dim) {
 			pm.setInsetDimension(ii->inset, dim);
 			changed = true;

src/insets/InsetCaption.cpp
@@ -299,7 +299,7 @@ docstring InsetCaption::xhtml(XHTMLStream & xs, OutputParams const & rp) const
 	if (rp.html_disable_captions)
 		return docstring();
 	InsetLayout const & il = getLayout();
-	string const tag = il.htmltag();
+	string const & tag = il.htmltag();
 	string attr = il.htmlattr();
 	if (!type_.empty()) {
 		string const our_class = "float-caption-" + type_;

commit 94a5481a1762c7c7b12220aadb211fbe83adf77b

Partially revert b627b870, which broke cursor movement.

src/Text3.cpp
@@ -476,7 +476,7 @@ void Text::dispatch(Cursor & cur, FuncRequest & cmd)
 	cur.noScreenUpdate();
 
 	LBUFERR(this == cur.text());
-	CursorSlice const & oldTopSlice = cur.top();
+	CursorSlice const oldTopSlice = cur.top();
 	bool const oldBoundary = cur.boundary();
 	bool const oldSelection = cur.selection();
 	// Signals that, even if needsUpdate == false, an update of the