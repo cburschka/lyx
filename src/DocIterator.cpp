cburschka/lyx

commit 17e435c47e36effd36d25cec900369e04f6acb4e

Check that DocIterator::sanitize only adds editable insets
This fixes the crash on ticket #9432, but the bug there has other causes.

src/DocIterator.cpp
@@ -555,6 +555,11 @@ void DocIterator::sanitize()
 			fixIfBroken();
 			break;
 		}
+		if (!inset->editable()) {
+			LYXERR0("Inset found on cursor stack is not editable.");
+			fixIfBroken();
+			break;
+		}
 		push_back(sl[i]);
 		top().inset_ = inset;
 		if (fixIfBroken())

commit c2f785bdcc769f0c3ab0de186291c810f3f0875d

Use isActive() instead of editable() in mathed.
Fixes crash introduced in [17e435c4/lyxgit].

editable() is more related to Texted. It is false for closed collapsable insets
Eventually the two methods should be merged.

src/DocIterator.cpp
@@ -577,8 +577,9 @@ void DocIterator::sanitize()
 			fixIfBroken();
 			break;
 		}
-		if (!inset->editable()) {
-			LYXERR0("Inset found on cursor stack is not editable.");
+		if ((inset->inMathed() && !inset->isActive())
+		    || (!inset->inMathed() && !inset->editable())) {
+			LYXERR0("Inset found on cursor stack is not active.");
 			fixIfBroken();
 			break;
 		}