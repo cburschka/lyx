839f37317f1f4d910bb8bbfa416ebcbda7761947
Jean-Marc Lasgouttes <lasgouttes@lyx.org>
2015-04-15 15:27:05 +0200

    Avoid requiring messages with empty language.
    This is a follow-up to commit 8aa2d6b1.

--- src/frontends/qt4/GuiApplication.cpp
+++ src/frontends/qt4/GuiApplication.cpp
@@ -189,15 +189,13 @@ void setLocale()
 	if (lyxrc.gui_language == "auto") {
 		theLocale = QLocale::system();
 		code = fromqstr(theLocale.name());
-		// Qt tries to outsmart us and transforms en_US to C.
-		if (code == "C")
-			code = "en_US";
 	} else {
 		Language const * l = languages.getLanguage(lyxrc.gui_language);
-		code = l ? l->code() : string();
+		code = l ? l->code() : "C";
 		theLocale = QLocale(toqstr(code));
 	}
-	Messages::guiLanguage(code);
+	// Qt tries to outsmart us and transforms en_US to C.
+	Messages::guiLanguage((code == "C") ? "en_US" : code);
 	QLocale::setDefault(theLocale);
 	setlocale(LC_NUMERIC, "C");
 }
