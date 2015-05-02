b53773232aeb18fb1561c33e868d65ade78d76c8
Juergen Spitzmueller <spitz@lyx.org>
2015-05-02 11:38:46 +0200

    sign.

--- src/frontends/qt4/EmptyTable.cpp
+++ src/frontends/qt4/EmptyTable.cpp
@@ -4,6 +4,7 @@
  * Licence details can be found in the file COPYING.
  *
  * \author John Levon
+ * \author Jürgen Spitzmüller
  *
  * Full author contact details are available in file CREDITS.
  */
