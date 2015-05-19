cburschka/lyx

commit c7a11c1da555d077a017957de75f493fa2635b11

tex2lyx/text.cpp: fix handling of \fbox
since a while now lyX supports inner boxes in \fbox, tex2lyx was outdated in this case

src/tex2lyx/text.cpp
@@ -1127,7 +1127,7 @@ void parse_box(Parser & p, ostream & os, unsigned outer_flags,
 			os << "use_makebox 1\n";
 		else
 			os << "use_makebox " << (inner_type == "makebox") << '\n';
-		if (outer_type == "fbox" || outer_type == "mbox")
+		if (outer_type == "mbox")
 			os << "width \"\"\n";
 		// for values like "1.5\width" LyX uses "1.5in" as width ad sets "width" as sepecial
 		else if (contains(width_unit, '\\'))
@@ -1260,8 +1260,7 @@ void parse_outer_box(Parser & p, ostream & os, unsigned flags, bool outer,
 			p.skip_spaces(true);
 		}
 	}
-	if (outer_type == "shaded" || outer_type == "fbox"
-		|| outer_type == "mbox") {
+	if (outer_type == "shaded" || outer_type == "mbox") {
 		// These boxes never have an inner box
 		;
 	} else if (p.next_token().asInput() == "\\parbox") {

src/tex2lyx/test/box-color-size-space-align.tex
@@ -124,6 +124,10 @@ blabla \begin{framed}\begin{framed}nested framed\end{framed}\end{framed} blabla
 
 blabla \fbox{fbox} blabla
 
+\fbox{\begin{minipage}[t]{0.9\columnwidth}%
+fbox around minipage%
+\end{minipage}}
+
 blabla \framebox{fr\textcolor{blue}{ame}box 1} blabla
 
 blabla \framebox[3cm]{framebox 2} blabla

commit 290e7a9e09fec5ebed1df84f696b4f72b0b4cfda

tex2lyx/text.cpp: fix a bug I introduced with [c7a11c1d/lyxgit]

src/tex2lyx/text.cpp
@@ -1165,7 +1165,7 @@ void parse_box(Parser & p, ostream & os, unsigned outer_flags,
 			os << "use_makebox 0\n";
 		else
 			os << "use_makebox " << (inner_type == "makebox") << '\n';
-		if (outer_type == "mbox")
+		if (outer_type == "mbox" || (outer_type == "fbox" && inner_type == ""))
 			os << "width \"\"\n";
 		// for values like "1.5\width" LyX uses "1.5in" as width ad sets "width" as sepecial
 		else if (contains(width_unit, '\\'))