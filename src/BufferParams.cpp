7a9b3a57aacdd27dc652daae9a1a4c4df91a324a
Richard Heck <rgheck@lyx.org>
2015-11-06 15:51:13 -0500

    The cite engine info is needed by makeDocumentClass().
    Fixes both #9797 and #9848.
    
    (cherry picked from commit ba99ea5b88850daa09a8a2af25e3a242bafe3acb)

--- src/BufferParams.cpp
+++ src/BufferParams.cpp
@@ -347,6 +347,8 @@ BufferParams::BufferParams()
 	: pimpl_(new Impl)
 {
 	setBaseClass(defaultBaseclass());
+	cite_engine_.push_back("basic");
+	cite_engine_type_ = ENGINE_TYPE_DEFAULT;
 	makeDocumentClass();
 	paragraph_separation = ParagraphIndentSeparation;
 	quotes_language = InsetQuotes::EnglishQuotes;
@@ -356,8 +358,6 @@ BufferParams::BufferParams()
 	papersize = PAPER_DEFAULT;
 	orientation = ORIENTATION_PORTRAIT;
 	use_geometry = false;
-	cite_engine_.push_back("basic");
-	cite_engine_type_ = ENGINE_TYPE_DEFAULT;
 	biblio_style = "plain";
 	use_bibtopic = false;
 	use_indices = false;
