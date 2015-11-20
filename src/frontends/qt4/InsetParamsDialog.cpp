45597f967ce25785d7c99073fe8c25a9b57206dd
Juergen Spitzmueller <spitz@lyx.org>
2015-11-20 10:57:05 +0100

    Disable Apply button in InsetParams dialogs if there is no inset to apply to.
    Fixes: #9386

--- src/frontends/qt4/InsetParamsDialog.cpp
+++ src/frontends/qt4/InsetParamsDialog.cpp
@@ -181,7 +181,7 @@ docstring InsetParamsDialog::checkWidgets(bool immediate)
 	bool const can_be_restored = !immediate && !read_only
 			&& ins && (ins != d->inset_ || d->changed_);
 	restorePB->setEnabled(can_be_restored);
-	applyPB->setEnabled(!immediate && lfun_ok && widget_ok && !read_only && valid_argument);
+	applyPB->setEnabled(ins && !immediate && lfun_ok && widget_ok && !read_only && valid_argument);
 	d->widget_->setEnabled(!read_only);
 	synchronizedCB->setEnabled(!immediate);
 	return argument;
