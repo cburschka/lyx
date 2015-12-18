e0dd5416348cecb98e0c881635d5de130cf1e35e
Kornel Benko <kornel@lyx.org>
2015-12-18 10:29:55 +0100

    Cmake export tests: Adapt exports to changed mathmacros path.
    Missed in last commit.

--- development/autotests/ExportTests.cmake
+++ development/autotests/ExportTests.cmake
@@ -278,7 +278,7 @@ foreach(libsubfolderx autotests/export lib/doc lib/examples lib/templates autote
     list(APPEND testlabel "examples")
   elseif (libsubfolderx MATCHES "lib/templates")
     list(APPEND testlabel "templates")
-  elseif (libsubfolderx MATCHES "development/mathmacros")
+  elseif (libsubfolderx MATCHES "autotests/mathmacros")
     list(APPEND testlabel "mathmacros")
   elseif (libsubfolderx MATCHES "autotests/.+")
     list(APPEND testlabel "autotests")
@@ -289,10 +289,6 @@ foreach(libsubfolderx autotests/export lib/doc lib/examples lib/templates autote
   message(STATUS "Handling export dir ${LIBSUB_SRC_DIR}")
   file(GLOB_RECURSE lyx_files RELATIVE "${LIBSUB_SRC_DIR}" "${LIBSUB_SRC_DIR}/*.lyx")
   list(SORT lyx_files)
-  if (libsubfolderx MATCHES "development/mathmacros")
-    # This is a zipped file, removing from tests
-    list(REMOVE_ITEM lyx_files testcases_master_child.lyx)
-  endif()
   # Now create 2 lists. One for files in a language dir, one without
   set(lang_lyx_files)
   set(nolang_lyx_files)
