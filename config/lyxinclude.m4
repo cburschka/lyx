cburschka/lyx

commit e1938aa2404e9e1a4343de7139e52cac4ac71739

Fix some compilation problems in C++11 mode
Only enable std::regex when the header <regex> is present (fix Mac OS X issue).

Remove support for concept checks. The page
  https://gcc.gnu.org/onlinedocs/libstdc++/manual/concept_checking.html
says about concept checking:

  Please note that the checks are based on the requirements in the
  original C++ standard, many of which were relaxed in the C++11
  standard and so valid C++11 code may be incorrectly rejected by the
  concept checks. Additionally, some correct C++03 code might be
  rejected by the concept checks, for example template argument types
  may need to be complete when used in a template definition, rather
  than at the point of instantiation. There are no plans to address
  these shortcomings.

Therefore it seems reasonable to remove our support for this.

config/lyxinclude.m4
@@ -239,15 +239,6 @@ AC_ARG_ENABLE(stdlib-debug,
 	  [enable_stdlib_debug=no])]
 )
 
-AC_ARG_ENABLE(concept-checks,
-  AC_HELP_STRING([--enable-concept-checks],[enable concept checks]),,
-  [AS_CASE([$build_type], [dev*|pre*], [enable_concept_checks=yes], 
-	  [enable_concept_checks=no])]
-  if test x$USE_QT5 = xyes ; then
-      enable_concept_checks=no
-  fi
-)
-
 ### set up optimization
 AC_ARG_ENABLE(optimization,
     AC_HELP_STRING([--enable-optimization[=value]],[enable compiler optimisation]),,
@@ -323,15 +314,6 @@ if test x$GXX = xyes; then
         ;;
     esac
   fi
-  if test x$enable_concept_checks = xyes ; then
-    case $gxx_version in
-      4.*)
-        lyx_flags="$lyx_flags concept-checks"
-	dnl FIXME check whether this makes sense with clang/libc++
-	AC_DEFINE(_GLIBCXX_CONCEPT_CHECKS, 1, [libstdc++ concept checking])
-	;;
-    esac
-  fi
   dnl enable_cxx11 can be yes/no/auto.
   dnl By default, it is auto and we enable C++11 when possible
   if test x$enable_cxx11 != xno ; then
@@ -357,6 +339,7 @@ if test x$GXX = xyes; then
 
   LYX_CXX_CXX11
   if test $lyx_use_cxx11 = yes ; then
+    AC_CHECK_HEADER([regex], [lyx_std_regex=yes], [lyx_std_regex=no])
     if test x$CLANG = xno || test $lyx_cv_lib_stdcxx = yes; then
       dnl <regex> in gcc is unusable in versions less than 4.9.0
       dnl see https://gcc.gnu.org/bugzilla/show_bug.cgi?id=53631
@@ -367,12 +350,15 @@ if test x$GXX = xyes; then
     else
       lyx_std_regex=yes
     fi
-
-    if test $lyx_std_regex = yes ; then
-      lyx_flags="$lyx_flags stdregex"
-      AC_DEFINE([LYX_USE_STD_REGEX], 1, [define to 1 if std::regex should be preferred to boost::regex])
-    fi
   fi
+else
+  dnl This is not gcc, not sure what setup we can do
+  AC_CHECK_HEADER([regex], [lyx_std_regex=yes], [lyx_std_regex=no])
+fi
+
+if test $lyx_std_regex = yes ; then
+  lyx_flags="$lyx_flags stdregex"
+  AC_DEFINE([LYX_USE_STD_REGEX], 1, [define to 1 if std::regex should be preferred to boost::regex])
 fi
 AM_CONDITIONAL([LYX_USE_STD_REGEX], test $lyx_std_regex = yes)
 ])dnl

INSTALL
@@ -106,7 +106,6 @@ flags:
     optimization        -O2         -O2         -O         -O2     -O2
     assertions                       X           X
     stdlib-debug                                 X
-    concept-checks                   X           X
     warnings                         X           X
     debug                            X           X           X       X
 
@@ -228,9 +227,6 @@ The following options allow you to tweak the generated code more precisely (see 
     --without-included-boost is specified). You may have to use
     --disable-stdlib-debug when linking development versions against
     your system's boost library.
-
-  o --enable-concept-checks adds some compile-time checks. There is no
-    run-time penalty.
 
   o --enable-monolithic-build[=boost,client,insets,mathed,core,tex2lyx,frontend-qt4]
     that enables monolithic build of the given parts of the source

commit 39717adfda1620c5eb2bee122622dff1a99ba7af

Fix the logic of selection of std::regex
This amends commit e1938aa2, which introduced some logic
errors: regex would be enabled for gcc versions which have unusable
<regex> header.

The new code separates better special gcc handling from special C++11
handling and should be more readable.

* set -std=c++11 in AM_CPPFLAGS instead of AM_CXXFLAGS, since the
  preprocessor uses this setting too.

* Before checking for header <regex>, set language to C++ and update
  value of AM_CPPFLAGS too.

* Separate code that checks for regex in its own macro.

* now unknown compiler which have the <regex> header, will use std::regex in C++11 mode.

config/lyxinclude.m4
@@ -151,7 +151,9 @@ dnl the C++11 standard.
 AC_DEFUN([LYX_CXX_CXX11],
 [AC_CACHE_CHECK([whether the compiler implements C++11],
                [lyx_cv_cxx_cxx11],
- [save_CXXFLAGS=$CXXFLAGS
+ [save_CPPFLAGS=$CPPFLAGS
+  CPPFLAGS="$AM_CPPFLAGS $CPPFLAGS"
+  save_CXXFLAGS=$CXXFLAGS
   CXXFLAGS="$AM_CXXFLAGS $CXXFLAGS"
   AC_LANG_PUSH(C++)
   AC_TRY_COMPILE([], [
@@ -161,8 +163,44 @@ AC_DEFUN([LYX_CXX_CXX11],
   ],
   [lyx_cv_cxx_cxx11=no], [lyx_cv_cxx_cxx11=yes ; lyx_flags="$lyx_flags c++11"])
  AC_LANG_POP(C++)
- CXXFLAGS=$save_CXXFLAGS])
-lyx_use_cxx11=$lyx_cv_cxx_cxx11
+ CXXFLAGS=$save_CXXFLAGS
+ CPPFLAGS=$save_CPPFLAGS])
+ lyx_use_cxx11=$lyx_cv_cxx_cxx11
+])
+
+dnl decide whether we want to use std::regex and set the
+dnl LYX_USE_STD_REGEX accordingly.
+AC_DEFUN([LYX_CXX_USE_REGEX],
+[lyx_std_regex=no
+ if test $lyx_use_cxx11 = yes; then
+   save_CPPFLAGS=$CPPFLAGS
+   CPPFLAGS="$AM_CPPFLAGS $CPPFLAGS"
+   save_CXXFLAGS=$CXXFLAGS
+   CXXFLAGS="$AM_CXXFLAGS $CXXFLAGS"
+   AC_LANG_PUSH(C++)
+   AC_CHECK_HEADER([regex], [lyx_std_regex=yes], [lyx_std_regex=no])
+   AC_LANG_POP(C++)
+   CXXFLAGS=$save_CXXFLAGS
+   CPPFLAGS=$save_CPPFLAGS
+   if test x$GXX = xyes && test $lyx_std_regex = yes ; then
+     AC_MSG_CHECKING([for correct regex implementation])
+     if test x$CLANG = xno || test $lyx_cv_lib_stdcxx = yes; then
+       dnl <regex> in gcc is unusable in versions less than 4.9.0
+       dnl see https://gcc.gnu.org/bugzilla/show_bug.cgi?id=53631
+       case $gxx_version in
+         4.0*|4.1*|4.2*|4.3*|4.4*|4.5*|4.6*|4.7*|4.8*) lyx_std_regex=no ;;
+         *) ;;
+       esac
+     fi
+   fi
+   AC_MSG_RESULT([$lyx_std_regex])
+ fi
+
+ if test $lyx_std_regex = yes ; then
+  lyx_flags="$lyx_flags std-regex"
+  AC_DEFINE([LYX_USE_STD_REGEX], 1, [define to 1 if std::regex should be preferred to boost::regex])
+ fi
+ AM_CONDITIONAL([LYX_USE_STD_REGEX], test $lyx_std_regex = yes)
 ])
 
 
@@ -270,7 +308,6 @@ if test "x$enable_assertions" = xyes ; then
 fi
 
 # set the compiler options correctly.
-lyx_std_regex=no
 if test x$GXX = xyes; then
   dnl clang++ pretends to be g++ 4.2.1; this is not useful
   if test x$CLANG = xno; then
@@ -325,46 +362,29 @@ if test x$GXX = xyes; then
       4.3*|4.4*|4.5*|4.6*)
         dnl Note that this will define __GXX_EXPERIMENTAL_CXX0X__.
         dnl The source code relies on that.
-        AM_CXXFLAGS="$AM_CXXFLAGS -std=c++0x";;
+        AM_CPPFLAGS="$AM_CPPFLAGS -std=c++0x";;
       clang)
         dnl presumably all clang versions support c++11.
 	dnl the deprecated-register warning is very annoying with Qt4.x right now.
-        AM_CXXFLAGS="$AM_CXXFLAGS -std=c++11 -Wno-deprecated-register";;
+        AM_CPPFLAGS="$AM_CPPFLAGS -std=c++11"
+        AM_CXXFLAGS="$AM_CXXFLAGS -Wno-deprecated-register";;
       *)
         AS_CASE([$host], [*cygwin*],
-                [AM_CXXFLAGS="$AM_CXXFLAGS -std=gnu++11"],
-                [AM_CXXFLAGS="$AM_CXXFLAGS -std=c++11"]);;
+                [AM_CPPFLAGS="$AM_CPPFLAGS -std=gnu++11"],
+                [AM_CPPFLAGS="$AM_CPPFLAGS -std=c++11"]);;
     esac
   fi
+fi
 
-  LYX_CXX_CXX11
-  if test $lyx_use_cxx11 = yes ; then
+LYX_CXX_CXX11
+if test $lyx_use_cxx11 = yes; then
+  if test x$GXX = xyes; then
     dnl We still use auto_ptr, which is obsoleted. Shut off the warnings.
     AM_CXXFLAGS="$AM_CXXFLAGS -Wno-deprecated-declarations"
-
-    AC_CHECK_HEADER([regex], [lyx_std_regex=yes], [lyx_std_regex=no])
-    if test x$CLANG = xno || test $lyx_cv_lib_stdcxx = yes; then
-      dnl <regex> in gcc is unusable in versions less than 4.9.0
-      dnl see https://gcc.gnu.org/bugzilla/show_bug.cgi?id=53631
-      case $gxx_version in
-        4.0*|4.1*|4.2*|4.3*|4.4*|4.5*|4.6*|4.7*|4.8*) ;;
-        *) lyx_std_regex=yes ;;
-      esac
-    else
-      lyx_std_regex=yes
-    fi
   fi
-else
-  dnl This is not gcc, not sure what setup we can do
-  AC_CHECK_HEADER([regex], [lyx_std_regex=yes], [lyx_std_regex=no])
 fi
-
-if test $lyx_std_regex = yes ; then
-  lyx_flags="$lyx_flags stdregex"
-  AC_DEFINE([LYX_USE_STD_REGEX], 1, [define to 1 if std::regex should be preferred to boost::regex])
-fi
-AM_CONDITIONAL([LYX_USE_STD_REGEX], test $lyx_std_regex = yes)
-])dnl
+LYX_CXX_USE_REGEX
+])
 
 dnl Usage: LYX_USE_INCLUDED_BOOST : select if the included boost should
 dnl        be used.