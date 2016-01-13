6e3a4ecfe78c5221e0d819c22afb69280c55c978
Peter Kümmel <kuemmel@lyx.org>
2016-01-13 15:42:43 +0100

    mingw.bat: circumvent critical python detection
    LyX could be build with a double click on mingw.bat.

--- development/cmake/mingw.bat
+++ development/cmake/mingw.bat
@@ -1,5 +1,10 @@
+REM Run this script via double click.
+
+REM Create a link and add  cmd.exe /k  in the preferences in front of the mingw.bat call to prevent closing the cmd window.
+
 echo off
 
+REM Install Qt from qt.io
 set PATH=C:\Qt\Qt5.5.1\5.5\mingw492_32\bin;%PATH%
 set PATH=C:\Qt\Qt5.5.1\Tools\mingw492_32\bin;%PATH%
 
@@ -9,20 +14,21 @@ set LYX_BUILD=%LYX_SOURCE%\..\compile-mingw
 echo LyX source: %LYX_SOURCE%
 echo LyX build : %LYX_BUILD%
 
-set GNUWIN32_DIR=%LYX_SOURCE%\..\msvc2010-deps
-set PATH="%GNUWIN32_DIR%\deps20\Python";%PATH%
+REM Download http://sourceforge.net/projects/lyx/files/Win_installers/Dependencies/lyx20-deps-msvc2010-x86.zip
+REM Extract on the same level as LyX sources
+set GNUWIN32_DIR=%LYX_SOURCE%\..\lyx20-deps-msvc2010-x86\deps20
 
 mkdir %LYX_BUILD%
 rmdir /s/q %LYX_BUILD%
 mkdir %LYX_BUILD%
 
 cd %LYX_BUILD%
-cmake %LYX_SOURCE% -GNinja -DLYX_3RDPARTY_BUILD=ON -DLYX_ENABLE_CXX11=ON -DLYX_USE_QT=QT5 -DLYX_MERGE_REBUILD=1 -DLYX_MERGE_FILES=1 -DLYX_NLS=1 -DLYX_INSTALL=1 -DLYX_RELEASE=1 -DLYX_CONSOLE=OFF 
+cmake %LYX_SOURCE% -G"MinGW Makefiles" -DLYX_PYTHON_EXECUTABLE=%GNUWIN32_DIR%\Python\python -DLYX_3RDPARTY_BUILD=1 -DLYX_ENABLE_CXX11=ON -DLYX_USE_QT=QT5 -DLYX_MERGE_REBUILD=1 -DLYX_MERGE_FILES=1 -DLYX_NLS=1 -DLYX_INSTALL=1 -DLYX_RELEASE=1 -DLYX_CONSOLE=OFF 
 
-ninja
-ninja install
-
-cd ..
+mingw32-make doc
+mingw32-make translations
+mingw32-make
+mingw32-make install/strip
 
 goto :eof
 :eof
