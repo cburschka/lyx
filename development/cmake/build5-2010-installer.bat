44fd57e4b45873e33b0b151ddafdcdebf1cd23df
Peter Kümmel <kuemmel@lyx.org>
2016-01-22 12:33:16 +0100

    python now will be found, rmdir could fail

--- development/cmake/build5-2010-installer.bat
+++ development/cmake/build5-2010-installer.bat
@@ -16,9 +16,8 @@ echo LyX build : %LYX_BUILD%
 
 REM first remove an existing compilation to assure a clean version
 mkdir %LYX_BUILD%
-rmdir /s/q %LYX_BUILD%
-mkdir %LYX_BUILD%
-
+del /s/q %LYX_BUILD%\*
+rmdir /s/q %LYX_BUILD%\LYX_INSTALLED
 
 REM Download http://sourceforge.net/projects/lyx/files/Win_installers/Dependencies/lyx20-deps-msvc2010-x86.zip
 REM Extract on the same level as LyX sources
@@ -28,7 +27,7 @@ REM MSVC 2010 tools
 call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" x86
 
 cd %LYX_BUILD%
-cmake %LYX_SOURCE% -G"NMake Makefiles" -DLYX_PYTHON_EXECUTABLE=%GNUWIN32_DIR%\Python\python -DLYX_3RDPARTY_BUILD=1 -DLYX_USE_QT=QT5 -DLYX_MERGE_REBUILD=1 -DLYX_MERGE_FILES=1 -DLYX_NLS=1 -DLYX_INSTALL=1 -DLYX_RELEASE=1 -DLYX_CONSOLE=OFF 
+cmake %LYX_SOURCE% -G"NMake Makefiles" -DLYX_USE_QT=QT5 -DLYX_MERGE_FILES=1 -DLYX_NLS=1 -DLYX_INSTALL=1 -DLYX_RELEASE=1 -DLYX_CONSOLE=OFF 
 
 nmake doc
 nmake translations
