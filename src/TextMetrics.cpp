0e1834f2d9978d9b43ab8420ef92056a06d87ab5
Richard Heck <rgheck@lyx.org>
2016-04-21 17:44:14 -0400

    Updates to splitting of consecutive environments.
    From Enrico.

--- src/TextMetrics.cpp
+++ src/TextMetrics.cpp
@@ -1594,7 +1594,8 @@ bool TextMetrics::cursorEnd(Cursor & cur)
 			boundary = true;
 		else
 			--end;
-	}
+	} else if (cur.paragraph().isEnvSeparator(end-1))
+		--end;
 	return text_->setCursor(cur, cur.pit(), end, true, boundary);
 }
 
