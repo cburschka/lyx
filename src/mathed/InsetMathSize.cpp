bb9e0e122a7277070651887630665dcb07671147
Richard Heck <rgheck@lyx.org>
2016-06-11 00:48:55 -0400

    Fix silly error outputting math sizes. Fixes bug #10129.
    (cherry picked from commit bb5c760b37cbc360a46c5cca83178c5b79c3b9b3)

--- src/mathed/InsetMathSize.cpp
+++ src/mathed/InsetMathSize.cpp
@@ -83,8 +83,7 @@ void InsetMathSize::mathmlize(MathStream & ms) const
 	stringstream attrs;
 	attrs << "displaystyle='" << (dispstyle ? "true" : "false")
 		<< "' scriptlevel='" << scriptlevel << "'";
-	ms << MTag("mstyle", attrs.str()) << ">"
-	   << cell(0) << ETag("mstyle");
+	ms << MTag("mstyle", attrs.str()) << cell(0) << ETag("mstyle");
 }
 
 
