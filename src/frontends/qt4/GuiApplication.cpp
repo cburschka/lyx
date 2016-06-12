1bd5ef9a754a5b521f9d067b68fdac26035ef342
Richard Heck <rgheck@lyx.org>
2016-06-11 23:26:02 -0400

    Add some comments to fix coverity #23386.

--- src/frontends/qt4/GuiApplication.cpp
+++ src/frontends/qt4/GuiApplication.cpp
@@ -1670,7 +1670,10 @@ void GuiApplication::dispatch(FuncRequest const & cmd, DispatchResult & dr)
 			boost::crc_32_type crc;
 			crc = for_each(fname.begin(), fname.end(), crc);
 			createView(crc.checksum());
+			// we know current_view_ is non-null, because createView sets it.
+			// coverity[FORWARD_NULL]
 			current_view_->openDocument(fname);
+			// FIXME but then why check current_view_ here?
 			if (current_view_ && !current_view_->documentBufferView())
 				current_view_->close();
 		} else {
