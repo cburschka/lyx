b02a93d7e809775b58f21e9b56d19f858e6fd3ba
Richard Heck <rgheck@lyx.org>
2016-07-10 01:12:42 -0400

    Fix thinko in Buffer::preview. It is possible for this routine to be called when we do not have a cloned Buffer, namely, if we do not have EXPORT_in_THREAD defined.
    (cherry picked from commit d8aab4af9e6e72c835f78ba54a46687b870c25fa)

--- src/Buffer.cpp
+++ src/Buffer.cpp
@@ -4311,10 +4311,10 @@ Buffer::ExportStatus Buffer::preview(string const & format, bool includeall) con
 	ExportStatus const status = doExport(format, true, false, result_file);
 	FileName const previewFile(result_file);
 
-	LATTEST (isClone());
-	d->cloned_buffer_->d->preview_file_ = previewFile;
-	d->cloned_buffer_->d->preview_format_ = format;
-	d->cloned_buffer_->d->preview_error_ = (status != ExportSuccess);
+	Impl * theimpl = isClone() ? d->cloned_buffer_->d : d;
+	theimpl->preview_file_ = previewFile;
+	theimpl->preview_format_ = format;
+	theimpl->preview_error_ = (status != ExportSuccess);
 
 	if (status != ExportSuccess)
 		return status;
@@ -4323,14 +4323,11 @@ Buffer::ExportStatus Buffer::preview(string const & format, bool includeall) con
 			return PreviewError;
 		else
 			return PreviewSuccess;
-	}
-	else {
-		// Successful export but no output file?
-		// Probably a bug in error detection.
-		LATTEST (status != ExportSuccess);
-
-		return status;
-	}
+	} 
+	// Successful export but no output file?
+	// Probably a bug in error detection.
+	LATTEST(status != ExportSuccess);
+	return status;
 }
 
 
