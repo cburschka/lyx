b9c759416731da59fcb9b41446e7206f7db90c37
Jean-Marc Lasgouttes <lasgouttes@lyx.org>
2016-07-21 10:15:39 +0200

    Revert "Reset beginOfBody() when changing paragraph layout"
    This reverts commit ef8e499821a789461b11df6994ec6b471d58dadb.

--- src/Paragraph.cpp
+++ src/Paragraph.cpp
@@ -2022,7 +2022,6 @@ docstring Paragraph::expandParagraphLabel(Layout const & layout,
 void Paragraph::applyLayout(Layout const & new_layout)
 {
 	d->layout_ = &new_layout;
-	setBeginOfBody();
 	LyXAlignment const oldAlign = d->params_.align();
 
 	if (!(oldAlign & d->layout_->alignpossible)) {
