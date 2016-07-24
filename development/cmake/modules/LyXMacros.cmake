fa85bf9b595e0ff5721fcf32c21473199d662453
Kornel Benko <kornel@lyx.org>
2016-07-24 11:20:08 +0200

    Cmake build: Added check for pythons polib module
    Idea stolen from http://public.kitware.com/pipermail/cmake/2011-January/041666.html

--- development/cmake/modules/LyXMacros.cmake
+++ development/cmake/modules/LyXMacros.cmake
@@ -379,3 +379,25 @@ macro(lyx_target_link_libraries _target)
   endforeach()
 endmacro()
 
+# Check if python module exists
+# Idea stolen from http://public.kitware.com/pipermail/cmake/2011-January/041666.html
+function(find_python_module module)
+  string(TOUPPER ${module} module_upper)
+  if(NOT LYX_PY_${module_upper})
+    if(ARGC GREATER 1 AND ARGV1 STREQUAL "REQUIRED")
+      set(LYX_PY_${module}_FIND_REQUIRED TRUE)
+    endif()
+    # A module's location is usually a directory, but for binary modules
+    # it's a .so file.
+    execute_process(COMMAND "${LYX_PYTHON_EXECUTABLE}" "-c"
+      "import re, ${module}; print(re.compile('/__init__.py.*').sub('',${module}.__file__))"
+      RESULT_VARIABLE _${module}_status
+      OUTPUT_VARIABLE _${module}_location
+      ERROR_QUIET OUTPUT_STRIP_TRAILING_WHITESPACE)
+    if(NOT _${module}_status)
+      set(LYX_PY_${module_upper} ${_${module}_location} CACHE STRING
+	"Location of Python module ${module}")
+    endif()
+  endif()
+  find_package_handle_standard_args(LYX_PY_${module} DEFAULT_MSG LYX_PY_${module_upper})
+endfunction(find_python_module)
