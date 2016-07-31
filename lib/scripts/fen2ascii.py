d7ecdedd6735dd85f8a1f2b33350addd195124c4
Richard Heck <rgheck@lyx.org>
2016-07-30 23:17:30 -0400

    Fix indentation of fen2ascii script so it will run.
    (cherry picked from commit 22e1063cb9b6bd84e6c1d3adf24b3d93a06b90bb)

--- lib/scripts/fen2ascii.py
+++ lib/scripts/fen2ascii.py
@@ -32,15 +32,15 @@ for i in range(8):
     cont = cont + 1
     tmp=""
     for j in comp[i]:
-	if j>='0' and j <= '9':
-	    for k in range(int(j)):
-		cont = cont + 1
-		x, mod = divmod(cont,2)
-		if mod : tmp = tmp + '| '
-		else : tmp = tmp + '|*'
-	else :
-	    tmp = tmp + '|' + j
-	    cont = cont + 1
+        if j>='0' and j <= '9':
+            for k in range(int(j)):
+                cont = cont + 1
+                x, mod = divmod(cont,2)
+                if mod : tmp = tmp + '| '
+                else : tmp = tmp + '|*'
+        else :
+            tmp = tmp + '|' + j
+            cont = cont + 1
 
     row = 8 - i
     print margin, row, tmp+"|"
