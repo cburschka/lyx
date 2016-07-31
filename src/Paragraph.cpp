9e5a42edbf71a6678ca572bfc9241668f602a66e
Richard Heck <rgheck@lyx.org>
2016-07-31 03:03:26 -0400

    Rename, for a little clarity.

--- src/Paragraph.cpp
+++ src/Paragraph.cpp
@@ -2409,18 +2409,18 @@ void Paragraph::latex(BufferParams const & bparams,
 		++column;
 
 		// Fully instantiated font
-		Font const font = getFont(bparams, i, outerfont);
+		Font const current_font = getFont(bparams, i, outerfont);
 
 		Font const last_font = running_font;
 
 		// Do we need to close the previous font?
 		if (open_font &&
-		    (font != running_font ||
-		     font.language() != running_font.language()))
+		    (current_font != running_font ||
+		     current_font.language() != running_font.language()))
 		{
 			column += running_font.latexWriteEndChanges(
 					os, bparams, runparams, basefont,
-					(i == body_pos-1) ? basefont : font);
+					(i == body_pos-1) ? basefont : current_font);
 			running_font = basefont;
 			open_font = false;
 		}
@@ -2431,7 +2431,7 @@ void Paragraph::latex(BufferParams const & bparams,
 		string const lang_end_command = runparams.use_polyglossia ?
 			"\\end{$$lang}" : lyxrc.language_command_end;
 		if (!running_lang.empty() &&
-		    font.language()->encoding()->package() == Encoding::CJK) {
+		    current_font.language()->encoding()->package() == Encoding::CJK) {
 				string end_tag = subst(lang_end_command,
 							"$$lang",
 							running_lang);
@@ -2442,28 +2442,28 @@ void Paragraph::latex(BufferParams const & bparams,
 		// Switch file encoding if necessary (and allowed)
 		if (!runparams.pass_thru && !style.pass_thru &&
 		    runparams.encoding->package() != Encoding::none &&
-		    font.language()->encoding()->package() != Encoding::none) {
+		    current_font.language()->encoding()->package() != Encoding::none) {
 			pair<bool, int> const enc_switch =
 			    	switchEncoding(os.os(), bparams, runparams,
-					*(font.language()->encoding()));
+					*(current_font.language()->encoding()));
 			if (enc_switch.first) {
 				column += enc_switch.second;
-				runparams.encoding = font.language()->encoding();
+				runparams.encoding = current_font.language()->encoding();
 			}
 		}
 
 		char_type const c = d->text_[i];
 
 		// Do we need to change font?
-		if ((font != running_font ||
-		     font.language() != running_font.language()) &&
+		if ((current_font != running_font ||
+		     current_font.language() != running_font.language()) &&
 			i != body_pos - 1)
 		{
 			odocstringstream ods;
-			column += font.latexWriteStartChanges(ods, bparams,
+			column += current_font.latexWriteStartChanges(ods, bparams,
 							      runparams, basefont,
 							      last_font);
-			running_font = font;
+			running_font = current_font;
 			open_font = true;
 			docstring fontchange = ods.str();
 			// check whether the fontchange ends with a \\textcolor
@@ -2489,7 +2489,7 @@ void Paragraph::latex(BufferParams const & bparams,
 			// style.pass_thru is false.
 			if (i != body_pos - 1) {
 				if (d->simpleTeXBlanks(runparams, os,
-						i, column, font, style)) {
+						i, column, current_font, style)) {
 					// A surrogate pair was output. We
 					// must not call latexSpecialChar
 					// in this iteration, since it would output
@@ -2502,7 +2502,7 @@ void Paragraph::latex(BufferParams const & bparams,
 
 		OutputParams rp = runparams;
 		rp.free_spacing = style.free_spacing;
-		rp.local_font = &font;
+		rp.local_font = &current_font;
 		rp.intitle = style.intitle;
 
 		// Two major modes:  LaTeX or plain
@@ -2752,7 +2752,7 @@ void doFontSwitch(vector<html::FontTag> & tagsToOpen,
 docstring Paragraph::simpleLyXHTMLOnePar(Buffer const & buf,
 				    XHTMLStream & xs,
 				    OutputParams const & runparams,
-				    Font const & outerfont, 
+				    Font const & outerfont,
 				    bool start_paragraph, bool close_paragraph,
 				    pos_type initial) const
 {
