10e90ad78cd68afc274e43dd1c3d7d5002ed13e5
Uwe Stöhr <uwestoehr@lyx.org>
2016-10-16 14:33:18 +0200

    Win build: fix a typo

--- development/cmake/build5-2015.bat
+++ development/cmake/build5-2015.bat
@@ -33,9 +33,9 @@ REM Save path
 set CALLED_FROM=%CD%
 
 REM Add path to qmake here or set PATH correctly on your system.
-set PATH=C:\Qt\Qt5.6.1-MSVC2015\5.6\msvc2015\bin;%PATH%
+set PATH=C:\Qt\Qt5.6.2-MSVC2015\5.6\msvc2015\bin;%PATH%
 
-REM Edit pathes here or set the environment variables on you system.
+REM Edit pathes here or set the environment variables on your system.
 set GNUWIN32_DIR=D:\LyXGit\Master\lyx-windows-deps-msvc2015
 set LYX_SOURCE=D:\LyXGit\Master\
 set LYX_BUILD=D:\LyXGit\Master\compile-2015
@@ -65,9 +65,6 @@ if not exist %LYX_BUILD% (
 	)
 )
 
-REM MSVC 2015 tools
-call "C:\Programme (x86)\MSVC2015\VC\vcvarsall.bat" x86
-
 cd "%LYX_BUILD%"
 
 REM start with a new cmake run
@@ -76,7 +73,7 @@ REM start with a new cmake run
 if "%1%" == "devel" (
 	REM Build solution to develop LyX
 	REM you can add the option "-GNinja" for a faster compilation
-	cmake %LYX_SOURCE% -G"Visual Studio 14 2015" -DLYX_USE_QT=QT5 -DLYX_ENABLE_EXPORT_TESTS=0 -DLYX_MERGE_FILES=0 -DLYX_NLS=1 -DLYX_INSTALL=0 -DLYX_RELEASE=0 -DLYX_CONSOLE=FORCE -DLYX_3RDPARTY_BUILD=1 %DEPENDENCIES_DOWNLOAD%
+	cmake %LYX_SOURCE% -G"Visual Studio 14 2015" -DLYX_ENABLE_CXX11=ON -DLYX_USE_QT=QT5 -DLYX_ENABLE_EXPORT_TESTS=0 -DLYX_MERGE_FILES=0 -DLYX_NLS=1 -DLYX_INSTALL=0 -DLYX_RELEASE=0 -DLYX_CONSOLE=FORCE -DLYX_3RDPARTY_BUILD=1 %DEPENDENCIES_DOWNLOAD%
 	msbuild src\LyX.vcxproj /p:Configuration=Debug
 	msbuild src\tex2lyx\tex2lyx.vcxproj /p:Configuration=Debug
 )
@@ -84,8 +81,7 @@ if "%1%" == "devel" (
 if "%1%" == "install" (
 	REM Build solution to develop LyX
 	REM set -DLYX_MERGE_REBUILD and -DLYX_MERGE_FILES to 1 for a version released with an installer
-	cmake %LYX_SOURCE% -GNinja -G"Visual Studio 14 2015" -DLYX_USE_QT=QT5 -DLYX_MERGE_REBUILD=0 -DLYX_MERGE_FILES=0 -DLYX_NLS=1 -DLYX_INSTALL=1 -DLYX_RELEASE=1 -DLYX_CONSOLE=OFF %DEPENDENCIES_DOWNLOAD%
-	REM msbuild lyx.sln         /p:Configuration=Release /t:ALL_BUILD
+	cmake %LYX_SOURCE% -GNinja -G"Visual Studio 14 2015" -DLYX_ENABLE_CXX11=ON -DLYX_USE_QT=QT5 -DLYX_MERGE_REBUILD=0 -DLYX_MERGE_FILES=0 -DLYX_NLS=1 -DLYX_INSTALL=1 -DLYX_RELEASE=1 -DLYX_CONSOLE=OFF %DEPENDENCIES_DOWNLOAD%
 	msbuild INSTALL.vcxproj /p:Configuration=Release
 )
 
