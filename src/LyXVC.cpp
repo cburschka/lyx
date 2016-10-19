cburschka/lyx

commit e4998f21b08992271af06339d0776ec179414df9

Set window title according to platform UI
The window title is built from the current file name and its
mofidication state. We use our own code instead of the automatic title
bar provided when windowFileName() is set because

1/ Qt does not keep the full path name
2/ Qt does not yield a nice application name
3/ Qt separates file name and app name with an em-dash. It seems that
   only KDE does that: Gnome does not, Windows does not either. I do not
   think that we can/want to detect a KDE environment at run-time.

The "read only" and "version control" status are shown in the status bar:

* for read only we use the tab read only emblem (with the right size)
* for version control, we show the name of the backend (using a new
  vcname() method of the backend).

The iconText() of the view is not updated anymore, since this is
deprecated in Qt5.

(cherry picked from commit 82808fea04315fd323ec074e8ad7865d190987d4)
(cherry picked from commit 9313f8565b41c9de4b1990e155fe3fe8468f3446)
(cherry picked from commit bbe36ce6ce87ae37b7c30c250efbff2f9c185de4)
(cherry picked from commit 7cbc0c16a841716fa8966e0f800ee2cdc0432e25)

src/LyXVC.cpp
@@ -46,6 +46,12 @@ LyXVC::~LyXVC()
 {}
 
 
+string LyXVC::vcname() const
+{
+	return vcs->vcname();
+}
+
+
 bool LyXVC::fileInVC(FileName const & fn)
 {
 	if (!RCS::findFile(fn).empty())

src/LyXVC.h
@@ -50,6 +50,8 @@ public:
 	LyXVC();
 	///
 	~LyXVC();
+	/// Name of the underlying VCS
+	std::string vcname() const;
 	/// Is \p fn under version control?
 	static bool fileInVC(support::FileName const & fn);
 	/** Not a good name perhaps. This function should be called whenever

src/frontends/qt4/GuiView.cpp
@@ -110,9 +110,7 @@
 #include <QSplitter>
 #include <QStackedWidget>
 #include <QStatusBar>
-#if QT_VERSION >= 0x050000
 #include <QSvgRenderer>
-#endif
 #include <QtConcurrentRun>
 #include <QTime>
 #include <QTimer>
@@ -553,7 +551,7 @@ GuiView::GuiView(int id)
 #endif
 
 #endif
-	resetWindowTitleAndIconText();
+	resetWindowTitle();
 
 	// use tabbed dock area for multiple docks
 	// (such as "source" and "messages")
@@ -576,6 +574,23 @@ GuiView::GuiView(int id)
 		busylabel, SLOT(show()));
 	connect(&d.processing_thread_watcher_, SIGNAL(finished()), 
 		busylabel, SLOT(hide()));
+
+	QFontMetrics const fm(statusBar()->fontMetrics());
+	int const roheight = max(int(d.normalIconSize), fm.height());
+	QSize const rosize(roheight, roheight);
+	QPixmap readonly = QIcon(getPixmap("images/", "emblem-readonly", "svgz,png")).pixmap(rosize);
+	read_only_ = new QLabel(statusBar());
+	read_only_->setPixmap(readonly);
+	read_only_->setScaledContents(true);
+	read_only_->setAlignment(Qt::AlignCenter);
+	read_only_->hide();
+	statusBar()->addPermanentWidget(read_only_);
+
+	version_control_ = new QLabel(statusBar());
+	version_control_->setAlignment(Qt::AlignCenter);
+	version_control_->setFrameStyle(QFrame::StyledPanel);
+	version_control_->hide();
+	statusBar()->addPermanentWidget(version_control_);
 
 	statusBar()->setSizeGripEnabled(true);
 	updateStatusBar();
@@ -1144,14 +1159,35 @@ void GuiView::updateWindowTitle(GuiWorkArea * wa)
 	if (wa != d.current_work_area_
 		|| wa->bufferView().buffer().isInternal())
 		return;
-	setWindowTitle(qt_("LyX: ") + wa->windowTitle());
-	setWindowIconText(wa->windowIconText());
-#if (QT_VERSION >= 0x040400)
-	// Sets the path for the window: this is used by OSX to 
+	Buffer const & buf = wa->bufferView().buffer();
+	// Set the windows title
+	docstring title = buf.fileName().displayName(130) + from_ascii("[*]");
+#ifndef Q_WS_MAC
+	title += from_ascii(" - LyX");
+#endif
+	setWindowTitle(toqstr(title));
+	// Sets the path for the window: this is used by OSX to
 	// allow a context click on the title bar showing a menu
 	// with the path up to the file
-	setWindowFilePath(toqstr(wa->bufferView().buffer().absFileName()));
-#endif
+	setWindowFilePath(toqstr(buf.absFileName()));
+	// Tell Qt whether the current document is changed
+	setWindowModified(!buf.isClean());
+
+	if (buf.isReadonly())
+		read_only_->show();
+	else
+		read_only_->hide();
+
+	if (buf.lyxvc().inUse()) {
+		version_control_->show();
+		if (buf.lyxvc().locking())
+			version_control_->setText(
+				toqstr(bformat(_("%1$s lock"),
+				               from_ascii(buf.lyxvc().vcname()))));
+		else
+			version_control_->setText(toqstr(buf.lyxvc().vcname()));
+	} else
+		version_control_->hide();
 }
 
 
@@ -1196,7 +1232,7 @@ void GuiView::on_lastWorkAreaRemoved()
 	updateDialog("document", "");
 	updateDialogs();
 
-	resetWindowTitleAndIconText();
+	resetWindowTitle();
 	updateStatusBar();
 
 	if (lyxrc.open_buffers_in_tabs)
@@ -1285,7 +1321,7 @@ bool GuiView::event(QEvent * e)
 			updateDialog("document", "");
 			updateDialogs();
 		} else {
-			resetWindowTitleAndIconText();
+			resetWindowTitle();
 		}
 		setFocus();
 		return QMainWindow::event(e);
@@ -1312,10 +1348,9 @@ bool GuiView::event(QEvent * e)
 	}
 }
 
-void GuiView::resetWindowTitleAndIconText()
+void GuiView::resetWindowTitle()
 {
 	setWindowTitle(qt_("LyX"));
-	setWindowIconText(qt_("LyX"));
 }
 
 bool GuiView::focusNextPrevChild(bool /*next*/)

src/frontends/qt4/GuiWorkArea.cpp
@@ -1373,34 +1373,8 @@ QVariant GuiWorkArea::inputMethodQuery(Qt::InputMethodQuery query) const
 
 void GuiWorkArea::updateWindowTitle()
 {
-	docstring maximize_title;
-	docstring minimize_title;
-
-	Buffer const & buf = d->buffer_view_->buffer();
-	FileName const file_name = buf.fileName();
-	if (!file_name.empty()) {
-		maximize_title = file_name.displayName(130);
-		minimize_title = from_utf8(file_name.onlyFileName());
-		if (buf.lyxvc().inUse()) {
-			if (buf.lyxvc().locking())
-				maximize_title +=  _(" (version control, locking)");
-			else
-				maximize_title +=  _(" (version control)");
-		}
-		if (!buf.isClean()) {
-			maximize_title += _(" (changed)");
-			minimize_title += char_type('*');
-		}
-		if (buf.isReadonly())
-			maximize_title += _(" (read only)");
-	}
-
-	QString const new_title = toqstr(maximize_title);
-	if (new_title != windowTitle()) {
-		QWidget::setWindowTitle(new_title);
-		QWidget::setWindowIconText(toqstr(minimize_title));
-		titleChanged(this);
-	}
+	d->lyx_view_->updateWindowTitle(this);
+	titleChanged(this);
 }
 
 

src/VCBackend.h
@@ -38,6 +38,8 @@ public:
 	VCS(Buffer * b) : vcstatus(NOLOCKING), owner_(b) {}
 	virtual ~VCS() {}
 
+	/// the name of the vc backend
+	virtual std::string vcname() const = 0;
 	/// register a file for version control
 	virtual void registrer(std::string const & msg) = 0;
 	/// can this operation be processed in the current VCS?
@@ -150,6 +152,8 @@ public:
 	/// get file from repo, the caller must ensure that it does not exist locally
 	static bool retrieve(support::FileName const & file);
 
+	virtual std::string vcname() const { return "RCS"; };
+
 	virtual void registrer(std::string const & msg);
 
 	virtual bool renameEnabled();
@@ -233,6 +237,8 @@ public:
 
 	/// get file from repo, the caller must ensure that it does not exist locally
 	static bool retrieve(support::FileName const & file);
+
+	virtual std::string vcname() const { return "CVS"; };
 
 	virtual void registrer(std::string const & msg);
 
@@ -373,6 +379,8 @@ public:
 	/// get file from repo, the caller must ensure that it does not exist locally
 	static bool retrieve(support::FileName const & file);
 
+	virtual std::string vcname() const { return "SVN"; };
+
 	virtual void registrer(std::string const & msg);
 
 	virtual bool renameEnabled();
@@ -480,6 +488,8 @@ public:
 
 	/// get file from repo, the caller must ensure that it does not exist locally
 	static bool retrieve(support::FileName const & file);
+
+	virtual std::string vcname() const { return "GIT"; };
 
 	virtual void registrer(std::string const & msg);
 

src/frontends/qt4/GuiView.h
@@ -24,6 +24,7 @@
 class QCloseEvent;
 class QDragEnterEvent;
 class QDropEvent;
+class QLabel;
 class QMenu;
 class QShowEvent;
 
@@ -220,12 +221,12 @@ public Q_SLOTS:
 	/// idle timeout.
 	/// clear any temporary message and replace with current status.
 	void clearMessage();
+	///
+	void updateWindowTitle(GuiWorkArea * wa);
 
 private Q_SLOTS:
 	///
-	void updateWindowTitle(GuiWorkArea * wa);
-	///
-	void resetWindowTitleAndIconText();
+	void resetWindowTitle();
 
 	///
 	void on_currentWorkAreaChanged(GuiWorkArea *);
@@ -456,6 +457,11 @@ private:
 	bool command_execute_;
 	/// Request to give focus to minibuffer
 	bool minibuffer_focus_;
+
+	/// Statusbar widget that shows read-only status
+	QLabel * read_only_;
+	/// Statusbar widget that shows version control status
+	QLabel * version_control_;
 
 };
 

status.22x
@@ -21,6 +21,9 @@ What's new
 
 * USER INTERFACE
 
+- The title bar is now set according to platform UI. Read-only and
+  version control are shown by indicators in the status bar.
+
 
 * DOCUMENTATION AND LOCALIZATION
 

commit 4c724a6072013247ac178f0acec06825585c6c55

Do some caching of window title and related UI
This should avoid performance problems related to the window update machinery.
Moreover this fixes a crash introduced by e4998f21 when closing a file.

Note that GuiWorkArea::Private already had a read_only_ member, but it
was unused.

Also rename LyXVC::vcname() to LyXVC::vcstatus() since it now contains
directly the UI string to be shown.

(cherry picked from commit 6cb05ce8bb58720dfef6e5707c1f3fad3949881f)
(cherry picked from commit 85bcf0d93f7b0137c1d3b4d030a1840cbfe5892e)

src/LyXVC.cpp
@@ -46,9 +46,14 @@ LyXVC::~LyXVC()
 {}
 
 
-string LyXVC::vcname() const
+docstring LyXVC::vcstatus() const
 {
-	return vcs->vcname();
+	if (!vcs)
+		return docstring();
+	if (locking())
+		return bformat(_("%1$s lock"), from_ascii(vcs->vcname()));
+	else
+		return from_ascii(vcs->vcname());
 }
 
 

src/LyXVC.h
@@ -14,6 +14,8 @@
 
 #include <boost/scoped_ptr.hpp>
 
+#include "support/docstring.h"
+
 #include <string>
 
 
@@ -50,8 +52,8 @@ public:
 	LyXVC();
 	///
 	~LyXVC();
-	/// Name of the underlying VCS
-	std::string vcname() const;
+	/// Status of the underlying VCS
+	docstring vcstatus() const;
 	/// Is \p fn under version control?
 	static bool fileInVC(support::FileName const & fn);
 	/** Not a good name perhaps. This function should be called whenever

src/frontends/qt4/GuiView.cpp
@@ -1180,12 +1180,7 @@ void GuiView::updateWindowTitle(GuiWorkArea * wa)
 
 	if (buf.lyxvc().inUse()) {
 		version_control_->show();
-		if (buf.lyxvc().locking())
-			version_control_->setText(
-				toqstr(bformat(_("%1$s lock"),
-				               from_ascii(buf.lyxvc().vcname()))));
-		else
-			version_control_->setText(toqstr(buf.lyxvc().vcname()));
+		version_control_->setText(toqstr(buf.lyxvc().vcstatus()));
 	} else
 		version_control_->hide();
 }

src/frontends/qt4/GuiWorkArea.cpp
@@ -47,7 +47,6 @@
 #include "support/convert.h"
 #include "support/debug.h"
 #include "support/gettext.h"
-#include "support/FileName.h"
 #include "support/lassert.h"
 #include "support/TempFile.h"
 
@@ -246,11 +245,12 @@ SyntheticMouseEvent::SyntheticMouseEvent()
 
 
 GuiWorkArea::Private::Private(GuiWorkArea * parent)
-: p(parent), screen_(0), buffer_view_(0), read_only_(false), lyx_view_(0),
-cursor_visible_(false), cursor_(0),
-need_resize_(false), schedule_redraw_(false), preedit_lines_(1),
-pixel_ratio_(1.0),
-completer_(new GuiCompleter(p, p)), dialog_mode_(false)
+: p(parent), screen_(0), buffer_view_(0), lyx_view_(0),
+  cursor_visible_(false), cursor_(0),
+  need_resize_(false), schedule_redraw_(false), preedit_lines_(1),
+  pixel_ratio_(1.0),
+  completer_(new GuiCompleter(p, p)), dialog_mode_(false),
+  read_only_(false), clean_(true)
 {
 }
 
@@ -1373,8 +1373,15 @@ QVariant GuiWorkArea::inputMethodQuery(Qt::InputMethodQuery query) const
 
 void GuiWorkArea::updateWindowTitle()
 {
-	d->lyx_view_->updateWindowTitle(this);
-	titleChanged(this);
+	Buffer const & buf = bufferView().buffer();
+	if (buf.fileName() != d->file_name_ || buf.isReadonly() != d->read_only_
+	    || buf.lyxvc().vcstatus() != d->vc_status_ || buf.isClean() != d->clean_) {
+		d->file_name_ = buf.fileName();
+		d->read_only_ = buf.isReadonly();
+		d->vc_status_ = buf.lyxvc().vcstatus();
+		d->clean_ = buf.isClean();
+		titleChanged(this);
+	}
 }
 
 

src/frontends/qt4/GuiWorkArea_Private.h
@@ -15,6 +15,7 @@
 #include "FuncRequest.h"
 #include "LyXRC.h"
 
+#include "support/FileName.h"
 #include "support/Timeout.h"
 
 #include <QMouseEvent>
@@ -150,8 +151,6 @@ struct GuiWorkArea::Private
 	QPaintDevice * screen_;
 	///
 	BufferView * buffer_view_;
-	/// Read only Buffer status cache.
-	bool read_only_;
 	///
 	GuiView * lyx_view_;
 	/// is the cursor currently displayed
@@ -187,6 +186,17 @@ struct GuiWorkArea::Private
 	/// when the menu is actually shown (after releasing on Windows)
 	/// and after the DEPM has done its job.
 	std::string context_menu_name_;
+
+	/// stuff related to window title
+	///
+	support::FileName file_name_;
+	///
+	bool read_only_;
+	///
+	docstring vc_status_;
+	///
+	bool clean_;
+
 }; // GuiWorkArea
 
 } // namespace frontend