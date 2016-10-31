20e667e208cbbf5aa1ca291389e9e6d45a85f11b
Juergen Spitzmueller <spitz@lyx.org>
2016-10-31 18:46:06 +0100

    Use Buffer::validate() when previewing single paragraph
    We used to use BufferParams::validate(), which does not validate the
    paragraphs.

--- src/Buffer.cpp
+++ src/Buffer.cpp
@@ -3755,7 +3755,7 @@ auto_ptr<TexRow> Buffer::getSourceCode(odocstream & os, string const & format,
 			// in order to know if we should output polyglossia
 			// macros (instead of babel macros)
 			LaTeXFeatures features(*this, params(), runparams);
-			params().validate(features);
+			validate(features);
 			runparams.use_polyglossia = features.usePolyglossia();
 			texrow.reset(new TexRow());
 			texrow->newline();
