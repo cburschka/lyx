9f2fcf20a51af2b63161a3290cf5fb02474b42b9
Guillaume Munch <gm@lyx.org>
2016-12-20 00:15:58 +0100

    Fix compilation with Qt5.8dev
    (cherry picked from commit 6d375ddeac49ac8c6340398c9797354edca3f5d1)

--- src/frontends/qt4/GuiViewSource.cpp
+++ src/frontends/qt4/GuiViewSource.cpp
@@ -223,14 +223,9 @@ void ViewSourceWidget::realUpdateView()
 	if (changed && !texrow_.get()) {
 		// position-to-row is unavailable
 		// we jump to the first modification
-		const QChar * oc = old.constData();
-		const QChar * nc = qcontent.constData();
+		int length = min(old.length(), qcontent.length());
 		int pos = 0;
-		while (*oc != '\0' && *nc != '\0' && *oc == *nc) {
-			++oc;
-			++nc;
-			++pos;
-		}
+		for (; pos < length && old.at(pos) == qcontent.at(pos); ++pos) {}
 		QTextCursor c = QTextCursor(viewSourceTV->document());
 		//get some space below the cursor
 		c.setPosition(pos);
