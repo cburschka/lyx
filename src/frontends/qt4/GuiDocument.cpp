cburschka/lyx

commit 2e255b1c4f3264571a6bd9ade8651c1657c995c6

Restructure InsetQuotes for better extensibilty
The current char-based implementation gets increasingly unreadable,
especially if styles are added that do not follow the strict
single-double paradigm.

src/frontends/qt4/GuiDocument.cpp
@@ -1067,20 +1067,10 @@ GuiDocument::GuiDocument(GuiView & lv)
 	encodinglist.sort();
 	langModule->encodingCO->addItems(encodinglist);
 
-	langModule->quoteStyleCO->addItem(
-		qt_("``text''"), InsetQuotes::EnglishQuotes);
-	langModule->quoteStyleCO->addItem(
-		qt_("''text''"), InsetQuotes::SwedishQuotes);
-	langModule->quoteStyleCO->addItem
-		(qt_(",,text``"), InsetQuotes::GermanQuotes);
-	langModule->quoteStyleCO->addItem(
-		qt_(",,text''"), InsetQuotes::PolishQuotes);
-	langModule->quoteStyleCO->addItem(
-		qt_("<<text>>"), InsetQuotes::FrenchQuotes);
-	langModule->quoteStyleCO->addItem(
-		qt_(">>text<<"), InsetQuotes::DanishQuotes);
-	langModule->quoteStyleCO->addItem(
-		qt_("\"text\""), InsetQuotes::PlainQuotes);
+	for (int i = 0; i < quoteparams.stylescount(); ++i) {
+		InsetQuotesParams::QuoteStyle qs = InsetQuotesParams::QuoteStyle(i);
+		langModule->quoteStyleCO->addItem(toqstr(quoteparams.getGuiLabel(qs)));
+	}
 
 	langModule->languagePackageCO->addItem(
 		qt_("Default"), toqstr("default"));
@@ -2660,7 +2650,7 @@ void GuiDocument::applyView()
 		}
 	}
 
-	bp_.quotes_style = (InsetQuotes::QuoteStyle) langModule->quoteStyleCO->itemData(
+	bp_.quotes_style = (InsetQuotesParams::QuoteStyle) langModule->quoteStyleCO->itemData(
 		langModule->quoteStyleCO->currentIndex()).toInt();
 
 	QString const langname = langModule->languageCO->itemData(

src/BufferParams.cpp
@@ -128,19 +128,19 @@ ParSepTranslator const & parseptranslator()
 
 
 // Quotes style
-typedef Translator<string, InsetQuotes::QuoteStyle> QuotesStyleTranslator;
+typedef Translator<string, InsetQuotesParams::QuoteStyle> QuotesStyleTranslator;
 
 
 QuotesStyleTranslator const init_quotesstyletranslator()
 {
 	QuotesStyleTranslator translator
-		(string_quotes_style[0], InsetQuotes::EnglishQuotes);
-	translator.addPair(string_quotes_style[1], InsetQuotes::SwedishQuotes);
-	translator.addPair(string_quotes_style[2], InsetQuotes::GermanQuotes);
-	translator.addPair(string_quotes_style[3], InsetQuotes::PolishQuotes);
-	translator.addPair(string_quotes_style[4], InsetQuotes::FrenchQuotes);
-	translator.addPair(string_quotes_style[5], InsetQuotes::DanishQuotes);
-	translator.addPair(string_quotes_style[6], InsetQuotes::PlainQuotes);
+		(string_quotes_style[0], InsetQuotesParams::EnglishQuotes);
+	translator.addPair(string_quotes_style[1], InsetQuotesParams::SwedishQuotes);
+	translator.addPair(string_quotes_style[2], InsetQuotesParams::GermanQuotes);
+	translator.addPair(string_quotes_style[3], InsetQuotesParams::PolishQuotes);
+	translator.addPair(string_quotes_style[4], InsetQuotesParams::FrenchQuotes);
+	translator.addPair(string_quotes_style[5], InsetQuotesParams::DanishQuotes);
+	translator.addPair(string_quotes_style[6], InsetQuotesParams::PlainQuotes);
 	return translator;
 }
 
@@ -395,7 +395,7 @@ BufferParams::BufferParams()
 	cite_engine_type_ = ENGINE_TYPE_DEFAULT;
 	makeDocumentClass();
 	paragraph_separation = ParagraphIndentSeparation;
-	quotes_style = InsetQuotes::EnglishQuotes;
+	quotes_style = InsetQuotesParams::EnglishQuotes;
 	fontsize = "default";
 
 	/*  PaperLayout */
@@ -2548,7 +2548,7 @@ Font const BufferParams::getFont() const
 }
 
 
-InsetQuotes::QuoteStyle BufferParams::getQuoteStyle(string const & qs) const
+InsetQuotesParams::QuoteStyle BufferParams::getQuoteStyle(string const & qs) const
 {
 	return quotesstyletranslator().find(qs);
 }

src/BufferParams.h
@@ -106,7 +106,7 @@ public:
 	 */
 	ParagraphSeparation paragraph_separation;
 	///
-	InsetQuotes::QuoteStyle quotes_style;
+	InsetQuotesParams::QuoteStyle quotes_style;
 	///
 	std::string fontsize;
 	/// Get the LayoutFile this document is using.
@@ -201,7 +201,7 @@ public:
 	Font const getFont() const;
 
 	/// translate quote style string to enum value
-	InsetQuotes::QuoteStyle getQuoteStyle(std::string const & qs) const;
+	InsetQuotesParams::QuoteStyle getQuoteStyle(std::string const & qs) const;
 
 	/* these are for the PaperLayout */
 	/// the papersize

src/Text3.cpp
@@ -1551,8 +1551,8 @@ void Text::dispatch(Cursor & cur, FuncRequest & cmd)
 		char_type c = ' ';
 		if (pos > 0 && (!cur.prevInset() || !cur.prevInset()->isSpace()))
 			c = par.getChar(pos - 1);
-		InsetQuotes::QuoteLevel const quote_level = (cmd.getArg(0) == "single")
-			? InsetQuotes::SingleQuotes : InsetQuotes::DoubleQuotes;
+		InsetQuotesParams::QuoteLevel const quote_level = (cmd.getArg(0) == "single")
+			? InsetQuotesParams::SingleQuotes : InsetQuotesParams::DoubleQuotes;
 		cur.insert(new InsetQuotes(cur.buffer(), c, quote_level, cmd.getArg(1), cmd.getArg(2)));
 		cur.buffer()->updateBuffer();
 		cur.posForward();

src/frontends/qt4/Menus.cpp
@@ -1669,7 +1669,7 @@ void MenuDefinition::expandQuotes(BufferView const * bv)
 	InsetQuotes const * qinset =
 		static_cast<InsetQuotes const *>(inset);
 
-	map<string, docstring> styles = qinset->getTypes();
+	map<string, docstring> styles = quoteparams.getTypes();
 	string const qtype = qinset->getType();
 	
 	map<string, docstring>::const_iterator qq = styles.begin();
@@ -1706,37 +1706,44 @@ void MenuDefinition::expandQuotes(BufferView const * bv)
 	}
 
 	if (!eqs.empty()) {
-		MenuItem item(MenuItem::Submenu, qt_("``text''"));
+		MenuItem item(MenuItem::Submenu,
+			      toqstr(quoteparams.getGuiLabel(InsetQuotesParams::EnglishQuotes)));
 		item.setSubmenu(eqs);
 		add(item);
 	}
 	if (!sqs.empty()) {
-		MenuItem item(MenuItem::Submenu, qt_("''text''"));
+		MenuItem item(MenuItem::Submenu,
+			      toqstr(quoteparams.getGuiLabel(InsetQuotesParams::SwedishQuotes)));
 		item.setSubmenu(sqs);
 		add(item);
 	}
 	if (!gqs.empty()) {
-		MenuItem item(MenuItem::Submenu, qt_(",,text``"));
+		MenuItem item(MenuItem::Submenu,
+			      toqstr(quoteparams.getGuiLabel(InsetQuotesParams::GermanQuotes)));
 		item.setSubmenu(gqs);
 		add(item);
 	}
 	if (!pqs.empty()) {
-		MenuItem item(MenuItem::Submenu, qt_(",,text''"));
+		MenuItem item(MenuItem::Submenu,
+			      toqstr(quoteparams.getGuiLabel(InsetQuotesParams::PolishQuotes)));
 		item.setSubmenu(pqs);
 		add(item);
 	}
 	if (!fqs.empty()) {
-		MenuItem item(MenuItem::Submenu, qt_("<<text>>"));
+		MenuItem item(MenuItem::Submenu,
+			      toqstr(quoteparams.getGuiLabel(InsetQuotesParams::FrenchQuotes)));
 		item.setSubmenu(fqs);
 		add(item);
 	}
 	if (!aqs.empty()) {
-		MenuItem item(MenuItem::Submenu, qt_(">>text<<"));
+		MenuItem item(MenuItem::Submenu,
+			      toqstr(quoteparams.getGuiLabel(InsetQuotesParams::DanishQuotes)));
 		item.setSubmenu(aqs);
 		add(item);
 	}
 	if (!qqs.empty()) {
-		MenuItem item(MenuItem::Submenu, qt_("\"text\""));
+		MenuItem item(MenuItem::Submenu,
+			      toqstr(quoteparams.getGuiLabel(InsetQuotesParams::PlainQuotes)));
 		item.setSubmenu(qqs);
 		add(item);
 	}

src/insets/InsetQuotes.h
@@ -5,6 +5,7 @@
  * Licence details can be found in the file COPYING.
  *
  * \author Jean-Marc Lasgouttes
+ *  \author Jürgen Spitzmüller
  *
  * Full author contact details are available in file CREDITS.
  */
@@ -22,8 +23,7 @@ namespace lyx {
 /** Quotes.
   Used for the various quotes. German, English, French, all either
   double or single **/
-class InsetQuotes : public Inset
-{
+class InsetQuotesParams {
 public:
 	///
 	enum QuoteStyle {
@@ -56,7 +56,30 @@ public:
 		///
 		DoubleQuotes
 	};
+	/// Returns the unicode character of a given quote
+	char_type getQuoteChar(QuoteStyle const &, QuoteLevel const &,
+			       QuoteSide const &) const;
+	/// Returns a map of quotation marks
+	std::map<std::string, docstring> getTypes() const;
+	///
+	docstring getLaTeXQuote(char_type c, std::string const &) const;
+	///
+	docstring getHTMLQuote(char_type c) const;
+	/// Returns a label suitable for dialog and menu
+	docstring const getGuiLabel(QuoteStyle const & qs);
+	///
+	int stylescount() const;
+};
 
+///
+extern InsetQuotesParams quoteparams;
+
+/** Quotes.
+  Used for the various quotes. German, English, French, all either
+  double or single **/
+class InsetQuotes : public Inset
+{
+public:
 	/** The constructor works like this:
 	  \begin{itemize}
 	    \item fls <- french single quote left
@@ -66,7 +89,7 @@ public:
 	  */
 	explicit InsetQuotes(Buffer * buf, std::string const & str = "eld");
 	/// Direct access to inner/outer quotation marks
-	InsetQuotes(Buffer * buf, char_type c, QuoteLevel level,
+	InsetQuotes(Buffer * buf, char_type c, InsetQuotesParams::QuoteLevel level,
 		    std::string const & side = std::string(),
 		    std::string const & style = std::string());
 	///
@@ -107,11 +130,9 @@ public:
 	InsetCode lyxCode() const { return QUOTE_CODE; }
 	/// should this inset be handled like a normal character
 	bool isChar() const { return true; }
-	
+
 	/// Returns the current quote type
 	std::string getType() const;
-	/// Returns a map of quotation marks
-	std::map<std::string, docstring> getTypes() const;
 
 private:
 	///
@@ -127,20 +148,22 @@ private:
 	///
 	docstring getQuoteEntity() const;
 	///
-	QuoteStyle getStyle(std::string const &);
+	InsetQuotesParams::QuoteStyle getStyle(std::string const &);
 
 	///
-	QuoteStyle style_;
+	InsetQuotesParams::QuoteStyle style_;
 	///
-	QuoteSide side_;
+	InsetQuotesParams::QuoteSide side_;
 	///
-	QuoteLevel level_;
+	InsetQuotesParams::QuoteLevel level_;
 	///
 	std::string fontenc_;
 	/// Code of the contextual language
 	std::string context_lang_;
 	/// Is this in a pass-thru context?
 	bool pass_thru_;
+	///
+	friend class InsetQuotesParams;
 
 protected:
 	/// \name Protected functions inherited from Inset class

commit 45df1e87a9b61e6eefc83de0f37e3c1566c19319

Fix glitch introduced in 2e255b1c4f
set correct combo values.

src/frontends/qt4/GuiDocument.cpp
@@ -1069,7 +1069,7 @@ GuiDocument::GuiDocument(GuiView & lv)
 
 	for (int i = 0; i < quoteparams.stylescount(); ++i) {
 		InsetQuotesParams::QuoteStyle qs = InsetQuotesParams::QuoteStyle(i);
-		langModule->quoteStyleCO->addItem(toqstr(quoteparams.getGuiLabel(qs)));
+		langModule->quoteStyleCO->addItem(toqstr(quoteparams.getGuiLabel(qs)), qs);
 	}
 
 	langModule->languagePackageCO->addItem(