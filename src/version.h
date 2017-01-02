78a76cc8d485d43fcae4356aaf5026c9a7ca344e
Richard Heck <rgheck@lyx.org>
2017-01-02 18:14:41 -0500

    Update tex2lyx tests.

--- src/version.h
+++ src/version.h
@@ -33,7 +33,7 @@ extern char const * const lyx_version_info;
 // Do not remove the comment below, so we get merge conflict in
 // independent branches. Instead add your own.
 #define LYX_FORMAT_LYX 525 // rgh: labelonly for references
-#define LYX_FORMAT_TEX2LYX 524
+#define LYX_FORMAT_TEX2LYX 525
 
 #if LYX_FORMAT_TEX2LYX != LYX_FORMAT_LYX
 #ifndef _MSC_VER
