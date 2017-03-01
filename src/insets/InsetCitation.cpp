dc385a8b97cfc0fcb1d7119655ea23688a567944
Jean-Marc Lasgouttes <lasgouttes@lyx.org>
2017-03-01 16:41:37 +0100

    Fix coverity false positive
    The fall through was intended at db889bc6.

--- src/insets/InsetCitation.cpp
+++ src/insets/InsetCitation.cpp
@@ -149,6 +149,7 @@ void InsetCitation::doDispatch(Cursor & cur, FuncRequest & cmd)
 			cmd = FuncRequest(LFUN_INSET_MODIFY, "changetype " + newcmdname);
 		}
 	}
+		// fall through
 	default:
 		InsetCommand::doDispatch(cur, cmd);
 	}
