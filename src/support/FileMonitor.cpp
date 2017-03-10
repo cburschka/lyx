f96d7a8b2cb45e98382e2d88f1156e74e7898f5e
Guillaume Munch <gm@lyx.org>
2017-03-10 22:41:48 +0100

    Remove legacy FileMonitor

--- src/support/FileMonitor.cpp
+++ src/support/FileMonitor.cpp
@@ -3,7 +3,7 @@
  * This file is part of LyX, the document processor.
  * Licence details can be found in the file COPYING.
  *
- * \author Angus Leeming
+ * \author Guillaume Munch
  *
  * Full author contact details are available in file CREDITS.
  */
@@ -12,170 +12,199 @@
 
 #include "support/FileMonitor.h"
 
+#include "support/debug.h"
 #include "support/FileName.h"
-#include "support/Timeout.h"
+#include "support/qstring_helpers.h"
+#include "support/unique_ptr.h"
 
-#include "support/bind.h"
-#include <boost/signals2/trackable.hpp>
+#include <QFile>
+#include <QTimer>
+
+#include <algorithm>
 
 using namespace std;
 
 namespace lyx {
 namespace support {
 
-class FileMonitor::Impl : public boost::signals2::trackable {
-public:
 
-	///
-	Impl(FileName const & file_with_path, int interval);
-
-	///
-	void monitorFile();
-
-	///
-	FileName filename_;
-
-	///
-	Timeout timer_;
-
-	/// This signal is emitted if the file is modified (has a new checksum).
-	FileMonitor::FileChangedSig fileChanged_;
-
-	/** We use these to ascertain whether a file (once loaded successfully)
-	 *  has changed.
-	 */
-	time_t timestamp_;
-	///
-	unsigned long checksum_;
-};
+FileSystemWatcher & FileSystemWatcher::instance()
+{
+	// This thread-safe because QFileSystemWatcher is thread-safe.
+	static FileSystemWatcher f;
+	return f;
+}
 
 
-FileMonitor::FileMonitor(FileName const & file_with_path, int interval)
-	: pimpl_(new Impl(file_with_path, interval))
+FileSystemWatcher::FileSystemWatcher()
+	: qwatcher_(make_unique<QFileSystemWatcher>())
 {}
 
 
-FileMonitor::~FileMonitor()
+//static
+FileMonitorPtr FileSystemWatcher::monitor(FileName const & file_with_path)
 {
-	delete pimpl_;
+	FileSystemWatcher & f = instance();
+	string const filename = file_with_path.absFileName();
+	weak_ptr<FileMonitorGuard> & wptr = f.store_[filename];
+	if (shared_ptr<FileMonitorGuard> mon = wptr.lock())
+		return make_unique<FileMonitor>(mon);
+	auto mon = make_shared<FileMonitorGuard>(filename, f.qwatcher_.get());
+	wptr = mon;
+	return make_unique<FileMonitor>(mon);
 }
 
 
-void FileMonitor::reset(FileName const & file_with_path) const
+//static
+void FileSystemWatcher::debug()
 {
-	if (pimpl_->filename_ == file_with_path)
-		return;
-
-	bool const monitor = pimpl_->timer_.running();
-	if (monitor)
-		stop();
-
-	pimpl_->filename_ = file_with_path;
-
-	if (monitor)
-		start();
-}
-
-
-FileName const & FileMonitor::filename() const
-{
-	return pimpl_->filename_;
-}
-
-
-void FileMonitor::start() const
-{
-	if (monitoring())
-		return;
-
-	if (!pimpl_->filename_.exists())
-		return;
-
-	pimpl_->timestamp_ = pimpl_->filename_.lastModified();
-	pimpl_->checksum_ = pimpl_->filename_.checksum();
-
-	if (pimpl_->timestamp_ && pimpl_->checksum_) {
-		pimpl_->timer_.start();
-	} else {
-		pimpl_->timestamp_ = 0;
-		pimpl_->checksum_ = 0;
-	}
-}
-
-
-void FileMonitor::stop() const
-{
-	pimpl_->timestamp_ = 0;
-	pimpl_->checksum_ = 0;
-	pimpl_->timer_.stop();
-}
-
-
-bool FileMonitor::monitoring() const
-{
-	return pimpl_->timer_.running();
-}
-
-
-unsigned long FileMonitor::checksum() const
-{
-	// If we aren't actively monitoring the file, then recompute the
-	// checksum explicitly.
-	if (!pimpl_->timer_.running() && !pimpl_->filename_.empty())
-		return pimpl_->filename_.checksum();
-
-	return pimpl_->checksum_;
-}
-
-
-boost::signals2::connection FileMonitor::connect(slot_type const & slot) const
-{
-	return pimpl_->fileChanged_.connect(slot);
-}
-
-
-//------------------------------
-// Implementation details follow
-//------------------------------
-
-
-FileMonitor::Impl::Impl(FileName const & file_with_path, int interval)
-	: filename_(file_with_path),
-	  timer_(interval, Timeout::ONETIME),
-	  timestamp_(0),
-	  checksum_(0)
-{
-	timer_.timeout.connect(bind(&Impl::monitorFile, this));
-}
-
-
-void FileMonitor::Impl::monitorFile()
-{
-	bool changed = false;
-
-	if (!filename_.exists()) {
-		changed = timestamp_ || checksum_;
-		timestamp_ = 0;
-		checksum_ = 0;
-
-	} else {
-		time_t const new_timestamp = filename_.lastModified();
-
-		if (new_timestamp != timestamp_) {
-			timestamp_ = new_timestamp;
-
-			unsigned long const new_checksum = filename_.checksum();
-			if (new_checksum != checksum_) {
-				checksum_ = new_checksum;
-				changed = true;
+	FileSystemWatcher & f = instance();
+	QStringList q_files = f.qwatcher_->files();
+	for (pair<string, weak_ptr<FileMonitorGuard>> pair : f.store_) {
+		string const & name = pair.first;
+		if (!pair.second.expired()) {
+			if (!q_files.contains(toqstr(name)))
+				LYXERR0("Monitored but not QFileSystemWatched (bad): " << name);
+			else {
+				//LYXERR0("Monitored and QFileSystemWatched (good): " << name);
 			}
 		}
 	}
+	for (QString const & qname : q_files) {
+		string const name = fromqstr(qname);
+		weak_ptr<FileMonitorGuard> & wptr = f.store_[name];
+		if (wptr.expired())
+			LYXERR0("QFileSystemWatched but not monitored (bad): " << name);
+	}
+}
 
-	timer_.start();
-	if (changed)
-		fileChanged_();
+
+FileMonitorGuard::FileMonitorGuard(string const & filename,
+                                   QFileSystemWatcher * qwatcher)
+	: filename_(filename), qwatcher_(qwatcher)
+{
+	QObject::connect(qwatcher, SIGNAL(fileChanged(QString const &)),
+	                 this, SLOT(notifyChange(QString const &)));
+	if (qwatcher_->files().contains(toqstr(filename)))
+		LYXERR0("This file is already being QFileSystemWatched: " << filename
+		        << ". This should not happen.");
+	refresh();
+}
+
+
+FileMonitorGuard::~FileMonitorGuard()
+{
+	qwatcher_->removePath(toqstr(filename_));
+}
+
+
+void FileMonitorGuard::refresh(bool new_file)
+{
+	QString const qfilename = toqstr(filename_);
+	if(!qwatcher_->files().contains(qfilename)) {
+		bool exists = QFile(qfilename).exists();
+		if (!exists || !qwatcher_->addPath(qfilename)) {
+			if (exists)
+				LYXERR(Debug::FILES,
+				       "Could not add path to QFileSystemWatcher: "
+				       << filename_);
+			QTimer::singleShot(1000, this, [=](){
+					refresh(new_file || !exists);
+				});
+		} else if (exists && new_file)
+			Q_EMIT fileChanged();
+	}
+}
+
+
+void FileMonitorGuard::notifyChange(QString const & path)
+{
+	if (path == toqstr(filename_)) {
+		Q_EMIT fileChanged();
+		// If the file has been modified by delete-move, we are notified of the
+		// deletion but we no longer track the file. See
+		// <https://bugreports.qt.io/browse/QTBUG-46483> (not a bug).
+		refresh();
+	}
+}
+
+
+FileMonitor::FileMonitor(std::shared_ptr<FileMonitorGuard> monitor)
+	: monitor_(monitor)
+{
+	connectToFileMonitorGuard();
+	refresh();
+}
+
+
+void FileMonitor::connectToFileMonitorGuard()
+{
+	QObject::connect(monitor_.get(), SIGNAL(fileChanged()),
+	                 this, SLOT(changed()));
+}
+
+
+boost::signals2::connection
+FileMonitor::connect(sig::slot_type const & slot)
+{
+	return fileChanged_.connect(slot);
+}
+
+
+void FileMonitor::disconnect()
+{
+	fileChanged_.disconnect_all_slots();
+	QObject::disconnect(this, SIGNAL(fileChanged()));
+}
+
+
+void FileMonitor::changed()
+{
+	// emit boost signal
+	fileChanged_();
+	Q_EMIT fileChanged();
+}
+
+
+FileMonitorBlocker FileMonitor::block(int delay)
+{
+	FileMonitorBlocker blocker = blocker_.lock();
+	if (!blocker)
+		blocker_ = blocker = make_shared<FileMonitorBlockerGuard>(this);
+	blocker->setDelay(delay);
+	return blocker;
+}
+
+
+FileMonitorBlockerGuard::FileMonitorBlockerGuard(FileMonitor * parent)
+	: QObject(parent), parent_(parent), delay_(0)
+{
+	QObject::disconnect(parent_->monitor_.get(), SIGNAL(fileChanged()),
+	                    parent_, SLOT(changed()));
+}
+
+
+void FileMonitorBlockerGuard::setDelay(int delay)
+{
+	delay_ = max(delay_, delay);
+}
+
+
+FileMonitorBlockerGuard::~FileMonitorBlockerGuard()
+{
+	// closures can only copy local copies
+	FileMonitor * parent = parent_;
+	// parent is also our QObject::parent() so we are deleted before parent.
+	// Even if delay_ is 0, the QTimer is necessary. Indeed, the notifications
+	// from QFileSystemWatcher that we meant to ignore are not going to be
+	// treated immediately, so we must yield to give us the opportunity to
+	// ignore them.
+	QTimer::singleShot(delay_, parent, [parent]() {
+			parent->connectToFileMonitorGuard();
+		});
 }
 
 } // namespace support
 } // namespace lyx
+
+#include "moc_FileMonitor.cpp"
