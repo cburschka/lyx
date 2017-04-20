cburschka/lyx

commit 244de5d2c10a990828eafdd72283fc87742dc133

Add 'needauth' option to converters that need explicit user authorization.
Addressing #10481.

This patch adds the new 'needauth' option for converters launching
external programs that are capable of running arbitrary code on behalf
of the user. These converters won't be run unless the user gives explicit
authorization, which is asked on-demand when the converter is about to
be run (question is not asked if the file is cached and calling the
converter is not needed).

The user prompt has a 3rd button so that he/she's not prompted again
for (any converter over) the same document (identified through
buffer->absFileName()).

Two preference options are added:

lyxrc.use_converter_needauth_forbidden disables any converter with
the 'needauth' option, which is meant to force user to an explicit
action via the preferences pane, before being able to use advanced
converters that can potentially bring security threats;

lyxrc.use_converter_needauth enables prompting the user for 'needauth'
converters, or bypasses the check if not enabled, falling back to the
previous behavior.

So, the first option is for maximum security, the second is for
maximum usability.

src/graphics/GraphicsCacheItem.cpp
@@ -14,6 +14,7 @@
 
 #include "GraphicsCacheItem.h"
 
+#include "Buffer.h"
 #include "GraphicsCache.h"
 #include "GraphicsConverter.h"
 #include "GraphicsImage.h"
@@ -42,7 +43,7 @@ class CacheItem::Impl : public boost::signals2::trackable {
 public:
 
 	///
-	Impl(FileName const & file);
+	Impl(FileName const & file, FileName const & doc_file);
 
 	/**
 	 *  If no file conversion is needed, then tryDisplayFormat() calls
@@ -93,6 +94,8 @@ public:
 
 	/// The filename we refer too.
 	FileName const filename_;
+	/// The document filename this graphic item belongs to
+	FileName const & doc_file_;
 	///
 	FileMonitor const monitor_;
 
@@ -125,8 +128,8 @@ public:
 };
 
 
-CacheItem::CacheItem(FileName const & file)
-	: pimpl_(new Impl(file))
+CacheItem::CacheItem(FileName const & file, FileName const & doc_file)
+  : pimpl_(new Impl(file,doc_file))
 {}
 
 
@@ -204,8 +207,8 @@ boost::signals2::connection CacheItem::connect(slot_type const & slot) const
 //------------------------------
 
 
-CacheItem::Impl::Impl(FileName const & file)
-	: filename_(file),
+CacheItem::Impl::Impl(FileName const & file, FileName const & doc_file)
+	: filename_(file), doc_file_(doc_file),
 	  monitor_(file, 2000),
 	  zipped_(false),
 	  remove_loaded_file_(false),
@@ -312,11 +315,10 @@ bool CacheItem::Impl::loadImage()
 }
 
 
-static string const findTargetFormat(string const & from)
-{
-	typedef vector<string> FormatList;
-	FormatList const & formats = Cache::get().loadableFormats();
+typedef vector<string> FormatList;
 
+static string const findTargetFormat(FormatList const & formats, string const & from)
+{
 	 // There must be a format to load from.
 	LASSERT(!formats.empty(), return string());
 
@@ -392,7 +394,7 @@ bool CacheItem::Impl::tryDisplayFormat(FileName & filename, string & from)
 		LYXERR(Debug::GRAPHICS, "\tCould not determine file format.");
 	}
 	LYXERR(Debug::GRAPHICS, "\n\tThe file contains " << from << " format data.");
-	to_ = findTargetFormat(from);
+	to_ = findTargetFormat(Cache::get().loadableFormats(), from);
 
 	if (from == to_) {
 		// No conversion needed!
@@ -438,7 +440,7 @@ void CacheItem::Impl::convertToDisplayFormat()
 	// Connect a signal to this->imageConverted and pass this signal to
 	// the graphics converter so that we can load the modified file
 	// on completion of the conversion process.
-	converter_ = make_unique<Converter>(filename, to_file_base.absFileName(),
+	converter_ = make_unique<Converter>(doc_file_, filename, to_file_base.absFileName(),
 	                                    from, to_);
 	converter_->connect(bind(&Impl::imageConverted, this, _1));
 	converter_->startConversion();

src/graphics/GraphicsConverter.cpp
@@ -12,9 +12,12 @@
 
 #include "GraphicsConverter.h"
 
+#include "Buffer.h"
 #include "Converter.h"
 #include "Format.h"
+#include "LyXRC.h"
 
+#include "frontends/alert.h"
 #include "support/lassert.h"
 #include "support/convert.h"
 #include "support/debug.h"
@@ -40,7 +43,7 @@ namespace graphics {
 class Converter::Impl : public boost::signals2::trackable {
 public:
 	///
-	Impl(FileName const &, string const &, string const &, string const &);
+	Impl(FileName const &, FileName const &, string const &, string const &, string const &);
 
 	///
 	void startConversion();
@@ -59,6 +62,8 @@ public:
 	///
 	SignalType finishedConversion;
 
+	///
+	FileName const & doc_fname_;
 	///
 	string script_command_;
 	///
@@ -79,9 +84,10 @@ bool Converter::isReachable(string const & from_format_name,
 }
 
 
-Converter::Converter(FileName const & from_file, string const & to_file_base,
+Converter::Converter(FileName const & doc_fname,
+		     FileName const & from_file, string const & to_file_base,
 		     string const & from_format, string const & to_format)
-	: pimpl_(new Impl(from_file, to_file_base, from_format, to_format))
+	: pimpl_(new Impl(doc_fname, from_file, to_file_base, from_format, to_format))
 {}
 
 
@@ -112,17 +118,20 @@ FileName const & Converter::convertedFile() const
 /** Build the conversion script.
  *  The script is output to the stream \p script.
  */
-static void build_script(string const & from_file, string const & to_file_base,
+static void build_script(string const & doc_fname,
+		  string const & from_file, string const & to_file_base,
 		  string const & from_format, string const & to_format,
 		  ostream & script);
 
 
-Converter::Impl::Impl(FileName const & from_file, string const & to_file_base,
+Converter::Impl::Impl(FileName const & doc_fname,
+		      FileName const & from_file, string const & to_file_base,
 		      string const & from_format, string const & to_format)
-	: valid_process_(false), finished_(false)
+	: doc_fname_(doc_fname), valid_process_(false), finished_(false)
 {
 	LYXERR(Debug::GRAPHICS, "Converter c-tor:\n"
-		<< "\tfrom_file:      " << from_file
+		<< "doc_fname:        " << doc_fname
+		<< "\n\tfrom_file:    " << from_file
 		<< "\n\tto_file_base: " << to_file_base
 		<< "\n\tfrom_format:  " << from_format
 		<< "\n\tto_format:    " << to_format);
@@ -134,7 +143,7 @@ Converter::Impl::Impl(FileName const & from_file, string const & to_file_base,
 
 	// The conversion commands are stored in a stringstream
 	ostringstream script;
-	build_script(from_file.toFilesystemEncoding(),
+	build_script(doc_fname_.absFileName(), from_file.toFilesystemEncoding(),
 		     to_file_.toFilesystemEncoding(),
 		     from_format, to_format, script);
 	LYXERR(Debug::GRAPHICS, "\tConversion script:"
@@ -263,7 +272,8 @@ static string const strip_digit(string const & format)
 }
 
 
-static void build_script(string const & from_file,
+static void build_script(string const & doc_fname,
+		  string const & from_file,
 		  string const & to_file,
 		  string const & from_format,
 		  string const & to_format,
@@ -371,6 +381,9 @@ static void build_script(string const & from_file,
 			tempfile.setAutoRemove(false);
 			outfile = tempfile.name().toFilesystemEncoding();
 		}
+
+		if (!theConverters().checkAuth(conv, doc_fname))
+			return;
 
 		// Store these names in the python script
 		script << "infile = "

lib/configure.py
@@ -749,8 +749,8 @@ def checkConverterEntries():
         rc_entry = [r'''\converter latex      lyx        "%% -f $$i $$o"	""
 \converter latexclipboard lyx        "%% -fixedenc utf8 -f $$i $$o"	""
 \converter literate   lyx        "%% -n -m noweb -f $$i $$o"	""
-\converter sweave   lyx        "%% -n -m sweave -f $$i $$o"	""
-\converter knitr   lyx        "%% -n -m knitr -f $$i $$o"	""'''], not_found = 'tex2lyx')
+\converter sweave   lyx        "%% -n -m sweave -f $$i $$o"	"needauth"
+\converter knitr   lyx        "%% -n -m knitr -f $$i $$o"	"needauth"'''], not_found = 'tex2lyx')
     if path == '':
         logger.warning("Failed to find tex2lyx on your system.")
 
@@ -763,24 +763,24 @@ def checkConverterEntries():
 \converter literate   dviluatex     "%%"	""'''])
     #
     checkProg('a Sweave -> LaTeX converter', ['Rscript --verbose --no-save --no-restore $$s/scripts/lyxsweave.R $$p$$i $$p$$o $$e $$r'],
-        rc_entry = [r'''\converter sweave   latex      "%%"	""
-\converter sweave   pdflatex   "%%"	""
-\converter sweave   xetex      "%%"	""
-\converter sweave   luatex     "%%"	""
-\converter sweave   dviluatex  "%%"	""'''])
+        rc_entry = [r'''\converter sweave   latex      "%%"	"needauth"
+\converter sweave   pdflatex   "%%"	"needauth"
+\converter sweave   xetex      "%%"	"needauth"
+\converter sweave   luatex     "%%"	"needauth"
+\converter sweave   dviluatex  "%%"	"needauth"'''])
     #
     checkProg('a knitr -> LaTeX converter', ['Rscript --verbose --no-save --no-restore $$s/scripts/lyxknitr.R $$p$$i $$p$$o $$e $$r'],
-        rc_entry = [r'''\converter knitr   latex      "%%"	""
-\converter knitr   pdflatex   "%%"	""
-\converter knitr   xetex      "%%"	""
-\converter knitr   luatex     "%%"	""
-\converter knitr   dviluatex  "%%"	""'''])
+        rc_entry = [r'''\converter knitr   latex      "%%"	"needauth"
+\converter knitr   pdflatex   "%%"	"needauth"
+\converter knitr   xetex      "%%"	"needauth"
+\converter knitr   luatex     "%%"	"needauth"
+\converter knitr   dviluatex  "%%"	"needauth"'''])
     #
     checkProg('a Sweave -> R/S code converter', ['Rscript --verbose --no-save --no-restore $$s/scripts/lyxstangle.R $$i $$e $$r'], 
-        rc_entry = [ r'\converter sweave      r      "%%"    ""' ])
+        rc_entry = [ r'\converter sweave      r      "%%"    "needauth"' ])
     #
     checkProg('a knitr -> R/S code converter', ['Rscript --verbose --no-save --no-restore $$s/scripts/lyxknitr.R $$p$$i $$p$$o $$e $$r tangle'],
-        rc_entry = [ r'\converter knitr      r      "%%"    ""' ])
+        rc_entry = [ r'\converter knitr      r      "%%"    "needauth"' ])
     #
     checkProg('an HTML -> LaTeX converter', ['html2latex $$i', 'gnuhtml2latex',
         'htmltolatex -input $$i -output $$o', 'htmltolatex.jar -input $$i -output $$o'],

src/Buffer.cpp
@@ -79,6 +79,7 @@
 #include "mathed/MathMacroTemplate.h"
 #include "mathed/MathSupport.h"
 
+#include "graphics/GraphicsCache.h"
 #include "graphics/PreviewLoader.h"
 
 #include "frontends/alert.h"
@@ -421,8 +422,8 @@ Buffer::Impl::Impl(Buffer * owner, FileName const & file, bool readonly_,
 	  ignore_parent(false),  toc_backend(owner), macro_lock(false), timestamp_(0),
 	  checksum_(0), wa_(0),  gui_(0), undo_(*owner), bibinfo_cache_valid_(false),
 	  bibfile_cache_valid_(false), cite_labels_valid_(false), preview_error_(false),
-	  inset(0), preview_loader_(0), cloned_buffer_(cloned_buffer), clone_list_(0),
-	  doing_export(false), parent_buffer(0),
+	  inset(0), preview_loader_(0), cloned_buffer_(cloned_buffer),
+	  clone_list_(0), doing_export(false), parent_buffer(0),
 	  word_count_(0), char_count_(0), blank_count_(0)
 {
 	if (!cloned_buffer_) {

src/Buffer.h
@@ -74,6 +74,7 @@ class FileNameList;
 
 namespace graphics {
 class PreviewLoader;
+class Cache;
 }
 
 
@@ -587,6 +588,8 @@ public:
 	void updatePreviews() const;
 	/// Remove any previewed LaTeX snippets associated with this buffer
 	void removePreviews() const;
+	///
+	graphics::Cache & graphicsCache() const;
 
 	/// Our main text (inside the top InsetText)
 	Text & text() const;

src/Converter.cpp
@@ -21,6 +21,7 @@
 #include "Format.h"
 #include "Language.h"
 #include "LaTeX.h"
+#include "LyXRC.h"
 #include "Mover.h"
 
 #include "frontends/alert.h"
@@ -100,7 +101,7 @@ Converter::Converter(string const & f, string const & t,
 		     string const & c, string const & l)
 	: from_(f), to_(t), command_(c), flags_(l),
 	  From_(0), To_(0), latex_(false), xml_(false),
-	  need_aux_(false), nice_(false)
+	  need_aux_(false), nice_(false), need_auth_(false)
 {}
 
 
@@ -128,6 +129,8 @@ void Converter::readFlags()
 			parselog_ = flag_value;
 		else if (flag_name == "nice")
 			nice_ = true;
+		else if (flag_name == "needauth")
+			need_auth_ = true;
 	}
 	if (!result_dir_.empty() && result_file_.empty())
 		result_file_ = "index." + formats.extension(to_);
@@ -275,6 +278,49 @@ OutputParams::FLAVOR Converters::getFlavor(Graph::EdgePath const & path,
 }
 
 
+bool Converters::checkAuth(Converter const & conv, string const & doc_fname)
+{
+	if (!conv.need_auth())
+		return true;
+	if (lyxrc.use_converter_needauth_forbidden) {
+		frontend::Alert::warning(
+			_("Potentially harmful external converters disabled"),
+			_("Requested operation needs use of a potentially harmful external converter program,"
+			  "which is forbidden by default.\nThese converters are tagged by the 'needauth' option. "
+			  "In order to unlock execution of these converters,\nplease, go to "
+			  "Preferences->File Handling->Converters and uncheck "
+			  "Security->Forbid needauth converters."), true);
+		return false;
+	}
+	if (!lyxrc.use_converter_needauth)
+		return true;
+	static const docstring security_title = _("Launch of external converter needs user authorization");
+	static const char security_warning[] = "LyX is about to run converter '%1$s' which is launching an external program "
+		"that normally acts as a picture/format converter. However, this external program is known to be able to "
+		"execute arbitrary actions on the system on behalf of the user, including dangerous ones such as deleting "
+		"files, if instructed to do so by a maliciously crafted .lyx document.\n\nWould you like to run the converter?\n\n"
+		"ANSWER RUN ONLY IF YOU TRUST THE ORIGIN/SENDER OF THE LYX DOCUMENT!";
+	int choice;
+	if (!doc_fname.empty()) {
+		LYXERR(Debug::FILES, "looking up: " << doc_fname);
+		if (auth_files_.find(doc_fname) == auth_files_.end()) {
+			choice = frontend::Alert::prompt(security_title,
+				bformat(_(security_warning), from_utf8(conv.command())),
+				0, 0, _("Do &NOT run"), _("&Run"), _("&Always run for this document"));
+			if (choice == 2)
+				auth_files_.insert(doc_fname);
+		} else {
+			choice = 1;
+		}
+	} else {
+		choice = frontend::Alert::prompt(security_title,
+			bformat(_(security_warning), from_utf8(conv.command())),
+			0, 0, _("Do &NOT run"), _("&Run"));
+	}
+	return choice != 0;
+}
+
+
 bool Converters::convert(Buffer const * buffer,
 			 FileName const & from_file, FileName const & to_file,
 			 FileName const & orig_from,
@@ -401,6 +447,9 @@ bool Converters::convert(Buffer const * buffer,
 				outfile = FileName(addName(package().temp_dir().absFileName(),
 						   "tmpfile.out"));
 		}
+
+		if (!checkAuth(conv, buffer->absFileName()))
+			return false;
 
 		if (conv.latex()) {
 			run_latex = true;

src/Converter.h
@@ -69,6 +69,8 @@ public:
 	bool xml() const { return xml_; }
 	///
 	bool need_aux() const { return need_aux_; }
+	/// Return whether or not the needauth option is set for this converter
+	bool need_auth() const { return need_auth_; }
 	///
 	bool nice() const { return nice_; }
 	///
@@ -77,6 +79,7 @@ public:
 	std::string const result_file() const { return result_file_; }
 	///
 	std::string const parselog() const { return parselog_; }
+
 private:
 	///
 	trivstring from_;
@@ -101,6 +104,8 @@ private:
 	bool need_aux_;
 	/// we need a "nice" file from the backend, c.f. OutputParams.nice.
 	bool nice_;
+	/// Use of this converter needs explicit user authorization
+	bool need_auth_;
 	/// If the converter put the result in a directory, then result_dir
 	/// is the name of the directory
 	trivstring result_dir_;
@@ -181,6 +186,16 @@ public:
 	const_iterator end() const { return converterlist_.end(); }
 	///
 	void buildGraph();
+
+	/// Check whether converter conv is authorized to be run for elements
+	/// within document doc_fname.
+	/// The check succeeds for safe converters, whilst for those potentially
+	/// able to execute arbitrary code, tagged with the 'needauth' option,
+	/// authorization is: always denied if lyxrc.use_converter_needauth_forbidden
+	/// is enabled; always allowed if the lyxrc.use_converter_needauth
+	/// is disabled; user is prompted otherwise
+	bool checkAuth(Converter const & conv, std::string const & doc_fname);
+
 private:
 	///
 	FormatList const
@@ -210,6 +225,8 @@ private:
 		  bool copy);
 	///
 	Graph G_;
+	/// set of document files authorized for external conversion
+	std::set<std::string> auth_files_;
 };
 
 /// The global instance.

src/LyXRC.cpp
@@ -189,6 +189,8 @@ LexerKeyword lyxrcTags[] = {
 	{ "\\thesaurusdir_path", LyXRC::RC_THESAURUSDIRPATH },
 	{ "\\ui_file", LyXRC::RC_UIFILE },
 	{ "\\use_converter_cache", LyXRC::RC_USE_CONVERTER_CACHE },
+	{ "\\use_converter_needauth", LyXRC::RC_USE_CONVERTER_NEEDAUTH },
+	{ "\\use_converter_needauth_forbidden", LyXRC::RC_USE_CONVERTER_NEEDAUTH_FORBIDDEN },
 	{ "\\use_lastfilepos", LyXRC::RC_USELASTFILEPOS },
 	{ "\\use_pixmap_cache", LyXRC::RC_USE_PIXMAP_CACHE },
 	{ "\\use_qimage", LyXRC::RC_USE_QIMAGE },
@@ -316,6 +318,8 @@ void LyXRC::setDefaults()
 	preview_hashed_labels  = false;
 	preview_scale_factor = 1.0;
 	use_converter_cache = true;
+	use_converter_needauth_forbidden = true;
+	use_converter_needauth = true;
 	use_system_colors = false;
 	use_tooltip = true;
 	use_pixmap_cache = false;
@@ -1112,6 +1116,12 @@ LyXRC::ReturnValues LyXRC::read(Lexer & lexrc, bool check_format)
 		case RC_USE_CONVERTER_CACHE:
 			lexrc >> use_converter_cache;
 			break;
+		case RC_USE_CONVERTER_NEEDAUTH_FORBIDDEN:
+			lexrc >> use_converter_needauth_forbidden;
+			break;
+		case RC_USE_CONVERTER_NEEDAUTH:
+			lexrc >> use_converter_needauth;
+			break;
 		case RC_CONVERTER_CACHE_MAXAGE:
 			lexrc >> converter_cache_maxage;
 			break;
@@ -1589,6 +1599,24 @@ void LyXRC::write(ostream & os, bool ignore_system_lyxrc, string const & name) c
 		    use_converter_cache != system_lyxrc.use_converter_cache) {
 			os << "\\use_converter_cache "
 			   << convert<string>(use_converter_cache) << '\n';
+		}
+		if (tag != RC_LAST)
+			break;
+
+	case RC_USE_CONVERTER_NEEDAUTH_FORBIDDEN:
+		if (ignore_system_lyxrc ||
+		    use_converter_needauth_forbidden != system_lyxrc.use_converter_needauth_forbidden) {
+			os << "\\use_converter_needauth_forbidden "
+			   << convert<string>(use_converter_needauth_forbidden) << '\n';
+		}
+		if (tag != RC_LAST)
+			break;
+
+	case RC_USE_CONVERTER_NEEDAUTH:
+		if (ignore_system_lyxrc ||
+		    use_converter_needauth != system_lyxrc.use_converter_needauth) {
+			os << "\\use_converter_needauth "
+			   << convert<string>(use_converter_needauth) << '\n';
 		}
 		if (tag != RC_LAST)
 			break;
@@ -2833,6 +2861,8 @@ void actOnUpdatedPrefs(LyXRC const & lyxrc_orig, LyXRC const & lyxrc_new)
 	case LyXRC::RC_USER_EMAIL:
 	case LyXRC::RC_USER_NAME:
 	case LyXRC::RC_USE_CONVERTER_CACHE:
+	case LyXRC::RC_USE_CONVERTER_NEEDAUTH_FORBIDDEN:
+	case LyXRC::RC_USE_CONVERTER_NEEDAUTH:
 	case LyXRC::RC_USE_SYSTEM_COLORS:
 	case LyXRC::RC_USE_TOOLTIP:
 	case LyXRC::RC_USE_PIXMAP_CACHE:
@@ -2923,6 +2953,14 @@ string const LyXRC::getDescription(LyXRCTags tag)
 		break;
 
 	case RC_CONVERTER:
+		break;
+
+	case RC_CONVERTER_NEEDAUTH_FORBIDDEN:
+		str = _("Forbid use of external converters with 'needauth' option to prevent undesired effects.");
+		break;
+
+	case RC_CONVERTER_NEEDAUTH:
+		str = _("Ask user before calling external converters with 'needauth' option to prevent undesired effects.");
 		break;
 
 	case RC_COPIER:

commit 31adae1f23c4a2767b4307d2c3e59972826b6805

Fix for crash/segfault introduced with [244de5d2/lyxgit], thanks Guillame.

src/graphics/GraphicsCacheItem.cpp
@@ -97,7 +97,7 @@ public:
 	/// The filename we refer too.
 	FileName const filename_;
 	/// The document filename this graphic item belongs to
-	FileName const & doc_file_;
+	FileName const doc_file_;
 	///
 	ActiveFileMonitorPtr monitor_;
 

src/graphics/GraphicsConverter.cpp
@@ -63,7 +63,7 @@ public:
 	SignalType finishedConversion;
 
 	///
-	FileName const & doc_fname_;
+	FileName const doc_fname_;
 	///
 	string script_command_;
 	///