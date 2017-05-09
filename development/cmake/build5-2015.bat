5120c25872ffa4e7f08e039fbf116ba0c111a2b6
Uwe Stöhr <uwestoehr@lyx.org>
2017-05-10 01:38:48 +0200

    build5-2015.bat: fix build script for current CMake releases
    - since CMake 3.5 the path to the cmake.exe is no longer by default written to Windows' PATH variable. Therefore this has to be done in the script.
    
    - Windows path can contain spaces (CMake's default installation folder does) so we must use quotes around the paths
    
    - set the CMAKE_PREFIX_PATH variable

--- development/cmake/build5-2015.bat
+++ development/cmake/build5-2015.bat
@@ -32,29 +32,39 @@ set CL=/MP
 REM Save path
 set CALLED_FROM=%CD%
 
-REM Add path to qmake here or set PATH correctly on your system.
-set PATH=C:\Qt\Qt5.6.2-MSVC2015\5.6\msvc2015\bin;%PATH%
-
 REM Edit pathes here or set the environment variables on your system.
-set GNUWIN32_DIR=D:\LyXGit\Master\lyx-windows-deps-msvc2015
-set LYX_SOURCE=D:\LyXGit\Master\
-set LYX_BUILD=D:\LyXGit\Master\compile-2015
+set CMAKE_PATH="C:\Program Files\CMake\bin"
+set QT_PATH="C:\Qt\Qt5.6.2-MSVC2015\5.6\msvc2015\bin"
+set GNUWIN32_DIR="D:\LyXGit\Master\lyx-windows-deps-msvc2015"
+set LYX_SOURCE="D:\LyXGit\Master"
+set LYX_BUILD="D:\LyXGit\Master\compile-2015"
 
 if [%LYX_SOURCE%]==[] (
 	set LYX_SOURCE=%~DP0\..\..
 )
-echo LyX source: "%LYX_SOURCE%"
+echo LyX source: %LYX_SOURCE%
 
 if [%LYX_BUILD%]==[] (
 	set LYX_BUILD=%CD%
 )
-echo LyX build : "%LYX_BUILD%"
+echo LyX build : %LYX_BUILD%
 
 if [%GNUWIN32_DIR%]==[] (
 	echo GNUWIN32_DIR not set.
 	echo Downloading win32 deps.
 	set DEPENDENCIES_DOWNLOAD="-DLYX_DEPENDENCIES_DOWNLOAD=1"
-) else set PATH="%GNUWIN32_DIR%\bin";%PATH%
+) else set PATH=%GNUWIN32_DIR%\bin;%PATH%
+echo GNUWIN32_DIR : %GNUWIN32_DIR%
+
+if [%QT_PATH%]==[] (
+	echo QT_PATH not set.
+) else set PATH=%QT_PATH%;%PATH%
+echo QT_PATH : %QT_PATH%
+
+if [%CMAKE_PATH%]==[] (
+	echo CMAKE_PATH not set.
+) else set PATH=%CMAKE_PATH%;%PATH%
+echo CMAKE_PATH : %CMAKE_PATH%
 
 if not exist %LYX_BUILD% (
 	echo creating "%LYX_BUILD%"
@@ -68,12 +78,12 @@ if not exist %LYX_BUILD% (
 cd "%LYX_BUILD%"
 
 REM start with a new cmake run
-::del CMakeCache.txt
+::del %LYX_BUILD%\CMakeCache.txt
 
 if "%1%" == "devel" (
 	REM Build solution to develop LyX
 	REM you can add the option "-GNinja" for a faster compilation
-	cmake %LYX_SOURCE% -G"Visual Studio 14 2015" -DLYX_USE_QT=QT5 -DLYX_ENABLE_EXPORT_TESTS=0 -DLYX_MERGE_FILES=0 -DLYX_NLS=1 -DLYX_INSTALL=0 -DLYX_RELEASE=0 -DLYX_CONSOLE=ON -DLYX_3RDPARTY_BUILD=1 %DEPENDENCIES_DOWNLOAD%
+	cmake %LYX_SOURCE% -G"Visual Studio 14 2015" -DCMAKE_PREFIX_PATH=%QT_PATH% -DLYX_USE_QT=QT5 -DLYX_ENABLE_EXPORT_TESTS=0 -DLYX_MERGE_FILES=0 -DLYX_NLS=1 -DLYX_INSTALL=0 -DLYX_RELEASE=0 -DLYX_CONSOLE=ON -DLYX_3RDPARTY_BUILD=1 %DEPENDENCIES_DOWNLOAD%
 	msbuild src\LyX.vcxproj /p:Configuration=Debug
 	msbuild src\tex2lyx\tex2lyx.vcxproj /p:Configuration=Debug
 )
@@ -81,7 +91,7 @@ if "%1%" == "devel" (
 if "%1%" == "install" (
 	REM Build solution to develop LyX
 	REM set -DLYX_MERGE_REBUILD and -DLYX_MERGE_FILES to 1 for a version released with an installer
-	cmake %LYX_SOURCE% -GNinja -G"Visual Studio 14 2015" -DLYX_USE_QT=QT5 -DLYX_MERGE_REBUILD=0 -DLYX_MERGE_FILES=0 -DLYX_NLS=1 -DLYX_INSTALL=1 -DLYX_RELEASE=1 -DLYX_CONSOLE=OFF %DEPENDENCIES_DOWNLOAD%
+	cmake %LYX_SOURCE% -GNinja -G"Visual Studio 14 2015" -DCMAKE_PREFIX_PATH=%QT_PATH% -DLYX_USE_QT=QT5 -DLYX_MERGE_REBUILD=0 -DLYX_MERGE_FILES=0 -DLYX_NLS=1 -DLYX_INSTALL=1 -DLYX_RELEASE=1 -DLYX_CONSOLE=OFF %DEPENDENCIES_DOWNLOAD%
 	msbuild INSTALL.vcxproj /p:Configuration=Release
 )
 
