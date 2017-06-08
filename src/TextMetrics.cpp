a33b633111f2c23a0f479bbc72f466b222fd8df4
Jean-Marc Lasgouttes <lasgouttes@lyx.org>
2017-06-08 16:46:49 +0200

    Revert "Fix bad cursor positioning when entering an inset"
    This is not ready yet.
    
    This reverts commit 7ca7873112314963c3a43987f32484749ec3af58.

--- src/TextMetrics.cpp
+++ src/TextMetrics.cpp
@@ -1379,9 +1379,7 @@ Inset * TextMetrics::editXY(Cursor & cur, int x, int y,
 
 	// Try to descend recursively inside the inset.
 	Inset * edited = inset->editXY(cur, x, y);
-	// FIXME: it is not clear that the test on position is needed
-	// Remove it if/when semantics of editXY is clarified
-	if (cur.text() == text_ && cur.pos() == it->pos) {
+	if (edited == inset && cur.pos() == it->pos) {
 		// non-editable inset, set cursor after the inset if x is
 		// nearer to that position (bug 9628)
 		bool bound = false; // is modified by getPosNearX
