4dea27c4c74ca7b29e0b574e5e3424b3acb2ec5e
Enrico Forestieri <forenr@lyx.org>
2017-06-20 01:14:55 +0200

    Fix bug #10546
    A labeled equation is also numbered.

--- src/mathed/MathParser.cpp
+++ src/mathed/MathParser.cpp
@@ -1799,6 +1799,7 @@ bool Parser::parse1(InsetMathGrid & grid, unsigned flags,
 			asArray(label, ar);
 			if (grid.asHullInset()) {
 				grid.asHullInset()->label(cellrow, label);
+				grid.asHullInset()->numbered(cellrow, true);
 			} else {
 				cell->push_back(createInsetMath(t.cs(), buf));
 				cell->push_back(MathAtom(new InsetMathBrace(ar)));
