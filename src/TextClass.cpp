5c545573507bbf55f78c294188537c6aff959bc0
Jean-Marc Lasgouttes <lasgouttes@lyx.org>
2017-08-31 12:03:54 +0200

    Honor trailing spaces in LabelString and friends
    Remove the trimming operation on LabelString, LabelStringAppendix,
    EndLabelString and LabelCounter. This trimming meant that quotes
    strings were not preserved, which is wrong.
    
    To preserve layouts, the Layout format has been updated to 64, so that
    layout2layout can remove extra spaces on existing strings, which
    ensures that behavior is unchanged for old layouts (courtesy of rgheck).
    
    It is now possible to re-add spaces in label strings where it makes
    sense.
    
    Fixes bug #10723.

--- src/TextClass.cpp
+++ src/TextClass.cpp
@@ -62,7 +62,7 @@ namespace lyx {
 // You should also run the development/tools/updatelayouts.py script,
 // to update the format of all of our layout files.
 //
-int const LAYOUT_FORMAT = 63; //spitz: new tags CiteFramework, MaxCiteNames, extended InsetCite syntax.
+int const LAYOUT_FORMAT = 64; //lasgouttes/rgheck: do not trim LabeString and friends.
 
 
 // Layout format for the current lyx file format. Controls which format is
