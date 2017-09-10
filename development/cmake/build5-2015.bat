78221d4cb891bcdc1ba7f1d50824f9cb57f2202a
Uwe Stöhr <uwestoehr@lyx.org>
2017-09-10 17:05:49 +0200

    Win installer: adapt a path

--- development/cmake/build5-2015.bat
+++ development/cmake/build5-2015.bat
@@ -36,8 +36,8 @@ REM Edit pathes here or set the environment variables on your system.
 set CMAKE_PATH="C:\Program Files\CMake\bin"
 set QT_PATH="C:\Qt\Qt5.6.2-MSVC-2015\5.6\msvc2015\bin"
 set GNUWIN32_DIR="D:\LyXGit\Master\lyx-windows-deps-msvc2015"
-set LYX_SOURCE="D:\LyXGit\Master"
-set LYX_BUILD="D:\LyXGit\Master\compile-2015"
+set LYX_SOURCE="D:\LyXGit\2.3.x"
+set LYX_BUILD="D:\LyXGit\2.3.x\compile-2015"
 
 if [%LYX_SOURCE%]==[] (
 	set LYX_SOURCE=%~DP0\..\..
