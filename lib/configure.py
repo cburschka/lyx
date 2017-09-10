6dd66a15a5806e2bda59f5a65920a15d2821c8e1
Juergen Spitzmueller <spitz@lyx.org>
2017-09-10 17:19:12 +0200

    configure.py: update lyxrc file format.

--- lib/configure.py
+++ lib/configure.py
@@ -1772,7 +1772,7 @@ if __name__ == '__main__':
     lyx_check_config = True
     lyx_kpsewhich = True
     outfile = 'lyxrc.defaults'
-    lyxrc_fileformat = 23
+    lyxrc_fileformat = 24
     rc_entries = ''
     lyx_keep_temps = False
     version_suffix = ''
