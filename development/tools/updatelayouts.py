69b03d254500be9c5e6c1be66b8a16f381f3a3d8
Juergen Spitzmueller <spitz@lyx.org>
2017-10-13 16:08:23 +0200

    updatelayouts.py: consider citeengines.

--- development/tools/updatelayouts.py
+++ development/tools/updatelayouts.py
@@ -21,15 +21,18 @@ from layout2layout import main as layout2layout
 def main(argv):
 
     toolsdir = os.path.dirname(argv[0])
-    layoutdir = os.path.join(toolsdir, '../../lib/layouts')
-    os.chdir(layoutdir)
-    for i in os.listdir("."):
-        (base, ext) = os.path.splitext(i)
-        if ext == ".old":
-            continue
-        args = ["layout2layout", i + ".old", i] 
-        shutil.copy(args[2], args[1])
-        layout2layout(args)
+    dirs = []
+    dirs.append(os.path.join(toolsdir, '../../lib/layouts'))
+    dirs.append(os.path.join(toolsdir, '../../lib/citeengines'))
+    for directory in dirs:
+        os.chdir(directory)
+        for i in os.listdir("."):
+            (base, ext) = os.path.splitext(i)
+            if ext == ".old":
+                continue
+            args = ["layout2layout", i + ".old", i] 
+            shutil.copy(args[2], args[1])
+            layout2layout(args)
 
     return 0
 
