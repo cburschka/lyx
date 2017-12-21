12f972754660b47be59668501258bf36c35b0454
Richard Heck <rgheck@lyx.org>
2017-12-21 18:46:10 -0500

    Dummy script for testing background export cancellation.

---
+++ development/tools/wait.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Dummy script for testing background cancellation processes.
+# To use: Create a "dummy" export format, and then use this
+# as the LyX --> dummy converter. It will just sit there,
+# allowing you to try to cancel.
+
+echo "Sleeping 30 seconds..."
+sleep 30;
+echo "Done.";
