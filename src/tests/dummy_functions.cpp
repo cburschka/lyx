9428ad56b9127a23af67808844ca4838f93563a5
Jean-Marc Lasgouttes <lasgouttes@lyx.org>
2018-01-01 23:54:13 +0100

    Fix 'make check'

--- src/tests/dummy_functions.cpp
+++ src/tests/dummy_functions.cpp
@@ -64,4 +64,12 @@ namespace lyx {
 		static frontend::FontMetrics dummy;
 		return dummy;
 	}
+
+	struct App {
+		bool cancel_export;
+	};
+
+	App app;
+	App * theApp() { return &app; }
+
 } // namespace lyx
