974553d8583954aa7741c4dc0ce83f9ca812b03b
Juergen Spitzmueller <spitz@lyx.org>
2018-01-11 13:43:35 +0100

    Fix Null-checking issue detected by Coverity.

--- src/Paragraph.cpp
+++ src/Paragraph.cpp
@@ -1429,14 +1429,12 @@ bool Paragraph::Private::latexSpecialT3(char_type const c, otexstream & os,
 
 void Paragraph::Private::validate(LaTeXFeatures & features) const
 {
-	Buffer const & buf = inset_owner_->buffer();
-	BufferParams const & bp = features.runparams().is_child
-		? buf.masterParams() : buf.params();
 	if (layout_->inpreamble && inset_owner_) {
 		// FIXME: Using a string stream here circumvents the encoding
 		// switching machinery of odocstream. Therefore the
 		// output is wrong if this paragraph contains content
 		// that needs to switch encoding.
+		Buffer const & buf = inset_owner_->buffer();
 		otexstringstream os;
 		os << layout_->preamble();
 		size_t const length = os.length();
@@ -1490,6 +1488,8 @@ void Paragraph::Private::validate(LaTeXFeatures & features) const
 	}
 
 	// then the contents
+	BufferParams const bp = features.runparams().is_child
+		? features.buffer().masterParams() : features.buffer().params();
 	for (pos_type i = 0; i < int(text_.size()) ; ++i) {
 		char_type c = text_[i];
 		if (c == 0x0022) {
