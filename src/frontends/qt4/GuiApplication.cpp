e63b891de56cfba8b2d8d3be597cd16c921c7859
Jean-Marc Lasgouttes <lasgouttes@lyx.org>
2018-01-24 14:05:57 +0100

    Better debug message

--- src/frontends/qt4/GuiApplication.cpp
+++ src/frontends/qt4/GuiApplication.cpp
@@ -1350,7 +1350,7 @@ static docstring makeDispatchMessage(docstring const & msg,
 			      || cmd.origin() == FuncRequest::COMMANDBUFFER);
 
 	if (cmd.action() == LFUN_SELF_INSERT || !verbose) {
-		LYXERR(Debug::ACTION, "dispatch msg is " << msg);
+		LYXERR(Debug::ACTION, "dispatch msg is `" << msg << "'");
 		return msg;
 	}
 
