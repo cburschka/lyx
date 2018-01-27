a9eee8e1c0ae1dc9af0126e0274bcbb5e05ed6a2
Juergen Spitzmueller <spitz@lyx.org>
2018-01-27 12:26:01 +0100

    Use proper localization chain for Box special width/height
    Fixes: #11000
    (cherry picked from commit e2160ec960de2f48e5b61dfab339902426d075f8)

--- src/frontends/qt4/GuiBox.cpp
+++ src/frontends/qt4/GuiBox.cpp
@@ -440,7 +440,7 @@ docstring GuiBox::dialogToParams() const
 		if (ids_spec_.contains(unit) && !isValidLength(fromqstr(value))) {
 			params.special = fromqstr(unit);
 			// Note: the unit is simply ignored in this case
-			params.width = Length(value.toDouble(), Length::IN);
+			params.width = Length(widgetToDouble(widthED), Length::IN);
 		} else {
 			params.special = "none";
 			// we must specify a valid length in this case
@@ -466,7 +466,7 @@ docstring GuiBox::dialogToParams() const
 		if (ids_spec_.contains(unit) && !isValidLength(fromqstr(value))) {
 			params.height_special = fromqstr(unit);
 			// Note: the unit is simply ignored in this case
-			params.height = Length(value.toDouble(), Length::IN);
+			params.height = Length(widgetToDouble(heightED), Length::IN);
 		} else {
 			params.height_special = "none";
 			params.height =
