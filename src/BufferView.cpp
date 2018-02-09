a3ad39c770e1512c6327f537dae971bfc738ebc4
Pavel Sanda <sanda@lyx.org>
2018-02-09 23:46:14 +0100

    Cosmetics per JMarc's suggestions.

--- src/BufferView.cpp
+++ src/BufferView.cpp
@@ -1709,11 +1709,12 @@ void BufferView::dispatch(FuncRequest const & cmd, DispatchResult & dr)
 		from = cur.selectionBegin();
 		to = cur.selectionEnd();
 
-		string newId = cmd.getArg(0);
-		bool fetchId=newId.empty(); //if we wait for groupId from first graphics inset
+		string const newId = cmd.getArg(0);
+		bool fetchId = newId.empty(); //if we wait for groupId from first graphics inset
 
 		InsetGraphicsParams grp_par;
-		InsetGraphics::string2params(graphics::getGroupParams(buffer_, newId), buffer_, grp_par);
+		if (!fetchId)
+			InsetGraphics::string2params(graphics::getGroupParams(buffer_, newId), buffer_, grp_par);
 
 		if (!from.nextInset())	//move to closest inset
 			from.forwardInset();
@@ -1722,15 +1723,12 @@ void BufferView::dispatch(FuncRequest const & cmd, DispatchResult & dr)
 			Inset * inset = from.nextInset();
 			if (!inset)
 				break;
-			if (inset->lyxCode() == GRAPHICS_CODE) {
-				InsetGraphics * ig = inset->asInsetGraphics();
-				if (!ig)
-					break;
+			InsetGraphics * ig = inset->asInsetGraphics();
+			if (ig) {
 				InsetGraphicsParams inspar = ig->getParams();
 				if (fetchId) {
 				        grp_par = inspar;
 					fetchId = false;
-
 				} else {
 					grp_par.filename = inspar.filename;
 					ig->setParams(grp_par);
