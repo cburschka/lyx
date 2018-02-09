3cc98a9bf6ed506d04b502863d6424d36cb778b5
Juergen Spitzmueller <spitz@lyx.org>
2018-02-09 13:23:50 +0100

    tex2lyx: normalize bib and bst paths
    Fixes: #7637

--- src/tex2lyx/text.cpp
+++ src/tex2lyx/text.cpp
@@ -4414,7 +4414,7 @@ void parse_text(Parser & p, ostream & os, unsigned flags, bool outer,
 				// \nocite{*} option
 				btprint.clear();
 			}
-			os << "bibfiles " << '"' << p.verbatim_item() << '"' << "\n";
+			os << "bibfiles " << '"' << normalize_filename(p.verbatim_item()) << '"' << "\n";
 			// Do we have addcontentsline?
 			if (contentslineContent == "\\refname") {
 				BibOpts = "bibtotoc";
@@ -4424,9 +4424,9 @@ void parse_text(Parser & p, ostream & os, unsigned flags, bool outer,
 			// Do we have a bibliographystyle set?
 			if (!bibliographystyle.empty()) {
 				if (BibOpts.empty())
-					BibOpts = bibliographystyle;
+					BibOpts = normalize_filename(bibliographystyle);
 				else
-					BibOpts = BibOpts + ',' + bibliographystyle;
+					BibOpts = BibOpts + ',' + normalize_filename(bibliographystyle);
 				// clear it because each bibtex entry has its style
 				// and we need an empty string to handle \phantomsection
 				bibliographystyle.clear();
