599b1222f7da0222cd1802af69abefb81145f566
Juergen Spitzmueller <spitz@lyx.org>
2018-03-09 12:15:19 +0100

    Reset buffer language only if we have something to reset.
    Fixes: #11063

--- src/CutAndPaste.cpp
+++ src/CutAndPaste.cpp
@@ -1239,13 +1239,13 @@ bool pasteClipboardText(Cursor & cur, ErrorList & errorList, bool asParagraphs,
 				Buffer buffer("", false);
 				buffer.setUnnamed(true);
 				available = buffer.importString(names[i], text, errorList);
-				// TeX2lyx (also used in the HTML chain) assumes English as document language
-				// if no language is explicitly set (as is the case here).
-				// We thus reset the temp buffer's language to the context language
-				buffer.changeLanguage(buffer.language(), cur.getFont().language());
 				if (available)
 					available = !buffer.paragraphs().empty();
 				if (available && !buffer.paragraphs()[0].empty()) {
+					// TeX2lyx (also used in the HTML chain) assumes English as document language
+					// if no language is explicitly set (as is the case here).
+					// We thus reset the temp buffer's language to the context language
+					buffer.changeLanguage(buffer.language(), cur.getFont().language());
 					cur.recordUndo();
 					pasteParagraphList(cur, buffer.paragraphs(),
 						buffer.params().documentClassPtr(), errorList);
