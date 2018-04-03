935442aaba08136fa5bd8c7407c46951d0e9d084
Richard Heck <rgheck@lyx.org>
2018-04-03 18:38:46 -0400

    Fix bug #11102.
    Ignore deleted material when generating a proposed label.
    
    (cherry picked from commit d9ebf6e2c75eca0f192fef0b7d822644e3b62e64)

--- src/Text.cpp
+++ src/Text.cpp
@@ -1977,7 +1977,7 @@ docstring Text::getPossibleLabel(Cursor const & cur) const
 	Layout const * layout = &(pars_[pit].layout());
 
 	docstring text;
-	docstring par_text = pars_[pit].asString();
+	docstring par_text = pars_[pit].asString(AS_STR_SKIPDELETE);
 
 	// The return string of math matrices might contain linebreaks
 	par_text = subst(par_text, '\n', '-');
