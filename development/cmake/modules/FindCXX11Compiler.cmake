1bf4d7b0fc2bc4c700e7cecc1e6e3c7faf4f03a5
Stephan Witt <switt@lyx.org>
2018-06-12 22:42:48 +0200

    Extend Clang compiler detection to cover Apple CLang
    CMake 3.0 and above recognize that Apple Clang is a different compiler than upstream Clang and so it gets a different CMAKE_CXX_COMPILER_ID.

--- development/cmake/modules/FindCXX11Compiler.cmake
+++ development/cmake/modules/FindCXX11Compiler.cmake
@@ -34,7 +34,7 @@
 INCLUDE(CheckCXXSourceCompiles)
 INCLUDE(FindPackageHandleStandardArgs)
 
-if (CMAKE_CXX_COMPILER_ID MATCHES "^[cC]lang$")
+if (CMAKE_CXX_COMPILER_ID MATCHES "^([cC]lang|AppleClang)$")
   set(CXX11_FLAG_CANDIDATES "--std=c++11 -Wno-deprecated-register")
 else()
   if (CYGWIN)
