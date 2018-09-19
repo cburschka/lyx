bebb077955994e872d1ff7eec18168163beb0c38
Juergen Spitzmueller <spitz@lyx.org>
2018-09-19 10:19:19 +0200

    Avoid infinite loop
    Fixes: #11295
    (cherry picked from commit 92bfa07a6205e6e2aae486510c148582e11d6017)

--- src/LyXAction.cpp
+++ src/LyXAction.cpp
@@ -1517,9 +1517,9 @@ void LyXAction::init()
  * \li Params: outer: If this is given, LyX will split the outermost environment in
                       the current nesting hierarchy.\n
                previous: If this is given, LyX will split the environment in the previous
-                      paragraph (is there is one).\n
-               before: If this is given, the new environment will be appended rather than
-                      prepended.
+                         paragraph (if there is one).\n
+               before:   If this is given, the new environment will be appended rather than
+                         prepended.
  * \li Origin: spitz, 23 Dec 2012
  * \endvar
  */
