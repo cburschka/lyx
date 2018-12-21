dff0c5729e6eec4c799f60171e4d0a377327ab10
Jean-Marc Lasgouttes <lasgouttes@lyx.org>
2018-12-21 15:36:55 +0100

    Make DEPM respect current cursor position.
    Spaces are now conserved when they surround current cursor. Examples:
      abc   |   def
    becomes
      abc | def
    after DEPM and
      abc |
    is kept as it is.
    
    Fixes ticket #11412.

--- src/Text2.cpp
+++ src/Text2.cpp
@@ -845,7 +845,7 @@ bool Text::deleteEmptyParagraphMechanism(Cursor & cur,
 		&& old.pos() == cur[depth].pos();
 
 	// If the chars around the old cursor were spaces, delete some of
-	// them , but only if the cursor has really moved.
+	// them, but only if the cursor has really moved.
 	if (!same_par_pos) {
 		// find range of spaces around cursors
 		int from = old.pos();
@@ -861,6 +861,9 @@ bool Text::deleteEmptyParagraphMechanism(Cursor & cur,
 
 		// If we are not at the extremity of the paragraph, keep one space
 		if (from != to && from > 0 && to < oldpar.size())
+			++from;
+
+		if (same_par && cur.pos() > from && cur.pos() < to)
 			++from;
 
 		// Remove spaces and adapt cursor.
@@ -879,7 +882,7 @@ bool Text::deleteEmptyParagraphMechanism(Cursor & cur,
 		}
 	}
 
-	// only do our magic if we changed paragraph
+	// only do our other magic if we changed paragraph
 	if (same_par)
 		return false;
 
