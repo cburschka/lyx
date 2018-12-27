35f0117b5fbeee20e49e0b1e46f1ac938ae84afb
Juergen Spitzmueller <spitz@lyx.org>
2018-12-27 15:29:23 +0100

    LFUN_TEXTSTYLE_UPDATE: Do not force any unspecified settings.
    Fixes: #11111 (yay!)
    (cherry picked from commit 2f9876a4102add659f42d0055ac6a6c23c764736)

--- src/Text3.cpp
+++ src/Text3.cpp
@@ -2368,7 +2368,7 @@ void Text::dispatch(Cursor & cur, FuncRequest & cmd)
 	// Set the freefont using the contents of \param data dispatched from
 	// the frontends and apply it at the current cursor location.
 	case LFUN_TEXTSTYLE_UPDATE: {
-		Font font;
+		Font font(ignore_font, ignore_language);
 		bool toggle;
 		if (font.fromString(to_utf8(cmd.argument()), toggle)) {
 			freefont = font;
