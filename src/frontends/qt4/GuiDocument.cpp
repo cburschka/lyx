cburschka/lyx

commit 27ba6f282dc36b5cb500bbd51c2f87fd697d0626

Rename legacy input encoding settings.
Format incremented to 573

src/frontends/qt4/GuiDocument.cpp
@@ -3201,11 +3201,11 @@ void GuiDocument::applyView()
 
 	// language & quotes
 	if (langModule->defaultencodingRB->isChecked()) {
-		bp_.inputenc = "auto";
+		bp_.inputenc = "auto-legacy";
 	} else {
 		int i = langModule->encodingCO->currentIndex();
 		if (i == 0)
-			bp_.inputenc = "default";
+			bp_.inputenc = "auto-legacy-plain";
 		else {
 			QString const enc_gui =
 				langModule->encodingCO->currentText();
@@ -3222,8 +3222,8 @@ void GuiDocument::applyView()
 			}
 			if (!found) {
 				// should not happen
-				lyxerr << "GuiDocument::apply: Unknown encoding! Resetting to default" << endl;
-				bp_.inputenc = "default";
+				lyxerr << "GuiDocument::apply: Unknown encoding! Resetting to utf8" << endl;
+				bp_.inputenc = "utf8";
 			}
 		}
 	}
@@ -3719,9 +3719,9 @@ void GuiDocument::paramsToDialog()
 	langModule->dynamicQuotesCB->setChecked(bp_.dynamic_quotes);
 
 	bool default_enc = true;
-	if (bp_.inputenc != "auto") {
+	if (bp_.inputenc != "auto-legacy") {
 		default_enc = false;
-		if (bp_.inputenc == "default") {
+		if (bp_.inputenc == "auto-legacy-plain") {
 			langModule->encodingCO->setCurrentIndex(0);
 		} else {
 			string enc_gui;
@@ -4087,7 +4087,7 @@ void GuiDocument::paramsToDialog()
 	if (nn >= 0)
 		fontModule->fontsDefaultCO->setCurrentIndex(nn);
 
-	if (bp_.fontenc == "auto" || bp_.fontenc == "default") {
+	if (bp_.fontenc == "auto-legacy" || bp_.fontenc == "auto-legacy-plain") {
 		fontModule->fontencCO->setCurrentIndex(
 			fontModule->fontencCO->findData(toqstr(bp_.fontenc)));
 		fontModule->fontencLE->setEnabled(false);

development/FORMAT
@@ -7,6 +7,9 @@ changes happened in particular if possible. A good example would be
 
 -----------------------
 
+2019-04-16  Günter Milde <milde@lyx.org>
+        * format incremented to 573: Rename legacy input encoding settings.
+
 2019-04-07  Kornel Benko <kornel@lyx.org>
 	* format incremented to 572: Extended Noto fonts.
 

lib/doc/Development.lyx
@@ -1,5 +1,5 @@
-#LyX 2.3 created this file. For more info see http://www.lyx.org/
-\lyxformat 544
+#LyX 2.4 created this file. For more info see https://www.lyx.org/
+\lyxformat 572
 \begin_document
 \begin_header
 \save_transient_properties true
@@ -14,7 +14,7 @@ logicalmkup
 \language english
 \language_package default
 \inputencoding auto
-\fontencoding global
+\fontencoding auto
 \font_roman "lmodern" "default"
 \font_sans "lmss" "default"
 \font_typewriter "lmtt" "default"
@@ -87,6 +87,7 @@ logicalmkup
 \papercolumns 1
 \papersides 2
 \paperpagestyle headings
+\tablestyle default
 \tracking_changes false
 \output_changes false
 \html_math_output 0
@@ -479,7 +480,7 @@ Add an entry to both format lists (for conversion and reversion) in
 status collapsed
 
 \begin_layout Plain Layout
-lib/lyx2lyx/lyx_2_3.py
+lib/lyx2lyx/lyx_2_4.py
 \end_layout
 
 \end_inset
@@ -582,8 +583,11 @@ src/tex2lyx/TODO.txt
 It would be nice if you could create a .lyx test file which contains instances
  of all changed or added features.
  This could then be used to test lyx2lyx and tex2lyx.
- Unfortunately, it has not yet been decided how to collect such examples,
- so please ask on the development list if you want to create one.
+ Test samples are collected under the corresponding subdirectories of 
+\family typewriter
+/autotests
+\family default
+.
 \end_layout
 
 \begin_layout Enumerate

lib/lyx2lyx/lyx_2_4.py
@@ -283,6 +283,22 @@ def revert_fonts(document, fm, fontmap):
 ###
 ###############################################################################
 
+def convert_inputencoding_namechange(document):
+    " Rename inputencoding settings. "
+    i = find_token(document.header, "\\inputencoding", 0)
+    if i == -1:
+        return
+    s = document.header[i].replace("auto", "auto-legacy")
+    document.header[i] = s.replace("default", "auto-legacy-plain")
+
+def revert_inputencoding_namechange(document):
+    " Rename inputencoding settings. "
+    i = find_token(document.header, "\\inputencoding", 0)
+    if i == -1:
+        return
+    s = document.header[i].replace("auto-legacy-plain", "default")
+    document.header[i] = s.replace("auto-legacy", "auto")
+
 def convert_notoFonts(document):
     " Handle Noto fonts definition to LaTeX "
 
@@ -1635,10 +1651,11 @@ convert = [
            [569, []],
            [570, []],
            [571, []],
-           [572, [convert_notoFonts]]  # Added options thin, light, extralight for Noto
+           [572, [convert_notoFonts]],  # Added options thin, light, extralight for Noto
+           [573, [convert_inputencoding_namechange]],
           ]
 
-revert =  [
+revert =  [[572, [revert_inputencoding_namechange]],
            [571, [revert_notoFonts]],
            [570, [revert_cmidruletrimming]],
            [569, [revert_bibfileencodings]],

src/Buffer.h
@@ -302,8 +302,8 @@ public:
 			   OutputParams const &,
 			   OutputWhat output = FullSource) const;
 	/** Export the buffer to LaTeX.
-	    If \p os is a file stream, and params().inputenc is "auto" or
-	    "default", and the buffer contains text in different languages
+	    If \p os is a file stream, and params().inputenc is "auto-legacy" or
+	    "auto-legacy-plain", and the buffer contains text in different languages
 	    with more than one encoding, then this method will change the
 	    encoding associated to \p os. Therefore you must not call this
 	    method with a string stream if the output is supposed to go to a

src/BufferParams.cpp
@@ -436,7 +436,7 @@ BufferParams::BufferParams()
 	fonts_sans_scale[1] = 100;
 	fonts_typewriter_scale[0] = 100;
 	fonts_typewriter_scale[1] = 100;
-	inputenc = "auto";
+	inputenc = "auto-legacy";
 	lang_package = "default";
 	graphics_driver = "default";
 	default_output_format = "default";
@@ -1786,6 +1786,8 @@ bool BufferParams::writeLaTeX(otexstream & os, LaTeXFeatures & features,
 		// get main font encodings
 		vector<string> fontencs = font_encodings();
 		// get font encodings of secondary languages
+		// FIXME: some languages (hebrew, ...) assume a standard font encoding as last
+		// option (for text in other languages).
 		features.getFontEncodings(fontencs);
 		if (!fontencs.empty()) {
 			os << "\\usepackage["
@@ -1799,7 +1801,9 @@ bool BufferParams::writeLaTeX(otexstream & os, LaTeXFeatures & features,
 		os << "\\usepackage{textcomp}\n";
 	if (features.mustProvide("pmboxdraw"))
 		os << "\\usepackage{pmboxdraw}\n";
-
+	
+	// FIXME: With Thai as document or secondary language, we must load babel
+	// before inputenc (see lib/languages).
 	// handle inputenc etc.
 	writeEncodingPreamble(os, features);
 
@@ -3245,7 +3249,7 @@ void BufferParams::writeEncodingPreamble(otexstream & os,
 	if (useNonTeXFonts)
 		return;
 
-	if (inputenc == "auto") {
+	if (inputenc == "auto-legacy") {
 		string const doc_encoding =
 			language->encoding()->latexName();
 		Encoding::Package const package =
@@ -3285,7 +3289,7 @@ void BufferParams::writeEncodingPreamble(otexstream & os,
 			else
 				os << "]{inputenc}\n";
 		}
-	} else if (inputenc != "default") {
+	} else if (inputenc != "auto-legacy-plain") {
 		switch (encoding().package()) {
 		case Encoding::none:
 		case Encoding::CJK:
@@ -3315,7 +3319,7 @@ void BufferParams::writeEncodingPreamble(otexstream & os,
 			break;
 		}
 	}
-	if (inputenc == "default" || features.isRequired("japanese")) {
+	if (inputenc == "auto-legacy-plain" || features.isRequired("japanese")) {
 		// don't default to [utf8]{inputenc} with TeXLive >= 18
 		os << "\\ifdefined\\UseRawInputEncoding\n";
 		os << "  \\UseRawInputEncoding\\fi\n";
@@ -3433,7 +3437,7 @@ Encoding const & BufferParams::encoding() const
 	// Main encoding for LaTeX output.
 	if (useNonTeXFonts)
 		return *(encodings.fromLyXName("utf8-plain"));
-	if (inputenc == "auto" || inputenc == "default")
+	if (inputenc == "auto-legacy" || inputenc == "auto-legacy-plain")
 		return *language->encoding();
 	if (inputenc == "utf8" && language->lang() == "japanese")
 		return *(encodings.fromLyXName("utf8-platex"));

src/DocIterator.cpp
@@ -745,7 +745,7 @@ Encoding const * DocIterator::getEncoding() const
 										  text.outerFont(sl.pit())).language();
 	// If we have a custom encoding for the buffer, we don't switch
 	// encodings (see output_latex::switchEncoding())
-	bool const customenc = bp.inputenc != "auto" && bp.inputenc != "default";
+	bool const customenc = bp.inputenc != "auto-legacy" && bp.inputenc != "auto-legacy-plain";
 	Encoding const * enc = customenc ? &bp.encoding() : lang->encoding();
 
 	// Some insets force specific encodings sometimes (e.g., listings in

src/PDFOptions.cpp
@@ -101,9 +101,12 @@ void PDFOptions::writeLaTeX(OutputParams & runparams, otexstream & os,
 	if (!runparams.hyperref_driver.empty())
 		opt += runparams.hyperref_driver + ",";
 
-	// since LyX uses unicode, also set the PDF strings to unicode strings with the
-	// hyperref option "unicode"
-	opt += "unicode=true,";
+	// Since LyX uses unicode, also set the PDF strings to unicode strings
+	// with the hyperref option "unicode".
+	// (With Xe/LuaTeX and pTeX, unicode=true is the default
+	// and the option leads to errors with some Japanese document classes)
+	if (!runparams.isFullUnicode() && !runparams.use_japanese)
+		opt += "unicode=true,";
 
 	// only use the hyperref settings if hyperref is enabled by the user
 	// see bug #7052

commit 337e3ff9a6b69818417fa62b39dd44fa5379c0d4

Fix Font selector GUI (font encoding) broken in 27ba6f282dc3.

src/frontends/qt4/GuiDocument.cpp
@@ -4096,7 +4096,7 @@ void GuiDocument::paramsToDialog()
 	if (nn >= 0)
 		fontModule->fontsDefaultCO->setCurrentIndex(nn);
 
-	if (bp_.fontenc == "auto-legacy" || bp_.fontenc == "auto-legacy-plain") {
+	if (bp_.fontenc == "auto" || bp_.fontenc == "default") {
 		fontModule->fontencCO->setCurrentIndex(
 			fontModule->fontencCO->findData(toqstr(bp_.fontenc)));
 		fontModule->fontencLE->setEnabled(false);