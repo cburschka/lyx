49255788ea155a8b7a0dab8e8e69ce1219c11084
Jean-Marc Lasgouttes <lasgouttes@lyx.org>
2019-05-23 11:30:35 +0200

    Always paint text decoration when painting inset.
    Fixes bug #11402.
    
    (cherry picked from commit 621c908f85650cc086b1d155062885eba016d1fa)

--- src/RowPainter.cpp
+++ src/RowPainter.cpp
@@ -118,6 +118,7 @@ void RowPainter::paintInset(Row::Element const & e) const
 	e.inset->drawBackground(pi_, x1, yo_);
 	e.inset->drawSelection(pi_, x1, yo_);
 	e.inset->draw(pi_, x1, yo_);
+	paintTextDecoration(e);
 
 	// Restore full_repaint status.
 	pi_.full_repaint = pi_full_repaint;
@@ -585,7 +586,6 @@ void RowPainter::paintText()
 
 		case Row::INSET:
 			paintInset(e);
-			paintTextDecoration(e);
 			break;
 
 		case Row::SPACE:
