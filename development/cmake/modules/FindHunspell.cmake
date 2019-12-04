9d0548e8773f58d412368826eb5c0602b5c33403
Kornel Benko <kornel@lyx.org>
2019-12-04 20:23:27 +0100

    Cmake build: Try to find hunspell.hxx for linunx _and_ for OSX

--- development/cmake/modules/FindHunspell.cmake
+++ development/cmake/modules/FindHunspell.cmake
@@ -5,7 +5,13 @@ else()
   find_library(HUNSPELL_LIBRARY NAMES "hunspell" "hunspell-1.2" PATHS "/usr/local/lib" ${SYSTEM_LIB_DIRS} "/usr/lib64")
 endif()
 
-FIND_PATH(HUNSPELL_INCLUDE_DIR "hunspell/hunspell.hxx")
+FIND_PATH(HUNSPELL_INCLUDE_DIR "hunspell.hxx")
+if (NOT HUNSPELL_INCLUDE_DIR)
+  FIND_PATH(HUNSPELL_INCLUDE_DIR "hunspell/hunspell.hxx")
+  if (HUNSPELL_INCLUDE_DIR)
+    set(HUNSPELL_INCLUDE_DIR "${HUNSPELL_INCLUDE_DIR}/hunspell")
+  endif()
+endif()
 
 # handle the QUIETLY and REQUIRED arguments and
 # set HUNSPELL_FOUND to TRUE if all listed variables are TRUE
