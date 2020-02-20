4e4bba441d7f98825a88b416dd63ccba83ee7f0f
Pavel Sanda <sanda@lyx.org>
2020-02-20 09:24:34 +0100

    gcc 4.7 leftover

--- src/support/unicode.h
+++ src/support/unicode.h
@@ -62,8 +62,6 @@ public:
 		char * out_buffer, size_t max_out_size);
 	/// target encoding
 	std::string to() const { return tocode_; }
-	// required by g++ 4.7
-	IconvProcessor(IconvProcessor &&) = default;
 };
 
 /// Get the global IconvProcessor instance of the current thread for
