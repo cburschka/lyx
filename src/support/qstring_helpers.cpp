3f364864d42227e2416302457e10e18debc74a3b
Juergen Spitzmueller <spitz@lyx.org>
2020-03-22 10:10:36 +0100

    Fix indentation

--- src/support/qstring_helpers.cpp
+++ src/support/qstring_helpers.cpp
@@ -71,15 +71,15 @@ std::string fromqstr(QString const & str)
 
 QString charFilterRegExp(QString const & filter)
 {
-    QString re = ".*";
-    for (int i = 0; i < filter.length(); ++i) {
-        QChar c = filter[i];
-        if (c.isLower())
-            re +=  "["+ QRegExp::escape(c) + QRegExp::escape(c.toUpper()) + "]";
-        else
-            re +=  QRegExp::escape(c);
-    }
-    return re;
+	QString re = ".*";
+	for (int i = 0; i < filter.length(); ++i) {
+		QChar c = filter[i];
+		if (c.isLower())
+			re +=  "["+ QRegExp::escape(c) + QRegExp::escape(c.toUpper()) + "]";
+		else
+			re +=  QRegExp::escape(c);
+	}
+	return re;
 }
 
 QString charFilterRegExpC(QString const & filter)
