9e71bdfd324d061b1b1c050867d43ed5938807af
Juergen Spitzmueller <spitz@lyx.org>
2020-04-18 08:01:39 +0200

    fix comment

--- src/insets/InsetInclude.cpp
+++ src/insets/InsetInclude.cpp
@@ -376,7 +376,7 @@ void InsetInclude::setParams(InsetCommandParams const & p)
 	// invalidate the cache
 	child_buffer_ = 0;
 
-	// reset in order to prevent loading new file
+	// reset in order to allow loading new file
 	failedtoload_ = false;
 
 	InsetCommand::setParams(p);
