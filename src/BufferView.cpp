7761e1317eaa2c880446f05b707b03067d665681
Jean-Marc Lasgouttes <lasgouttes@lyx.org>
2020-05-23 16:47:48 +0200

    Fix first draw of auto-open inset
    note-next and reference-next should trigger an update so that metrics
    are updated when the cursor ends up in an auto-open inset.
    
    Fixes bug #11870.

--- src/BufferView.cpp
+++ src/BufferView.cpp
@@ -1531,6 +1531,10 @@ void BufferView::dispatch(FuncRequest const & cmd, DispatchResult & dr)
 
 	case LFUN_NOTE_NEXT:
 		gotoInset(this, NOTE_CODE, false);
+		// FIXME: if SinglePar is changed to act on the inner
+		// paragraph, this will not be OK anymore. The update is
+		// useful for auto-open collapsible insets.
+		dr.screenUpdate(Update::SinglePar | Update::FitCursor);
 		break;
 
 	case LFUN_REFERENCE_NEXT: {
@@ -1538,6 +1542,10 @@ void BufferView::dispatch(FuncRequest const & cmd, DispatchResult & dr)
 		tmp.push_back(LABEL_CODE);
 		tmp.push_back(REF_CODE);
 		gotoInset(this, tmp, true);
+		// FIXME: if SinglePar is changed to act on the inner
+		// paragraph, this will not be OK anymore. The update is
+		// useful for auto-open collapsible insets.
+		dr.screenUpdate(Update::SinglePar | Update::FitCursor);
 		break;
 	}
 
