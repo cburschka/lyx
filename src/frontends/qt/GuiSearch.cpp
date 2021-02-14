2baa3a46a6ad3576fc94fa0b915adc30ba2bbdff
Juergen Spitzmueller <spitz@lyx.org>
2021-02-14 17:18:00 +0100

    Transform simple search dialog to dock widget (#2625)
    Also solves #8054

--- src/frontends/qt/GuiSearch.cpp
+++ src/frontends/qt/GuiSearch.cpp
@@ -17,6 +17,7 @@
 #include "lyxfind.h"
 #include "qt_helpers.h"
 #include "FuncRequest.h"
+#include "LyX.h"
 #include "BufferView.h"
 #include "Buffer.h"
 #include "Cursor.h"
@@ -25,11 +26,14 @@
 #include "GuiKeySymbol.h"
 #include "GuiView.h"
 
+#include "support/debug.h"
 #include "support/gettext.h"
 #include "frontends/alert.h"
 
 #include <QLineEdit>
+#include <QSettings>
 #include <QShowEvent>
+#include "QSizePolicy"
 
 using namespace std;
 
@@ -48,8 +52,8 @@ static void uniqueInsert(QComboBox * box, QString const & text)
 }
 
 
-GuiSearch::GuiSearch(GuiView & lv)
-	: GuiDialog(lv, "findreplace", qt_("Find and Replace"))
+GuiSearchWidget::GuiSearchWidget(QWidget * parent)
+	:	QWidget(parent)
 {
 	setupUi(this);
 
@@ -57,33 +61,38 @@ GuiSearch::GuiSearch(GuiView & lv)
 	setFixedHeight(sizeHint().height());
 
 	// align items in grid on top
-	mainGridLayout->setAlignment(Qt::AlignTop);
+	gridLayout->setAlignment(Qt::AlignTop);
 
-	connect(buttonBox, SIGNAL(clicked(QAbstractButton *)),
-		this, SLOT(slotButtonBox(QAbstractButton *)));
 	connect(findPB, SIGNAL(clicked()), this, SLOT(findClicked()));
+	connect(findPrevPB, SIGNAL(clicked()), this, SLOT(findPrevClicked()));
+	connect(minimizePB, SIGNAL(clicked()), this, SLOT(minimizeClicked()));
 	connect(replacePB, SIGNAL(clicked()), this, SLOT(replaceClicked()));
+	connect(replacePrevPB, SIGNAL(clicked()), this, SLOT(replacePrevClicked()));
 	connect(replaceallPB, SIGNAL(clicked()), this, SLOT(replaceallClicked()));
 	connect(findCO, SIGNAL(editTextChanged(QString)),
 		this, SLOT(findChanged()));
 
 	setFocusProxy(findCO);
 
-	bc().setPolicy(ButtonPolicy::NoRepeatedApplyReadOnlyPolicy);
-	bc().setCancel(buttonBox->button(QDialogButtonBox::Close));
-
 	findCO->setCompleter(0);
 	replaceCO->setCompleter(0);
 
 	replacePB->setEnabled(false);
+	replacePrevPB->setEnabled(false);
 	replaceallPB->setEnabled(false);
 }
 
 
-void GuiSearch::keyPressEvent(QKeyEvent * ev)
+void GuiSearchWidget::keyPressEvent(QKeyEvent * ev)
 {
 	KeySymbol sym;
 	setKeySymbol(&sym, ev);
+
+	// catch Return and Shift-Return
+	if (ev->key() == Qt::Key_Return) {
+		findClicked(ev->modifiers() == Qt::ShiftModifier);
+		return;
+	}
 
 	// we catch the key sequences for forward and backwards search
 	if (sym.isOK()) {
@@ -98,53 +107,96 @@ void GuiSearch::keyPressEvent(QKeyEvent * ev)
 			findClicked(true);
 			return;
 		}
+		if (fr == FuncRequest(LFUN_DIALOG_TOGGLE, "findreplace")) {
+			dispatch(fr);
+			return;
+		}
 	}
-	QDialog::keyPressEvent(ev);
+	QWidget::keyPressEvent(ev);
 }
 
 
-void GuiSearch::showEvent(QShowEvent * e)
+void GuiSearchWidget::minimizeClicked(bool const toggle)
+{
+	if (toggle)
+		minimized_ = !minimized_;
+
+	replaceLA->setHidden(minimized_);
+	replaceCO->setHidden(minimized_);
+	replacePB->setHidden(minimized_);
+	replacePrevPB->setHidden(minimized_);
+	replaceallPB->setHidden(minimized_);
+	wordsCB->setHidden(minimized_);
+	caseCB->setHidden(minimized_);
+	if (minimized_) {
+		minimizePB->setText(qt_("Ex&pand"));
+		minimizePB->setToolTip("Show replace and option widgets");
+	} else {
+		minimizePB->setText(qt_("&Minimize"));
+		minimizePB->setToolTip("Hide replace and option widgets");
+	}
+
+	Q_EMIT needSizeUpdate();
+	Q_EMIT needTitleBarUpdate();
+}
+
+
+void GuiSearchWidget::showEvent(QShowEvent * e)
 {
 	findChanged();
 	findPB->setFocus();
 	findCO->lineEdit()->selectAll();
-	GuiDialog::showEvent(e);
+	QWidget::showEvent(e);
 }
 
 
-void GuiSearch::findChanged()
+void GuiSearchWidget::findChanged()
 {
 	findPB->setEnabled(!findCO->currentText().isEmpty());
-	bool const replace = !findCO->currentText().isEmpty() && !isBufferReadonly();
+	findPrevPB->setEnabled(!findCO->currentText().isEmpty());
+	bool const replace = !findCO->currentText().isEmpty()
+			&& bv_ && !bv_->buffer().isReadonly();
 	replacePB->setEnabled(replace);
+	replacePrevPB->setEnabled(replace);
 	replaceallPB->setEnabled(replace);
 	replaceLA->setEnabled(replace);
 	replaceCO->setEnabled(replace);
 }
 
 
-void GuiSearch::findClicked(bool const backwards)
+void GuiSearchWidget::findClicked(bool const backwards)
 {
 	docstring const needle = qstring_to_ucs4(findCO->currentText());
-	find(needle, caseCB->isChecked(), wordsCB->isChecked(),
-		(!backwards && !backwardsCB->isChecked()));
+	find(needle, caseCB->isChecked(), wordsCB->isChecked(), !backwards);
 	uniqueInsert(findCO, findCO->currentText());
 	findCO->lineEdit()->selectAll();
 }
 
 
-void GuiSearch::replaceClicked()
+void GuiSearchWidget::findPrevClicked()
+{
+	findClicked(true);
+}
+
+
+void GuiSearchWidget::replaceClicked(bool const backwards)
 {
 	docstring const needle = qstring_to_ucs4(findCO->currentText());
 	docstring const repl = qstring_to_ucs4(replaceCO->currentText());
 	replace(needle, repl, caseCB->isChecked(), wordsCB->isChecked(),
-		!backwardsCB->isChecked(), false);
+		!backwards, false);
 	uniqueInsert(findCO, findCO->currentText());
 	uniqueInsert(replaceCO, replaceCO->currentText());
 }
 
 
-void GuiSearch::replaceallClicked()
+void GuiSearchWidget::replacePrevClicked()
+{
+	replaceClicked(true);
+}
+
+
+void GuiSearchWidget::replaceallClicked()
 {
 	replace(qstring_to_ucs4(findCO->currentText()),
 		qstring_to_ucs4(replaceCO->currentText()),
@@ -154,7 +206,7 @@ void GuiSearch::replaceallClicked()
 }
 
 
-void GuiSearch::find(docstring const & search, bool casesensitive,
+void GuiSearchWidget::find(docstring const & search, bool casesensitive,
 			 bool matchword, bool forward)
 {
 	docstring const sdata =
@@ -163,7 +215,7 @@ void GuiSearch::find(docstring const & search, bool casesensitive,
 }
 
 
-void GuiSearch::replace(docstring const & search, docstring const & replace,
+void GuiSearchWidget::replace(docstring const & search, docstring const & replace,
 			    bool casesensitive, bool matchword,
 			    bool forward, bool all)
 {
@@ -171,6 +223,91 @@ void GuiSearch::replace(docstring const & search, docstring const & replace,
 		replace2string(replace, search, casesensitive,
 				     matchword, all, forward);
 	dispatch(FuncRequest(LFUN_WORD_REPLACE, sdata));
+}
+
+void GuiSearchWidget::saveSession(QSettings & settings, QString const & session_key) const
+{
+	settings.setValue(session_key + "/casesensitive", caseCB->isChecked());
+	settings.setValue(session_key + "/words", wordsCB->isChecked());
+	settings.setValue(session_key + "/minimized", minimized_);
+}
+
+
+void GuiSearchWidget::restoreSession(QString const & session_key)
+{
+	QSettings settings;
+	caseCB->setChecked(settings.value(session_key + "/casesensitive", false).toBool());
+	wordsCB->setChecked(settings.value(session_key + "/words", false).toBool());
+	minimized_ = settings.value(session_key + "/minimized", false).toBool();
+	// initialize hidings
+	minimizeClicked(false);
+}
+
+
+GuiSearch::GuiSearch(GuiView & parent, Qt::DockWidgetArea area, Qt::WindowFlags flags)
+	: DockView(parent, "findreplace", qt_("Search and Replace"), area, flags),
+	  widget_(new GuiSearchWidget(this))
+{
+	setWidget(widget_);
+	widget_->setBufferView(bufferview());
+	setFocusProxy(widget_->findCO);
+
+	connect(widget_, SIGNAL(needTitleBarUpdate()), this, SLOT(updateTitle()));
+	connect(widget_, SIGNAL(needSizeUpdate()), this, SLOT(updateSize()));
+}
+
+void GuiSearch::onBufferViewChanged()
+{
+	widget_->setEnabled((bool)bufferview());
+	widget_->setBufferView(bufferview());
+}
+
+
+void GuiSearch::updateView()
+{
+	updateTitle();
+	updateSize();
+}
+
+
+void GuiSearch::saveSession(QSettings & settings) const
+{
+	Dialog::saveSession(settings);
+	widget_->saveSession(settings, sessionKey());
+}
+
+
+void GuiSearch::restoreSession()
+{
+	DockView::restoreSession();
+	widget_->restoreSession(sessionKey());
+}
+
+
+void GuiSearch::updateTitle()
+{
+	if (widget_->isMinimized()) {
+		// remove title bar
+		setTitleBarWidget(new QWidget());
+		titleBarWidget()->hide();
+	} else
+		// restore title bar
+		setTitleBarWidget(nullptr);
+}
+
+
+void GuiSearch::updateSize()
+{
+	widget_->setFixedHeight(widget_->sizeHint().height());
+	if (widget_->isMinimized())
+		// FIXME still a bit too tall
+		setFixedHeight(widget_->sizeHint().height());
+	else {
+		// undo setFixedHeight
+		setMaximumHeight(QWIDGETSIZE_MAX);
+		setMinimumHeight(0);
+	}
+	update();
 }
 
 
--- src/frontends/qt/GuiSearch.h
+++ src/frontends/qt/GuiSearch.h
@@ -14,43 +14,98 @@
 #define GUISEARCH_H
 
 #include "GuiDialog.h"
+#include "DockView.h"
+
+#include <QDockWidget>
+
 #include "ui_SearchUi.h"
 
 namespace lyx {
 namespace frontend {
 
-class GuiSearch : public GuiDialog, public Ui::SearchUi
+class GuiSearch;
+
+class GuiSearchWidget : public QWidget, public Ui::SearchUi
 {
 	Q_OBJECT
 
 public:
-	GuiSearch(GuiView & lv);
+	GuiSearchWidget(QWidget * parent);
+	///
+	void saveSession(QSettings & settings, QString const & session_key) const;
+	///
+	void restoreSession(QString const & session_key);
+	///
+	void setBufferView(BufferView const * bv) { bv_ = bv; }
+	///
+	bool isMinimized() { return minimized_; }
 
 private Q_SLOTS:
 	void findChanged();
 	void findClicked(bool const backwards = false);
-	void replaceClicked();
+	void findPrevClicked();
+	void replaceClicked(bool const backwards = false);
+	void replacePrevClicked();
 	void replaceallClicked();
+	void minimizeClicked(bool const toggle = true);
+Q_SIGNALS:
+	void needTitleBarUpdate() const;
+	void needSizeUpdate() const;
 
 private:
 	///
 	void keyPressEvent(QKeyEvent * e) override;
 	///
 	void showEvent(QShowEvent * e) override;
-	///
-	bool initialiseParams(std::string const &) override { return true; }
-	void clearParams() override {}
-	void dispatchParams() override {}
-	bool isBufferDependent() const override { return true; }
-
 	/// Searches occurrence of string
 	void find(docstring const & search,
 		  bool casesensitive, bool matchword, bool forward);
-
 	/// Replaces occurrence of string
 	void replace(docstring const & search, docstring const & replace,
 		     bool casesensitive, bool matchword,
 		     bool forward, bool all);
+	///
+	BufferView const * bv_;
+	///
+	bool minimized_ = false;
+};
+
+
+class GuiSearch : public DockView
+{
+	Q_OBJECT
+
+public:
+	GuiSearch(
+		GuiView & parent, ///< the main window where to dock.
+		Qt::DockWidgetArea area = Qt::BottomDockWidgetArea, ///< Position of the dock (and also drawer)
+		Qt::WindowFlags flags = {});
+
+	/// Controller inherited method.
+	///@{
+	bool initialiseParams(std::string const &) override { return true; }
+	void clearParams() override {}
+	void dispatchParams() override {}
+	bool isBufferDependent() const override { return true; }
+	void updateView() override;
+	void saveSession(QSettings & settings) const override;
+	void restoreSession() override;
+	bool wantInitialFocus() const override { return true; }
+	///@}
+
+public Q_SLOTS:
+	///
+	void onBufferViewChanged() override;
+
+private Q_SLOTS:
+	/// update title display
+	void updateTitle();
+	/// update dock size
+	void updateSize();
+
+private:
+	/// The encapsulated widget.
+	GuiSearchWidget * widget_;
 };
 
 } // namespace frontend
