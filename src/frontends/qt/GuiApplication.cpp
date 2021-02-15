a0a0119b48dece10f621a20fd84eef14ff200720
Enrico Forestieri <forenr@lyx.org>
2021-02-15 22:43:36 +0100

    Account for true dpi
    We have to account for the physical rather than logical dpi.
    
    See #12132.

--- src/frontends/qt/GuiApplication.cpp
+++ src/frontends/qt/GuiApplication.cpp
@@ -1112,7 +1112,7 @@ GuiApplication::GuiApplication(int & argc, char ** argv)
 
 	// needs to be done before reading lyxrc
 	QWidget w;
-	lyxrc.dpi = (w.logicalDpiX() + w.logicalDpiY()) / 2;
+	lyxrc.dpi = (w.physicalDpiX() + w.physicalDpiY()) / 2;
 
 	guiApp = this;
 
