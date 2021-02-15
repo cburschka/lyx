93e7ab1f3e0535f15da73b0dc2fe71ee4e1b0281
Richard Kimberly Heck <rikiheck@lyx.org>
2021-02-15 10:55:20 -0500

    Do not write \default_view_format if it is empty.

--- src/LyXRC.cpp
+++ src/LyXRC.cpp
@@ -2729,8 +2729,9 @@ void LyXRC::write(ostream & os, bool ignore_system_lyxrc, string const & name) c
 			break;
 		// fall through
 	case RC_DEFAULT_VIEW_FORMAT:
-		if (ignore_system_lyxrc ||
-		    default_view_format != system_lyxrc.default_view_format) {
+		if ((ignore_system_lyxrc ||
+			default_view_format != system_lyxrc.default_view_format)
+			&& !default_view_format.empty()) {
 			os << "\\default_view_format " << default_view_format << '\n';
 		}
 		if (tag != RC_LAST)
