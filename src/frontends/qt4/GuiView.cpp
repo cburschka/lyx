9671f8ca2023c43e49baa69be61b1bb4ccdcb227
Juergen Spitzmueller <spitz@lyx.org>
2021-03-13 12:21:50 +0100

    Backport getContentsMargins() deprecation fix

--- src/frontends/qt4/GuiView.cpp
+++ src/frontends/qt4/GuiView.cpp
@@ -4390,8 +4390,7 @@ bool GuiView::lfunUiToggle(string const & ui_component)
 		menuBar()->setVisible(!menuBar()->isVisible());
 	} else
 	if (ui_component == "frame") {
-		int l, t, r, b;
-		getContentsMargins(&l, &t, &r, &b);
+		int const l = contentsMargins().left();
 		//are the frames in default state?
 		d.current_work_area_->setFrameStyle(QFrame::NoFrame);
 		if (l == 0) {
