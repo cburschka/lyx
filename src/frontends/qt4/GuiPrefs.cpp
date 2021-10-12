1c1c83eced9686949019f47c6bbde3bfe63c0b10
Juergen Spitzmueller <spitz@lyx.org>
2021-10-12 07:42:21 +0200

    Prevent null pointer
    No status entry needed (this bug crept in after the last release)

--- src/frontends/qt4/GuiPrefs.cpp
+++ src/frontends/qt4/GuiPrefs.cpp
@@ -3340,7 +3340,8 @@ void PrefShortcuts::on_searchLE_textEdited()
 	for (int i = 0; i < matched.size(); ++i)
 		if (!isAlwaysHidden(*matched[i])) {
 			matched[i]->setHidden(false);
-			matched[i]->parent()->setExpanded(true);
+			if (matched[i]->parent())
+				matched[i]->parent()->setExpanded(true);
 		}
 }
 
