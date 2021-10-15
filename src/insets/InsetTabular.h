dfa97f80d9fc1c8c6a4cd03234c4429fa95362a0
Jean-Marc Lasgouttes <lasgouttes@lyx.org>
2021-10-15 16:20:40 +0200

    Explicit InsetTableCell copy constructor
    Use explicit default syntax (C++11).
    
    Adapted from:
    commit bff97ba76d65da3f9048ef107b29ee642bdd5229
    commit 3d46cc302bcff979a874ef2ea9c8a56a85aaec77

--- src/insets/InsetTabular.h
+++ src/insets/InsetTabular.h
@@ -53,6 +53,9 @@ class InsetTableCell : public InsetText
 public:
 	///
 	InsetTableCell(Buffer * buf);
+	/// We need this since generation of the default is deprecated
+	/// (since we declare the assignment constuctor below).
+	InsetTableCell(InsetTableCell const & in) = default;
 	///
 	InsetCode lyxCode() const { return CELL_CODE; }
 	///
