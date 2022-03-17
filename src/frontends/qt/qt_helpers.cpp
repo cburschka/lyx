b9ca608c2f7ca2e1434e8e7cfc9e9c83f643c6c4
Juergen Spitzmueller <spitz@lyx.org>
2022-03-17 08:47:43 +0100

    Properly color red checkbox labels in validation (part of #12508)

--- src/frontends/qt/qt_helpers.cpp
+++ src/frontends/qt/qt_helpers.cpp
@@ -223,11 +223,22 @@ bool ColorSorter(ColorCode lhs, ColorCode rhs)
 void setValid(QWidget * widget, bool valid)
 {
 	if (valid) {
-		widget->setPalette(QPalette());
+		if (qobject_cast<QCheckBox*>(widget) != nullptr)
+			// Check boxes need to be treated differenty, see
+			// https://forum.qt.io/topic/93253/
+			widget->setStyleSheet("");
+		else
+			widget->setPalette(QPalette());
 	} else {
-		QPalette pal = widget->palette();
-		pal.setColor(QPalette::Active, QPalette::WindowText, QColor(255, 0, 0));
-		widget->setPalette(pal);
+		if (qobject_cast<QCheckBox*>(widget) != nullptr) {
+			// Check boxes need to be treated differenty, see
+			// https://forum.qt.io/topic/93253/
+			widget->setStyleSheet("QCheckBox:unchecked{ color: red; }QCheckBox:checked{ color: red; }");
+		} else {
+			QPalette pal = widget->palette();
+			pal.setColor(QPalette::Active, QPalette::WindowText, QColor(255, 0, 0));
+			widget->setPalette(pal);
+		}
 	}
 }
 
