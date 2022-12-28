8b5bfa971bc9b3f2e49b48f25a2a2fee03004016
Thibaut Cuvelier <tcuvelier@lyx.org>
2022-12-28 02:33:11 +0100

    Amend febd1855eb: fix compilability of tex2lyx

--- src/tex2lyx/dummy_impl.cpp
+++ src/tex2lyx/dummy_impl.cpp
@@ -99,8 +99,6 @@ void lyx_exit(int)
 namespace xml {
 docstring StartTag::writeTag() const { return docstring(); }
 docstring StartTag::writeEndTag() const { return docstring(); }
-bool StartTag::operator==(FontTag const & rhs) const { return rhs == *this; }
-bool FontTag::operator==(StartTag const & tag) const { FontTag const * const ftag = tag.asFontTag(); if (!ftag) return false; return (font_type_ == ftag->font_type_); }
 }
 
 } // namespace lyx
