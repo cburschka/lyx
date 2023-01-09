fa5b08e24d398cb76049f2962ae171485f480133
Thibaut Cuvelier <tcuvelier@lyx.org>
2023-01-09 04:16:34 +0100

    Simplify simplehtml_validity.py

--- development/autotests/simplehtml_validity.py
+++ development/autotests/simplehtml_validity.py
@@ -12,17 +12,14 @@
 # Run:
 #     pip install html5validator>=0.4.2
 
-import collections
-import glob
 import sys
-import tempfile
 import os
 
 import html5validator
 
 
 if len(sys.argv) != 2:
-    print('Expecting one argument, the path to the LyX-create xhtml file')
+    print('Expecting one argument, the path to the LyX-created XHTML file')
     sys.exit(-1)
 if not os.path.exists(sys.argv[1]):
     print('The given path does not point to an existing file')
@@ -35,19 +32,9 @@ xhtml_list = [xhtml_file_name]
 validator = html5validator.Validator(format='text')
 error_count = validator.validate(xhtml_list)
 
-n_invalid = 0
-n_valid = 0
 if error_count == 0:
-    n_valid += 1
-    print(f'> Found no validation error!')
+    print('> Found no validation error!')
     sys.exit(0)
 else:
-    n_invalid += 1
-    print(f'> Found {error_count} validation error{"" if error_count == 1 else "s"}!')
+    print('> Found a validation error!')
     sys.exit(-2)
-
-if n_invalid == 0:
-    print("That's excellent! Give yourself a pat on the back!")
-    sys.exit(0)
-else:
-    sys.exit(-3)
